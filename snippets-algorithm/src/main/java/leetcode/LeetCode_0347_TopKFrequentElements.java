f5b370f559771085adde4fc33de09cdffc6dcca8
Grey927 <410486047@qq.com>
2020-10-11 23:12:46 +0800

    leetcode 347 top k frequent elements

--- snippets-algorithm/src/main/java/leetcode/LeetCode_0347_TopKFrequentElements.java
+++ snippets-algorithm/src/main/java/leetcode/LeetCode_0347_TopKFrequentElements.java
@@ -1,8 +1,78 @@
+/*
+Given a non-empty array of integers, return the k most frequent elements.
+
+        Example 1:
+
+        Input: nums = [1,1,1,2,2,3], k = 2
+        Output: [1,2]
+        Example 2:
+
+        Input: nums = [1], k = 1
+        Output: [1]
+        Note:
+
+        You may assume k is always valid, 1 ≤ k ≤ number of unique elements.
+        Your algorithm's time complexity must be better than O(n log n), where n is the array's size.
+        It's guaranteed that the answer is unique, in other words the set of the top k frequent elements is unique.
+        You can return the answer in any order.
+*/
 package leetcode;
 
-// TODO
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.PriorityQueue;
+import java.util.Set;
+
+
 // Hash表词频    + 小根堆（拿次数排序，大小是K）
 public class LeetCode_0347_TopKFrequentElements {
 
+    public static class Node {
+        public int value;
+        public int times;
+
+        public Node(int value, int times) {
+            this.value = value;
+            this.times = times;
+        }
+    }
+
+    public static class MyComparator implements Comparator<Node> {
+
+        @Override
+        public int compare(Node o1, Node o2) {
+            return o1.times - o2.times;
+        }
+    }
+
+    public int[] topKFrequent(int[] nums, int k) {
+        if (nums == null || nums.length == 0 || nums.length < k) {
+            return null;
+        }
+        HashMap<Integer, Node> freq = new HashMap<>();
+        for (int i : nums) {
+            if (!freq.containsKey(i)) {
+                freq.put(i, new Node(i, 1));
+            } else {
+                freq.put(i, new Node(i, freq.get(i).times + 1));
+            }
+        }
+        PriorityQueue<Node> topK = new PriorityQueue<>(new MyComparator());
+        Set<Integer> keys = freq.keySet();
+        for (Integer key : keys) {
+            Node t = freq.get(key);
+            if (topK.size() <= k || topK.peek().times < t.times) {
+                topK.add(t);
+            }
+            if (topK.size() > k) {
+                topK.poll();
+            }
+        }
+        int[] res = new int[k];
+        for (int i = 0; i < k; i++) {
+            res[i] = topK.poll().value;
+        }
+        return res;
+    }
 
 }
