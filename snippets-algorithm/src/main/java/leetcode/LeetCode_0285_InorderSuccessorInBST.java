3eecd71f7bb813cd137934743d0f8d224bb1d086
zenghui <410486047@qq.com>
2020-10-12 11:08:57 +0800

    leetcode 285 in order successor in BST

--- snippets-algorithm/src/main/java/leetcode/LeetCode_0285_InorderSuccessorInBST.java
+++ snippets-algorithm/src/main/java/leetcode/LeetCode_0285_InorderSuccessorInBST.java
@@ -1,7 +1,10 @@
 package leetcode;
 
-// TODO
-// 中序遍历的后继节点 用morris方式来做
+import java.util.ArrayList;
+import java.util.List;
+
+
+// leetcode加锁，可以看lintcode对应的题目：https://www.lintcode.com/problem/inorder-successor-in-bst/description
 public class LeetCode_0285_InorderSuccessorInBST {
 
     public static class TreeNode {
@@ -10,8 +13,36 @@ public class LeetCode_0285_InorderSuccessorInBST {
         public TreeNode right;
     }
 
-    public static TreeNode inorderSuccessor(TreeNode head, TreeNode p) {
+    // 递归方式
+    // 中序遍历的后继节点
+    public TreeNode inorderSuccessor(TreeNode head, TreeNode p) {
+        if (head == null) {
+            return null;
+        }
+        List<TreeNode> dp = new ArrayList<>();
+        in(head, dp);
+        boolean tag = false;
+        for (TreeNode t : dp) {
+            if (t == p) {
+                tag = true;
+                continue;
+            }
+            if (tag) {
+                return t;
+            }
+        }
         return null;
     }
 
+    public static void in(TreeNode head, List<TreeNode> dp) {
+        if (head == null) {
+            return;
+        }
+        in(head.left, dp);
+        dp.add(head);
+        in(head.right, dp);
+    }
+
+// TODO
+// 用morris方式来做
 }
