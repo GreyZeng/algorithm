62f540d4c53ff851bd83358fd17300c374444a08
zenghui <410486047@qq.com>
2020-10-14 16:05:52 +0800

    leetcode 371 sum of two integer

--- snippets-algorithm/src/main/java/leetcode/LeetCode_0371_SumOfTwoIntegers.java
+++ snippets-algorithm/src/main/java/leetcode/LeetCode_0371_SumOfTwoIntegers.java
@@ -3,7 +3,13 @@ package leetcode;
 public class LeetCode_0371_SumOfTwoIntegers {
 
     public static int getSum(int a, int b) {
-        return -1;
+        int sum = a;
+        while (b != 0) {
+            sum = a ^ b;
+            b = (a & b) << 1;
+            a = sum;
+        }
+        return sum;
     }
 
 }
