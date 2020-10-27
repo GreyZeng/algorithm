a196223a7c934f70360663b59c2f41bb471d1678
Grey927 <410486047@qq.com>
2020-10-27 21:43:43 +0800

    First unique char

--- src/main/java/leetcode/LeetCode_0387_FirstUniqueCharacterInString.java
+++ src/main/java/leetcode/LeetCode_0387_FirstUniqueCharacterInString.java
@@ -1,9 +1,31 @@
+/*Given a string, find the first non-repeating character in it and return its index. If it doesn't exist, return -1.
+
+Examples:
+
+s = "leetcode"
+return 0.
+
+s = "loveleetcode"
+return 2.
+ 
+
+Note: You may assume the string contains only lowercase English letters.*/
 package leetcode;
 
-// TODO
 public class LeetCode_0387_FirstUniqueCharacterInString {
 
 	public int firstUniqChar(String s) {
+		int[] count = new int[26];
+		char[] str = s.toCharArray();
+		int N = str.length;
+		for (int i = 0; i < N; i++) {
+			count[str[i] - 'a']++;
+		}
+		for (int i = 0; i < N; i++) {
+			if (count[str[i] - 'a'] == 1) {
+				return i;
+			}
+		}
 		return -1;
 	}
 
