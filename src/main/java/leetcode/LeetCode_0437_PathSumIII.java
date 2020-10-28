d939e373af93fe1492bbb2db829a7dd6e9959cbc
zenghui <410486047@qq.com>
2020-10-28 14:17:20 +0800

    leetcode 437 Path sum III

--- src/main/java/leetcode/LeetCode_0437_PathSumIII.java
+++ src/main/java/leetcode/LeetCode_0437_PathSumIII.java
@@ -25,35 +25,59 @@
         3. -3 -> 11*/
 package leetcode;
 
-
 import java.util.HashMap;
 
 // 先序遍历
 public class LeetCode_0437_PathSumIII {
-    public class TreeNode {
-        int val;
-        TreeNode left;
-        TreeNode right;
+	public class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
 
-        TreeNode() {
-        }
+		TreeNode() {
+		}
 
-        TreeNode(int val) {
-            this.val = val;
-        }
+		TreeNode(int val) {
+			this.val = val;
+		}
 
-        TreeNode(int val, TreeNode left, TreeNode right) {
-            this.val = val;
-            this.left = left;
-            this.right = right;
-        }
-    }
+		TreeNode(int val, TreeNode left, TreeNode right) {
+			this.val = val;
+			this.left = left;
+			this.right = right;
+		}
+	}
+	public static int pathSum(TreeNode root, int sum) {
+		HashMap<Integer, Integer> preMap = new HashMap<>();
+		preMap.put(0, 1);
+		return p(root, sum, 0, preMap);
+	}
 
-    // TODO
-    public static int pathSum(TreeNode root, int sum) {
-        return -1;
-    }
+	public static int p(TreeNode root, int sum, int preAll, HashMap<Integer, Integer> preMap) {
+		if (root == null) {
+			return 0;
+		}
+		int all = preAll + root.val;
+		int ans = 0;
+		if (preMap.containsKey(all - sum)) {
+			ans = preMap.get(all - sum);
+		}
+		if (preMap.containsKey(all)) {
+			preMap.put(all, preMap.get(all) + 1);
+		} else {
+			preMap.put(all, 1);
+		}
+		ans += p(root.left, sum, all, preMap);
+		ans += p(root.right, sum, all, preMap);
+		if (preMap.containsKey(all)) {
+			if (preMap.get(all) == 1) {
+				preMap.remove(all);
+			} else {
+				preMap.put(all, preMap.get(all) - 1);
+			}
+		}
+		return ans;
 
-
+	}
 
 }
