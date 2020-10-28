49d196a5f7324038c44cd8da2025df84ada8c1d3
zenghui <410486047@qq.com>
2020-10-28 12:01:41 +0800

    add note

--- src/main/java/leetcode/LeetCode_0297_SerializeAndDeserializeBinaryTree.java
+++ src/main/java/leetcode/LeetCode_0297_SerializeAndDeserializeBinaryTree.java
@@ -3,7 +3,6 @@ package leetcode;
 import java.util.LinkedList;
 import java.util.Queue;
 
-// TODO
 // 序列化和反序列化二叉树
 public class LeetCode_0297_SerializeAndDeserializeBinaryTree {
 
--- src/main/java/leetcode/LeetCode_0437_PathSumIII.java
+++ src/main/java/leetcode/LeetCode_0437_PathSumIII.java
@@ -1,8 +1,59 @@
+/*You are given a binary tree in which each node contains an integer value.
+
+        Find the number of paths that sum to a given value.
+
+        The path does not need to start or end at the root or a leaf, but it must go downwards (traveling only from parent nodes to child nodes).
+
+        The tree has no more than 1,000 nodes and the values are in the range -1,000,000 to 1,000,000.
+
+        Example:
+
+        root = [10,5,-3,3,2,null,11,3,-2,null,1], sum = 8
+
+        10
+        /  \
+        5   -3
+        / \    \
+        3   2   11
+        / \   \
+        3  -2   1
+
+        Return 3. The paths that sum to 8 are:
+
+        1.  5 -> 3
+        2.  5 -> 2 -> 1
+        3. -3 -> 11*/
 package leetcode;
 
-// TODO
+
+import java.util.HashMap;
+
 // 先序遍历
 public class LeetCode_0437_PathSumIII {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    // TODO
+    public static int pathSum(TreeNode root, int sum) {
+        return -1;
+    }
+
 
 
 }
