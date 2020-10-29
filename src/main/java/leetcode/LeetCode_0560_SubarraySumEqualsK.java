d5ddb52150aa7041b57c692d328716ace4d9a5fa
Grey927 <410486047@qq.com>
2020-10-29 21:46:19 +0800

    leetcode 560 subarray sum equal k

--- src/main/java/leetcode/LeetCode_0560_SubarraySumEqualsK.java
+++ src/main/java/leetcode/LeetCode_0560_SubarraySumEqualsK.java
@@ -1,11 +1,54 @@
+/*Given an array of integers nums and an integer k, 
+return the total number of continuous subarrays whose sum equals to k.
+
+ 
+
+Example 1:
+
+Input: nums = [1,1,1], k = 2
+Output: 2
+Example 2:
+
+Input: nums = [1,2,3], k = 3
+Output: 2
+ 
+
+Constraints:
+
+1 <= nums.length <= 2 * 10^4
+-1000 <= nums[i] <= 1000
+-10^7 <= k <= 10^7*/
 package leetcode;
 
-// TODO
+import java.util.HashMap;
+
 // arr[L...R] = sum -> arr[0..R] - arr[0...L-1] = sum
 // 子数组一定要以i结尾
 // Map前缀和（key: 前缀和， value: 前缀和出现的最小位置）
 // 初始化 Map.put(0,-1)
 // （-100，3，1，2，100）
 public class LeetCode_0560_SubarraySumEqualsK {
+	public static int subarraySum(int[] nums, int k) {
+		int N = nums.length;
+		// 初始化前缀和数组
+		HashMap<Integer, Integer> dp = new HashMap<>();
+		dp.put(0, 1);
+		int all = 0;
+		int ans = 0;
+		int g = 0;
+		for (int i = 0; i < N; i++) {
+			all += nums[i];
+			g = all - k;
+			if (dp.containsKey(g)) {
+				ans += dp.get(g);
+			}
+			if (!dp.containsKey(all)) {
+				dp.put(all, 1);
+			} else {
+				dp.put(all, dp.get(all) + 1);
+			}
+		}
+		return ans;
 
+	}
 }
