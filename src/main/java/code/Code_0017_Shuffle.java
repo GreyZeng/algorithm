e66474e635458fb19bfbe81a92c3b50c1cecb5a6
Grey927 <410486047@qq.com>
2020-10-31 23:31:28 +0800

    shuffle and wiggle sort ii

---
+++ src/main/java/code/Code_0017_Shuffle.java
@@ -0,0 +1,83 @@
+package code;
+
+// 完美洗牌问题
+// 给定一个长度为偶数的数组arr，长度记为2*N。
+// 前N个为左部分，后N个为右部分。 arr就可以表示为{L1,L2,..,Ln,R1,R2,..,Rn}， 
+// 请将数组调整成{R1,L1,R2,L2,..,Rn,Ln}的样子。
+//一个结论：当arr长度S = 3^k - 1 (偶数)的时候，环的出发点1,3,9...3^(k-1)
+// 前提：
+// 1) 数组从1开始计算
+// 2) 数组长度偶数
+
+public class Code_0017_Shuffle {
+
+	public static void shuffle(int[] arr) {
+		if (arr == null || arr.length == 0 || (arr.length & 1) != 0) {
+			return;
+		}
+		shuffle(arr, 0, arr.length - 1);
+	}
+
+	public static void swap(int[] nums, int L, int R) {
+		if (nums == null || nums.length <= 1 || R == L) {
+			return;
+		}
+		nums[L] = nums[L] ^ nums[R];
+		nums[R] = nums[L] ^ nums[R];
+		nums[L] = nums[L] ^ nums[R];
+	}
+
+	public static void shuffle(int[] arr, int L, int R) {
+
+		while (R - L + 1 > 0) {
+			int len = R - L + 1;
+			int base = 3;
+			int k = 1;
+			while (base <= (len + 1) / 3) {
+				base *= 3;
+				k++;
+			}
+			int half = (base - 1) / 2;
+			int mid = (L + R) / 2;
+			rotate(arr, L + half, mid, mid + half);
+			toNext(arr, L, base - 1, k);
+			L = L + base - 1;
+		}
+	}
+
+	// i位置下一个位置应该去哪里
+	private static int findNextIndex(int i, int N) {
+		if (i <= N / 2) {
+			return 2 * i;
+		}
+		return (i - N / 2) * 2 - 1;
+	}
+
+	private static void toNext(int[] arr, int start, int len, int k) {
+		for (int i = 0, trigger = 1; i < k; i++, trigger *= 3) {
+			int pre = arr[start + trigger - 1];
+			int next = findNextIndex(trigger, len);
+			while (next != trigger) {
+				int t = arr[next + start - 1];
+				arr[next + start - 1] = pre;
+				pre = t;
+				next = findNextIndex(next, len);
+			}
+			arr[next + start - 1] = pre;
+		}
+	}
+
+	// L..R做逆序调整
+	public static void reverse(int[] arr, int L, int R) {
+		while (L < R) {
+			swap(arr, L++, R--);
+		}
+	}
+
+	// L..M部分和M+1..R部分互换
+	public static void rotate(int[] arr, int L, int M, int R) {
+		reverse(arr, L, M);
+		reverse(arr, M + 1, R);
+		reverse(arr, L, R);
+	}
+}
