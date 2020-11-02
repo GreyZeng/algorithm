e5c26a3ff42e70ba923e835887738c5623d4412e
zenghui <410486047@qq.com>
2020-11-02 11:54:59 +0800

    leetcode 494 target sum (todo:改动态规划)

--- src/main/java/leetcode/LeetCode_0494_TargetSum.java
+++ src/main/java/leetcode/LeetCode_0494_TargetSum.java
@@ -1,11 +1,64 @@
+/*You are given a list of non-negative integers, a1, a2, ..., an, and a target, S. Now you have 2 symbols + and -.
+For each integer, you should choose one from + and - as its new symbol.
+
+		Find out how many ways to assign symbols to make sum of integers equal to target S.
+
+		Example 1:
+
+		Input: nums is [1, 1, 1, 1, 1], S is 3.
+		Output: 5
+		Explanation:
+
+		-1+1+1+1+1 = 3
+		+1-1+1+1+1 = 3
+		+1+1-1+1+1 = 3
+		+1+1+1-1+1 = 3
+		+1+1+1+1-1 = 3
+
+		There are 5 ways to assign symbols to make the sum of nums be target 3.
+
+
+		Constraints:
+
+		The length of the given array is positive and will not exceed 20.
+		The sum of elements in the given array will not exceed 1000.
+		Your output answer is guaranteed to be fitted in a 32-bit integer.*/
 package leetcode;
-// TODO
+
 public class LeetCode_0494_TargetSum {
+    public static int findTargetSumWays(int[] arr, int sum) {
+        int max = 0;
+        for (int i : arr) {
+            max += i;
+        }
+        if (sum > max) {
+            return 0;
+        }
+        if (sum == max) {
+            return 1;
+        }
+        int M = arr.length + 1;
+        int N = max << 1 + 1;
 
-	public static int findTargetSumWays(int[] arr, int num) {
-		return -1;
-	}
+        int[][] dp = new int[M][N];
+        dp[N - 1][sum + max] = 1;
+        // TODO 改动态规划
 
+        return dp[0][sum + max];
+    }
 
+    public static int findTargetSumWays2(int[] arr, int sum) {
+        return process(arr, 0, sum);
+    }
 
+    public static int process(int[] arr, int i, int rest) {
+        if (i == arr.length) {
+            return rest == 0 ? 1 : 0;
+        }
+        // i位置添加+号
+        int p1 = process(arr, i + 1, rest + arr[i]);
+        // i位置添加-号
+        int p2 = process(arr, i + 1, rest - arr[i]);
+        return p1 + p2;
+    }
 }
