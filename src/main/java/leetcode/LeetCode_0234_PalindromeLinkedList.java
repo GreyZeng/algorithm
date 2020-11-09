f4cf5d9b15cd6625934c53414c5db5d6c3ed4aea
GreyZeng <410486047@qq.com>
2020-11-09 22:51:16 +0800

    list ref

--- src/main/java/leetcode/LeetCode_0234_PalindromeLinkedList.java
+++ src/main/java/leetcode/LeetCode_0234_PalindromeLinkedList.java
@@ -15,146 +15,77 @@ package leetcode;
 
 import java.util.Stack;
 
+import code.Code_0033_LinkedListMid.Node;
 
 public class LeetCode_0234_PalindromeLinkedList {
-    public static class ListNode {
-        int val;
-        ListNode next;
+	public static class ListNode {
+		int val;
+		ListNode next;
 
-        ListNode() {
-        }
+		ListNode() {
+		}
 
-        ListNode(int val) {
-            this.val = val;
-        }
+		ListNode(int val) {
+			this.val = val;
+		}
 
-        ListNode(int val, ListNode next) {
-            this.val = val;
-            this.next = next;
-        }
-    }
+		ListNode(int val, ListNode next) {
+			this.val = val;
+			this.next = next;
+		}
+	}
 
-    // 利用栈O(n)
-    public static boolean isPalindrome(ListNode head) {
-        if (head == null || head.next == null) {
-            return true;
-        }
-        Stack<Integer> stack = new Stack<>();
-        ListNode c = head;
-        while (c != null) {
-            stack.push(c.val);
-            c = c.next;
-        }
-        ListNode t = head;
-        while (t != null) {
-            if (stack.peek() == t.val) {
-                stack.pop();
-                t = t.next;
-            } else {
-                return false;
-            }
-        }
-        return true;
-    }
+	// 利用栈O(n/2)
+	public static boolean isPalindrome(ListNode head) {
+		if (head == null || head.next == null) {
+			return true;
+		}
+		if (head.next.next == null) {
+			return head.val == head.next.val;
+		}
+		ListNode slow = head.next;
+		ListNode fast = head.next.next;
+		while (fast.next != null && fast.next.next != null) {
+			slow = slow.next;
+			fast = fast.next.next;
+		}
+		slow = slow.next;
+		fast = head;
+		Stack<ListNode> stack = new Stack<>();
+		while (slow != null) {
+			stack.push(slow);
+			slow = slow.next;
+		}
+		while (!stack.isEmpty()) {
+			if (stack.pop().val != fast.val) {
+				return false;
+			}
+			fast = fast.next;
+		}
+		return true;
 
-    //利用栈O(n/2)
-    // 利用快慢指针，快指针到终点，慢指针到中间位置(
-    public static boolean isPalindrome2(ListNode head) {
-        if (head == null || head.next == null) {
-            return true;
-        }
-        ListNode c = head;
-        ListNode r = head.next;
-        while (c.next != null && c.next.next != null) {
-            c = c.next.next;
-            r = r.next;
-        }
-        // r来到中点位置（奇数是中点后一个位置，偶数是下中点位置）
+	}
 
-        Stack<Integer> stack = new Stack<>();
-        while (r != null) {
-            stack.push(r.val);
-            r = r.next;
-        }
+	// 利用栈O(n)
+	public static boolean isPalindrome3(ListNode head) {
+		Stack<ListNode> stack = new Stack<>();
+		ListNode c = head;
+		while (c != null) {
+			stack.push(c);
+			c = c.next;
+		}
+		c = head;
+		while (c != null) {
+			if (c.val != stack.pop().val) {
+				return false;
+			}
+			c = c.next;
+		}
+		return true;
+	}
 
-        ListNode t = head;
-        while (!stack.isEmpty()) {
-            if (stack.peek() == t.val) {
-                stack.pop();
-                t = t.next;
-            } else {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    //修改原链表，空间O(1)
-    public static boolean isPalindrome3(ListNode head) {
-        // 0个点和一个点都是
-        if (head == null || head.next == null) {
-            return true;
-        }
-        // 两个点的话，第0个和第一个点的值相等即为回环
-        // 否则不是回环
-        if (head.next.next == null) {
-            return head.next.val == head.val;
-        }
-        ListNode cur = head; // 存一下头节点的位置，便于后续还原整个链表
-
-        // 偶数到上中点位置，奇数到中点位置
-        ListNode fast = cur.next;
-        ListNode slow = cur;
-        while (fast.next != null && fast.next.next != null) {
-            fast = fast.next.next;
-            slow = slow.next;
-        }
-
-        ListNode rightComplete = slow.next;
-        slow.next = null; // 断开为两个链表
-
-        ListNode rightStart = rightComplete; // 存一下rightStart的位置，便于后续还原整个链表
-        // 以rightStart为头的链表做一次单链表反转，得到新链表的头节点，假设这个反转后的链表头节点为lastNode
-        ListNode lastNode = null;
-        ListNode next;
-        while (rightStart != null) {
-            next = rightStart.next;
-            rightStart.next = lastNode;
-            lastNode = rightStart;
-            rightStart = next;
-        }
-
-        ListNode n3 = lastNode;
-        ListNode n4 = lastNode;
-
-        // 左边从头开始向中间遍历，右边从尾部开始向中间遍历
-        // 只要有不匹配的就为false，两个有一个遍历结束即为true
-
-        boolean result = true;
-
-        while (n3 != null && cur != null) {
-            if (n3.val != cur.val) {
-                result = false;
-                break;
-            }
-            n3 = n3.next;
-            cur = cur.next;
-        }
-        ListNode newHead = null;
-        ListNode temp;
-        while (n4 != null) {
-            temp = n4.next;
-            n4.next = newHead;
-            newHead = n4;
-            n4 = temp;
-        }
-
-        ListNode fin = head;
-        // 还原链表
-        while (fin.next != null) {
-            fin = fin.next;
-        }
-        fin.next = newHead;
-        return result;
-    }
+	// TODO 修改原链表，空间O(1)
+	public static boolean isPalindrome2(ListNode head) {
+		return false;
+	}
 }
