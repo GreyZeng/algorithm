b96a37235bb9b7ff780720c9b3dd55793db94a6e
zenghui <410486047@qq.com>
2020-11-12 16:49:47 +0800

    add note

--- src/main/java/leetcode/LeetCode_0958_CheckCompletenessOfABinaryTree.java
+++ src/main/java/leetcode/LeetCode_0958_CheckCompletenessOfABinaryTree.java
@@ -3,4 +3,27 @@ package leetcode;
 // TODO
 public class LeetCode_0958_CheckCompletenessOfABinaryTree {
 
+	public static class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode() {
+		}
+
+		TreeNode(int val) {
+			this.val = val;
+		}
+
+		TreeNode(int val, TreeNode left, TreeNode right) {
+			this.val = val;
+			this.left = left;
+			this.right = right;
+		}
+	}
+
+	public static boolean isCompleteTree(TreeNode root) {
+		return false;
+	}
+
 }
