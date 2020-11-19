2d2389b64beea013a3c6e4e706606f7e3d9c0de9
GreyZeng <410486047@qq.com>
2020-11-19 23:27:46 +0800

    dfs topological sort

--- src/main/java/lintcode/LintCode_0127_TopologicalSorting.java
+++ src/main/java/lintcode/LintCode_0127_TopologicalSorting.java
@@ -58,9 +58,58 @@ public class LintCode_0127_TopologicalSorting {
 		return map;
 	}
 
-	// TODO 使用DFS来实现
-	public ArrayList<DirectedGraphNode> topSort2(ArrayList<DirectedGraphNode> graph) {
-		return null;
+	public static class Record {
+		public DirectedGraphNode node;
+		public long out;
+
+		public Record(DirectedGraphNode n, long o) {
+			node = n;
+			out = o;
+		}
+	}
+
+	public static class MyComparator implements Comparator<Record> {
+
+		@Override
+		public int compare(Record o1, Record o2) {
+			if (o2.out > o1.out) {
+				return 1;
+			} else if (o1.out > o2.out) {
+				return -1;
+			} else {
+				return 0;
+			}
+		}
+	}
+	// DFS方式
+	public static ArrayList<DirectedGraphNode> topSort2(ArrayList<DirectedGraphNode> graph) {
+
+		HashMap<DirectedGraphNode, Record> order = new HashMap<>();
+		for (DirectedGraphNode cur : graph) {
+			f(cur, order);
+		}
+		ArrayList<Record> recordArr = new ArrayList<>();
+		for (Record r : order.values()) {
+			recordArr.add(r);
+		}
+		recordArr.sort(new MyComparator());
+		ArrayList<DirectedGraphNode> ans = new ArrayList<DirectedGraphNode>();
+		for (Record r : recordArr) {
+			ans.add(r.node);
+		}
+		return ans;
+	} 
+	public static Record f(DirectedGraphNode cur, HashMap<DirectedGraphNode, Record> order) {
+		if (order.containsKey(cur)) {
+			return order.get(cur);
+		}
+		long out = 1;
+		for (DirectedGraphNode next : cur.neighbors) {
+			out += f(next, order).out;
+		}
+		Record ans = new Record(cur, out);
+		order.put(cur, ans);
+		return ans;
 	}
 
 	
