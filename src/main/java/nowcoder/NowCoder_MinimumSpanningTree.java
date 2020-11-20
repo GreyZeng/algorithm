dd843e9e63baa0ffa2a5424e993b32ca0a98c76b
zenghui <410486047@qq.com>
2020-11-20 16:37:31 +0800

    add todo

--- src/main/java/nowcoder/NowCoder_MinimumSpanningTree.java
+++ src/main/java/nowcoder/NowCoder_MinimumSpanningTree.java
@@ -36,9 +36,9 @@ import java.util.Stack;
 // https://www.nowcoder.com/questionTerminal/c23eab7bb39748b6b224a8a3afbe396b
 // 最小生成树 K算法，P算法
 public class NowCoder_MinimumSpanningTree {
-	
-	//K算法 （边权值排序，并查集）（如果无向图会少一侧的情况，按情况补充即可）
-	//最小生成树算法之Kruskal
+
+	// K算法 （边权值排序，并查集）（如果无向图会少一侧的情况，按情况补充即可）
+	// 最小生成树算法之Kruskal
 	// 1）总是从权值最小的边开始考虑，依次考察权值依次变大的边
 	// 2）当前的边要么进入最小生成树的集合，要么丢弃
 	// 3）如果当前的边进入最小生成树的集合中不会形成环，就要当前边
@@ -70,16 +70,44 @@ public class NowCoder_MinimumSpanningTree {
 		return ans;
 	}
 
-//	P算法 
-//	  1）可以从任意节点出发来寻找最小生成树
-//	  2）某个点加入到被选取的点中后，解锁这个点出发的所有新的边
-//	  3）在所有解锁的边中选最小的边，然后看看这个边会不会形成环
-//	  4）如果会，不要当前边，继续考察剩下解锁的边中最小的边，重复3）
-//	  5）如果不会，要当前边，将该边的指向点加入到被选取的点中，重复2）
-//	  6）当所有点都被选取，最小生成树就得到了
+//		P算法 TODO
+//		  1）可以从任意节点出发来寻找最小生成树
+//		  2）某个点加入到被选取的点中后，解锁这个点出发的所有新的边
+//		  3）在所有解锁的边中选最小的边，然后看看这个边会不会形成环
+//		  4）如果会，不要当前边，继续考察剩下解锁的边中最小的边，重复3）
+//		  5）如果不会，要当前边，将该边的指向点加入到被选取的点中，重复2）
+//		  6）当所有点都被选取，最小生成树就得到了
 	public static Set<Edge> P(Graph graph) {
-		return null;
+		// 解锁的边进入小根堆
+		PriorityQueue<Edge> priorityQueue = new PriorityQueue<>(new MyComparator());
+
+		// 哪些点被解锁出来了
+		HashSet<Node> nodeSet = new HashSet<>();
+		Set<Edge> result = new HashSet<>(); // 依次挑选的的边在result里
+		for (Node node : graph.nodes.values()) { // 随便挑了一个点
+			// node 是开始点
+			if (!nodeSet.contains(node)) {
+				nodeSet.add(node);
+				for (Edge edge : node.edges) { // 由一个点，解锁所有相连的边
+					priorityQueue.add(edge);
+				}
+				while (!priorityQueue.isEmpty()) {
+					Edge edge = priorityQueue.poll(); // 弹出解锁的边中，最小的边
+					Node toNode = edge.to; // 可能的一个新的点
+					if (!nodeSet.contains(toNode)) { // 不含有的时候，就是新的点
+						nodeSet.add(toNode);
+						result.add(edge);
+						for (Edge nextEdge : toNode.edges) {
+							priorityQueue.add(nextEdge);
+						}
+					}
+				}
+			}
+			break;
+		}
+		return result;
 	}
+
 	public static class Graph {
 		public HashMap<Integer, Node> nodes;
 		public HashSet<Edge> edges;
@@ -173,38 +201,44 @@ public class NowCoder_MinimumSpanningTree {
 		}
 	}
 
-	
-	//输入第一行，两个整数n,m;
-	//接下来m行，每行三个整数a,b,c，表示有路连接编号为a和b的人家，修路要花费的代价为c。
-	//数据保证能将每户人家都连接起来。
-	//注意重边的情况。n≤10000, m≤100,000，边权0<c≤10000。
+	// 输入第一行，两个整数n,m;
+	// 接下来m行，每行三个整数a,b,c，表示有路连接编号为a和b的人家，修路要花费的代价为c。
+	// 数据保证能将每户人家都连接起来。
+	// 注意重边的情况。n≤10000, m≤100,000，边权0<c≤10000。
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		int n = in.nextInt();
 		int m = in.nextInt();
 		Graph graph = new Graph();
+		HashSet<String> set = new HashSet<>();
 		for (int i = 0; i < m; i++) {
-			int a = in.nextInt();
-			int b = in.nextInt();
-			int c = in.nextInt(); 
-			Node A = new Node(a);
-			Node B = new Node(b);
-			if (!graph.nodes.containsKey(a)) {
-				graph.nodes.put(a, A); 
+			int from = in.nextInt();
+			int to = in.nextInt();
+			int weight = in.nextInt();
+			if (set.contains("" + from + "_" + to + "_" + weight)) {
+				continue;
 			} else {
-				A = graph.nodes.get(a);
+				set.add("" + from + "_" + to + "_" + weight);
 			}
-			if (!graph.nodes.containsKey(b)) {
-				graph.nodes.put(b, B); 
-			} else {
-				B = graph.nodes.get(b);
+			if (!graph.nodes.containsKey(from)) {
+				graph.nodes.put(from, new Node(from));
 			}
-			Edge edge = new Edge(c, A, B);
-			graph.edges.add(edge);
+			if (!graph.nodes.containsKey(to)) {
+				graph.nodes.put(to, new Node(to));
+			}
+			Node fromNode = graph.nodes.get(from);
+			Node toNode = graph.nodes.get(to);
+			Edge newEdge = new Edge(weight, fromNode, toNode);
+			fromNode.nexts.add(toNode);
+			fromNode.out++;
+			toNode.in++;
+			fromNode.edges.add(newEdge);
+			toNode.edges.add(newEdge);
+			graph.edges.add(newEdge);
 		}
-		Set<Edge> result = K(graph);
-		//Set<Edge> result = P(graph);
-		
+		// Set<Edge> result = K(graph);
+		Set<Edge> result = P(graph);
+
 		int sum = 0;
 		for (Edge edge : result) {
 			sum += edge.weight;
@@ -212,4 +246,5 @@ public class NowCoder_MinimumSpanningTree {
 		System.out.println(sum);
 		in.close();
 	}
+
 }
