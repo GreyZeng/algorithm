55fb7aa7ba6cb0d79fd51564e9a7ac9d5e3a9c9f
zenghui <410486047@qq.com>
2020-11-24 14:27:22 +0800

    knapsack

--- src/main/java/nowcoder/NowCoder_Knapsack.java
+++ src/main/java/nowcoder/NowCoder_Knapsack.java
@@ -33,49 +33,84 @@ package nowcoder;
 import java.util.Scanner;
 
 public class NowCoder_Knapsack {
-    
-    public static int getMaxValue(int[] w, int[] v, int bag) {
-        if (w == null || v == null || w.length == 0 || v.length == 0) {
-            return 0;
-        }
-        int N = w.length;
-        return p(w, v, N, 0, bag);
-    }
+	public static int getMaxValue(int[] w, int[] v, int bag) {
+		if (w == null || v == null || w.length == 0 || v.length == 0) {
+			return 0;
+		}
+		int N = w.length;
+		int[][] dp = new int[N + 1][bag + 1];
+		for (int i = 0; i <= N; i++) {
+			for (int j = 0; j <= bag; j++) {
+				dp[i][j] = -1;
+			}
+		}
+		p2(w, v, N, 0, bag, dp);
+		return dp[0][bag];
+	}
 
-    // 0..i-1位置已经做好了选择，从i以后开始做决策
-    // rest 背包中还剩下的容量是多少
-    public static int p(int[] w, int[] v, int len, int i, int rest) {
-        // rest空间不能为负数
-        if (rest < 0) {
-            return -1;
-        }
-        if (i == len) {
-            return 0;
-        }
-        int p1 = p(w, v, len, i + 1, rest);
-        
-        int p2 = p(w, v, len, i + 1, rest - w[i]);
-        
-        if (p2 != -1) { // p2 不为-1才能算做正常解
-            p2 += v[i];
-        } 
-        return Math.max(p1, p2);
-    }
+	// 0..i-1位置已经做好了选择，从i以后开始做决策
+	// rest 背包中还剩下的容量是多少
+	public static int p2(int[] w, int[] v, int len, int i, int rest, int[][] dp) {
+		if (dp[i][rest] != -1) {
+			return dp[i][rest];
+		}
+		// rest空间不能为负数
 
-    
-    
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int N = in.nextInt();
-        int bag = in.nextInt();
-        int[] w = new int[N];
-        int[] v = new int[N];
-        for (int i = 0; i < N; i++) {
-            v[i] = in.nextInt();
-            w[i] = in.nextInt();
-        }
-        System.out.println(getMaxValue(w, v, bag));
-        in.close();
-    }
+		if (i == len) {
+			dp[i][rest] = 0;
+			return dp[i][rest];
+		}
+		int p1 = p(w, v, len, i + 1, rest);
+
+		int p2 = p(w, v, len, i + 1, rest - w[i]);
+
+		if (p2 != -1) { // p2 不为-1才能算做正常解
+			p2 += v[i];
+		}
+		dp[i][rest] = Math.max(p1, p2);
+		return dp[i][rest];
+	}
+
+	public static int getMaxValue2(int[] w, int[] v, int bag) {
+		if (w == null || v == null || w.length == 0 || v.length == 0) {
+			return 0;
+		}
+		int N = w.length;
+		return p(w, v, N, 0, bag);
+	}
+
+	// 0..i-1位置已经做好了选择，从i以后开始做决策
+	// rest 背包中还剩下的容量是多少
+	public static int p(int[] w, int[] v, int len, int i, int rest) {
+		// rest空间不能为负数
+		if (rest < 0) {
+			return -1;
+		}
+		if (i == len) {
+			return 0;
+		}
+		int p1 = p(w, v, len, i + 1, rest);
+
+		int p2 = p(w, v, len, i + 1, rest - w[i]);
+
+		if (p2 != -1) { // p2 不为-1才能算做正常解
+			p2 += v[i];
+		}
+		return Math.max(p1, p2);
+	}
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int N = in.nextInt();
+		int bag = in.nextInt();
+		int[] w = new int[N];
+		int[] v = new int[N];
+		for (int i = 0; i < N; i++) {
+			v[i] = in.nextInt();
+			w[i] = in.nextInt();
+		}
+		System.out.println(getMaxValue(w, v, bag));
+		in.close();
+	}
 
 }
