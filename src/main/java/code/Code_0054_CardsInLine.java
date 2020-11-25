20f396e4e6b02fdca5e1af1a2a44397582f1df8a
zenghui <410486047@qq.com>
2020-11-25 09:47:50 +0800

    add note

--- src/main/java/code/Code_0054_CardsInLine.java
+++ src/main/java/code/Code_0054_CardsInLine.java
@@ -1,5 +1,9 @@
 package code;
-
+//给定一个整型数组arr，代表数值不同的纸牌排成一条线，
+//        玩家A和玩家B依次拿走每张纸牌，
+//        规定玩家A先拿，玩家B后拿，
+//        但是每个玩家每次只能拿走最左或最右的纸牌，
+//        玩家A和玩家B都绝顶聪明。请返回最后获胜者的分数。
 public class Code_0054_CardsInLine {
     public static int win2(int[] arr) {
         int n = arr.length;
