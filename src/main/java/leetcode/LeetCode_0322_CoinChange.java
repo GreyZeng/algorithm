da5e165fe12b85a3db58e1ec0cc4bf0fe47c41bd
zenghui <410486047@qq.com>
2020-11-27 11:59:44 +0800

    add todo

--- src/main/java/leetcode/LeetCode_0322_CoinChange.java
+++ src/main/java/leetcode/LeetCode_0322_CoinChange.java
@@ -36,36 +36,23 @@ If that amount of money cannot be made up by any combination of the coins, retur
         0 <= amount <= 10^4*/
 package leetcode;
 
-// TODO 记忆化搜索 斜率优化
-// 暴力解
+
 public class LeetCode_0322_CoinChange {
 
-	public int coinChange(int[] coins, int amount) {
-		if (amount == 0) {
-			return 0;
-		}
-		if (coins.length == 0) {
-			return -1;
-		}
-		return p(coins, 0, amount);
-	}
+    public int coinChange(int[] coins, int amount) {
+        if (coins == null || coins.length == 0 || amount < 0) {
+            return -1;
+        }
+        int N = coins.length;
+        int[][] dp = new int[N + 1][amount + 1];
+        // dp[i][j] 包含i号硬币搞定j数量钱的最小拼凑数量
+        for (int i = 1; i < amount + 1; i++) {
+            // 没有硬币了，如果amount数量不为0，就一定没有拼凑的机会
+            dp[N][i] = -1;
+        }
+        // TODO
+        return -1;
+    }
 
-	// 用掉了0..i-1号硬币，从i号开始选，还剩下rest要处理
-	public static int p(int[] coins, int index, int rest) {
-		if (rest == 0) {
-			return 0;
-		}
-		if (index == coins.length) {
-			return -1;
-		}
-		int p = Integer.MAX_VALUE;
-		for (int n = 0; n * coins[index] <= rest; n++) {
-			int p1 = p(coins, index + 1, rest - n * coins[index]);
-			if (p1 != -1) {
-				p = Math.min(p, p1);
-			}
-		}
-		return p;
-	}
 
 }
