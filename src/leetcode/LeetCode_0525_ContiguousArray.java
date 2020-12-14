cb9fb462dd603604097717482705f1f4f8295984
zenghui <410486047@qq.com>
2020-12-14 14:22:43 +0800

    leetcode 525 contiguous array

---
+++ src/leetcode/LeetCode_0525_ContiguousArray.java
@@ -0,0 +1,44 @@
+//Given a binary array, find the maximum length of a contiguous subarray with equal number of 0 and 1.
+//
+//        Example 1:
+//        Input: [0,1]
+//        Output: 2
+//        Explanation: [0, 1] is the longest contiguous subarray with equal number of 0 and 1.
+//        Example 2:
+//        Input: [0,1,0]
+//        Output: 2
+//        Explanation: [0, 1] (or [1, 0]) is a longest contiguous subarray with equal number of 0 and 1.
+//        Note: The length of the given binary array will not exceed 50,000.
+package leetcode;
+
+import java.util.HashMap;
+
+// 预处理一下原数组，遇到0变为-1，遇到1保持1，遇到其他变为0
+// 求子数组之和为0的最大子数组长度
+public class LeetCode_0525_ContiguousArray {
+    public static int findMaxLength(int[] nums) {
+        int N = nums.length;
+        int cur = 0;
+        for (; cur < N; cur++) {
+            if (nums[cur] == 0) {
+                nums[cur] = -1;
+            } else if (nums[cur] == 1) {
+                nums[cur] = 1;
+            } else {
+                nums[cur] = 0;
+            }
+        }
+        HashMap<Integer, Integer> map = new HashMap<>(N);
+        map.put(0, -1);
+        int sum = 0;
+        int max = 0;
+        for (cur = 0; cur < N; cur++) {
+            sum += nums[cur];
+            if (!map.containsKey(sum)) {
+                map.put(sum, cur);
+            }
+            max = Math.max(max, cur - map.get(sum));
+        }
+        return max;
+    }
+}
--- src/lintcode/LintCode_0911_MaximumSizeSubarraySumEqualsK.java
+++ src/lintcode/LintCode_0911_MaximumSizeSubarraySumEqualsK.java
@@ -3,26 +3,26 @@ package lintcode;
 import java.util.HashMap;
 
 public class LintCode_0911_MaximumSizeSubarraySumEqualsK {
-  public int maxSubArrayLen(int[] arr, int k) {
-    if (arr == null || arr.length == 0) {
-      return 0;
+    public int maxSubArrayLen(int[] arr, int k) {
+        if (arr == null || arr.length == 0) {
+            return 0;
+        }
+        int N = arr.length;
+        HashMap<Integer, Integer> map = new HashMap<>(N);
+        int cur = 0;
+        int max = 0;
+        int sum = 0;
+        map.put(0, -1);
+        for (; cur < arr.length; cur++) {
+            sum += arr[cur];
+            if (!map.containsKey(sum)) {
+                map.put(sum, cur);
+            }
+            if (map.containsKey(sum - k)) {
+                max = Math.max(max, cur - map.get(sum - k));
+            }
+        }
+        return max;
     }
-    int N = arr.length;
-    HashMap<Integer, Integer> map = new HashMap<>(N);
-    int cur = 0;
-    int max = 0;
-    int sum = 0;
-    map.put(0,-1);
-    for (; cur < arr.length; cur++) {
-      sum += arr[cur];
-      if (!map.containsKey(sum)) {
-        map.put(sum,cur);
-      }
-      if (map.containsKey(sum - k )) {
-        max = Math.max(max, cur - map.get(sum -k ));
-      }
-    }
-    return max;
-  }
 
 }
