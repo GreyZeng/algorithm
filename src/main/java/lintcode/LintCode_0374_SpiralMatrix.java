e664649941ae5dec86b0f2339a37c9ab08c542fd
zenghui <410486047@qq.com>
2020-12-22 12:02:45 +0800

    add todo

---
+++ src/main/java/lintcode/LintCode_0374_SpiralMatrix.java
@@ -0,0 +1,24 @@
+//描述
+//        给定一个包含 m x n 个要素的矩阵，（m 行, n 列），按照螺旋顺序，返回该矩阵中的所有要素。
+//
+//        样例
+//        样例 1:
+//
+//        输入:[[ 1, 2, 3 ], [ 4, 5, 6 ], [ 7, 8, 9 ]]
+//        输出: [1,2,3,6,9,8,7,4,5]
+//        样例 2
+//
+//        输入:[[ 6,4,1 ], [ 7,8,9 ]]
+//        输出: [6,4,1,9,8,7]
+package lintcode;
+
+
+import java.util.List;
+
+// https://www.lintcode.com/problem/spiral-matrix/description
+// TODO
+public class LintCode_0374_SpiralMatrix {
+    public static List<Integer> spiralOrder(int[][] matrix) {
+        return null;
+    }
+}
