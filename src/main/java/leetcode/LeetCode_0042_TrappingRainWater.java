423a38380663560794cc172dad1e0ad7d17b5282
zenghui <410486047@qq.com>
2020-12-28 17:03:54 +0800

    add todo

--- src/main/java/leetcode/LeetCode_0042_TrappingRainWater.java
+++ src/main/java/leetcode/LeetCode_0042_TrappingRainWater.java
@@ -1,34 +1,50 @@
 package leetcode;
 
+/**
+ * @author Grey
+ */
+//Constraints:
+// n == height.length
+// 0 <= n <= 3 * 10^4
+// 0 <= height[i] <= 10^5
 public class LeetCode_0042_TrappingRainWater {
 
-	// 双指针
-	public static int trap(int[] arr) {
-		if (arr == null || arr.length < 3) {
-			return 0;
-		}
-		int N = arr.length - 1;
-		int LMAX = arr[0];
-		int RMAX = arr[N];
-		int L = 1;
-		int R = arr.length - 2;
-		int res = 0;
-		while (L <= R) {
-			if (LMAX < RMAX) {
-				if (arr[L] < LMAX) {
-					res += (LMAX - arr[L++]);
-				} else {
-					LMAX = Math.max(LMAX, arr[L++]);
-				}
-			} else {
-				if (arr[R] < RMAX) {
-					res += (RMAX - arr[R--]);
-				} else {
-					RMAX = Math.max(RMAX, arr[R--]);
-				}
-			}
-		}
-		return res;
-	}
+    // 双指针
+    // TODO
+    public static int trap(int[] arr) {
+        if (null == arr || arr.length <= 2) {
+            return 0;
+        }
+        int n = arr.length;
+        // 考虑每个位置上的水能留下多少
+        int leftMax = arr[0];
+        int rightMax = arr[n - 1];
+        int l = 1;
+        int r = n - 2;
+        int water = 0;
+        while (l <= r) {
+            if (leftMax > rightMax) {
+                if (arr[l] < rightMax) {
+                    water += (rightMax - arr[l]);
+                } else {
+                    leftMax = Math.max(arr[l], leftMax);
+                }
+                l++;
+            } else {
+                if (arr[r] < leftMax) {
+                    water += (leftMax - arr[r]);
+                } else {
+                    rightMax = Math.max(arr[r], rightMax);
+                }
+                r--;
+            }
+        }
+        return water;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
+        System.out.println(trap(arr));
+    }
 
 }
