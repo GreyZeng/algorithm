097256416e8aca4d73bf0594321b36486d20aa56
GreyZeng <410486047@qq.com>
2021-01-12 18:13:27 +0800

    leetcode 222

--- src/main/java/leetcode/LeetCode_0222_CountCompleteTreeNodes.java
+++ src/main/java/leetcode/LeetCode_0222_CountCompleteTreeNodes.java
@@ -45,11 +45,35 @@ public class LeetCode_0222_CountCompleteTreeNodes {
             this.right = right;
         }
     }
-    // TODO
-    public static int countNodes(TreeNode head) {
-        return -1;
-	}
-   
 
-    
+    public static int countNodes(TreeNode head) {
+        if (head == null) {
+            return 0;
+        }
+        int h = maxLenOfLeft(head, 1);
+        return count(head, 1, h);
+    }
+
+    private static int count(TreeNode head, int level, int h) {
+        if (level == h) {
+            return 1;
+        }
+        if (maxLenOfLeft(head.right, level + 1) == h) {
+            // 左树一定是满的
+            return (1 << (h - level)) + count(head.right, level + 1, h);
+        } else {
+            // 右数一定是满的，注意高度是 h - level - 1
+            return (1 << (h - level - 1)) + count(head.left, level + 1, h);
+        }
+    }
+
+    public static int maxLenOfLeft(TreeNode root, int level) {
+        while (root != null) {
+            root = root.left;
+            level++;
+        }
+        return level - 1;
+    }
+
+
 }
