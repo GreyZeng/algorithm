542c5df5c608aaabb824f542af1cca1d5c6c19fc
GreyZeng <410486047@qq.com>
2021-01-14 23:11:21 +0800

    two sorted array to topk

---
+++ src/main/java/nowcoder/NowCoder_TwoSortedArraySumTopK.java
@@ -0,0 +1,16 @@
+package nowcoder;
+
+//https://www.nowcoder.com/practice/7201cacf73e7495aa5f88b223bbbf6d1\
+//给定两个有序数组arr1和arr2，再给定一个正数K
+//求两个数累加和最大的前K个，两个数必须分别来自arr1和arr2
+//tips:
+//大根堆 sum来组织
+//右下角最大，先进大根堆，然后弹出，左边和上面的依次加入
+//不要重复加入
+//O(K*logK)
+public class NowCoder_TwoSortedArraySumTopK {
+    // TODO
+    public static int[] topK(int[] arr1, int arr2,int k) {
+        return null;
+    }
+}
