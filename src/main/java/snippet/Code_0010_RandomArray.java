1042d1bb64a5c1935f4d8c1069a0ecfbc11cf13f
GreyZeng <410486047@qq.com>
2021-01-22 13:26:52 +0800

    random array

--- src/main/java/snippet/Code_0010_RandomArray.java
+++ src/main/java/snippet/Code_0010_RandomArray.java
@@ -22,11 +22,53 @@ package snippet;
 //
 //        空间复杂度可以做到O(1)
 public class Code_0010_RandomArray {
-    // TODO
     public static int[] randomArray(int M) {
+        if (M == 1) {
+            return new int[]{1};
+        }
+        int half = (M + 1) / 2;
+        int[] base = randomArray(half);
+        int[] result = new int[M];
+        int index = 0;
+        for (; index < half; index++) {
+            result[index] = base[index] * 2 - 1;
+        }
+        for (int i = 0; index < M; i++, index++) {
+            result[index] = base[i] * 2;
+        }
+        return result;
+    }
+    // 检验函数
+    public static boolean isValid(int[] arr) {
+        int N = arr.length;
+        for (int i = 0; i < N; i++) {
+            for (int k = i + 1; k < N; k++) {
+                for (int j = k + 1; j < N; j++) {
 
-        return null;
+
+                    if (arr[i] + arr[j] == 2 * arr[k]) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
     }
 
+    public static void main(String[] args) {
+        System.out.println("test begin");
+        for (int N = 1; N < 1000; N++) {
+            int[] arr = randomArray(N);
+            if (!isValid(arr)) {
+                System.out.println("Oops!");
+            }
+        }
+        System.out.println("test end");
+
+        System.out.println(isValid(randomArray(1042)));
+        System.out.println(isValid(randomArray(2981)));
+
+
+    }
 
 }
