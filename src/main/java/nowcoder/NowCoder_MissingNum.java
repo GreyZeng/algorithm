c1be649e5f77f9296da3f009b858c58161a6e277
zenghui <410486047@qq.com>
2021-01-25 10:00:05 +0800

    missing number

--- src/main/java/nowcoder/NowCoder_MissingNum.java
+++ src/main/java/nowcoder/NowCoder_MissingNum.java
@@ -1,5 +1,4 @@
 /*链接：https://www.nowcoder.com/questionTerminal/ceeeb34083934e79ad56019cd2cef4c4
-tips: https://www.processon.com/view/link/5f74af881e085307a0768f41#map
         来源：牛客网
 
 给定一个有序的正数数组arr和一个正数range，如果可以自由选择arr中的数字，想累加得 到 1~range 范围上所有的数，返回arr最少还缺几个数。
@@ -14,6 +13,7 @@ arr = {1,5,7}，range = 15
         接下来一行N个整数表示数组内的元素*/
 package nowcoder;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 /**
@@ -21,21 +21,44 @@ import java.util.Scanner;
  * @version 1.0
  * @date 2021/1/25 0:06
  */
-// TODO
 public class NowCoder_MissingNum {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
-        int k = in.nextInt();
+        int aim = in.nextInt();
         int[] arr = new int[n];
         for (int i = 0; i < n; i++) {
             arr[i] = in.nextInt();
         }
-        System.out.println(missing(arr, k));
+        System.out.println(missing(arr, aim));
         in.close();
     }
 
-    public static int missing(int[] arr, int k) {
-        return -1;
+    // 如果要实现1~range所有目标，但整个目标还没有达到1~aim，你永远缺range+1，一定是最省且最经济的，补上range+1后，能达到的数是1~2*range+1
+    // 先将数组排序，依次考察如何最经济使用i位置的数
+    public static int missing(int[] arr, int aim) {
+        int miss = 0;
+        long range = 0;
+        Arrays.sort(arr);
+        for (int i = 0; i < arr.length; i++) {
+            while (arr[i] > range + 1) {
+                range += (range + 1);
+                miss++;
+                if (range >= aim) {
+                    return miss;
+                }
+            }
+            range += arr[i];
+            if (range >= aim) {
+                return miss;
+            }
+
+        }
+
+        while (aim >= range + 1) {
+            range += range + 1;
+            miss++;
+        }
+        return miss;
     }
 }
