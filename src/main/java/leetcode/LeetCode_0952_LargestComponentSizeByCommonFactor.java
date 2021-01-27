7562f0772bfae182da7ccc29bf0640f2a57daa0f
GreyZeng <410486047@qq.com>
2021-01-27 12:14:25 +0800

    add todo

--- src/main/java/leetcode/LeetCode_0952_LargestComponentSizeByCommonFactor.java
+++ src/main/java/leetcode/LeetCode_0952_LargestComponentSizeByCommonFactor.java
@@ -5,11 +5,43 @@
         Return the size of the largest connected component in the graph.*/
 package leetcode;
 
+import java.util.*;
+
 // TODO
 public class LeetCode_0952_LargestComponentSizeByCommonFactor {
-    public static int largestComponentSize(int[] arr) {
-        return -1;
-    }
+	public static int largestComponentSize(int[] arr) {
+		Map<Integer, Integer> map = new HashMap<>();
+		Set<Integer> set = new HashSet<>();
+		for (int i = 0; i < arr.length; i++) {
+			int num = (int) Math.sqrt(arr[i]);
+			for (int j = 2; j <= num; j++) {
+				
+			}
+		}
+		return -1;
+	}
 
+	public static int gcd(int m, int n) {
+		return n == 0 ? m : gcd(n, m % n);
+	}
 
+	// 并查集
+	public class UnionFind {
+
+		public UnionFind(int[] arr) {
+
+		}
+
+		public void union(int i, int j) {
+
+		}
+
+		public boolean isSame(int i, int j) {
+			return false;
+		}
+
+		public int maxSize() {
+			return -1;
+		}
+	}
 }
