bc4e903603a4b16e0dffcb6608c93af5ad620af7
GreyZeng <410486047@qq.com>
2021-01-27 13:11:34 +0800

    LargestComponentSizeByCommonFactor

--- src/main/java/leetcode/LeetCode_0952_LargestComponentSizeByCommonFactor.java
+++ src/main/java/leetcode/LeetCode_0952_LargestComponentSizeByCommonFactor.java
@@ -7,41 +7,89 @@ package leetcode;
 
 import java.util.*;
 
-// TODO
 public class LeetCode_0952_LargestComponentSizeByCommonFactor {
 	public static int largestComponentSize(int[] arr) {
+		UnionFind uf = new UnionFind(arr.length);
 		Map<Integer, Integer> map = new HashMap<>();
-		Set<Integer> set = new HashSet<>();
 		for (int i = 0; i < arr.length; i++) {
 			int num = (int) Math.sqrt(arr[i]);
-			for (int j = 2; j <= num; j++) {
-				
+			for (int j = 1; j <= num; j++) {
+				if (arr[i] % j == 0) {
+					if (j != 1) {
+						if (!map.containsKey(j)) {
+							map.put(j, i);
+						} else {
+							uf.union(map.get(j), i);
+						}
+
+					}
+					int other = arr[i] / j;
+					if (other != 1) {
+						if (!map.containsKey(other)) {
+							map.put(other, i);
+						} else {
+							uf.union(map.get(other), i);
+						}
+					}
+				}
 			}
 		}
-		return -1;
+		return uf.maxSize();
 	}
-
-	public static int gcd(int m, int n) {
-		return n == 0 ? m : gcd(n, m % n);
-	}
-
+ 
 	// 并查集
-	public class UnionFind {
+	public static class UnionFind {
+		private int[] parents; // parents[i] = j arr[i]的父亲是arr[j]
+		private int[] sizes; // sizes[i] = X arr[i]所在的集合大小为X
 
-		public UnionFind(int[] arr) {
-
+		public UnionFind(int len) {
+			parents = new int[len];
+			sizes = new int[len];
+			for (int i = 0; i < len; i++) {
+				parents[i] = i;
+				sizes[i] = 1;
+			}
 		}
 
-		public void union(int i, int j) {
-
-		}
-
-		public boolean isSame(int i, int j) {
-			return false;
+		public int size() {
+			int ans = 0;
+			for (int i = 0; i < sizes.length; i++) {
+				ans += sizes[i] != 0 ? 1 : 0;
+			}
+			return ans;
 		}
 
 		public int maxSize() {
-			return -1;
+			int ans = 0;
+			for (int size : sizes) {
+				ans = Math.max(ans, size);
+			}
+			return ans;
+		}
+
+		private int findHead(int element) {
+			Stack<Integer> path = new Stack<>();
+			while (element != parents[element]) {
+				path.push(element);
+				element = parents[element];
+			}
+			while (!path.isEmpty()) {
+				parents[path.pop()] = element;
+			}
+			return element;
+		}
+
+		// a 和 b 分别是两个数的位置，不是值
+		public void union(int a, int b) {
+			int aF = findHead(a);
+			int bF = findHead(b);
+			if (aF != bF) {
+				int big = sizes[aF] >= sizes[bF] ? aF : bF;
+				int small = big == aF ? bF : aF;
+				parents[small] = big;
+				sizes[big] = sizes[aF] + sizes[bF];
+				sizes[small] = 0;
+			}
 		}
 	}
 }
