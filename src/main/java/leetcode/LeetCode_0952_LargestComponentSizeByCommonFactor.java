4e6d8ca8b12411ee8e26e16ff614659186862a26
zenghui <410486047@qq.com>
2021-01-27 10:28:50 +0800

    LargestComponentSizeByCommonFactor

---
+++ src/main/java/leetcode/LeetCode_0952_LargestComponentSizeByCommonFactor.java
@@ -0,0 +1,13 @@
+/*Given a non-empty array of unique positive integers A, consider the following graph:
+
+        There are A.length nodes, labelled A[0] to A[A.length - 1];
+        There is an edge between A[i] and A[j] if and only if A[i] and A[j] share a common factor greater than 1.
+        Return the size of the largest connected component in the graph.*/
+package leetcode;
+
+
+public class LeetCode_0952_LargestComponentSizeByCommonFactor {
+    public static int largestComponentSize(int[] arr) {
+        return -1;
+    }
+}
