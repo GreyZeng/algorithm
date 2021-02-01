942f3436939825a3baf7190c50cea901af443c61
zenghui <410486047@qq.com>
2021-02-01 11:26:25 +0800

    10 ways

---
+++ src/main/java/snippet/Code_0023_10Ways.java
@@ -0,0 +1,11 @@
+package snippet;
+
+//假设给你N个0，和N个1，你必须用全部数字拼序列
+// 返回有多少个序列满足：任何前缀串，1的数量都不少于0的数量
+// TODO
+public class Code_0023_10Ways {
+    public static int ways(int N) {
+        return -1;
+    }
+}
+
