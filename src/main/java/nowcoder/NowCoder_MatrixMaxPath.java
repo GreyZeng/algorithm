81928e4020e40947475c0656d2b936cf0c2c2f02
zenghui <410486047@qq.com>
2021-02-07 15:30:17 +0800

    matrix mat path

--- src/main/java/nowcoder/NowCoder_MatrixMaxPath.java
+++ src/main/java/nowcoder/NowCoder_MatrixMaxPath.java
@@ -55,8 +55,61 @@ public class NowCoder_MatrixMaxPath {
         in.close();
     }
 
-    // TODO
     public static int max(int[][] matrix) {
-        return -1;
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int[][][] dp = new int[m][n][m];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                for (int k = 0; k < m; k++) {
+                    dp[i][j][k] = -1;
+                }
+            }
+        }
+        return p(matrix, m, n, 0, 0, 0, dp);
+    }
+
+    // 第一个点：(x1,y1), 第二个点(x2,y2)
+    // 重要限制：来到同一个位置时，只获得一份
+    public static int p(int[][] matrix, int m, int n, int x1, int y1, int x2, int[][][] dp) {
+        if (dp[x1][y1][x2] != -1) {
+            return dp[x1][y1][x2];
+        }
+        if (x1 == m - 1 && y1 == n - 1) {
+            dp[m - 1][n - 1][x2] = matrix[x1][y1];
+            return dp[m - 1][n - 1][x2];
+        }
+        int y2 = getY2(m, n, x1, y1, x2);
+        int next = -1;
+        // 下右
+        if (x1 < m - 1 && y2 < n - 1) {
+            next = Math.max(next, p(matrix, m, n, x1 + 1, y1, x2, dp));
+        }
+        // 右下
+        if (y1 < n - 1 && x2 < m - 1) {
+            next = Math.max(next, p(matrix, m, n, x1, y1 + 1, x2 + 1, dp));
+        }
+        // 右右
+        if (y1 < n - 1 && y2 < n - 1) {
+            next = Math.max(next, p(matrix, m, n, x1, y1 + 1, x2, dp));
+        }
+        // 下下
+        if (x1 < m - 1 && x2 < m - 1) {
+            next = Math.max(next, p(matrix, m, n, x1 + 1, y1, x2 + 1, dp));
+        }
+        if (matrix[x1][y1] == -1 || matrix[x2][y2] == -1 || next == -1) {
+            dp[x1][y1][x2] = -1;
+            return dp[x1][y1][x2];
+        }
+        if (x1 == x2) {
+            dp[x1][y1][x2] = matrix[x1][y1] + next;
+            return dp[x1][y1][x2];
+        }
+        dp[x1][y1][x2] = matrix[x1][y1] + matrix[x2][y2] + next;
+        return dp[x1][y1][x2];
+    }
+
+    private static int getY2(int m, int n, int x1, int y1, int x2) {
+        return x1 + y1 - x2;
     }
 }
