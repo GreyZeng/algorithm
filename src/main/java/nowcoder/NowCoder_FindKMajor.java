8e7ff63a38557cf64989415f7fd7bba65502f060
zenghui <410486047@qq.com>
2021-02-08 10:32:44 +0800

    find k major

--- src/main/java/nowcoder/NowCoder_FindKMajor.java
+++ src/main/java/nowcoder/NowCoder_FindKMajor.java
@@ -27,11 +27,16 @@
 // 时间复杂度O(n),额外空间复杂度O(k)。
 package nowcoder;
 
-import java.util.Scanner;
+import java.util.*;
 
-// TODO
 public class NowCoder_FindKMajor {
     public static void main(String[] args) {
+        /*int[] arr = {1, 2, 3, 1, 2, 3, 4};
+        int k = 7;
+        System.out.println(major(arr, k));
+        int[] arr2 = {1, 1, 2, 3};
+        int k2 = 1;
+        System.out.println(major(arr2, k2));*/
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
         int k = in.nextInt();
@@ -39,11 +44,86 @@ public class NowCoder_FindKMajor {
         for (int i = 0; i < n; i++) {
             arr[i] = in.nextInt();
         }
-        System.out.println(major(arr, k));
+        List<Integer> result = major(arr, k);
+        for (int i = 0; i < result.size(); i++) {
+            if (i != result.size() - 1) {
+                System.out.print(result.get(i) + " ");
+            } else {
+                System.out.print(result.get(i));
+            }
+        }
+        System.out.println();
         in.close();
     }
 
-    public static int major(int[] arr, int k) {
-        return -1;
+    public static List<Integer> major(int[] arr, int k) {
+        List<Integer> result = new ArrayList<>();
+        if (k < 2) {
+            result.add(-1);
+            return result;
+        }
+        if (k > arr.length) {
+            for (int j : arr) {
+                result.add(j);
+            }
+            return result;
+        }
+        HashMap<Integer, Integer> map = new HashMap<>(k);
+        for (int j : arr) {
+            if (map.containsKey(j)) {
+                map.put(j, map.get(j) + 1);
+            } else {
+                if (map.size() == k - 1) {
+                    deleteOne(map);
+                } else {
+                    map.put(j, 1);
+                }
+            }
+        }
+        if (map.isEmpty()) {
+            result.add(-1);
+            return result;
+        }
+        HashMap<Integer, Integer> checked = check(arr, map);
+        if (checked.isEmpty()) {
+            result.add(-1);
+            return result;
+        }
+        Set<Integer> keys = checked.keySet();
+        for (int key : keys) {
+            if (checked.get(key) > arr.length / k) {
+                result.add(key);
+            }
+        }
+        if (result.isEmpty()) {
+            result.add(-1);
+        }
+        return result;
+    }
+
+    private static HashMap<Integer, Integer> check(int[] arr, HashMap<Integer, Integer> map) {
+        HashMap<Integer, Integer> checked = new HashMap<>();
+        for (int j : arr) {
+            if (map.containsKey(j)) {
+                if (checked.containsKey(j)) {
+                    checked.put(j, checked.get(j) + 1);
+                } else {
+                    checked.put(j, 1);
+                }
+            }
+        }
+        return checked;
+    }
+
+    private static void deleteOne(HashMap<Integer, Integer> map) {
+        Iterator<Integer> iterator = map.keySet().iterator();
+        while (iterator.hasNext()) {
+            Integer key = iterator.next();
+            if (map.get(key) == 1) {
+                iterator.remove();
+            } else {
+                map.put(key, map.get(key) - 1);
+            }
+        }
     }
 }
