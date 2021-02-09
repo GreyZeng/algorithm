09bb6b4f8586b1b1389295fcd2a878ca8962272c
GreyZeng <410486047@qq.com>
2021-02-09 21:12:44 +0800

    leetcode 516 longest palindromic sub sequence

--- src/main/java/leetcode/LeetCode_0516_LongestPalindromicSubsequence.java
+++ src/main/java/leetcode/LeetCode_0516_LongestPalindromicSubsequence.java
@@ -25,7 +25,30 @@
 package leetcode;
 
 public class LeetCode_0516_LongestPalindromicSubsequence {
+    
     public static int longestPalindromeSubseq(String s) {
+        char[] str = s.toCharArray();
+        int n = str.length;
+        // L...R 最大回文长度存dp[L][R]中
+        int[][] dp = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = 1;
+        }
+        for (int i = 0; i < n - 1; i++) {
+            dp[i][i + 1] = str[i] == str[i + 1] ? 2 : 1;
+        }
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = i + 2; j < n; j++) {
+                dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1]);
+                if (str[i] == str[j]) {
+                    dp[i][j] = Math.max(dp[i][j], dp[i + 1][j - 1] + 2);
+                }
+            }
+        }
+        return dp[0][n - 1];
+    }
+    
+    public static int longestPalindromeSubseq2(String s) {
         char[] str1 = s.toCharArray();
         char[] reverseStr = reverse(str1);
         return lcp(str1, reverseStr);
@@ -46,24 +69,23 @@ public class LeetCode_0516_LongestPalindromicSubsequence {
         int[][] dp = new int[m][n];
         dp[0][0] = str1[0] == str2[0] ? 1 : 0;
         for (int i = 1; i < m; i++) {
-			dp[i][0] = Math.max(dp[i - 1][0], str1[i] == str2[0] ? 1 : 0);
-		}
-		for (int j = 1; j < n; j++) {
-			dp[0][j] = Math.max(dp[0][j - 1], str1[0] == str2[j] ? 1 : 0);
-		}
-
+            dp[i][0] = Math.max(dp[i - 1][0], str1[i] == str2[0] ? 1 : 0);
+        }
+        for (int j = 1; j < n; j++) {
+            dp[0][j] = Math.max(dp[0][j - 1], str1[0] == str2[j] ? 1 : 0);
+        }
         for (int i = 1; i < m; i++) {
             for (int j = 1; j < n; j++) {
                 dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
                 if (str1[i] == str2[j]) {
-                    dp[i][j] =Math.max(dp[i][j],dp[i - 1][j - 1] + 1) ;
+                    dp[i][j] = Math.max(dp[i][j], dp[i - 1][j - 1] + 1);
                 }
             }
         }
         return dp[m - 1][n - 1];
     }
-    public static void main(String[] args) {
-        String test = "A1BC2D33FG2H1I";
-		System.out.println(longestPalindromeSubseq(test));
-    }
+
+    
+
+
 }
