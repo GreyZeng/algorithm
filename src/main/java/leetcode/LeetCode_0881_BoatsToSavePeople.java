8b1fbc7a4a848d3cee7ce48d634ff84dddda41bd
zenghui <410486047@qq.com>
2021-02-09 15:06:47 +0800

    leetcode 881 boats to save people

--- src/main/java/leetcode/LeetCode_0881_BoatsToSavePeople.java
+++ src/main/java/leetcode/LeetCode_0881_BoatsToSavePeople.java
@@ -27,14 +27,65 @@
         1 <= people[i] <= limit <= 3 * 10^4*/
 package leetcode;
 
+import java.util.Arrays;
+
 /**
  * @author Young
  * @version 1.0
  * @date 2021/2/9 13:30
  */
-// TODO
 public class LeetCode_0881_BoatsToSavePeople {
-    public int numRescueBoats(int[] people, int limit) {
-        return -1;
+    public static int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+        int pivot = limit / 2;
+        int lessMostRight = getLessMostRight(people, pivot);
+        int L = lessMostRight;
+        int R = lessMostRight + 1;
+        int leftRemain = lessMostRight + 1;
+        int rightRemain = people.length - leftRemain;
+        int merge = 0;
+        while (L >= 0 && R < people.length) {
+            if (people[L] + people[R] <= limit) {
+                L--;
+                R++;
+                merge++;
+                leftRemain--;
+                rightRemain--;
+            } else {
+                L--;
+            }
+        }
+
+        return (leftRemain / 2) + (leftRemain % 2) + rightRemain + merge;
+    }
+
+
+    // 得到小于等于aim的最右位置
+    public static int getLessMostRight(int[] arr, int aim) {
+        int L = 0;
+        int R = arr.length - 1;
+        int r = L;
+        while (L <= R) {
+            int mid = (L + ((R - L) >> 1));
+            if (arr[mid] > aim) {
+                R = mid - 1;
+            } else {
+                r = mid;
+                L = mid + 1;
+            }
+        }
+        return r;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2};
+        int aim = 3;
+        System.out.println(numRescueBoats(arr, aim));
+        int[] arr2 = {3, 2, 2, 1};
+
+        System.out.println(numRescueBoats(arr2, aim));
+        int[] arr3 = {3, 5, 3, 4};
+        aim = 5;
+        System.out.println(numRescueBoats(arr3, aim));
     }
 }
