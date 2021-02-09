2bcc3e17e4e65dca8726db4e28ee1a8459bd21c0
zenghui <410486047@qq.com>
2021-02-09 15:23:09 +0800

    leetcode 516

---
+++ src/main/java/leetcode/LeetCode_0516_LongestPalindromicSubsequence.java
@@ -0,0 +1,31 @@
+/*Given a string s, find the longest palindromic subsequence's length in s. You may assume that the maximum length of s is 1000.
+
+        Example 1:
+        Input:
+
+        "bbbab"
+        Output:
+        4
+        One possible longest palindromic subsequence is "bbbb".
+
+
+        Example 2:
+        Input:
+
+        "cbbd"
+        Output:
+        2
+        One possible longest palindromic subsequence is "bb".
+
+
+        Constraints:
+
+        1 <= s.length <= 1000
+        s consists only of lowercase English letters.*/
+package leetcode;
+// TODO
+public class LeetCode_0516_LongestPalindromicSubsequence {
+    public static int longestPalindromeSubseq(String s) {
+        return -1;
+    }
+}
