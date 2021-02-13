605167d3830447b5bd6561cfe96b10a79ea6fcac
GreyZeng <410486047@qq.com>
2021-02-13 13:21:18 +0800

    temp commit

---
+++ src/main/java/snippet/Code_0044_SplitNum.java
@@ -0,0 +1,19 @@
+/*给定一个正数1，裂开的方法有一种，(1) 给定一个正数2，裂开的方法有两种，(1和1)、(2) 给定一个正数3，裂开的方法有三种，(1、1、1)、(1、2)、(3) 给定一个正数4，
+		裂开的方法有五种，(1、1、1、1)、(1、1、2)、(1、3)、(2、2)、 (4)
+		给定一个正数n，求裂开的方法数。 动态规划优化状态依赖的技巧
+		f(int pre, int rest)
+
+		可以优化枚举行为（前面有：分硬币方法，股票问题3）*/
+package snippet;
+
+
+public class Code_0044_SplitNum {
+    public static int split(int n) {
+        return -1;
+    }
+
+    //
+    public static int f(int pre, int rest) {
+        return -1;
+    }
+}
