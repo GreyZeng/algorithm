4629ffa270d3416c3bea5e4eb8f19933ad49c1a5
GreyZeng <410486047@qq.com>
2021-02-13 17:05:04 +0800

    split number

--- src/main/java/snippet/Code_0044_SplitNum.java
+++ src/main/java/snippet/Code_0044_SplitNum.java
@@ -9,11 +9,80 @@ package snippet;
 
 public class Code_0044_SplitNum {
     public static int split(int n) {
-        return -1;
+        return f(1, n);
     }
 
-    //
     public static int f(int pre, int rest) {
-        return -1;
+        if (rest == 0) {
+            return 1;
+        }
+        if (pre > rest) {
+            // 前一次划分的东西，比rest还多，说明rest无论怎么划分，都不满足要求，因为划分必须按升序来
+            return 0;
+        }
+        int way = 0;
+        for (int i = pre; i <= rest; i++) {
+            way += f(i, rest - i);
+        }
+        return way;
     }
+
+
+    public static int split2(int n) {
+        // 行pre
+        // 列rest
+        // dp[0][...] 弃而不用
+        int[][] dp = new int[n + 1][n + 1];
+        // dp[...][0] = 0 第0列为1
+        for (int i = 1; i <= n; i++) {
+            dp[i][0] = 1;
+            // 对角线都是1(因为对角线表示pre的值和rest相等，由于需要递增，所以只有一种方式）
+            // dp[i][i] = 1;
+        }
+        // 左下半区全为0
+        for (int pre = n; pre >= 1; pre--) {
+            for (int rest = pre; rest <= n; rest++) {
+                for (int i = pre; i <= rest; i++) {
+                    dp[pre][rest] += dp[i][rest - i];
+                }
+            }
+        }
+        return dp[1][n];
+    }
+
+
+    public static int split3(int n) {
+        // 行pre
+        // 列rest
+        // dp[0][...] 弃而不用
+        int[][] dp = new int[n + 1][n + 1];
+        // dp[...][0] = 0 第0列为1
+        for (int i = 1; i <= n; i++) {
+            dp[i][0] = 1;
+            // 对角线都是1(因为对角线表示pre的值和rest相等，由于需要递增，所以只有一种方式）
+            dp[i][i] = 1;
+        }
+        // 左下半区全为0
+        for (int pre = n - 1; pre >= 1; pre--) {
+            for (int rest = pre + 1; rest <= n; rest++) {
+//                dp[pre][rest] += dp[pre][rest - pre];
+//                dp[pre][rest] += dp[pre + 1][rest - pre - 1];
+//                dp[pre][rest] += dp[pre + 2][rest - pre - 2];
+//                dp[pre][rest] += dp[pre + 3][rest - pre - 3];
+//                dp[pre][rest] += dp[pre + 4][rest - pre - 4];
+//                dp[pre][rest] += dp[pre + 5][rest - pre - 5];
+
+
+//                dp[pre + 1][rest] += dp[pre + 1][rest - pre - 1];
+//                dp[pre + 1][rest] += dp[pre + 2][rest - pre - 2];
+//                dp[pre + 1][rest] += dp[pre + 3][rest - pre - 3];
+//                dp[pre + 1][rest] += dp[pre + 4][rest - pre - 4];
+//                dp[pre + 1][rest] += dp[pre + 5][rest - pre - 5];
+                dp[pre][rest] = dp[pre + 1][rest] + dp[pre][rest - pre];
+            }
+        }
+        return dp[1][n];
+    }
+
+
 }
