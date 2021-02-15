9a1c1752972df31210cf1a750d63367a4dc720fc
GreyZeng <410486047@qq.com>
2021-02-15 16:14:16 +0800

    recover bst

--- src/main/java/leetcode/LeetCode_0099_RecoverBinarySearchTree.java
+++ src/main/java/leetcode/LeetCode_0099_RecoverBinarySearchTree.java
@@ -3,6 +3,8 @@
         Follow up: A solution using O(n) space is pretty straight forward. Could you devise a constant space solution?*/
 package leetcode;
 
+import java.util.Stack;
+
 public class LeetCode_0099_RecoverBinarySearchTree {
     public class TreeNode {
         int val;
@@ -22,8 +24,38 @@ public class LeetCode_0099_RecoverBinarySearchTree {
             this.right = right;
         }
     }
-    // TODO
-    public void recoverTree(TreeNode root) {
 
+    // 中序遍历 第一次降序的头节点，第二次降序的尾节点
+    // 这里只需要交换两个节点的val就可以AC，实际上更好的处理方式应该是节点真实的进行交换
+    public static void recoverTree(TreeNode root) {
+        TreeNode[] twoErrNodes = getTwoErrNodes(root);
+        twoErrNodes[0].val = twoErrNodes[0].val ^ twoErrNodes[1].val;
+        twoErrNodes[1].val = twoErrNodes[0].val ^ twoErrNodes[1].val;
+        twoErrNodes[0].val = twoErrNodes[0].val ^ twoErrNodes[1].val;
+    }
+
+    // 中序遍历
+    public static TreeNode[] getTwoErrNodes(TreeNode head) {
+        TreeNode[] errs = new TreeNode[2];
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode pre = null;
+        TreeNode cur = head;
+        while (!stack.isEmpty() || cur != null) {
+            if (cur != null) {
+                stack.push(cur);
+                cur = cur.left;
+            } else {
+                cur = stack.pop();
+                if (pre != null && pre.val > cur.val) {
+                    // 第一次降序的头节点
+                    errs[0] = errs[0] == null ? pre : errs[0];
+                    // 第二次降序的尾节点
+                    errs[1] = cur;
+                }
+                pre = cur;
+                cur = cur.right;
+            }
+        }
+        return errs;
     }
 }
