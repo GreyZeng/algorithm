b3f34f5e1a5f95f03a70aed1419200252d3ff8eb
zenghui <410486047@qq.com>
2021-02-25 11:30:15 +0800

    leetcode 87 todo

--- src/main/java/leetcode/LeetCode_0087_ScrambleString.java
+++ src/main/java/leetcode/LeetCode_0087_ScrambleString.java
@@ -38,9 +38,58 @@ s1.length == s2.length
 1 <= s1.length <= 30
 s1 and s2 consist of lower-case English letters.*/
 package leetcode;
-// TODO
+
 public class LeetCode_0087_ScrambleString {
-    public boolean isScramble(String s1, String s2) {
+
+    public static boolean isScramble(String s1, String s2) {
+        if (s1 == null && s2 == null) {
+            return true;
+        }
+        if (s1 == null) {
+            return false;
+        }
+        if (s2 == null) {
+            return false;
+        }
+        char[] str1 = s1.toCharArray();
+        char[] str2 = s2.toCharArray();
+        if (!isValid(str1, str2)) {
+            return false;
+        }
+        return f(str1, str2, 0, 0, str2.length);
+    }
+
+    // str1中，L1往后（包括L1）一共k个字符串 以及  str2中，L2往后（包括L2）一共k个字符串 是否互为扰动串
+    private static boolean f(char[] str1, char[] str2, int L1, int L2, int k) {
+        if (k == 1) {
+            // base case， 针对这样的情况，只需要判断str1[L1], str2[L2]
+            return str1[L1] == str2[L2];
+        }
+        // 枚举第一刀的位置
+//        for (int left = 1; left < k; left++) {
+//            if (f(str1, str2, L1 + left, L2, k - left)) {
+//                return true;
+//            }
+//        }
         return false;
     }
+
+    private static boolean isValid(char[] str1, char[] str2) {
+        if (str1.length != str2.length) {
+            return false;
+        }
+        int[] map = new int[26];
+        for (char c : str1) {
+            map[c - 'a']++;
+        }
+        for (char c : str2) {
+            map[c - 'a']--;
+            if (map[c - 'a'] < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
 }
