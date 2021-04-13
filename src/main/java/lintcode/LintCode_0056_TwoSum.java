ac41a7f92b015be15fd4c4d24a34884643c6171d
GreyZeng <410486047@qq.com>
2021-04-13 23:17:52 +0800

    lintcode 56 two sum

---
+++ src/main/java/lintcode/LintCode_0056_TwoSum.java
@@ -0,0 +1,18 @@
+package lintcode;
+
+// https://www.lintcode.com/problem/56/
+public class LintCode_0056_TwoSum {
+    public int[] twoSum(int[] numbers, int target) {
+        // write your code here
+        int[] result = new int[2];
+        Map<Integer,Integer> map = new HashMap<>();
+        for (int i = 0; i < numbers.length; i++) {
+            if (map.containsKey(target - numbers[i])) {
+                return new int[]{ map.get(target - numbers[i]),i};
+            } else {
+                map.put(numbers[i],i);
+            }
+        }
+        return result;
+    }
+}
