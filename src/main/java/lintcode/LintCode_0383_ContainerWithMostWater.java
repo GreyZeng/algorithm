5e48ad2ab235359f171d7d6e9ae5bffd80703989
zenghui <410486047@qq.com>
2021-04-16 14:39:40 +0800

    add todo

---
+++ src/main/java/lintcode/LintCode_0383_ContainerWithMostWater.java
@@ -0,0 +1,15 @@
+package lintcode;
+
+/**
+ * https://www.lintcode.com/problem/383/
+ *
+ * @author <a href="mailto:410486047@qq.com">Grey</a>
+ * @date 2021/4/16
+ * @since
+ */
+public class LintCode_0383_ContainerWithMostWater {
+    // TODO
+    public int maxArea(int[] heights) {
+        return -1;
+    }
+}
