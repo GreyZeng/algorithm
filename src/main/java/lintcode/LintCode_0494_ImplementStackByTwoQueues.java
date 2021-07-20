522b9d14a0add84975aa9915ec2747852e4f510f
GreyZeng <410486047@qq.com>
2021-07-21 00:00:19 +0800

    stack vs queue

--- src/main/java/lintcode/LintCode_0494_ImplementStackByTwoQueues.java
+++ src/main/java/lintcode/LintCode_0494_ImplementStackByTwoQueues.java
@@ -8,19 +8,39 @@ public class LintCode_0494_ImplementStackByTwoQueues{
         private Queue<Integer> queue = new LinkedList<>();
         private Queue<Integer> helper = new LinkedList<>();
         public void push(int x) {
-            
+            queue.offer(x);
         }
     
         public void pop() {
-            
+            if (isEmpty()) {
+                return;
+            }
+            int result = 0;
+            while(!isEmpty()) {
+                result = queue.poll();
+                if (!isEmpty()) {
+                    helper.offer(result);
+                }
+            }
+            Queue<Integer> t = queue;
+            queue = helper;
+            helper = t;
         }
     
         public int top() {
-            return -1;
+            int result = 0;
+            while(!isEmpty()) {
+                result = queue.poll();
+                helper.offer(result);
+            }
+            Queue<Integer> t = queue;
+            queue = helper;
+            helper = t;
+            return result;
         }
     
         public boolean isEmpty() {
-            return false;
+            return queue.isEmpty();
         }
     }
 }
