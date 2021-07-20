9ef40e8d627c1ef2eec7c8f074d4d026c3ba641c
GreyZeng <410486047@qq.com>
2021-07-20 23:16:06 +0800

    design circular queue

--- src/main/java/leetcode/LeetCode_0622_DesignCircularQueue.java
+++ src/main/java/leetcode/LeetCode_0622_DesignCircularQueue.java
@@ -33,53 +33,43 @@ The number of operations will be in the range of [1, 1000].
 Please do not use the built-in Queue library.*/
 package leetcode;
 
-
 public class LeetCode_0622_DesignCircularQueue {
     static class MyCircularQueue {
         private int[] arr;
-        private int pushi;
-        private int popi;
-        private int size;
-        private int max;
-        private int f;
+        private int pushPosition;
+        private int popPosition;
+        private int rear;
+        private int currentSize;
+        private int limit;
 
-        /**
-         * Initialize your data structure here. Set the size of the queue to be k.
-         */
         public MyCircularQueue(int k) {
-            max = k;
             arr = new int[k];
+            limit = k;
         }
 
-        /**
-         * Insert an element into the circular queue. Return true if the operation is successful.
-         */
         public boolean enQueue(int value) {
-            if (size < max) {
-                size++;
-                arr[pushi] = value;
-                f = pushi;
-                pushi = nextIndex(pushi);
-                return true;
+            if (isFull()) {
+                return false;
             }
-            return false;
+            currentSize++;
+            rear = pushPosition;
+            arr[pushPosition] = value;
+            pushPosition = newPosition(pushPosition);
+            return true;
         }
 
-        private int nextIndex(int i) {
-            return i >= max - 1 ? 0 : i + 1;
+        public int newPosition(int c) {
+            return (c == limit - 1) ? 0 : c + 1;
         }
 
-        /**
-         * Delete an element from the circular queue. Return true if the operation is successful.
-         */
         public boolean deQueue() {
-            if (size > 0) {
-                size--;
-                // arr[popi] = 0;
-                popi = nextIndex(popi);
-                return true;
+            if (isEmpty()) {
+                return false;
             }
-            return false;
+            currentSize--;
+            popPosition = newPosition(popPosition);
+            return true;
+
         }
 
         /**
@@ -89,7 +79,7 @@ public class LeetCode_0622_DesignCircularQueue {
             if (isEmpty()) {
                 return -1;
             }
-            return arr[popi];
+            return arr[popPosition];
         }
 
         /**
@@ -99,36 +89,21 @@ public class LeetCode_0622_DesignCircularQueue {
             if (isEmpty()) {
                 return -1;
             }
-            return arr[f];
+            return arr[rear];
         }
 
         /**
          * Checks whether the circular queue is empty or not.
          */
         public boolean isEmpty() {
-            return size == 0;
+            return currentSize == 0;
         }
 
         /**
          * Checks whether the circular queue is full or not.
          */
         public boolean isFull() {
-            return size == max;
+            return currentSize == limit;
         }
-    }
-
-    public static void main(String[] args) {
-        MyCircularQueue circularQueue = new MyCircularQueue(3); // set the size to be 3
-        System.out.println(circularQueue.enQueue(1));  // return true
-        System.out.println(circularQueue.enQueue(2)); // return true
-        System.out.println(circularQueue.enQueue(3)); // return true
-        System.out.println(circularQueue.enQueue(4)); // return false, the queue is full
-        System.out.println(circularQueue.Front());  //
-        System.out.println(circularQueue.Rear());  // return 3
-
-        System.out.println(circularQueue.isFull());  // return true
-        System.out.println(circularQueue.deQueue());  // return true
-        System.out.println(circularQueue.enQueue(4));  // return true
-        System.out.println(circularQueue.Rear());  // return 4
     }
 }
