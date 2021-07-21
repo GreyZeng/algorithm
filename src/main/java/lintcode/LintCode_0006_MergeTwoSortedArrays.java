f018f6cf3d5546860577d642aac288c74a7bf8e9
zenghui <410486047@qq.com>
2021-07-21 11:53:21 +0800

    merge two sort list

--- src/main/java/lintcode/LintCode_0006_MergeTwoSortedArrays.java
+++ src/main/java/lintcode/LintCode_0006_MergeTwoSortedArrays.java
@@ -2,9 +2,37 @@ package lintcode;
 
 // How can you optimize your algorithm if one array is very large and the other is very small?
 public class LintCode_0006_MergeTwoSortedArrays {
-    // TODO
+    // TODO 
+
     public int[] mergeSortedArray(int[] A, int[] B) {
-         return null;
+        int m = A.length;
+        int n = B.length;
+        int[] helper = new int[m + n];
+        int i = 0;
+        int j = 0;
+        int index = 0;
+        while (i < m && j < n) {
+            if (A[i] > B[j]) {
+                helper[index++] = B[j++];
+            } else if(A[i] < B[j]) {
+                helper[index++] = A[i++];
+            } else {
+                if (m > n) {
+                    // n小就先拷贝A[j]，使得更快跳出循环
+                    helper[index++] = B[j++];
+                } else {
+                    // m小就先拷贝i，使得更快跳出循环
+                    helper[index++] = A[i++];
+                }
+            }
+        }
+        while (i < m) {
+            helper[index++] = A[i++];
+        }
+        while (j < n) {
+            helper[index++] = B[j++];
+        }
+        return helper;
     }
     
 }
--- src/main/java/lintcode/LintCode_0064_MergeTwoSortedArrays.java
+++ src/main/java/lintcode/LintCode_0064_MergeTwoSortedArrays.java
@@ -7,8 +7,27 @@ package lintcode;
 //  (size that is greater or equal to m + n) to hold additional elements from B. 
 // The number of elements initialized in A and B are m and n respectively.
 public class LintCode_0064_MergeTwoSortedArrays {
-    // TODO
     public void mergeSortedArray(int[] A, int m, int[] B, int n) {
-        
+        int[] helper = new int[m + n];
+        int i = 0;
+        int j = 0;
+        int index = 0;
+        while (i < m && j < n) {
+            if (A[i] > B[j]) {
+                helper[index++] = B[j++];
+            } else {
+                helper[index++] = A[i++];
+            }
+        }
+        while (i < m) {
+            helper[index++] = A[i++];
+        }
+        while (j < n) {
+            helper[index++] = B[j++];
+        }
+
+        for (int k = 0; k < (m + n); k++) {
+            A[k] = helper[k];
+        }
     }
 }
