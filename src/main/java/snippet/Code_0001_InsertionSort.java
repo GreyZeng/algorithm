5c48da959c3014c9a8a287d6d56d76c08ab6d368
GreyZeng <410486047@qq.com>
2021-07-25 21:43:23 +0800

    bubble sort

--- src/main/java/snippet/Code_0001_InsertionSort.java
+++ src/main/java/snippet/Code_0001_InsertionSort.java
@@ -2,14 +2,7 @@ package snippet;
 
 import java.util.Arrays;
 
-/**
- * 插入排序 过程： 想让arr[0~0]上有序，这个范围只有一个数，当然是有序的。
- * 想让arr[0~1]上有序，所以从arr[1]开始往前看，如果arr[1]<arr[0]，就交换。否则什么也不做。 …
- * 想让arr[0~i]上有序，所以从arr[i]开始往前看，arr[i]这个数不停向左移动，一直移动到左边的数字不再比自己大，停止移动。
- * 最后一步，想让arr[0~N-1]上有序， arr[N-1]这个数不停向左移动，一直移动到左边的数字不再比自己大，停止移动。
- * <p>
- * 估算时发现这个算法流程的复杂程度，会因为数据状况的不同而不同。
- */
+
 public class Code_0001_InsertionSort {
 	public static void insertionSort(int[] arr) {
 		if (arr == null || arr.length < 2) {
