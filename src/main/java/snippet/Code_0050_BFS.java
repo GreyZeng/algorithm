27142b968c92d5368ab82183f488805c0b3823e0
GreyZeng <410486047@qq.com>
2021-07-25 20:58:26 +0800

    move docs

--- src/main/java/snippet/Code_0050_BFS.java
+++ src/main/java/snippet/Code_0050_BFS.java
@@ -1,10 +1,10 @@
 package snippet;
 
-import snippet.graph.Node;
-
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.Queue;
+
+import snippet.graph.Node;
 
 /**
  * 宽度优先遍历 宽度优先遍历 1，利用队列实现 2，从源节点开始依次按照宽度进队列，然后弹出 3，每弹出一个点，把该节点所有没有进过队列的邻接点放入队列
--- src/main/java/snippet/Code_0051_DFS.java
+++ src/main/java/snippet/Code_0051_DFS.java
@@ -1,9 +1,9 @@
 package snippet;
 
-import snippet.graph.Node;
-
 import java.util.HashSet;
 import java.util.Stack;
+
+import snippet.graph.Node;
 
 /**
  * 深度优先遍历 1，利用栈实现 2，从源节点开始把节点按照深度放入栈，然后弹出 3，每弹出一个点，把该节点下一个没有进过栈的邻接点放入栈 4，直到栈变空
