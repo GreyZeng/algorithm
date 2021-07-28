c707e0d9a5efbabdd23a9cc990432d577163e10c
GreyZeng <410486047@qq.com>
2021-07-29 00:57:01 +0800

    mono stack todo

--- src/main/java/leetcode/LeetCode_0085_MaximalRectangle.java
+++ src/main/java/leetcode/LeetCode_0085_MaximalRectangle.java
@@ -14,10 +14,37 @@
 // 最优解 O(N^2)
 package leetcode;
 
-
 public class LeetCode_0085_MaximalRectangle {
-    public static int maximalRectangle(char[][] matrix) {
-        return -1;   
-    }
+	// 一定要以某一行作为底的矩形中，最大矩形是多少
+	// 和全局最大PK一下，如果比全局最大还大，则更新全局最大值
+	// 最后返回全局最大值
+	public static int maximalRectangle(char[][] matrix) {
+		if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+			return 0;
+		}
+		int[] help = new int[matrix[0].length];
+		for (int i = 0; i < help.length; i++) {
+			help[i] = matrix[0][i] == '0' ? 0 : 1;
+		}
+		int max = getMax(help);
+		int row = matrix.length;
+		for (int i = 1; i < row; i++) {
+			merge(help, matrix[i]);
+			max = Math.max(max, getMax(help));
+		}
+		return max;
+	}
+
+	// 把m1叠加
+	private static void merge(int[] help, char[] m2) {
+		for (int i = 0; i < help.length; i++) {
+			help[i] = m2[i] == '0' ? 0 : help[i] + 1;
+		}
+	}
+
+	private static int getMax(int[] arr) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
