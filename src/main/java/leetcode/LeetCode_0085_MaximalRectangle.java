a2a433b15061308fecdacd8f53d0aa2136f0d309
zenghui <410486047@qq.com>
2021-07-29 09:36:42 +0800

    leetcode 85 maximal rec

--- src/main/java/leetcode/LeetCode_0085_MaximalRectangle.java
+++ src/main/java/leetcode/LeetCode_0085_MaximalRectangle.java
@@ -17,66 +17,42 @@ package leetcode;
 import java.util.Stack;
 
 public class LeetCode_0085_MaximalRectangle {
-	// 一定要以某一行作为底的矩形中，最大矩形是多少
-	// 和全局最大PK一下，如果比全局最大还大，则更新全局最大值
-	// 最后返回全局最大值
-	public static int maximalRectangle(char[][] matrix) {
-		if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
-			return 0;
-		}
-		int[] help = new int[matrix[0].length];
-	 
-		int max = 0;
-		int row = matrix.length;
-		for (int i = 0; i < row; i++) {
-			merge(help, matrix[i]);
-			max = Math.max(max, getMax(help));
-		}
-		return max;
-	}
 
-	// 把m1叠加
-	private static void merge(int[] help, char[] m2) {
-		for (int i = 0; i < help.length; i++) {
-			help[i] = m2[i] == '0' ? 0 :  (help[i] +  1);
-		}
-	}
+    public static int maximalRectangle(char[][] matrix) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return 0;
+        }
+        int[] help = new int[matrix[0].length];
+        int max = 0;
+        for (char[] chars : matrix) {
+            merge(help, chars);
+            max = Math.max(max, getMax(help));
+        }
+        return max;
+    }
 
-	private static int getMax(int[] arr) {
-		Stack<Integer> stack = new Stack<>();
-		int max = 0;
-		for (int i = 0; i < arr.length; i++) {
-			while (!stack.isEmpty() && arr[stack.peek()] >= arr[i]) {
-				int m = stack.pop();
-				if (stack.isEmpty()) {
-					max = Math.max(max, (m + 1) * arr[m]);
-				} else {
-					max = Math.max(max, (i - m + 1) * arr[m] );
-				}
-				
-			}
-			if (stack.isEmpty()) {
-				max = Math.max(arr[i] * (i + 1), max);
-			} else {
-				max = Math.max(arr[i] * (i - stack.peek()), max);
-			}
-			stack.push(i);
-		}
-		while (!stack.isEmpty()) {
-			int i = stack.pop();
-			if (stack.isEmpty()) {
-				max = Math.max(arr[i] * (i + 1), max);
-			} else {
-				max = Math.max(arr[i] * (i - stack.peek()), max);
-			}
-			 
-		}
-		return max;
-	}
-	public static void main(String[] args) {
-		char[][] rec = {{'0','0','1'} ,{'1','1','1'}};
-		int res = maximalRectangle(rec);
-		System.out.println(res);
-	}
+    private static void merge(int[] help, char[] m2) {
+        for (int i = 0; i < help.length; i++) {
+            help[i] = m2[i] == '0' ? 0 : (help[i] + 1);
+        }
+    }
 
+    private static int getMax(int[] arr) {
+        Stack<Integer> stack = new Stack<>();
+        int max = 0;
+        for (int i = 0; i < arr.length; i++) {
+            while (!stack.isEmpty() && arr[stack.peek()] >= arr[i]) {
+                int m = stack.pop();
+                int r = stack.isEmpty() ? -1 : stack.peek();
+                max = Math.max(arr[m] * (i - r - 1), max);
+            }
+            stack.push(i);
+        }
+        while (!stack.isEmpty()) {
+            int m = stack.pop();
+            int r = stack.isEmpty() ? -1 : stack.peek();
+            max = Math.max(arr[m] * (arr.length - 1 - r), max);
+        }
+        return max;
+    }
 }
