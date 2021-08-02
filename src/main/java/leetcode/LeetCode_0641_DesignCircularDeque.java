7dd5e1fc82f6736cbd8723fe3c89e36d0c7b7966
zenghui <410486047@qq.com>
2021-08-02 10:17:22 +0800

    add todo

--- src/main/java/leetcode/LeetCode_0641_DesignCircularDeque.java
+++ src/main/java/leetcode/LeetCode_0641_DesignCircularDeque.java
@@ -1,71 +1,61 @@
 package leetcode;
 
+// TODO
 public class LeetCode_0641_DesignCircularDeque {
     class MyCircularDeque {
-        private int[] arr;
-        private int size;
-        private int limit;
-        private int pushIndex;
-        private int popIndex;
-        /**
-         * Initialize your data structure here. Set the size of the deque to be k.
-         */
+        
         public MyCircularDeque(int k) {
-
+            
         }
 
-        /**
-         * Adds an item at the front of Deque. Return true if the operation is successful.
-         */
+      
         public boolean insertFront(int value) {
             return false;
         }
 
-        /**
-         * Adds an item at the rear of Deque. Return true if the operation is successful.
-         */
+    
         public boolean insertLast(int value) {
+            if (isFull()) {
+                return false;
+            }
             return false;
         }
 
-        /**
-         * Deletes an item from the front of Deque. Return true if the operation is successful.
-         */
+ 
         public boolean deleteFront() {
+            if (isEmpty()) {
+                return false;
+            }
             return false;
         }
 
-        /**
-         * Deletes an item from the rear of Deque. Return true if the operation is successful.
-         */
+ 
         public boolean deleteLast() {
+            if (isEmpty()) {
+                return false;
+            }
             return false;
         }
-
-        /**
-         * Get the front item from the deque.
-         */
+ 
         public int getFront() {
+            if (isEmpty()) {
+                return -1;
+            }
             return -1;
         }
 
-        /**
-         * Get the last item from the deque.
-         */
+ 
         public int getRear() {
+            if (isEmpty()) {
+                return -1;
+            }
             return -1;
         }
-
-        /**
-         * Checks whether the circular deque is empty or not.
-         */
+ 
         public boolean isEmpty() {
             return false;
         }
-
-        /**
-         * Checks whether the circular deque is full or not.
-         */
+ 
         public boolean isFull() {
             return false;
         }
