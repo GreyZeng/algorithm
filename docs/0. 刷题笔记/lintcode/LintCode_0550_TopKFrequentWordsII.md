88f6fb51459261ef079ad7a16924553ad667a9d8
zenghui <410486047@qq.com>
2021-08-04 12:12:01 +0800

    todo

--- docs/0. 刷题笔记/lintcode/LintCode_0550_TopKFrequentWordsII.md
+++ docs/0. 刷题笔记/lintcode/LintCode_0550_TopKFrequentWordsII.md
@@ -4,7 +4,72 @@
 
 ## 思路
 
+由于要统计每个字符串的次数，以及字典序，所以，我们需要把用户每次add的字符串封装成一个对象，这个对象中包括了这个字符串和这个字符串出现的次数。
 
+假设我们封装的对象如下：
+
+```java
+public class Word {
+    public String value; // 对应的字符串
+    public int times; // 对应的字符串出现的次数
+
+    public Word(String v, int t) {
+        value = v;
+        times = t;
+    }
+}
+```
+
+topk的要求是： 出现次数多的排前面,如果次数一样，字典序小的排前面
+
+很容易想到用有序表+比较器来做。
+
+比较器的规则定义成和topk的要求一样，然后把元素元素加入使用比较器的有序表中，如果要返回topk，直接从这个有序表弹出返回给用户即可。比较器的定义如下：
+
+```java
+public class TopKComparator implements Comparator<Word> {
+    @Override
+    public int compare(Word o1, Word o2) {
+        // 次数大的排前面，次数一样字典序在小的排前面
+        return o1.times == o2.times ? o1.value.compareTo(o2.value) : (o2.times - o1.times);
+    }
+}
+```
+
+有序表配置这个比较器即可
+
+```
+TreeSet<Word>  topK = new TreeSet<>(new TopKComparator());
+```
+
+所以topk()方法很简单，只需要从有序表里面把元素拿出来返回给用户即可
+
+```
+public List<String> topk() {
+      List<String> result = new ArrayList<>();
+      for (Word word : topK) {
+          result.add(word.value);
+      }
+      return result;
+}
+```
+
+时间复杂度 O(K)
+
+以上步骤不复杂，接下来是add的逻辑，add的每次操作都有可能对前面我们设置的topK有序表造成影响，
+
+所以在每次add操作的时候需要有一个机制可以**告诉topK这个有序表，需要淘汰什么元素，需要新加哪个元素**，让topK这个有序表时时刻刻只存topk个元素，
+
+这样就可以确保topK()方法比较单纯，时间复杂度保持在O(K)
+
+所以接下来的问题是：**如何告诉topK这个有序表，需要淘汰什么元素，需要新加哪个元素**?
+
+我们可以通过堆来维持一个门槛
+
+
+
+add方法，复杂度O(log K)
+topk方法，复杂度O(K)
 
 ## 更多
 
--- src/main/java/lintcode/LintCode_0550_TopKFrequentWordsII.java
+++ src/main/java/lintcode/LintCode_0550_TopKFrequentWordsII.java
@@ -33,166 +33,55 @@ package lintcode;
 
 import java.util.ArrayList;
 import java.util.Comparator;
-import java.util.HashMap;
 import java.util.List;
 import java.util.TreeSet;
 
 // https://www.lintcode.com/problem/top-k-frequent-words-ii/description
 public class LintCode_0550_TopKFrequentWordsII {
     public class TopK {
-        public class Node {
+        private TreeSet<Word> topK;
+        public TopK(int k) {
+            topK = new TreeSet<>(new TopKComparator());
+        }
+
+        public void add(String str) {
+
+        }
+
+        public List<String> topk() {
+            List<String> result = new ArrayList<>();
+            for (Word word : topK) {
+                result.add(word.value);
+            }
+            return result;
+        }
+
+        private class Word {
             public String value;
             public int times;
 
-            public Node(String v, int t) {
+            public Word(String v, int t) {
                 value = v;
                 times = t;
             }
         }
 
-        // 控制门槛，次数由少到多，次数一样的，字典序从大到小
-        // 保证heap[0] 位置是最先可能被淘汰的位置
-        public class MyComparator implements Comparator<Node> {
+        private class TopKComparator implements Comparator<Word> {
             @Override
-            public int compare(Node o1, Node o2) {
-                if (o1.times < o2.times) {
-                    return -1;
-                } else if (o1.times > o2.times) {
-                    return 1;
-                } else {
-                    return o2.value.compareTo(o1.value);
-                }
+            public int compare(Word o1, Word o2) {
+                // 次数大的排前面，次数一样字典序在小的排前面
+                return o1.times == o2.times ? o1.value.compareTo(o2.value) : (o2.times - o1.times);
             }
         }
+        private class ThresholdComparator implements Comparator<Word> {
 
-        // 次数由多到少，次数一样，按字典序由小到大
-        public class TreeSetComp implements Comparator<Node> {
             @Override
-            public int compare(Node o1, Node o2) {
-                if (o1.times < o2.times) {
-                    return 1;
-                } else if (o1.times > o2.times) {
-                    return -1;
-                } else {
-                    return o1.value.compareTo(o2.value);
-                }
-            }
-
-        }
-
-        private Node[] heap; // heap[0]门槛
-        private int heapSize;
-        // private int k;
-        // node节点在heap的哪个位置
-        private HashMap<Node, Integer> indexMap;
-        // 通过valueMap可以找到某个字符串对应的Node内存地址
-        private HashMap<String, Node> valueMap;
-        private Comparator<Node> comp;
-        private TreeSet<Node> set; // 存候选
-
-        public TopK(int k) {
-            heap = new Node[k];
-            indexMap = new HashMap<>();
-            valueMap = new HashMap<>();
-            heapSize = 0;
-            comp = new MyComparator();
-            set = new TreeSet<>(new TreeSetComp());
-        }
-
-        public void add(String word) {
-            if (heap.length == 0) {
-                return;
-            }
-            int pre;
-            Node node;
-            if (valueMap.containsKey(word)) {
-                node = valueMap.get(word);
-                if (set.contains(node)) {
-                    set.remove(node);
-                }
-                pre = indexMap.get(node);
-                node.times++;
-            } else {
-                node = new Node(word, 1);
-                valueMap.put(word, node);
-                pre = -1;
-                indexMap.put(node, pre); // -1表示还没入堆,新增节点
-            }
-            // 要考虑堆满的情况，indexMap和valueMap会把word先缓存下来，等到次数到达一定程度了，再把节点加入到堆中
-            if (pre == -1) { // 新增节点或者淘汰后的节点
-                if (heapSize == heap.length) {
-                    // 堆满了，要查看当前节点能否替换堆中的第一个节点
-                    if (comp.compare(heap[0], node) < 0) {
-                        set.remove(heap[0]);
-                        set.add(node);
-                        indexMap.put(heap[0], -1);
-                        heap[0] = node;
-                        indexMap.put(node, 0);
-                        heapify(0);
-                    }
-                } else {
-                    set.add(node);
-                    heap[heapSize] = node;
-                    indexMap.put(node, heapSize);
-                    heapInsert(heapSize++);
-                }
-            } else {
-                set.add(node);
-                heapify(pre);
-            }
-
-        }
-
-        private void heapInsert(int i) {
-            while (comp.compare(heap[i], heap[(i - 1) / 2]) < 0) {
-                swap(i, (i - 1) / 2);
-                i = (i - 1) / 2;
+            public int compare(Word o1, Word o2) {
+                // 设置堆门槛，堆顶元素最先被淘汰
+                return o1.times == o2.times ? o2.value.compareTo(o2.value) : (o1.times - o2.times);
             }
         }
 
-        private void heapify(int i) {
-            int leftChild = 2 * i + 1;
-            while (leftChild < heapSize) {
-                Node best;
-                if (leftChild + 1 < heapSize) {
-                    Node left = heap[leftChild];
-                    Node right = heap[leftChild + 1];
-                    if (comp.compare(left, right) < 0) {
-                        best = left;
-                    } else {
-                        best = right;
-                    }
-                } else {
-                    best = heap[leftChild];
-                }
-                if (comp.compare(heap[i], best) < 0) {
-                    break;
-                }
-                int bestIndex = best == heap[leftChild] ? leftChild : leftChild + 1;
-                swap(bestIndex, i);
-                i = bestIndex;
-                leftChild = 2 * i + 1;
-            }
-        }
 
-        private void swap(int i, int j) {
-            indexMap.put(heap[i], j);
-            indexMap.put(heap[j], i);
-            Node t = heap[i];
-            heap[i] = heap[j];
-            heap[j] = t;
-        }
-
-        // heap维持好堆的状态，直接遍历前k个即可
-        public List<String> topk() {
-            List<String> ans = new ArrayList<>();
-            for (Node n : set) {
-                ans.add(n.value);
-            }
-            return ans;
-        }
     }
-
-    
-
 }
