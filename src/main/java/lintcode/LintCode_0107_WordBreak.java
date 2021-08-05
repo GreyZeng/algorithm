11fde3a1cf865762ff78fa646e847edca33c46f0
zenghui <410486047@qq.com>
2021-08-05 17:06:16 +0800

    lintcode 107 word break

--- src/main/java/lintcode/LintCode_0107_WordBreak.java
+++ src/main/java/lintcode/LintCode_0107_WordBreak.java
@@ -1,6 +1,5 @@
 package lintcode;
 
-import java.util.HashSet; 
 import java.util.Set;
 
 
@@ -12,77 +11,21 @@ import java.util.Set;
 // s.length <= 1e5 
 // dict.size <= 1e5 
 public class LintCode_0107_WordBreak {
-	public static void main(String[] args) {
-		String s = "aaab";
-		Set<String> wordSet = new HashSet<>();
-		wordSet.add("aa");
-		wordSet.add("b");
-//		 wordSet.add("a");
-//		 wordSet.add("b");
-//		s = "aaaaaaa";
-// 		 wordSet.add("aaaa");
-// 		 wordSet.add("aa");
+    public static boolean wordBreak(String s, Set<String> wordDict) {
+        return process(s, 0, wordDict) != 0;
+    }
 
-		System.out.println(wordBreak(s, wordSet));
-
-	}
-
-	public static class Node {
-		public boolean end;
-		public Node[] nexts;
-
-		public Node() {
-			end = false;
-			nexts = new Node[26];
-		}
-	}
-
-	// 最优解前缀树
-	public static boolean wordBreak(String s, Set<String> wordSet) {
-		if (null == wordSet) {
-			return false;
-		}
-		if (wordSet.isEmpty()) {
-			return s.length() == 0;
-		}
-		Node root = buildTrie(wordSet);
-		char[] str = s.toCharArray();
-		int N = str.length;
-		boolean[] dp = new boolean[N + 1];
-		dp[N] = true;
-		for (int index = N - 1; index >= 0; index--) {
-			Node cur = root;
-			for (int i = index; i < N; i++) {
-				int p = str[i] - 'a';
-				if (cur.nexts[p] == null) {
-					break;
-				}
-				cur = cur.nexts[p];
-				if (cur.end && dp[i + 1]) {
-					dp[index] = true;
-					break;
-				}
-			}
-		}
-		return dp[0];
-
-	}
-
-	// 构造前缀树
-	private static Node buildTrie(Set<String> wordDict) {
-		Node root = new Node();
-
-		for (String str : wordDict) {
-			Node cur = root;
-			char[] s = str.toCharArray();
-			for (char c : s) { 
-				if (cur.nexts[c - 'a'] == null) {
-					cur.nexts[c - 'a'] = new Node();
-				}
-				cur = cur.nexts[c - 'a'];
-			}
-			cur.end = true;
-		}
-		return root;
-	}
+    public static int process(final String s, int from, final Set<String> wordDict) {
+        if (from == s.length()) {
+            return 1;
+        }
+        int result = 0;
+        for (int to = from; to < s.length(); to++) {
+            // substring [from, to+1)
+            if (wordDict.contains(s.substring(from, to + 1))) {
+                result += process(s, to + 1, wordDict);
+            }
+        }
+        return result;
+    }
 }
