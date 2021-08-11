9d418e2113b32067e1a015a5775f993cd8daa279
GreyZeng <410486047@qq.com>
2021-08-11 23:17:05 +0800

    max happy problem

--- src/main/java/nowcoder/NowCoder_MaxHappy.java
+++ src/main/java/nowcoder/NowCoder_MaxHappy.java
@@ -24,77 +24,84 @@ import java.util.Set;
  */
 // https://ac.nowcoder.com/acm/problem/51178
 public class NowCoder_MaxHappy {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int count = sc.nextInt();
-		Map<Integer, Employee> map = new HashMap<>();
-		List<Integer> tmp = new LinkedList<>();
-		for (int i = 1; i <= count; i++) {
-			int happy = sc.nextInt();
-			map.put(i, new Employee(happy));
-			tmp.add(i);
-		}
-		Set<Integer> notBoss = new HashSet<>();
-		for (int i = 1; i <= count; i++) {
-			if (i != count) {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int count = sc.nextInt();
+        Map<Integer, Employee> map = new HashMap<>();
+        List<Integer> tmp = new LinkedList<>();
+        for (int i = 1; i <= count; i++) {
+            int happy = sc.nextInt();
+            map.put(i, new Employee(happy));
+            tmp.add(i);
+        }
+        Set<Integer> notBoss = new HashSet<>();
+        for (int i = 1; i <= count; i++) {
+            if (i != count) {
                 int child = sc.nextInt();
-				int father = sc.nextInt();
-				notBoss.add(child);
-				Employee f = map.get(father);
-				Employee c = map.get(child);
-				f.nexts.add(c);
-			}
-		}
-		int bossIndex = 0;
-		for (Integer it : tmp) {
-			if (!notBoss.contains(it)) {
-				bossIndex = it;
-				break;
-			}
-		}
-		Employee boss = map.get(bossIndex);
-		System.out.println(maxHappy(boss));
-		sc.close(); 
-	}
+                int father = sc.nextInt();
+                notBoss.add(child);
+                Employee f = map.get(father);
+                Employee c = map.get(child);
+                f.subordinates.add(c);
+            }
+        }
+        int bossIndex = 0;
+        for (Integer it : tmp) {
+            if (!notBoss.contains(it)) {
+                bossIndex = it;
+                break;
+            }
+        }
+        Employee boss = map.get(bossIndex);
+        System.out.println(maxHappy(boss));
+        sc.close();
+    }
 
-	public static class Employee {
-		public int happy;
-		public List<Employee> nexts;
+    public static class Employee {
+        public int happy;
+        public List<Employee> subordinates;
 
-		public Employee(int happy) {
-			this.happy = happy;
-			this.nexts = new ArrayList<>();
-		}
-	}
+        public Employee(int happy) {
+            this.happy = happy;
+            this.subordinates = new ArrayList<>();
+        }
+    }
 
-	public static class Info {
-		public int yes;
-		public int no;
 
-		public Info(int yes, int no) {
-			this.yes = yes;
-			this.no = no;
-		}
-	}
+    public static class Info {
+        public int yes;
+        public int no;
 
-	public static int maxHappy(Employee boss) {
-		Info info = p(boss);
-		return Math.max(info.yes, info.no);
-	}
+        public Info(int yes, int no) {
+            this.yes = yes;
+            this.no = no;
+        }
+    }
 
-	public static Info p(Employee e) {
-		if (e.nexts == null || e.nexts.isEmpty()) {
-			return new Info(e.happy, 0);
-		}
-		int yes = e.happy;
-		int no = 0;
-		List<Employee> nexts = e.nexts;
-		for (Employee emp : nexts) {
-			Info n = p(emp);
-			yes += n.no;
-			no += Math.max(n.yes, n.no);
-		}
-		return new Info(yes, no);
-	}
+    public static int maxHappy(Employee boss) {
+        if (boss == null) {
+            return 0;
+        }
+        Info info = p(boss);
+        return Math.max(info.yes, info.no);
+    }
+
+    public static Info p(Employee boss) {
+        if (boss.subordinates == null || boss.subordinates.isEmpty()) {
+            return new Info(boss.happy, 0);
+        }
+        List<Employee> subordinates = boss.subordinates;
+        int yes = boss.happy;
+        int no = 0;
+        for (Employee e : subordinates) {
+            Info info = p(e);
+            // boss参加了,下属可以不参加
+            yes += info.no;
+            // boss没有参加,下属可以参加也可以不参加
+            no += Math.max(info.yes, info.no);
+        }
+        return new Info(yes, no);
+    }
+
 
 }
