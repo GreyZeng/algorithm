7bffda6003ee1adef676b7cd9bb141c2b084199d
GreyZeng <410486047@qq.com>
2021-08-12 12:16:22 +0800

    split sticks

--- src/main/java/lintcode/LintCode_1872_MinimumCostToConnectSticks.java
+++ src/main/java/lintcode/LintCode_1872_MinimumCostToConnectSticks.java
@@ -6,24 +6,29 @@ import java.util.PriorityQueue;
 // https://www.lintcode.com/problem/minimum-cost-to-connect-sticks/description
 public class LintCode_1872_MinimumCostToConnectSticks {
 	public static int MinimumCost(List<Integer> sticks) {
-		if (sticks == null || sticks.size() <=1) {
+		if (null == sticks) {
 			return 0;
 		}
-		if (sticks.size() == 2) {
-			return sticks.get(1) + sticks.get(0);
+		if (sticks.isEmpty()) {
+			return 0;
+		}
+		if (sticks.size() <= 2) {
+			return sticks.get(0) + sticks.get(1);
 		}
 		PriorityQueue<Integer> queue = new PriorityQueue<>();
-		for (Integer i : sticks) {
-			queue.add(i);
-		}
-		int cost = 0 ;
-		while (queue.size() > 1) {
-			int i = queue.poll();
-			int j = queue.poll();
-			cost += (i + j);
-			queue.add(i+j);
+		for (Integer s : sticks) {
+			queue.offer(s);
 		}
 		
+		int cost = 0;
+		while (queue.size() != 1) {
+			int a = queue.poll();
+			int b = queue.poll();
+			cost += (a + b);
+			queue.offer(a + b);
+		}
 		return cost;
-	} 
+		
+		
+	}
 }
