b7dd9e6f385ef150b3615683605e64a5e62ecd08
zenghui <410486047@qq.com>
2021-08-26 16:22:35 +0800

    robot walk problem

--- src/main/java/nowcoder/NowCoder_RobotWalk.java
+++ src/main/java/nowcoder/NowCoder_RobotWalk.java
@@ -39,105 +39,136 @@ package nowcoder;
 import java.util.Scanner;
 
 public class NowCoder_RobotWalk {
+    public static int MOD = (int) 1e9 + 7;
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int N = in.nextInt();
-        int M = in.nextInt();
-        int K = in.nextInt();
-        int P = in.nextInt();
-        System.out.println(walk4(N, M, K, P));
+        int len = in.nextInt();
+        int start = in.nextInt() - 1;
+        int step = in.nextInt();
+        int end = in.nextInt() - 1;
+        System.out.println(ways2(len, start, step, end) % MOD);
+        System.out.println(ways3(len, start, step, end) % MOD);
+        System.out.println(ways4(len, start, step, end) % MOD);
         in.close();
     }
 
-
-    // 暴力递归
-    public static int walk(int N, int M, int K, int P) {
-        return p(N, M, K, P);
+    // 暴力递归 无法AC
+    // 从start开始，恰好走step步，到达end的方法数
+    public static long ways(int len, int start, int step, int end) {
+        if (step == 0) {
+            // 没有步数可以走了
+            // M正好是P位置，找到一种方法，否则没有方法
+            return (start == end ? 1 : 0);
+        }
+        // K != 0 还有步数可以走
+        long ways = 0;
+        if (start == 0) {
+            // 只能往右边走
+            ways += ways(len, start + 1, step - 1, end);
+        } else if (start == len - 1) {
+            ways += ways(len, start - 1, step - 1, end);
+        } else {
+            ways = (ways(len, start - 1, step - 1, end) + ways(len, start + 1, step - 1, end));
+        }
+        return ways;
     }
 
-    public static int p(int N, int M, int rest, int P) {
-        if (rest == 0) {
-            return M == P ? 1 : 0;
-        }
-        if (M == 1) {
-            return p(N, M + 1, rest - 1, P);
-        }
-        if (M == N) {
-            return p(N, M - 1, rest - 1, P);
-        }
-        return p(N, M - 1, rest - 1, P) + p(N, M + 1, rest - 1, P);
-    }
-
-    // 缓存
-    public static int walk2(int N, int M, int K, int P) {
-        int[][] dp = new int[N + 1][K + 1];
-        for (int i = 0; i < N + 1; i++) {
-            for (int j = 0; j < K + 1; j++) {
-                dp[i][j] = -1;
+    // 缓存法，可以AC
+    public static long ways2(int len, int start, int step, int end) {
+        long[][] dp = new long[len][step + 1];
+        for (int i = 0; i < len; i++) {
+            for (int j = 0; j <= step; j++) {
+                dp[i][j] = -1L;
             }
         }
-        return p2(N, M, K, P, dp);
+        p(len, start, step, end, dp);
+        return dp[start][step];
+
     }
 
-    public static int p2(int N, int M, int K, int P, int[][] dp) {
-        if (dp[M][K] != -1) {
-            return dp[M][K];
+    private static long p(int len, int start, int step, int end, long[][] dp) {
+        if (dp[start][step] != -1L) {
+            return dp[start][step] % MOD;
         }
-        if (K == 0) {
-            dp[M][K] = (M == P ? 1 : 0);
-            return dp[M][K];
+        if (step == 0) {
+            // 没有步数可以走了
+            // M正好是P位置，找到一种方法，否则没有方法
+            long ans = (start == end ? 1 : 0);
+            dp[start][step] = ans;
+            return ans;
         }
-        if (M == 1) {
-            dp[M][K] = p2(N, M + 1, K - 1, P, dp);
-            return dp[M][K];
+        // K != 0 还有步数可以走
+        long ways = 0;
+        if (start == 0) {
+            // 只能往右边走
+            ways += (p(len, start + 1, step - 1, end, dp));
+        } else if (start == len - 1) {
+            ways += (p(len, start - 1, step - 1, end, dp));
+        } else {
+            ways = (p(len, start - 1, step - 1, end, dp) + p(len, start + 1, step - 1, end, dp));
         }
-        if (M == N) {
-            dp[M][K] = p2(N, M - 1, K - 1, P, dp);
-            return dp[M][K];
-        }
-        dp[M][K] = p2(N, M - 1, K - 1, P, dp) + p2(N, M + 1, K - 1, P, dp);
-        return dp[M][K];
+        dp[start][step] = ways;
+        return ways;
     }
 
-    public static int MOD = (int) 1e9 + 7;
-
-    public static int walk3(int N, int M, int K, int P) {
-        int[][] dp = new int[N + 1][K + 1];
-        dp[P][0] = 1;
-        for (int j = 1; j <= K; j++) {
-            for (int i = 1; i <= N; i++) {
-                if (i == 1) {
-                    dp[i][j] = dp[2][j - 1] % MOD;
-                } else if (i == N) {
-                    dp[i][j] = dp[N - 1][j - 1] % MOD;
+    // 动态规划
+    public static long ways3(int len, int start, int step, int end) {
+        long[][] dp = new long[len][step + 1];
+        for (int i = 0; i < len; i++) {
+            for (int j = 0; j <= step; j++) {
+                if (j == 0) {
+                    if (i == end) {
+                        dp[i][j] = 1;
+                    } else {
+                        dp[i][j] = 0;
+                    }
                 } else {
-                    dp[i][j] = (dp[i + 1][j - 1] + dp[i - 1][j - 1]) % MOD;
+                    dp[i][j] = -1L;
                 }
             }
         }
-        return dp[M][K] % MOD;
+        // 从第一列开始
+        // 每列从上到下
+        for (int i = 1; i < step + 1; i++) {
+            for (int j = 0; j < len; j++) {
+                long ways = 0;
+                if (j == 0) {
+                    ways += dp[j + 1][i - 1];
+                } else if (j == len - 1) {
+                    ways += dp[j - 1][i - 1];
+                } else {
+                    ways = dp[j - 1][i - 1] + dp[j + 1][i - 1];
+                }
+                dp[j][i] = ways % MOD;
+            }
+        }
+        return dp[start][step];
     }
 
     // 空间压缩
-    public static int walk4(int N, int M, int K, int P) {
-        int[] dp = new int[N + 1];
-        dp[P] = 1;
-        int t2 = 0;
-        for (int i = 1; i <= K; i++) {
-            for (int j = 1; j <= N; j++) {
-                int t = dp[j];
-                if (j == 1) {
-                    dp[j] = dp[2] % MOD;
-                } else if (j == N) {
-                    dp[j] = t2 % MOD;
+    public static long ways4(int len, int start, int step, int end) {
+        // int times = step + 1;
+        long[] dp = new long[len];
+        // 第一列
+        dp[end] = 1L;
+        // 从第一列开始
+        // 每列从上到下
+        long tmp = 0;
+        for (int i = 1; i < step + 1; i++) {
+            for (int j = 0; j < len; j++) {
+                long ways = dp[j];
+                if (j == 0) {
+                    dp[j] = dp[j + 1] % MOD;
+                } else if (j == len - 1) {
+                    dp[j] = tmp % MOD;
                 } else {
-                    dp[j] = t2 % MOD + dp[j + 1] % MOD;
+                    dp[j] = tmp % MOD + dp[j + 1] % MOD;
                 }
-                t2 = t;
+                tmp = ways;
             }
         }
-        return dp[M] % MOD;
-
+        return dp[start];
     }
 
 
