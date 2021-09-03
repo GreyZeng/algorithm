3c8d929fba54d927a1b36372938e8f0885efb531
zenghui <410486047@qq.com>
2021-09-03 14:58:23 +0800

    leetcode 13 roman to integer

--- src/main/java/leetcode/LeetCode_0013_RomanToInteger.java
+++ src/main/java/leetcode/LeetCode_0013_RomanToInteger.java
@@ -1,69 +1,108 @@
-/**
- * 罗马数字转整数
- * Symbol       Value
- * I             1
- * V             5
- * X             10
- * L             50
- * C             100
- * D             500
- * M             1000
- * For example, two is written as II in Roman numeral, just two one's added together. Twelve is written as, XII, which is simply X + II. The number twenty seven is written as XXVII, which is XX + V + II.
- * <p>
- * Roman numerals are usually written largest to smallest from left to right.
- * However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four.
- * The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
- * <p>
- * I can be placed before V (5) and X (10) to make 4 and 9.
- * X can be placed before L (50) and C (100) to make 40 and 90.
- * C can be placed before D (500) and M (1000) to make 400 and 900.
- * Given an integer, convert it to a roman numeral. Input is guaranteed to be within the range from 1 to 3999.
- */
+// 罗马数字包含以下七种字符: I， V， X， L，C，D 和 M。
+
+// 字符          数值
+// I             1
+// V             5
+// X             10
+// L             50
+// C             100
+// D             500
+// M             1000
+
+// 例如， 罗马数字 2 写做 II ，即为两个并列的 1。12 写做 XII ，即为 X + II 。 27 写做  XXVII, 即为 XX + V + II 。
+
+// 通常情况下，罗马数字中小的数字在大的数字的右边。但也存在特例，例如 4 不写做 IIII，而是 IV。数字 1 在数字 5 的左边，所表示的数等于大数 5 减小数 1 得到的数值 4 。同样地，数字 9 表示为 IX。这个特殊的规则只适用于以下六种情况：
+
+//     I 可以放在 V (5) 和 X (10) 的左边，来表示 4 和 9。
+//     X 可以放在 L (50) 和 C (100) 的左边，来表示 40 和 90。 
+//     C 可以放在 D (500) 和 M (1000) 的左边，来表示 400 和 900。
+
+// 给定一个罗马数字，将其转换成整数。输入确保在 1 到 3999 的范围内。
+
+ 
+
+// 示例 1:
+
+// 输入: "III"
+// 输出: 3
+
+// 示例 2:
+
+// 输入: "IV"
+// 输出: 4
+
+// 示例 3:
+
+// 输入: "IX"
+// 输出: 9
+
+// 示例 4:
+
+// 输入: "LVIII"
+// 输出: 58
+// 解释: L = 50, V= 5, III = 3.
+
+// 示例 5:
+
+// 输入: "MCMXCIV"
+// 输出: 1994
+// 解释: M = 1000, CM = 900, XC = 90, IV = 4.
+
+ 
+
+// 提示：
+
+//     1 <= s.length <= 15
+//     s 仅含字符 ('I', 'V', 'X', 'L', 'C', 'D', 'M')
+//     题目数据保证 s 是一个有效的罗马数字，且表示整数在范围 [1, 3999] 内
+//     题目所给测试用例皆符合罗马数字书写规则，不会出现跨位等情况。
+//     IL 和 IM 这样的例子并不符合题目要求，49 应该写作 XLIX，999 应该写作 CMXCIX 。
+//     关于罗马数字的详尽书写规则，可以参考 罗马数字 - Mathematics 。
+
+// 来源：力扣（LeetCode）
+// 链接：https://leetcode-cn.com/problems/roman-to-integer
+// 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 package leetcode;
 
 public class LeetCode_0013_RomanToInteger {
 
 
     public static int romanToInt(String s) {
-        char[] chars = s.toCharArray();
-        int res = 0;
-        for (int i = 1; i < chars.length; i++) {
-            if (singleMatch(chars[i]) > singleMatch(chars[i - 1])) {
-                res += (-singleMatch(chars[i - 1]));
-            } else {
-                res += (singleMatch(chars[i - 1]));
-            }
-        }
-        res += singleMatch(chars[chars.length - 1]);
-        return res;
-    }
-
-    public static int singleMatch(char s) {
-        int p = 0;
-        switch (s) {
-            case 'I':
-                p = 1;
-                break;
-            case 'V':
-                p = 5;
-                break;
-            case 'X':
-                p = 10;
-                break;
-            case 'L':
-                p = 50;
-                break;
-            case 'C':
-                p = 100;
-                break;
-            case 'D':
-                p = 500;
-                break;
-            case 'M':
-                p = 1000;
-                break;
-        }
-        return p;
+        int nums[] = new int[s.length()];
+		for (int i = 0; i < s.length(); i++) {
+			switch (s.charAt(i)) {
+			case 'M':
+				nums[i] = 1000;
+				break;
+			case 'D':
+				nums[i] = 500;
+				break;
+			case 'C':
+				nums[i] = 100;
+				break;
+			case 'L':
+				nums[i] = 50;
+				break;
+			case 'X':
+				nums[i] = 10;
+				break;
+			case 'V':
+				nums[i] = 5;
+				break;
+			case 'I':
+				nums[i] = 1;
+				break;
+			}
+		}
+		int sum = 0;
+		for (int i = 0; i < nums.length - 1; i++) {
+			if (nums[i] < nums[i + 1]) {
+				sum -= nums[i];
+			} else {
+				sum += nums[i];
+			}
+		}
+		return sum + nums[nums.length - 1];
     }
 }
 
