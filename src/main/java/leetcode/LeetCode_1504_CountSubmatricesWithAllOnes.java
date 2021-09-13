9a2c27a4b1295e477afa71eb28d3f05746e4192f
zenghui <410486047@qq.com>
2021-09-13 09:20:33 +0800

    add todo

---
+++ src/main/java/leetcode/LeetCode_1504_CountSubmatricesWithAllOnes.java
@@ -0,0 +1,12 @@
+package leetcode;
+
+//tips: 必须以每一行做底的子矩阵是多少，求和即可
+//        N为长的矩形一共包含的子矩阵有 (N*(N+1)) / 2
+//        相等的时候不算
+
+//https://leetcode-cn.com/problems/count-submatrices-with-all-ones/
+public class LeetCode_1504_CountSubmatricesWithAllOnes {
+    public int numSubmat(int[][] mat) {
+        return -1;
+    }
+}
