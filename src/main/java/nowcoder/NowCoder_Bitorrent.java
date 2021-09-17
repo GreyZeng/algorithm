57cc35c83d89dfe84edc2114b17f451bd8398782
zenghui <410486047@qq.com>
2021-09-17 11:36:27 +0800

    reservoir sampling

--- src/main/java/nowcoder/NowCoder_Bitorrent.java
+++ src/main/java/nowcoder/NowCoder_Bitorrent.java
@@ -51,8 +51,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
-//tips:
-//        hashmap head表 和 tail表 和链表配合
+//tips: hashmap head表 和 tail表 和链表配合
 public class NowCoder_Bitorrent {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
