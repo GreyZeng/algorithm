876f604d9b413c5d93dba6c7ef6fcd937226deac
GreyZeng <410486047@qq.com>
2021-09-22 13:44:29 +0800

    add todo

--- src/main/java/leetcode/LeetCode_0215_KthLargestElementInAnArray.java
+++ src/main/java/leetcode/LeetCode_0215_KthLargestElementInAnArray.java
@@ -9,6 +9,51 @@ import java.util.PriorityQueue;
  * 方法2. bfprt算法 (严格收敛到O(N),但是空间复杂度O(N))
  */
 public class LeetCode_0215_KthLargestElementInAnArray {
+    
+    // 快排改进算法
+    // 第K小 == 第 nums.length - k + 1 大
+    public static int findKthLargest2(int[] nums, int k) {
+        return p(nums, 0, nums.length - 1, k - 1);
+    }
+
+    // nums在L...R范围上，如果要排序（从大到小）的话，请返回index位置的值
+    public static int p(int[] nums, int L, int R, int index) {
+        if (L == R) {
+            return nums[L];
+        }
+        int pivot = nums[L + (int) (Math.random() * (R - L + 1))];
+        int[] range = partition(nums, L, R, pivot);
+        if (index >= range[0] && index <= range[1]) {
+            return nums[index];
+        } else if (index < range[0]) {
+            return p(nums, L, range[0] - 1, index);
+        } else {
+            return p(nums, range[1] + 1, R, index);
+        }
+    }
+
+    private static int[] partition(int[] nums, int l, int r, int pivot) {
+        int i = l;
+        int more = l - 1;//大于区域
+        int less = r + 1; // 小于区域
+        while (i < less) {
+            if (nums[i] > pivot) {
+                swap(nums, i++, ++more);
+            } else if (nums[i] < pivot) {
+                swap(nums, i, --less);
+            } else {
+                i++;
+            }
+        }
+        return new int[]{more + 1, less - 1};
+    }
+
+    public static void swap(int[] nums, int t, int m) {
+        int tmp = nums[m];
+        nums[m] = nums[t];
+        nums[t] = tmp;
+    }
+
     // bfprt算法
     public static int findKthLargest(int[] nums, int k) {
         return bfprt(nums, 0, nums.length - 1, k - 1);
@@ -49,51 +94,8 @@ public class LeetCode_0215_KthLargestElementInAnArray {
     public static int getMedian(int[] arr, int L, int R) {
         Arrays.sort(arr, L, R);
         return arr[(R + L) / 2];
-
     }
-    // 快排改进算法
-    // 第K小 == 第 nums.length - k 大
-    public static int findKthLargest2(int[] nums, int k) {
-        return p(nums, 0, nums.length - 1, k - 1);
-    }
-
-    // nums在L...R范围上，如果要排序的话，请返回index位置的值
-    public static int p(int[] nums, int L, int R, int index) {
-        if (L == R) {
-            return nums[L];
-        }
-        int pivot = nums[L + (int) (Math.random() * (R - L + 1))];
-        int[] range = partition(nums, L, R, pivot);
-        if (index >= range[0] && index <= range[1]) {
-            return nums[index];
-        } else if (index < range[0]) {
-            return p(nums, L, range[0] - 1, index);
-        } else {
-            return p(nums, range[1] + 1, R, index);
-        }
-    }
-
-    private static int[] partition(int[] nums, int l, int r, int pivot) {
-        int i = l;
-        int more = l - 1;//大于区域
-        int less = r + 1; // 小于区域
-        while (i < less) {
-            if (nums[i] > pivot) {
-                swap(nums, i++, ++more);
-            } else if (nums[i] < pivot) {
-                swap(nums, i, --less);
-            } else {
-                i++;
-            }
-        }
-        return new int[]{more + 1, less - 1};
-    }
-
-    public static void swap(int[] nums, int t, int m) {
-        int tmp = nums[m];
-        nums[m] = nums[t];
-        nums[t] = tmp;
-    }
+    
     // 第K大 --> 第 (len - K + 1) 小
     public static int findKthLargest3(int[] nums, int k) {
         PriorityQueue<Integer> h = new PriorityQueue<>();
--- src/main/java/lintcode/LintCode_0464_SortIntegersII.java
+++ src/main/java/lintcode/LintCode_0464_SortIntegersII.java
@@ -3,15 +3,22 @@ package lintcode;
 import java.util.Stack;
 
 /**
- * 快速排序 给定一个数组arr，和一个整数num。请把小于num的数放在数组的左边，等于num的数放在中间，大于num的数放在数组的右边。
+ * 快速排序 
+ * 
+ * partition过程
+ * 给定一个数组arr，和一个整数num。请把小于num的数放在数组的左边，等于num的数放在中间，大于num的数放在数组的右边。
  * <p>
  * 要求额外空间复杂度O(1)，时间复杂度O(N)
  * <p>
- * 快速排序3.0(随机快排+荷兰国旗技巧优化) 在arr[L..R]范围上，进行快速排序的过程： 1）在这个范围上，随机选一个数记为num，
+ * 快速排序3.0(随机快排+荷兰国旗技巧优化) 在arr[L..R]范围上，进行快速排序的过程： 
+ * 0）在这个范围上，随机选一个数记为num，
  * 1）用num对该范围做partition，< num的数在左部分，== num的数中间，>num的数在右部分。假设== num的数所在范围是[a,b]
- * 2）对arr[L..a-1]进行快速排序(递归) 3）对arr[b+1..R]进行快速排序(递归)
- * 因为每一次partition都会搞定一批数的位置且不会再变动，所以排序能完成 1）通过分析知道，划分值越靠近中间，性能越好；越靠近两边，性能越差
- * 2）随机选一个数进行划分的目的就是让好情况和差情况都变成概率事件 3）把每一种情况都列出来，会有每种情况下的时间复杂度，但概率都是1/N
+ * 2）对arr[L..a-1]进行快速排序(递归) 
+ * 3）对arr[b+1..R]进行快速排序(递归)
+ * 因为每一次partition都会搞定一批数的位置且不会再变动，所以排序能完成
+ * 1）通过分析知道，划分值越靠近中间，性能越好；越靠近两边，性能越差
+ * 2）随机选一个数进行划分的目的就是让好情况和差情况都变成概率事件 
+ * 3）把每一种情况都列出来，会有每种情况下的时间复杂度，但概率都是1/N
  * 4）那么所有情况都考虑，时间复杂度就是这种概率模型下的长期期望！
  * <p>
  * 时间复杂度O(N*logN)，额外空间复杂度O(logN)都是这么来的。
@@ -26,6 +33,7 @@ public class LintCode_0464_SortIntegersII {
 		}
 		process(arr, 0, arr.length - 1);
 	}
+
 	private static void process(int[] arr, int L, int R) {
 		if (L >= R) {
 			return;
@@ -35,6 +43,7 @@ public class LintCode_0464_SortIntegersII {
 		process(arr, L, range[0] - 1);
 		process(arr, range[1] + 1, R);
 	}
+	
 	public static void swap(int[] arr, int i, int j) {
 		if (i != j) {
 			arr[i] = arr[i] ^ arr[j];
@@ -42,7 +51,7 @@ public class LintCode_0464_SortIntegersII {
 			arr[i] = arr[i] ^ arr[j];
 		}
 	}
-
+	// 以arr[R]作为分割跑荷兰国旗问题
 	public static int[] sortColors(int[] arr, int L, int R) {
 		int target = arr[R];
 		int less = L - 1;
