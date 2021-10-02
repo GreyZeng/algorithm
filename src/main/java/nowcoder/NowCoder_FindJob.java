374af6b38141a7ee64d1b4a10dfe4e8c4988f079
zenghui <410486047@qq.com>
2021-10-02 18:03:21 +0800

    tree map find job

--- src/main/java/nowcoder/NowCoder_FindJob.java
+++ src/main/java/nowcoder/NowCoder_FindJob.java
@@ -7,15 +7,11 @@
 //        选工作的标准是在难度不超过自身能力值的情况下，选择报酬最高的岗位
 //        给定一个int类型的数组arr，表示所有人的能力
 //        返回int类型的数组，表示每个人按照标准选工作后所能获得的最高报酬
-//tips:
-//        排序
-//        难度一样，只保留最高报酬的
-//        难度更大，报酬却更小的也删掉
-//        然后加入有序表，找离自己最近的最大的值
 // 链接：https://www.nowcoder.com/questionTerminal/5e1b251c90ba4e6982cb349076ed4449
 //来源：牛客网
 //
-//为了找到自己满意的工作，牛牛收集了每种工作的难度和报酬。牛牛选工作的标准是在难度不超过自身能力值的情况下，牛牛选择报酬最高的工作。在牛牛选定了自己的工作后，牛牛的小伙伴们来找牛牛帮忙选工作，牛牛依然使用自己的标准来帮助小伙伴们。牛牛的小伙伴太多了，于是他只好把这个任务交给了你。
+//为了找到自己满意的工作，牛牛收集了每种工作的难度和报酬。牛牛选工作的标准是在难度不超过自身能力值的情况下，牛牛选择报酬最高的工作。
+// 在牛牛选定了自己的工作后，牛牛的小伙伴们来找牛牛帮忙选工作，牛牛依然使用自己的标准来帮助小伙伴们。牛牛的小伙伴太多了，于是他只好把这个任务交给了你。
 //
 //输入描述:
 //每个输入包含一个测试用例。
@@ -38,12 +34,14 @@
 //100
 //1000
 //1001
+//tips:
+//        排序
+//        难度一样，只保留最高报酬的
+//        难度更大，报酬却更小的也删掉
+//        然后加入有序表，找离自己最近的最大的值
 package nowcoder;
 
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.Scanner;
-import java.util.TreeMap;
+import java.util.*;
 
 // https://www.nowcoder.com/questionTerminal/5e1b251c90ba4e6982cb349076ed4449
 public class NowCoder_FindJob {
@@ -57,74 +55,58 @@ public class NowCoder_FindJob {
         }
     }
 
-    // 难度从小到大
-    // 收益从大到小
-    public static class MyComparator implements Comparator<Job> {
-
-        @Override
-        public int compare(Job o1, Job o2) {
-            // 如果用减法或者加法，有可能会导致越界错误
-            if (o1.hard > o2.hard) {
-                return 1;
-            } else if (o1.hard < o2.hard) {
-                return -1;
-            } else {
-                if (o2.money > o1.money) {
-                    return 1;
-                } else if (o2.money < o1.money) {
-                    return -1;
-                }
-            }
-            return 0;
-        }
-    }
-
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int N = in.nextInt();
-        int M = in.nextInt();
-        Job[] jobs = new Job[N];
-        for (int i = 0; i < N; i++) {
-            int Di = in.nextInt();
-            int Pi = in.nextInt();
-            jobs[i] = new Job(Di, Pi);
+        int jobNum = in.nextInt();
+        int peopleNum = in.nextInt();
+        Job[] jobs = new Job[jobNum];
+        for (int i = 0; i < jobNum; i++) {
+            int hard = in.nextInt();
+            int money = in.nextInt();
+            jobs[i] = new Job(hard, money);
         }
-        int[] abilities = new int[M];
-        for (int i = 0; i < M; i++) {
-            int Ai = in.nextInt();
-            abilities[i] = Ai;
+        int[] abilities = new int[peopleNum];
+        for (int i = 0; i < peopleNum; i++) {
+            int ability = in.nextInt();
+            abilities[i] = ability;
         }
         int[] money = findJob(jobs, abilities);
-        for (int i = 0; i < money.length; i++) {
-            System.out.println(money[i]);
+        for (int j : money) {
+            System.out.println(j);
         }
         in.close();
     }
 
     public static int[] findJob(Job[] jobs, int[] abilities) {
-        if (jobs == null) {
-            return null;
-        }
-        Arrays.sort(jobs, new MyComparator());
-        TreeMap<Integer, Integer> map = new TreeMap<>();
-        Job pre = jobs[0];
-        map.put(pre.hard, pre.money);
-        for (int i = 1; i < jobs.length; i++) {
-            if (pre.money < jobs[i].money) {
-                pre = jobs[i];
-                map.put(pre.hard, pre.money);
-            }
-        }
-        int[] money = new int[abilities.length];
-        int i = 0;
-        for (int ability : abilities) {
-            Integer m = map.floorKey(ability);
-            if (m == null) {
-                money[i++] = 0;
+        int[] values = new int[abilities.length];
+        // 难度低的排前面
+        // 难度相同的时候收益高的排前面
+        Arrays.sort(jobs, (o1, o2) -> {
+            if (o1.hard == o2.hard) {
+                return Integer.compare(o2.money, o1.money);
             } else {
-                money[i++] = map.get(m);
+                return o1.hard > o2.hard ? 1 : -1;
+            }
+        });
+        // 经过上述排序，jobs[0]的数据一定是难度最低的，且收益相对高的
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        map.put(jobs[0].hard, jobs[0].money);
+        int preMoney = jobs[0].money;
+        for (int i = 1; i < jobs.length; i++) {
+            // 只有比这个收益高的才加入，否则没必要加入
+            if (preMoney < jobs[i].money) {
+                map.put(jobs[i].hard, jobs[i].money);
+                preMoney = jobs[i].money;
             }
         }
-        return money;
+        for (int i = 0; i < abilities.length; i++) {
+            Integer m = map.floorKey(abilities[i]);
+            if (m == null) {
+                values[i] = 0;
+            } else {
+                values[i] = map.get(m);
+            }
+        }
+        return values;
     }
 }
