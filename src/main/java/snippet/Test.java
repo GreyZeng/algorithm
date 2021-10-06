08cd555116c0e56bd835432723c2585da7e46249
GreyZeng <410486047@qq.com>
2021-10-06 15:35:40 +0800

    add notes

--- src/main/java/snippet/Test.java
+++ src/main/java/snippet/Test.java
@@ -1,11 +1,6 @@
 package snippet;
 
+// for test and snippet code
 public class Test {
-    public static void main(String[] args) {
-        System.out.println(Integer.toUnsignedLong(Integer.MIN_VALUE));
-    }
-    private static long getUnsignedInt(int data) {
-        // data & 0xFFFFFFFF 和 data & 0xFFFFFFFFL 结果是不同的，需要注意，有可能与 JDK 版本有关
-        return data & 0xFFFFFFFFL;
-    }
+    
 }
