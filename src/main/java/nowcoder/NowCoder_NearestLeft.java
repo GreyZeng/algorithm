e6035086b1125cc222a69694746b6271702d5b7c
GreyZeng <410486047@qq.com>
2021-10-08 13:23:32 +0800

    binary search

--- src/main/java/nowcoder/NowCoder_NearestLeft.java
+++ src/main/java/nowcoder/NowCoder_NearestLeft.java
@@ -47,20 +47,17 @@ public class NowCoder_NearestLeft {
         if (arr[n - 1] < target) {
             return -1;
         }
-        int L = 0;
-        int R = n - 1;
+        int l = 0;
+        int r = n - 1;
         int res = -1;
-        while (L <= R) {
-            int mid = L + ((R - L) >> 1);
-            if (arr[mid] == target) {
+        while (l <= r) {
+            int mid = l + ((r - l) >> 1);
+            if (arr[mid] >= target) {
                 res = mid;
-                R = mid - 1;
-            } else if (arr[mid] > target) {
-                res = mid;
-                R = mid - 1;
+                r = mid - 1;
             } else {
                 // arr[mid] < target
-                L = mid + 1;
+                l = mid + 1;
             }
         }
         return res;
