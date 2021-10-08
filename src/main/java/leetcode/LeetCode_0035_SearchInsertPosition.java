93690f2149de908504b2b8dd4869fcd497852b42
GreyZeng <410486047@qq.com>
2021-10-08 13:36:08 +0800

    update

--- src/main/java/leetcode/LeetCode_0035_SearchInsertPosition.java
+++ src/main/java/leetcode/LeetCode_0035_SearchInsertPosition.java
@@ -47,20 +47,17 @@ public class LeetCode_0035_SearchInsertPosition {
         if (arr[n - 1] < target) {
             return n;
         }
-        int L = 0;
-        int R = n - 1;
+        int l = 0;
+        int r = n - 1;
         int res = n;
-        while (L <= R) {
-            int mid = L + ((R - L) >> 1);
-            if (arr[mid] == target) {
+        while (l <= r) {
+            int mid = l + ((r - l) >> 1);
+            if (arr[mid] >= target) {
                 res = mid;
-                R = mid - 1;
-            } else if (arr[mid] > target) {
-                res = mid;
-                R = mid - 1;
+                r = mid - 1;
             } else {
                 // arr[mid] < target
-                L = mid + 1;
+                l = mid + 1;
             }
         }
         return res;
