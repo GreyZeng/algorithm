670878b55a75ebec28b5a8b63a4bf55f8eea29ec
zenghui <410486047@qq.com>
2021-10-08 14:15:41 +0800

    bit nums

--- src/main/java/leetcode/LeetCode_0136_SingleNumber.java
+++ src/main/java/leetcode/LeetCode_0136_SingleNumber.java
@@ -3,14 +3,10 @@ package leetcode;
 public class LeetCode_0136_SingleNumber {
 
     public static int singleNumber(int[] nums) {
-        if (nums.length == 1) {
-            return nums[0];
-        }
-        int L = nums[0];
+        int ans = nums[0];
         for (int i = 1; i < nums.length; i++) {
-            L ^= nums[i];
+            ans ^= nums[i];
         }
-        return L;
+        return ans;
     }
-
 }
