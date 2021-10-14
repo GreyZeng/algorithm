cd368d54516e4e47dd7fa1b7701b82a98f97dbc8
zenghui <410486047@qq.com>
2021-10-14 17:02:08 +0800

    update notes

--- src/main/java/leetcode/LeetCode_0218_TheSkylineProblem.java
+++ src/main/java/leetcode/LeetCode_0218_TheSkylineProblem.java
@@ -1,9 +1,7 @@
 package leetcode;
 
 import java.util.*;
-//【举例】
-//		matrix ={{2,5,6}, {1,7,4}, {4,6,7}, {3,6,5}, {10,13,2}, {9,11,3}, {12,14,4},{10,12,5} }
-//		返回: {{1,2,4},{2,4,6}, {4,6,7}, {6,7,4}, {9,10,3}, {10,12,5}, {12,14,4}}
+
 // 有序表的使用
 //tips:
 //		最大高度的变化
@@ -19,73 +17,77 @@ import java.util.*;
 //		第二个有序表 记录轮廓
 //		为了防止纸片大楼，在排序的时候，把+放在-之前
 public class LeetCode_0218_TheSkylineProblem {
-	public static class Node {
-		public int x;
-		public int h;
+    public static class Node {
+        public int x;
+        public int height;
 
-		public Node(int x, int h) {
-			this.x = x;
-			this.h = h;
-		}
-	}
+        public Node(int x, int height) {
+            this.x = x;
+            this.height = height;
+        }
+    }
 
-	public static class MyComparator implements Comparator<Node> {
+    public static class MyComparator implements Comparator<Node> {
+        @Override
+        public int compare(Node o1, Node o2) {
+            return o1.x - o2.x;
+        }
+    }
 
-		@Override
-		public int compare(Node o1, Node o2) {
-			if (o1.x != o2.x) {
-				return o1.x - o2.x;
-			}
-			if (o1.h != o2.h) {
-				return o1.h > o2.h ? -1 : 1;
-			}
-			return 0;
-		}
+    // 轮廓线只出现在最大高度变化的位置
+    // 所以需要记录某个大楼从哪个位置开始增加了一个高度
+    // 从哪个位置减少了一个高度
+    public static List<List<Integer>> getSkyline(int[][] buildings) {
+        List<Node> items = buildNodes(buildings);
+        // 最大高度出现的次数
+        TreeMap<Integer, Integer> heightTimes = new TreeMap<>();
+        TreeMap<Integer, Integer> result = new TreeMap<>();
+        for (Node item : items) {
+            if (item.height > 0) {
+                if (!heightTimes.containsKey(item.height)) {
+                    heightTimes.put(item.height, 1);
+                } else {
+                    heightTimes.put(item.height, heightTimes.get(item.height) + 1);
+                }
+            } else {
+                if (heightTimes.get(-item.height) == 1) {
+                    heightTimes.remove(-item.height);
+                } else {
+                    heightTimes.put(-item.height, heightTimes.get(-item.height) - 1);
+                }
+            }
+            if (heightTimes.isEmpty()) {
+                result.put(item.x, 0);
+            } else {
+                result.put(item.x, heightTimes.lastKey());
+            }
+        }
+        List<List<Integer>> ans = new ArrayList<>();
+        for (Map.Entry<Integer, Integer> entry : result.entrySet()) {
+            int curX = entry.getKey();
+            int curMaxHeight = entry.getValue();
+            if (ans.isEmpty() || ans.get(ans.size() - 1).get(1) != curMaxHeight) {
+                ans.add(new ArrayList<>(Arrays.asList(curX, curMaxHeight)));
+            }
+        }
+        return ans;
+    }
 
-	}
+    private static List<Node> buildNodes(int[][] buildings) {
+        List<Node> list = new ArrayList<>();
+        for (int[] building : buildings) {
+            // 在某个位置增加了一个高度
+            list.add(new Node(building[0], building[2]));
+            // 在某个位置减少了一个高度
+            list.add(new Node(building[1], -building[2]));
+        }
+        list.sort(new MyComparator());
+        return list;
+    }
 
-	public static List<List<Integer>> getSkyline(int[][] buildings) {
-		List<Node> items = buildNodes(buildings);
-		TreeMap<Integer, Integer> map = new TreeMap<>();
-		TreeMap<Integer, Integer> result = new TreeMap<>();
-		for (Node n : items) {
-			if (n.h > 0) {
-				if (!map.containsKey(n.h)) {
-					map.put(n.h, 1);
-				} else {
-					map.put(n.h, map.get(n.h) + 1);
-				}
-			} else {
-				if (map.get(-n.h) == 1) {
-					map.remove(-n.h);
-				} else {
-					map.put(-n.h, map.get(-n.h) - 1);
-				}
-			}
-			if (map.isEmpty()) {
-				result.put(n.x, 0);
-			} else {
-				result.put(n.x, map.lastKey());
-			}
-		}
-		List<List<Integer>> ans = new ArrayList<>();
-		for (Map.Entry<Integer, Integer> entry : result.entrySet()) {
-			int curX = entry.getKey();
-			int curMaxHeight = entry.getValue();
-			if (ans.isEmpty() || ans.get(ans.size() - 1).get(1) != curMaxHeight) {
-				ans.add(new ArrayList<>(Arrays.asList(curX, curMaxHeight)));
-			}
-		}
-		return ans;
-	}
-
-	private static List<Node> buildNodes(int[][] buildings) {
-		List<Node> list = new ArrayList<>();
-		for (int[] building : buildings) {
-			list.add(new Node(building[0], building[2]));
-			list.add(new Node(building[1], -building[2]));
-		}
-		list.sort(new MyComparator());
-		return list;
-	}
+    public static void main(String[] args) {
+        int[][] build = {{0,2,3},{2,5,3}};
+        List<List<Integer>> skyline = getSkyline(build);
+        System.out.println(skyline);
+    }
 }
