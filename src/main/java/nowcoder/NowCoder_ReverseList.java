50c1c1793610c2796ea4e4bf5b29cba107f896eb
GreyZeng <410486047@qq.com>
2021-10-19 22:46:34 +0800

    add nowcoder reverse linkedlist

---
+++ src/main/java/nowcoder/NowCoder_ReverseList.java
@@ -0,0 +1,59 @@
+// 链接：https://www.nowcoder.com/questionTerminal/b66a251dec8847f386bbe6cd96b7e9c8
+// 来源：牛客网
+
+// 实现反转单向链表和双向链表的函数。
+// 如 1->2->3 反转后变成 3->2->1。
+
+// 输入描述:
+// 第一行一个整数 n，表示单链表的长度。
+
+// 第二行 n 个整数 val 表示单链表的各个节点。
+
+// 第三行一个整数 m，表示双链表的长度。
+
+// 第四行 m 个整数 val 表示双链表的各个节点。
+
+
+// 输出描述:
+// 在给定的函数内返回相应链表的头指针。
+// 示例1
+// 输入
+// 3
+// 1 2 3
+// 4
+// 1 2 3 4
+// 输出
+// 3 2 1
+// 4 3 2 1
+
+// 备注:
+// 1 \le n,m\le10000001≤n,m≤1000000
+// -1000000 \le val \le 1000000−1000000≤val≤1000000
+package nowcoder;
+
+import java.util.Scanner;
+
+public class NowCoder_ReverseList {
+    // TODO
+    // public static class ListNode {
+    //     int v;
+    //     ListNode next;
+
+    //     public ListNode(int v) {
+    //         this.v = v;
+    //     }
+    // }
+    // public static void main(String[] args) {
+    //     Scanner in = new Scanner(System.in);
+    //     int l1 = in.nextInt();
+    //     ListNode head = new ListNode(in.nextInt());
+    //     ListNode cur = head;
+    //     for (int i = 1; i < l1; i++) {
+    //         cur.next = new ListNode(in.nextInt());
+    //         cur = cur.next;
+    //     }
+    //     int l2 = in.nextInt();
+    //     ListNode 
+    //     in.close();
+    // }
+}
