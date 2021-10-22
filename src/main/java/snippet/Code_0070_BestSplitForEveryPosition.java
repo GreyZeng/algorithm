12c836f15509debcd18baee81142429bb909871c
GreyZeng <410486047@qq.com>
2021-10-22 23:12:11 +0800

    update

--- src/main/java/snippet/Code_0070_BestSplitForEveryPosition.java
+++ src/main/java/snippet/Code_0070_BestSplitForEveryPosition.java
@@ -1,5 +1,15 @@
 package snippet;
+// 把Code_0065_BestSplitForAll中提到的，min{左部分累加和，右部分累加和}，定义为S(N-1)，也就是说：
 
+// `S(N-1)`表示在`arr[0…N-1]`范围上，做最优划分所得到的`min{左部分累加和，右部分累加和}的最大值`, 
+// 现在要求返回一个长度为N的s数组，
+// s[i]表示在`arr[0…i]`范围上，做最优划分所得到的min{左部分累加和，右部分累加和}的最大值, 得到整个s数组的过程，做到时间复杂度O(N)
+
+// tips:
+// 1. sum和范围有单调性
+// 2. 范式遵从 差(好(S左，S右))
+//    都有O(N)的优化
+// 3. 前缀和加速（前缀和数组多加一位，不需要判断边界）
 public class Code_0070_BestSplitForEveryPosition {
 
 	public static int[] bestSplit1(int[] arr) {
@@ -51,41 +61,41 @@ public class Code_0070_BestSplitForEveryPosition {
 		return ans;
 	}
 
+	// 四边形不等式优化
 	public static int[] bestSplit3(int[] arr) {
 		if (arr == null || arr.length == 0) {
 			return new int[0];
 		}
-		int N = arr.length;
-		int[] ans = new int[N];
-		ans[0] = 0;
-		// arr =   {5, 3, 1, 3}
-		//          0  1  2  3
-		// sum ={0, 5, 8, 9, 12}
-		//       0  1  2  3   4
-		// 0~2 ->  sum[3] - sum[0]
-		// 1~3 ->  sum[4] - sum[1]
-		int[] sum = new int[N + 1];
-		for (int i = 0; i < N; i++) {
+		int n = arr.length;
+		if (n == 1) {
+			return new int[] { 0 };
+		}
+		int[] sum = new int[n + 1];
+		for (int i = 0; i < n; i++) {
 			sum[i + 1] = sum[i] + arr[i];
 		}
-		// 最优划分
-		// 0~range-1上，最优划分是左部分[0~best]  右部分[best+1~range-1]
-		int best = 0;
-		for (int range = 1; range < N; range++) {
-			while (best + 1 < range) {
-				int before = Math.min(sum(sum, 0, best), sum(sum, best + 1, range));
-				int after = Math.min(sum(sum, 0, best + 1), sum(sum, best + 2, range));
-				// 注意，一定要是>=，只是>会出错,在包含0的情况下
-				// eg: [5,0,0,0,5,5,5]
-				if (after >= before) {
-					best++;
+		// s[0] = 0
+		int[] s = new int[n];
+		s[1] = Math.min(arr[0], arr[1]);
+		int bestSplitPosition = 1;
+		int begin = 0;
+		for (int i = 2; i < n; i++) {
+			// 分割点从1开始，不回退
+			int ans = s[i - 1];
+			for (begin = bestSplitPosition; begin <= i; begin++) {
+				int right = sum(sum, begin, i);
+				int left = sum(sum, 0, begin - 1);
+				if (Math.min(right, left) >= ans) {
+					bestSplitPosition = begin;
+					ans = Math.min(right, left);
 				} else {
 					break;
 				}
 			}
-			ans[range] = Math.min(sum(sum, 0, best), sum(sum, best + 1, range));
+			s[i] = ans;
 		}
-		return ans;
+		return s;
+
 	}
 
 	public static int[] randomArray(int len, int max) {
@@ -114,17 +124,21 @@ public class Code_0070_BestSplitForEveryPosition {
 		int max = 30;
 		int testTime = 1000000;
 		System.out.println("测试开始");
+		int[] arr = null;
+		int[] ans1 = null;
+		int[] ans2 = null;
+		int[] ans3 = null;
 		for (int i = 0; i < testTime; i++) {
 			int len = (int) (Math.random() * N);
-			int[] arr = randomArray(len, max);
-			int[] ans1 = bestSplit1(arr);
-			int[] ans2 = bestSplit2(arr);
-			int[] ans3 = bestSplit3(arr);
+			arr = randomArray(len, max);
+			ans1 = bestSplit1(arr);
+			ans2 = bestSplit2(arr);
+			ans3 = bestSplit3(arr);
 			if (!isSameArray(ans1, ans2) || !isSameArray(ans1, ans3)) {
 				System.out.println("Oops!");
+				break;
 			}
 		}
-		System.out.println("测试结束");
+		System.out.println("测试结束"); 
 	}
-
 }
