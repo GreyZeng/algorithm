ace32ffdb19cae964cdb2894a0e9e82f31a235af
GreyZeng <410486047@qq.com>
2021-10-25 13:50:11 +0800

    fixme

--- src/main/java/leetcode/LeetCode_0887_SuperEggDrop.java
+++ src/main/java/leetcode/LeetCode_0887_SuperEggDrop.java
@@ -48,167 +48,192 @@ package leetcode;
 //        S是客观上要解决的次数 K表示的棋子
 //
 //        可以继续优化（有可能转换成斐波那契问题）
-// leetcode测试链接：https://leetcode.com/problems/super-egg-drop
 // 方法1和方法2会超时
 // 方法3勉强通过
 // 方法4打败100%
 // 方法5打败100%，方法5是在方法4的基础上做了进一步的常数优化
 public class LeetCode_0887_SuperEggDrop {
 
-    public static int superEggDrop1(int kChess, int nLevel) {
-        if (nLevel < 1 || kChess < 1) {
-            return 0;
-        }
-        return process1(nLevel, kChess);
-    }
+	// 暴力递归方式
+	// FIXME
+	public static int superEggDrop1(int k, int n) {
+		if (n < 1 || k < 1) {
+			return 0;
+		}
+		return p(n, k);
+	}
 
-    // rest还剩多少层楼需要去验证
-    // k还有多少颗棋子能够使用
-    // 一定要验证出最高的不会碎的楼层！但是每次都是坏运气。
-    // 返回至少需要扔几次？
-    public static int process1(int rest, int k) {
-        if (rest == 0) {
-            return 0;
-        }
-        if (k == 1) {
-            return rest;
-        }
-        int min = Integer.MAX_VALUE;
-        for (int i = 1; i != rest + 1; i++) { // 第一次扔的时候，仍在了i层
-            min = Math.min(min, Math.max(process1(i - 1, k - 1), process1(rest - i, k)));
-        }
-        return min + 1;
-    }
+	// 剩下rest层，剩下k个鸡蛋，一定要验出最好层数方法数量
+	public static int p(int rest, int k) {
+		if (rest == 0 || k == 1) {
+			// 剩下一个鸡蛋了，只能逐层尝试
+			return rest;
+		}
+		// 最后一个鸡蛋弃而不用
+		// rest层要k-1个鸡蛋来验
+		int min = p(rest, k - 1) + 0;
+		// 枚举最后一个鸡蛋验证几层
+		for (int i = 1; i <= rest; i++) {
+			min = Math.min(min, Math.max(min, p(rest - i, k - 1) + i));
+		}
+		return min;
+	}
 
-    public static int superEggDrop2(int kChess, int nLevel) {
-        if (nLevel < 1 || kChess < 1) {
-            return 0;
-        }
-        if (kChess == 1) {
-            return nLevel;
-        }
-        int[][] dp = new int[nLevel + 1][kChess + 1];
-        for (int i = 1; i != dp.length; i++) {
-            dp[i][1] = i;
-        }
-        for (int i = 1; i != dp.length; i++) {
-            for (int j = 2; j != dp[0].length; j++) {
-                int min = Integer.MAX_VALUE;
-                for (int k = 1; k != i + 1; k++) {
-                    min = Math.min(min, Math.max(dp[k - 1][j - 1], dp[i - k][j]));
-                }
-                dp[i][j] = min + 1;
-            }
-        }
-        return dp[nLevel][kChess];
-    }
+//	 public static int superEggDrop1(int kChess, int nLevel) {
+//	        if (nLevel < 1 || kChess < 1) {
+//	            return 0;
+//	        }
+//	        return process1(nLevel, kChess);
+//	    }
+//
+//	    // rest还剩多少层楼需要去验证
+//	    // k还有多少颗棋子能够使用
+//	    // 一定要验证出最高的不会碎的楼层！但是每次都是坏运气。
+//	    // 返回至少需要扔几次？
+//	    public static int process1(int rest, int k) {
+//	        if (rest == 0) {
+//	            return 0;
+//	        }
+//	        if (k == 1) {
+//	            return rest;
+//	        }
+//	        int min = Integer.MAX_VALUE;
+//	        for (int i = 1; i != rest + 1; i++) { // 第一次扔的时候，仍在了i层
+//	            min = Math.min(min, Math.max(process1(i - 1, k - 1), process1(rest - i, k)));
+//	        }
+//	        return min + 1;
+//	    }
+	public static int superEggDrop2(int kChess, int nLevel) {
+		if (nLevel < 1 || kChess < 1) {
+			return 0;
+		}
+		if (kChess == 1) {
+			return nLevel;
+		}
+		int[][] dp = new int[nLevel + 1][kChess + 1];
+		for (int i = 1; i != dp.length; i++) {
+			dp[i][1] = i;
+		}
+		for (int i = 1; i != dp.length; i++) {
+			for (int j = 2; j != dp[0].length; j++) {
+				int min = Integer.MAX_VALUE;
+				for (int k = 1; k != i + 1; k++) {
+					min = Math.min(min, Math.max(dp[k - 1][j - 1], dp[i - k][j]));
+				}
+				dp[i][j] = min + 1;
+			}
+		}
+		return dp[nLevel][kChess];
+	}
 
-    public static int superEggDrop3(int kChess, int nLevel) {
-        if (nLevel < 1 || kChess < 1) {
-            return 0;
-        }
-        if (kChess == 1) {
-            return nLevel;
-        }
-        int[][] dp = new int[nLevel + 1][kChess + 1];
-        for (int i = 1; i != dp.length; i++) {
-            dp[i][1] = i;
-        }
-        int[][] best = new int[nLevel + 1][kChess + 1];
-        for (int i = 1; i != dp[0].length; i++) {
-            dp[1][i] = 1;
-            best[1][i] = 1;
-        }
-        for (int i = 2; i < nLevel + 1; i++) {
-            for (int j = kChess; j > 1; j--) {
-                int ans = Integer.MAX_VALUE;
-                int bestChoose = -1;
-                int down = best[i - 1][j];
-                int up = j == kChess ? i : best[i][j + 1];
-                for (int first = down; first <= up; first++) {
-                    int cur = Math.max(dp[first - 1][j - 1], dp[i - first][j]);
-                    if (cur <= ans) {
-                        ans = cur;
-                        bestChoose = first;
-                    }
-                }
-                dp[i][j] = ans + 1;
-                best[i][j] = bestChoose;
-            }
-        }
-        return dp[nLevel][kChess];
-    }
+	public static int superEggDrop3(int kChess, int nLevel) {
+		if (nLevel < 1 || kChess < 1) {
+			return 0;
+		}
+		if (kChess == 1) {
+			return nLevel;
+		}
+		int[][] dp = new int[nLevel + 1][kChess + 1];
+		for (int i = 1; i != dp.length; i++) {
+			dp[i][1] = i;
+		}
+		int[][] best = new int[nLevel + 1][kChess + 1];
+		for (int i = 1; i != dp[0].length; i++) {
+			dp[1][i] = 1;
+			best[1][i] = 1;
+		}
+		for (int i = 2; i < nLevel + 1; i++) {
+			for (int j = kChess; j > 1; j--) {
+				int ans = Integer.MAX_VALUE;
+				int bestChoose = -1;
+				int down = best[i - 1][j];
+				int up = j == kChess ? i : best[i][j + 1];
+				for (int first = down; first <= up; first++) {
+					int cur = Math.max(dp[first - 1][j - 1], dp[i - first][j]);
+					if (cur <= ans) {
+						ans = cur;
+						bestChoose = first;
+					}
+				}
+				dp[i][j] = ans + 1;
+				best[i][j] = bestChoose;
+			}
+		}
+		return dp[nLevel][kChess];
+	}
 
-    public static int superEggDrop4(int kChess, int nLevel) {
-        if (nLevel < 1 || kChess < 1) {
-            return 0;
-        }
-        int[] dp = new int[kChess];
-        int res = 0;
-        while (true) {
-            res++;
-            int previous = 0;
-            for (int i = 0; i < dp.length; i++) {
-                int tmp = dp[i];
-                dp[i] = dp[i] + previous + 1;
-                previous = tmp;
-                if (dp[i] >= nLevel) {
-                    return res;
-                }
-            }
-        }
-    }
+	public static int superEggDrop4(int kChess, int nLevel) {
+		if (nLevel < 1 || kChess < 1) {
+			return 0;
+		}
+		int[] dp = new int[kChess];
+		int res = 0;
+		while (true) {
+			res++;
+			int previous = 0;
+			for (int i = 0; i < dp.length; i++) {
+				int tmp = dp[i];
+				dp[i] = dp[i] + previous + 1;
+				previous = tmp;
+				if (dp[i] >= nLevel) {
+					return res;
+				}
+			}
+		}
+	}
 
-    public static int superEggDrop5(int kChess, int nLevel) {
-        if (nLevel < 1 || kChess < 1) {
-            return 0;
-        }
-        int bsTimes = log2N(nLevel) + 1;
-        if (kChess >= bsTimes) {
-            return bsTimes;
-        }
-        int[] dp = new int[kChess];
-        int res = 0;
-        while (true) {
-            res++;
-            int previous = 0;
-            for (int i = 0; i < dp.length; i++) {
-                int tmp = dp[i];
-                dp[i] = dp[i] + previous + 1;
-                previous = tmp;
-                if (dp[i] >= nLevel) {
-                    return res;
-                }
-            }
-        }
-    }
+	public static int superEggDrop5(int kChess, int nLevel) {
+		if (nLevel < 1 || kChess < 1) {
+			return 0;
+		}
+		int bsTimes = log2N(nLevel) + 1;
+		if (kChess >= bsTimes) {
+			return bsTimes;
+		}
+		int[] dp = new int[kChess];
+		int res = 0;
+		while (true) {
+			res++;
+			int previous = 0;
+			for (int i = 0; i < dp.length; i++) {
+				int tmp = dp[i];
+				dp[i] = dp[i] + previous + 1;
+				previous = tmp;
+				if (dp[i] >= nLevel) {
+					return res;
+				}
+			}
+		}
+	}
 
-    public static int log2N(int n) {
-        int res = -1;
-        while (n != 0) {
-            res++;
-            n >>>= 1;
-        }
-        return res;
-    }
+	public static int log2N(int n) {
+		int res = -1;
+		while (n != 0) {
+			res++;
+			n >>>= 1;
+		}
+		return res;
+	}
 
-    public static void main(String[] args) {
-        int maxN = 500;
-        int maxK = 30;
-        int testTime = 1000;
-        System.out.println("测试开始");
-        for (int i = 0; i < testTime; i++) {
-            int N = (int) (Math.random() * maxN) + 1;
-            int K = (int) (Math.random() * maxK) + 1;
-            int ans2 = superEggDrop2(K, N);
-            int ans3 = superEggDrop3(K, N);
-            int ans4 = superEggDrop4(K, N);
-            int ans5 = superEggDrop5(K, N);
-            if (ans2 != ans3 || ans4 != ans5 || ans2 != ans4) {
-                System.out.println("出错了!");
-            }
-        }
-        System.out.println("测试结束");
-    }
+	public static void main(String[] args) {
+//		int maxN = 500;
+//		int maxK = 30;
+//		int testTime = 1000;
+//		System.out.println("测试开始");
+//		for (int i = 0; i < testTime; i++) {
+//			int N = (int) (Math.random() * maxN) + 1;
+//			int K = (int) (Math.random() * maxK) + 1;
+//			int ans2 = superEggDrop2(K, N);
+//			int ans3 = superEggDrop3(K, N);
+//			int ans4 = superEggDrop4(K, N);
+//			int ans5 = superEggDrop5(K, N);
+//			if (ans2 != ans3 || ans4 != ans5 || ans2 != ans4) {
+//				System.out.println("出错了!");
+//			}
+//		}
+//		System.out.println("测试结束");
+		int sum = superEggDrop1(2, 6);
+		System.out.println(sum);
+	}
 
 }
