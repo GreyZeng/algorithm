a8d9b1eb330d7373b2812a080eb50e5234feea10
zenghui <410486047@qq.com>
2021-10-25 11:19:07 +0800

    update notes

--- src/main/java/leetcode/LeetCode_0887_SuperEggDrop.java
+++ src/main/java/leetcode/LeetCode_0887_SuperEggDrop.java
@@ -1,5 +1,40 @@
 package leetcode;
-
+//给你 k 枚相同的鸡蛋，并可以使用一栋从第 1 层到第 n 层共有 n 层楼的建筑。
+//
+//        已知存在楼层 f ，满足 0 <= f <= n ，任何从 高于 f 的楼层落下的鸡蛋都会碎，从 f 楼层或比它低的楼层落下的鸡蛋都不会破。
+//
+//        每次操作，你可以取一枚没有碎的鸡蛋并把它从任一楼层 x 扔下（满足 1 <= x <= n）。如果鸡蛋碎了，你就不能再次使用它。如果某枚鸡蛋扔下后没有摔碎，则可以在之后的操作中 重复使用 这枚鸡蛋。
+//
+//        请你计算并返回要确定 f 确切的值 的 最小操作次数 是多少？
+//
+//         
+//        示例 1：
+//
+//        输入：k = 1, n = 2
+//        输出：2
+//        解释：
+//        鸡蛋从 1 楼掉落。如果它碎了，肯定能得出 f = 0 。
+//        否则，鸡蛋从 2 楼掉落。如果它碎了，肯定能得出 f = 1 。
+//        如果它没碎，那么肯定能得出 f = 2 。
+//        因此，在最坏的情况下我们需要移动 2 次以确定 f 是多少。
+//        示例 2：
+//
+//        输入：k = 2, n = 6
+//        输出：3
+//        示例 3：
+//
+//        输入：k = 3, n = 14
+//        输出：4
+//         
+//
+//        提示：
+//
+//        1 <= k <= 100
+//        1 <= n <= 104
+//
+//        来源：力扣（LeetCode）
+//        链接：https://leetcode-cn.com/problems/super-egg-drop
+//        著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 //tips:
 //        如果 k > log2N 二分处理 int f(i,j) 剩余i层楼，还剩下j颗 最少需要多少次数 考虑最差情况
 //
@@ -20,14 +55,14 @@ public class LeetCode_0887_SuperEggDrop {
         if (nLevel < 1 || kChess < 1) {
             return 0;
         }
-        return Process1(nLevel, kChess);
+        return process1(nLevel, kChess);
     }
 
     // rest还剩多少层楼需要去验证
     // k还有多少颗棋子能够使用
     // 一定要验证出最高的不会碎的楼层！但是每次都是坏运气。
     // 返回至少需要扔几次？
-    public static int Process1(int rest, int k) {
+    public static int process1(int rest, int k) {
         if (rest == 0) {
             return 0;
         }
@@ -36,7 +71,7 @@ public class LeetCode_0887_SuperEggDrop {
         }
         int min = Integer.MAX_VALUE;
         for (int i = 1; i != rest + 1; i++) { // 第一次扔的时候，仍在了i层
-            min = Math.min(min, Math.max(Process1(i - 1, k - 1), Process1(rest - i, k)));
+            min = Math.min(min, Math.max(process1(i - 1, k - 1), process1(rest - i, k)));
         }
         return min + 1;
     }
