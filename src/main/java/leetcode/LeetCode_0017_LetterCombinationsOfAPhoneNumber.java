7bd9229194c777ece32bce311394d0ce79e64e1a
GreyZeng <410486047@qq.com>
2021-11-19 13:54:25 +0800

    leetcode 17

--- src/main/java/leetcode/LeetCode_0017_LetterCombinationsOfAPhoneNumber.java
+++ src/main/java/leetcode/LeetCode_0017_LetterCombinationsOfAPhoneNumber.java
@@ -5,36 +5,34 @@ import java.util.List;
 
 public class LeetCode_0017_LetterCombinationsOfAPhoneNumber {
 
-	public static final char[][] map = { {}, {}, { 'a', 'b', 'c' }, { 'd', 'e', 'f' }, { 'g', 'h', 'i' },
-			{ 'j', 'k', 'l' }, { 'm', 'n', 'o' }, { 'p', 'q', 'r', 's' }, { 't', 'u', 'v' }, { 'w', 'x', 'y', 'z' } };
+    public static final char[][] MAP = {{}, {}, {'a', 'b', 'c'}, {'d', 'e', 'f'}, {'g', 'h', 'i'},
+            {'j', 'k', 'l'}, {'m', 'n', 'o'}, {'p', 'q', 'r', 's'}, {'t', 'u', 'v'}, {'w', 'x', 'y', 'z'}};
 
-	public static List<String> letterCombinations(String digits) {
-		if (null == digits) {
-			return null;
-		}
-		if (0 == digits.length()) {
-			return new ArrayList<>();
-		}
-		char[] strs = digits.toCharArray();
-		char[] path = new char[strs.length];
-		List<String> ans = new ArrayList<>();
-		process(strs, 0, path, ans);
-		return ans;
-	}
+    public static List<String> letterCombinations(String digits) {
+        if (null == digits) {
+            return null;
+        }
+        if (0 == digits.length()) {
+            return new ArrayList<>();
+        }
+        char[] str = digits.toCharArray();
+        char[] path = new char[str.length];
+        List<String> ans = new ArrayList<>();
+        process(str, 0, path, ans);
+        return ans;
+    }
 
-	// str = ['2','3'] 3 3
-	// str[....index-1]，按出的结果是什么都在path里
-	// str[index...] 按完之后，有哪些组合，放入到ans里
-	public static void process(char[] str, int index, char[] path, List<String> ans) {
-		if (index == str.length) {
-			ans.add(new String(path));
-			return;
-		}
-		char[] w = map[str[index] - '0'];
-		for(char c : w) {
-			path[index]=c;
-			process(str, index+1, path,ans);
-		}
-	}
+    // 从i往后搞定所有的匹配
+    public static void process(char[] str, int i, char[] path, List<String> ans) {
+        if (i == str.length) {
+            ans.add(String.valueOf(path));
+            return;
+        }
+        char[] t = MAP[str[i] - '0'];
+        for (char c : t) {
+            path[i] = c;
+            process(str, i + 1, path, ans);
+        }
+    }
 
 }
