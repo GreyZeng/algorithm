36d2ef213af396666316b095cd0cfa32efb65a18
GreyZeng <410486047@qq.com>
2021-12-03 19:21:07 +0800

    count and say

--- src/main/java/leetcode/medium/LeetCode_0038_CountAndSay.java
+++ src/main/java/leetcode/medium/LeetCode_0038_CountAndSay.java
@@ -28,9 +28,10 @@ Output: "1211"
 Explanation: For n = 3 the term was "21" in which we have two groups "2" and "1", "2" can be read as "12" which means frequency = 1 and value = 2, the same way "1" is read as "11", so the answer is the concatenation of "12" and "11" which is "1211".
 */
 
-package leetcode;
+package leetcode.medium;
 
 // 测评链接：https://leetcode.com/problems/count-and-say/
+@Deprecated
 public class LeetCode_0038_CountAndSay {
 
 	public static String countAndSay(int n) {
