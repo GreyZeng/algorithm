67de545a34ab7a616d5091824b815256f1aa6526
zenghui <410486047@qq.com>
2021-12-03 14:38:13 +0800

    leetcode 49 group anagrams

--- src/main/java/leetcode/medium/LeetCode_0049_GroupAnagrams.java
+++ src/main/java/leetcode/medium/LeetCode_0049_GroupAnagrams.java
@@ -27,7 +27,7 @@
 //		来源：力扣（LeetCode）
 //		链接：https://leetcode-cn.com/problems/group-anagrams
 //		著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
-package leetcode;
+package leetcode.medium;
 
 import java.util.*;
 
