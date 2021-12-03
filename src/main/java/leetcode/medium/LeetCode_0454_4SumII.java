7458394b979b3ba3b7ac2c48bed026d2aca503bc
zenghui <410486047@qq.com>
2021-12-03 16:27:17 +0800

    leetcode 454 4sumii

--- src/main/java/leetcode/medium/LeetCode_0454_4SumII.java
+++ src/main/java/leetcode/medium/LeetCode_0454_4SumII.java
@@ -18,7 +18,7 @@
         The two tuples are:
         1. (0, 0, 0, 1) -> A[0] + B[0] + C[0] + D[1] = 1 + (-2) + (-1) + 2 = 0
         2. (1, 1, 0, 0) -> A[1] + B[1] + C[0] + D[0] = 2 + (-1) + (-1) + 0 = 0*/
-package leetcode;
+package leetcode.medium;
 
 
 import java.util.HashMap;
