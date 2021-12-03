fd7521325857da7e34fad8e1479a19ad801207b4
GreyZeng <410486047@qq.com>
2021-12-03 18:30:38 +0800

    leetcode 260 singlenum III

---
+++ src/main/java/leetcode/medium/LeetCode_0260_SingleNumberIII.java
@@ -0,0 +1,28 @@
+package leetcode.medium;
+
+/**
+ * @author Grey
+ * @date 2021年7月18日 下午11:53:15
+ * @since 1.8
+ */
+// https://www.cnblogs.com/greyzeng/p/15385402.html
+public class LeetCode_0260_SingleNumberIII {
+    public static int[] singleNumber(int[] arr) {
+        int eor = 0;
+        for (int n : arr) {
+            eor ^= n;
+        }
+        // 假设出现奇数次的两种数为 a和b
+        // eor = a ^ b
+        // 获取最右侧的1
+        int a = 0;
+        int rightOne = eor & ((~eor) + 1);
+        for (int n : arr) {
+            if ((n & rightOne) == 0) {
+                a ^= n;
+            }
+        }
+        int b = a ^ eor;
+        return new int[]{a, b};
+    }
+}
