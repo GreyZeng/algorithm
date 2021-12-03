8de434bc6293dca34321a9c02f45225e6ee43bb6
zenghui <410486047@qq.com>
2021-12-03 11:27:53 +0800

    remove nth node from end of list

--- src/main/java/leetcode/medium/LeetCode_0019_RemoveNthNodeFromEndofList.java
+++ src/main/java/leetcode/medium/LeetCode_0019_RemoveNthNodeFromEndofList.java
@@ -1,11 +1,13 @@
-package leetcode;
+package leetcode.medium;
 
 // note 数够n+1个，然后依次遍历到尾部
+// https://www.cnblogs.com/greyzeng/p/14675184.html
 public class LeetCode_0019_RemoveNthNodeFromEndofList {
 
     public static class ListNode {
         int val;
         ListNode next;
+
         ListNode(int val) {
             this.val = val;
         }
@@ -23,7 +25,7 @@ public class LeetCode_0019_RemoveNthNodeFromEndofList {
                 pre = pre.next;
             }
             cur = cur.next;
-        } 
+        }
         if (pre == null) {
             return head.next;
         }
