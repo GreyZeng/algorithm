9cc5c9424ca171e4897eafe3e6be2dbf7df436de
GreyZeng <410486047@qq.com>
2021-12-05 16:14:47 +0800

    leetcode 152 maximum product subarray

--- src/main/java/leetcode/medium/LeetCode_0152_MaximumProductSubarray.java
+++ src/main/java/leetcode/medium/LeetCode_0152_MaximumProductSubarray.java
@@ -1,4 +1,4 @@
-package leetcode;
+package leetcode.medium;
 
 //Given an integer array nums, find the contiguous subarray within an array (containing at least one number) which has the largest product.
 //
@@ -29,6 +29,4 @@ public class LeetCode_0152_MaximumProductSubarray {
         }
         return max;
     }
-
-
 }
