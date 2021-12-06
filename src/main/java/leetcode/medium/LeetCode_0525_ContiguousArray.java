023bee13587537769f4f4f683b7bf2782a55aa00
GreyZeng <410486047@qq.com>
2021-12-06 21:23:25 +0800

    contiguous array

--- src/main/java/leetcode/medium/LeetCode_0525_ContiguousArray.java
+++ src/main/java/leetcode/medium/LeetCode_0525_ContiguousArray.java
@@ -9,9 +9,10 @@
 //        Output: 2
 //        Explanation: [0, 1] (or [1, 0]) is a longest contiguous subarray with equal number of 0 and 1.
 //        Note: The length of the given binary array will not exceed 50,000.
-package leetcode;
+package leetcode.medium;
 
 import java.util.HashMap;
+import java.util.Map;
 
 // 预处理一下原数组，遇到0变为-1，遇到1保持1，遇到其他变为0
 // 求子数组之和为0的最大子数组长度
@@ -22,13 +23,10 @@ public class LeetCode_0525_ContiguousArray {
         for (; cur < N; cur++) {
             if (nums[cur] == 0) {
                 nums[cur] = -1;
-            } else if (nums[cur] == 1) {
-                nums[cur] = 1;
-            } else {
-                nums[cur] = 0;
             }
         }
-        HashMap<Integer, Integer> map = new HashMap<>(N);
+        // 累加和为0的最长数组长度
+        Map<Integer, Integer> map = new HashMap<>(N);
         map.put(0, -1);
         int sum = 0;
         int max = 0;
@@ -40,5 +38,10 @@ public class LeetCode_0525_ContiguousArray {
             max = Math.max(max, cur - map.get(sum));
         }
         return max;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {0, 1, 0, 1, 0, 1, 0};
+        System.out.println(findMaxLength(nums));
     }
 }
