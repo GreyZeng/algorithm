2edaf7fe850aa3aa695ed8550df6e7185ae1b2b4
GreyZeng <410486047@qq.com>
2021-12-06 19:45:00 +0800

    leetcode 128

--- src/main/java/leetcode/medium/LeetCode_0128_LongestConsecutiveSequence.java
+++ src/main/java/leetcode/medium/LeetCode_0128_LongestConsecutiveSequence.java
@@ -1,6 +1,7 @@
-package leetcode;
+package leetcode.medium;
 
 import java.util.HashMap;
+import java.util.Map;
 
 //Given an unsorted array of integers, find the length of the longest consecutive elements sequence.
 //
@@ -16,20 +17,22 @@ public class LeetCode_0128_LongestConsecutiveSequence {
     // Hash表标记开始位置和长度
     // 两个hash表 ，一个表示头<num, 数量> 一个表示尾<num, 数量>
     public static int longestConsecutive(int[] nums) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        int max = 0;
-        for (int num : nums) {
-            if (!map.containsKey(num)) {
-                map.put(num, 1);
-                int L = map.get(num - 1) == null ? 0 : map.get(num - 1);
-                int R = map.get(num + 1) == null ? 0 : map.get(num + 1);
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        Map<Integer, Integer> map = new HashMap<>();
+        int max = 1;
+        for (int n : nums) {
+            if (!map.containsKey(n)) {
+                map.put(n, 1);
+                int L = map.get(n - 1) == null ? 0 : map.get(n - 1);
+                int R = map.get(n + 1) == null ? 0 : map.get(n + 1);
                 int all = L + R + 1;
-                map.put(num - L, all);
-                map.put(num + R, all);
-                max = Math.max(max, all);
+                map.put(n - L, all);
+                map.put(n + R, all);
+                max = Math.max(all, max);
             }
         }
         return max;
     }
-
 }
