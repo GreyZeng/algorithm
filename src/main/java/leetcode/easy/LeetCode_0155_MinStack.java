4012afbfbc2102cd750e687a07173037df12abb8
GreyZeng <410486047@qq.com>
2021-12-17 23:10:45 +0800

    min stack o1

--- src/main/java/leetcode/easy/LeetCode_0155_MinStack.java
+++ src/main/java/leetcode/easy/LeetCode_0155_MinStack.java
@@ -2,8 +2,9 @@ package leetcode.easy;
 
 import java.util.Stack;
 
+// https://leetcode-cn.com/problems/min-stack
 public class LeetCode_0155_MinStack {
-    public class MinStack {
+    static class MinStack {
         Stack<Integer> m;
         Stack<Integer> n;
 
@@ -28,6 +29,48 @@ public class LeetCode_0155_MinStack {
 
         public int getMin() {
             return m.peek();
+        }
+    }
+
+    static class MinStackO1 {
+        private final long offset;
+        private Stack<Long> stack;
+
+        public MinStackO1() {
+            // 二进制：11111111111111111111111111111111
+            offset = 4294967295L;
+            stack = new Stack<>();
+        }
+
+        public boolean isEmpty() {
+            return stack.isEmpty();
+        }
+
+        public void push(int num) {
+            long left = ((long) (num) << 32);
+            long right;
+            if (isEmpty()) {
+                right = ((long) (num) & offset);
+            } else {
+                int min = Math.min(num, getMin());
+                right = ((long) (min) & offset);
+            }
+            stack.push((left | right));
+        }
+
+        public int pop() {
+            long out = stack.pop();
+            return (int) (out >>> 32);
+        }
+
+        public int top() {
+            long peek = stack.peek();
+            return (int) (peek >>> 32);
+        }
+
+        public int getMin() {
+            long peek = stack.peek();
+            return (int) (peek & offset);
         }
     }
 
