7a636f6999ac2529d853b2fd6f6004f396adaa11
GreyZeng <410486047@qq.com>
2021-12-26 19:16:06 +0800

    merge two sorted list

--- src/main/java/leetcode/easy/LeetCode_0088_MergeSortedArray.java
+++ src/main/java/leetcode/easy/LeetCode_0088_MergeSortedArray.java
@@ -24,16 +24,16 @@ public class LeetCode_0088_MergeSortedArray {
 
     // 从尾部开始处理
     public static void merge(int[] nums1, int m, int[] nums2, int n) {
-        int index = (m + n);
+        int len = m + n;
         while (m > 0 && n > 0) {
-            if (nums1[m - 1] >= nums2[n - 1]) {
-                nums1[--index] = nums1[--m];
+            if (nums1[m - 1] > nums2[n - 1]) {
+                nums1[--len] = nums1[--m];
             } else {
-                nums1[--index] = nums2[--n];
+                nums1[--len] = nums2[--n];
             }
         }
         while (n > 0) {
-            nums1[--index] = nums2[--n];
+            nums1[--len] = nums2[--n];
         }
     }
 }
