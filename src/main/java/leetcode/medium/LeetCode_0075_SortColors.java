6429efb8b6ee5ae07d712f60c6876c9ae9d02b13
zenghui <410486047@qq.com>
2021-12-29 16:29:29 +0800

    sort colors

--- src/main/java/leetcode/medium/LeetCode_0075_SortColors.java
+++ src/main/java/leetcode/medium/LeetCode_0075_SortColors.java
@@ -1,52 +1,31 @@
 package leetcode.medium;
 
-//Given an array with n objects colored red, white or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white and blue.
-//
-//        Here, we will use the integers 0, 1, and 2 to represent the color red, white, and blue respectively.
-//
-//        Note: You are not suppose to use the library's sort function for this problem.
-//
-//        Example:
-//
-//        Input: [2,0,2,1,1,0]
-//        Output: [0,0,1,1,2,2]
-//        Follow up:
-//
-//        A rather straight forward solution is a two-pass algorithm using counting sort.
-//        First, iterate the array counting number of 0's, 1's, and 2's, then overwrite array with total number of 0's, then 1's and followed by 2's.
-//        Could you come up with a one-pass algorithm using only constant space?
+// 荷兰国旗问题
+// 仅0，1，2，所有的0放左边，所有的1放中间，所有的2放右边
+//https://leetcode.com/problems/sort-colors/
 public class LeetCode_0075_SortColors {
-    // 荷兰国旗问题
+
     public static void sortColors(int[] nums) {
         int l = -1;
         int r = nums.length;
         int i = 0;
         while (i < r) {
-            if (nums[i] < 1) {
-                swap(nums, ++l, i++);
-            } else if (nums[i] > 1) {
-                swap(nums, --r, i);
+            if (nums[i] > 1) {
+                swap(nums, i, --r);
+            } else if (nums[i] < 1) {
+                swap(nums, i++, ++l);
             } else {
-                // nums[i] == 1
                 i++;
             }
         }
     }
 
-    public static void swap(int[] arr, int i, int j) {
-        if (i != j) {
-            arr[i] = arr[i] ^ arr[j];
-            arr[j] = arr[i] ^ arr[j];
-            arr[i] = arr[i] ^ arr[j];
+    private static void swap(int[] nums, int l, int r) {
+        if (l == r) {
+            return;
         }
+        nums[l] = nums[l] ^ nums[r];
+        nums[r] = nums[l] ^ nums[r];
+        nums[l] = nums[l] ^ nums[r];
     }
-
-    public static void main(String[] args) {
-        int[] nums = { 0, 1, 2, 1, 0, 2, 2, 0, 2, 1 };
-        sortColors(nums);
-        for (int num : nums) {
-            System.out.print(num + " ");
-        }
-    }
-
 }
