e6cf892780dd4862f6a26c2177e8c21425bf17f2
zenghui <410486047@qq.com>
2021-12-29 08:39:13 +0800

    leetcode 532 reverse pairs

--- src/main/java/leetcode/LeetCode_0493_ReversePairs.java
+++ src/main/java/leetcode/LeetCode_0493_ReversePairs.java
@@ -15,64 +15,64 @@ The length of the given array will not exceed 50,000.
 All the numbers in the input array are in the range of 32-bit integer.*/
 package leetcode;
 
-// TODO 方法1： 树状数组
-// 方法2： 归并排序
+// 方法1： 归并排序
+// TODO 方法2： 树状数组
 // 小和问题是一个数右边有多少个数比它自己大
 // 降序对问题就是求一个数右边有多少个数比它小，可以从右往左来算
 public class LeetCode_0493_ReversePairs {
 
-	public static int reversePairs(int[] A) {
-		if (null == A || A.length < 2) {
-			return 0;
-		}
-		int size = A.length;
-		return p(A, 0, size - 1);
-	}
+    public static int reversePairs(int[] A) {
+        if (null == A || A.length < 2) {
+            return 0;
+        }
+        int size = A.length;
+        return p(A, 0, size - 1);
+    }
 
-	public static int p(int[] A, int L, int R) {
-		if (L == R) {
-			return 0;
-		}
-		int mid = ((R - L) >> 1) + L;
-		return p(A, L, mid) + p(A, mid + 1, R) + merge(A, L, mid, R);
-	}
+    public static int p(int[] A, int L, int R) {
+        if (L == R) {
+            return 0;
+        }
+        int mid = ((R - L) >> 1) + L;
+        return p(A, L, mid) + p(A, mid + 1, R) + merge(A, L, mid, R);
+    }
 
-	public static int merge(int[] A, int l, int mid, int r) {
-		int s = l;
-		int e = mid + 1;
-		int pair = 0;
-		while (s <= mid && e <= r) {
-			if ((long) A[s] - (long) A[e] > (long) (A[e])) {
-				pair += (r - e + 1);
-				s++;
-			} else {
-				e++;
-			}
-		}
-		int[] helper = new int[r - l + 1];
-		int i = l;
-		int j = mid + 1;
-		int index = 0;
+    public static int merge(int[] A, int l, int mid, int r) {
+        int s = l;
+        int e = mid + 1;
+        int pair = 0;
+        while (s <= mid && e <= r) {
+            if ((long) A[s] - (long) A[e] > (long) (A[e])) {
+                pair += (r - e + 1);
+                s++;
+            } else {
+                e++;
+            }
+        }
+        int[] helper = new int[r - l + 1];
+        int i = l;
+        int j = mid + 1;
+        int index = 0;
 
-		while (i <= mid && j <= r) {
-			if (A[i] > A[j]) {
-				helper[index++] = A[i++];
-			} else {
-				helper[index++] = A[j++];
-			}
-		}
-		while (i <= mid) {
-			helper[index++] = A[i++];
-		}
-		while (j <= r) {
-			helper[index++] = A[j++];
-		}
-		int k = 0;
-		for (int num : helper) {
-			A[l + (k++)] = num;
-		}
+        while (i <= mid && j <= r) {
+            if (A[i] > A[j]) {
+                helper[index++] = A[i++];
+            } else {
+                helper[index++] = A[j++];
+            }
+        }
+        while (i <= mid) {
+            helper[index++] = A[i++];
+        }
+        while (j <= r) {
+            helper[index++] = A[j++];
+        }
+        int k = 0;
+        for (int num : helper) {
+            A[l + (k++)] = num;
+        }
 
-		return pair;
-	}
+        return pair;
+    }
 
 }
--- src/main/java/lintcode/LintCode_0532_ReversePairs.java
+++ src/main/java/lintcode/LintCode_0532_ReversePairs.java
@@ -6,70 +6,66 @@ package lintcode;
  * array, find out the total number of reverse order pairs in this array.
  * Summary: if a [i] > a [j] and i < j, a [i] and a [j] form a reverse order
  * pair.
- * 
+ * <p>
  * 样例
- * 
+ * <p>
  * Example1
- * 
+ * <p>
  * Input: A = [2, 4, 1, 3, 5] Output: 3 Explanation: (2, 1), (4, 1), (4, 3) are
  * reverse pairs
- * 
+ * <p>
  * Example2
- * 
+ * <p>
  * Input: A = [1, 2, 3, 4] Output: 0 Explanation: No reverse pair
- * 
+ *
  * @author Grey
  * @date 2021年7月22日 下午1:33:22
  * @since
  */
 public class LintCode_0532_ReversePairs {
-	public static void main(String[] args) {
-		int[] A = {2, 4, 1, 3, 5};
-		long result = reversePairs(A);
-		System.out.println(result);
-	}
-	public static long reversePairs(int[] A) {
-		if (null == A || A.length < 2) {
-			return 0;
-		}
-		int size = A.length;
-		return p(A, 0, size - 1);
-	}
 
-	public static long p(int[] A, int L, int R) {
-		if (L == R) {
-			return 0l;
-		}
-		int mid = ((R - L) >> 1) + L;
-		return p(A, L, mid) + p(A, mid + 1, R) + merge(A, L, mid, R);
-	}
 
-	public static long merge(int[] A, int l, int mid, int r) {
-		int[] help = new int[r - l + 1];
-		int s1 = l;
-		int s2 = mid + 1;
-		int index = 0;
-		long result = 0;
-		while (s1 <= mid && s2 <= r) {
-			if (A[s1] > A[s2]) {
-				result += (r - s2 + 1);
-				help[index++] = A[s1++];
-			} else if (A[s1] < A[s2]) {
-				help[index++] = A[s2++];
-			} else {
-				help[index++] = A[s2++];
-			}
-		}
-		while (s1 <= mid) {
-			help[index++] = A[s1++];
-		}
-		while (s2 <= r) {
-			help[index++] = A[s2++];
-		}
-		index = 0;
-		for (int v : help) {
-			A[l + (index++)] = v;
-		}
-		return result;
-	}
+    public static long reversePairs(int[] A) {
+        if (null == A || A.length < 2) {
+            return 0;
+        }
+        return process(A, 0, A.length - 1);
+    }
+
+    private static long process(int[] a, int l, int r) {
+        if (l == r) {
+            return 0L;
+        }
+        int m = l + ((r - l) >> 1);
+        return process(a, l, m) + process(a, m + 1, r) + merge(a, l, m, r);
+    }
+
+    private static long merge(int[] a, int l, int m, int r) {
+        int[] help = new int[r - l + 1];
+        int index = 0;
+        int s1 = l;
+        int s2 = m + 1;
+        long ans = 0L;
+        while (s1 <= m && s2 <= r) {
+            if (a[s1] < a[s2]) {
+                help[index++] = a[s2++];
+            } else if (a[s1] > a[s2]) {
+                ans += (r - s2 + 1);
+                help[index++] = a[s1++];
+            } else {
+                help[index++] = a[s2++];
+            }
+        }
+        while (s1 <= m) {
+            help[index++] = a[s1++];
+        }
+        while (s2 <= r) {
+            help[index++] = a[s2++];
+        }
+        index = 0;
+        for (int n : help) {
+            a[l + (index++)] = n;
+        }
+        return ans;
+    }
 }
