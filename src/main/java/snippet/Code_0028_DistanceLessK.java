642d4a2ee6e6314c1d5f1a2fcb89b0c98ffea0e2
GreyZeng <410486047@qq.com>
2022-01-05 23:04:47 +0800

    update

--- src/main/java/snippet/Code_0028_DistanceLessK.java
+++ src/main/java/snippet/Code_0028_DistanceLessK.java
@@ -4,10 +4,11 @@ import java.util.Arrays;
 import java.util.PriorityQueue;
 
 /**
- * 已知一个几乎有序的数组。几乎有序是指，如果把数组排好顺序的话，每个元素移动的距离一定不超过k，并且k相对于数组长度来说是比较小的。 
+ * 已知一个几乎有序的数组。几乎有序是指，如果把数组排好顺序的话，每个元素移动的距离一定不超过k，并且k相对于数组长度来说是比较小的。
  * 请选择一个合适的排序策略，对这个数组进行排序。
  * tips:
  * 加k个数进堆，然后再加入一个，弹出一个，最后堆里面剩下的继续弹出即可
+ * O(N*logK)
  */
 public class Code_0028_DistanceLessK {
     public static void sortedArrDistanceLessK(int[] arr, int k) {
