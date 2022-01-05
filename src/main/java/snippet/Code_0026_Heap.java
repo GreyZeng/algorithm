e9e9d5179ee15b4905b7f2ceeb19cb84ac155431
GreyZeng <410486047@qq.com>
2022-01-05 22:56:48 +0800

    heap and heap sort

--- src/main/java/snippet/Code_0026_Heap.java
+++ src/main/java/snippet/Code_0026_Heap.java
@@ -1,6 +1,12 @@
 package snippet;
 
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 /**
+ * 什么是完全二叉树
+ * 如果一个树是满的，它是完全二叉树，即便不是满的，也是从左到右依次变满的
+ * <p>
  * 堆结构
  * <p>
  * 1）堆结构就是用数组实现的完全二叉树结构
@@ -32,68 +38,12 @@ package snippet;
  * 右孩子 2 * i + 1 即：i << 1 | 1
  * <p>
  * 父节点 i / 2 即：i >> 1
+ * <p>
+ * 大根堆：完全二叉树中，每棵树的最大值都是头节点的值
+ *
+ * heapify和heapInsert都是logN级别的复杂度，因为N个节点的二叉树高度是logN
  */
 public class Code_0026_Heap {
-    public static class MyMinHeap {
-        public int[] values;
-        public int limit;
-        public int heapSize;
-
-        public MyMinHeap(int limit) {
-            this.limit = limit;
-            values = new int[limit];
-        }
-
-        public boolean isEmpty() {
-            return heapSize == 0;
-        }
-
-        public boolean isFull() {
-            return heapSize == limit;
-        }
-
-        public void push(int v) {
-            values[heapSize] = v;
-            heapInsert(heapSize++);
-        }
-
-        public int pop() {
-            int res = values[0];
-            swap(0, --heapSize);
-            heapify(0);
-            return res;
-        }
-
-        private void swap(int i, int j) {
-            int t = values[i];
-            values[i] = values[j];
-            values[j] = t;
-        }
-
-        // 自顶向下
-        private void heapify(int index) {
-            int leftChild = index * 2 + 1;
-            while (leftChild < heapSize) {
-                int smallest = leftChild + 1 < heapSize ? Math.min(values[leftChild], values[leftChild + 1]) : values[leftChild];
-                if (smallest >= values[index]) {
-                    break;
-                }
-                smallest = smallest == values[leftChild] ? leftChild : leftChild + 1;
-                swap(smallest, index);
-                index = smallest;
-                leftChild = index * 2 + 1;
-            }
-        }
-
-        // 自底向上
-        private void heapInsert(int index) {
-            while (values[(index - 1) / 2] > values[index]) {
-                swap((index - 1) / 2, index);
-                index = (index - 1) / 2;
-            }
-        }
-    }
-
 
     public static class MyMaxHeap {
         private int[] heap;
@@ -115,41 +65,40 @@ public class Code_0026_Heap {
         }
 
         public void push(int value) {
-            if (isFull()) {
+            if (heapSize == limit) {
                 throw new RuntimeException("heap is full");
             }
             heap[heapSize] = value;
+            // value  heapSize
             heapInsert(heap, heapSize++);
         }
 
         public int pop() {
-            if (isEmpty()) {
-                throw new RuntimeException("heap is empty");
-            }
-            int max = heap[0];
+            int ans = heap[0];
             swap(heap, 0, --heapSize);
             heapify(heap, 0, heapSize);
-            return max;
+            return ans;
         }
 
+
         private void heapInsert(int[] arr, int index) {
-            while (arr[(index - 1) / 2] < arr[index]) {
-                swap(arr, (index - 1) / 2, index);
+            while (arr[index] > arr[(index - 1) / 2]) {
+                swap(arr, index, (index - 1) / 2);
                 index = (index - 1) / 2;
             }
         }
 
         private void heapify(int[] arr, int index, int heapSize) {
-            int left = 2 * index + 1;
+            int left = index * 2 + 1;
             while (left < heapSize) {
-                int largest = left + 1 < heapSize ? Math.max(arr[left], arr[left + 1]) : arr[left];
-                if (arr[index] >= largest) {
+                int largest = left + 1 < heapSize && arr[left + 1] > arr[left] ? left + 1 : left;
+                largest = arr[largest] > arr[index] ? largest : index;
+                if (largest == index) {
                     break;
                 }
-                largest = (largest == arr[left]) ? left : left + 1;
-                swap(arr, index, largest);
+                swap(arr, largest, index);
                 index = largest;
-                left = 2 * index + 1;
+                left = index * 2 + 1;
             }
         }
 
@@ -158,30 +107,114 @@ public class Code_0026_Heap {
             arr[i] = arr[j];
             arr[j] = tmp;
         }
+
+    }
+
+    public static class RightMaxHeap {
+        private int[] arr;
+        private final int limit;
+        private int size;
+
+        public RightMaxHeap(int limit) {
+            arr = new int[limit];
+            this.limit = limit;
+            size = 0;
+        }
+
+        public boolean isEmpty() {
+            return size == 0;
+        }
+
+        public boolean isFull() {
+            return size == limit;
+        }
+
+        public void push(int value) {
+            if (size == limit) {
+                throw new RuntimeException("heap is full");
+            }
+            arr[size++] = value;
+        }
+
+        public int pop() {
+            int maxIndex = 0;
+            for (int i = 1; i < size; i++) {
+                if (arr[i] > arr[maxIndex]) {
+                    maxIndex = i;
+                }
+            }
+            int ans = arr[maxIndex];
+            arr[maxIndex] = arr[--size];
+            return ans;
+        }
+
+    }
+
+
+    public static class MyComparator implements Comparator<Integer> {
+
+        @Override
+        public int compare(Integer o1, Integer o2) {
+            return o2 - o1;
+        }
+
     }
 
     public static void main(String[] args) {
-        int limit = 5;
-        MyMinHeap heap = new MyMinHeap(limit);
-        heap.push(3);
-        heap.push(4);
-        heap.push(1);
-        heap.push(7);
-        heap.push(7);
-
-
-        MyMaxHeap maxHeap = new MyMaxHeap(limit);
-        maxHeap.push(heap.pop());
-        maxHeap.push(heap.pop());
-        maxHeap.push(heap.pop());
-        maxHeap.push(heap.pop());
-        maxHeap.push(heap.pop());
-
-
-        for (int i = 0; i < limit; i++) {
-            System.out.println(maxHeap.pop());
+        // 小根堆
+        PriorityQueue<Integer> heap = new PriorityQueue<>(new MyComparator());
+        heap.add(5);
+        heap.add(5);
+        heap.add(5);
+        heap.add(3);
+        //  5 , 3
+        System.out.println(heap.peek());
+        heap.add(7);
+        heap.add(0);
+        heap.add(7);
+        heap.add(0);
+        heap.add(7);
+        heap.add(0);
+        System.out.println(heap.peek());
+        while (!heap.isEmpty()) {
+            System.out.println(heap.poll());
         }
+        int value = 1000;
+        int limit = 100;
+        int testTimes = 1000000;
+        for (int i = 0; i < testTimes; i++) {
+            int curLimit = (int) (Math.random() * limit) + 1;
+            MyMaxHeap my = new MyMaxHeap(curLimit);
+            RightMaxHeap test = new RightMaxHeap(curLimit);
+            int curOpTimes = (int) (Math.random() * limit);
+            for (int j = 0; j < curOpTimes; j++) {
+                if (my.isEmpty() != test.isEmpty()) {
+                    System.out.println("Oops!");
+                }
+                if (my.isFull() != test.isFull()) {
+                    System.out.println("Oops!");
+                }
+                if (my.isEmpty()) {
+                    int curValue = (int) (Math.random() * value);
+                    my.push(curValue);
+                    test.push(curValue);
+                } else if (my.isFull()) {
+                    if (my.pop() != test.pop()) {
+                        System.out.println("Oops!");
+                    }
+                } else {
+                    if (Math.random() < 0.5) {
+                        int curValue = (int) (Math.random() * value);
+                        my.push(curValue);
+                        test.push(curValue);
+                    } else {
+                        if (my.pop() != test.pop()) {
+                            System.out.println("Oops!");
+                        }
+                    }
+                }
+            }
+        }
+        System.out.println("finish!");
     }
-
-
 }
