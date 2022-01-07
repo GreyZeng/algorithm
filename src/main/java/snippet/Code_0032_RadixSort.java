58cbaf0fbfd3b5af06d4622e1307c6afe96c2c8d
GreyZeng <410486047@qq.com>
2022-01-07 22:24:34 +0800

    add notes

--- src/main/java/snippet/Code_0032_RadixSort.java
+++ src/main/java/snippet/Code_0032_RadixSort.java
@@ -1,5 +1,7 @@
 package snippet;
 
+import java.util.Arrays;
+
 /**
  * 基数排序 一般来讲，基数排序要求，样本是10进制的正整数 1. 找到最大值，这个最大值是几位的 2. 其他数不足这个位数的，用0补齐 3.
  * 准备10个桶，每个桶是队列 4. 从个位依次进桶 5. 然后依次倒出 6. 根据十位数进桶 7. 依次倒出 ....... 桶排序思想下的排序：计数排序
@@ -13,62 +15,145 @@ package snippet;
  */
 public class Code_0032_RadixSort {
 
-    /**
-     * 只针对非负数
-     *
-     * @param arr
-     */
+    // only for no-negative value
     public static void radixSort(int[] arr) {
-        if (null == arr || arr.length < 1) {
+        if (arr == null || arr.length < 2) {
             return;
         }
-
-        int max = arr[0];
-        for (int i = 1; i < arr.length; i++) {
-            max = Math.max(arr[i], max);
-        }
-        int maxBit = getMaxBit(max);
-
-
-        int[] help = new int[arr.length];
-        for (int i = 1; i <= maxBit; i++) {
-            // 个位，十位，百位 这样的顺序求
-            int[] count = new int[10];
-            for (int j = 0; j < arr.length; j++) {
-                count[getBitValue(arr[j], i)]++;
-            }
-            for (int j = 1; j < 10; j++) {
-                count[j] += count[j - 1];
-            }
-            for (int j = arr.length - 1; j >= 0; j--) { 
-            	int d = getBitValue(arr[j],i);
-                help[count[d] - 1]= arr[j];
-                count[d]--;
-            }
-            for (int j = 0; j < arr.length; j++) {
-                arr[j] = help[j];
-            }
-        }
-    } 
-    public static int getBitValue(int x, int d) {
-		return ((x / ((int) Math.pow(10, d - 1))) % 10);
-	}
-
-    public static int getMaxBit(int num) {
-        int times = 1;
-        while (num >= 10) {
-            num /= 10;
-            times++;
-        }
-        return times;
+        radixSort(arr, 0, arr.length - 1, maxbits(arr));
     }
 
+    public static int maxbits(int[] arr) {
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            max = Math.max(max, arr[i]);
+        }
+        int res = 0;
+        while (max != 0) {
+            res++;
+            max /= 10;
+        }
+        return res;
+    }
+
+    // arr[L..R]排序  ,  最大值的十进制位数digit
+    public static void radixSort(int[] arr, int L, int R, int digit) {
+        final int radix = 10;
+        int i = 0, j = 0;
+        // 有多少个数准备多少个辅助空间
+        int[] help = new int[R - L + 1];
+        for (int d = 1; d <= digit; d++) { // 有多少位就进出几次
+            // 10个空间
+            // count[0] 当前位(d位)是0的数字有多少个
+            // count[1] 当前位(d位)是(0和1)的数字有多少个
+            // count[2] 当前位(d位)是(0、1和2)的数字有多少个
+            // count[i] 当前位(d位)是(0~i)的数字有多少个
+            int[] count = new int[radix]; // count[0..9]
+            for (i = L; i <= R; i++) {
+                // 103  1   3
+                // 209  1   9
+                j = getDigit(arr[i], d);
+                count[j]++;
+            }
+            for (i = 1; i < radix; i++) {
+                count[i] = count[i] + count[i - 1];
+            }
+            for (i = R; i >= L; i--) {
+                j = getDigit(arr[i], d);
+                help[count[j] - 1] = arr[i];
+                count[j]--;
+            }
+            for (i = L, j = 0; i <= R; i++, j++) {
+                arr[i] = help[j];
+            }
+        }
+    }
+
+    public static int getDigit(int x, int d) {
+        return ((x / ((int) Math.pow(10, d - 1))) % 10);
+    }
+
+    // for test
+    public static void comparator(int[] arr) {
+        Arrays.sort(arr);
+    }
+
+    // for test
+    public static int[] generateRandomArray(int maxSize, int maxValue) {
+        int[] arr = new int[(int) ((maxSize + 1) * Math.random())];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) ((maxValue + 1) * Math.random());
+        }
+        return arr;
+    }
+
+    // for test
+    public static int[] copyArray(int[] arr) {
+        if (arr == null) {
+            return null;
+        }
+        int[] res = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            res[i] = arr[i];
+        }
+        return res;
+    }
+
+    // for test
+    public static boolean isEqual(int[] arr1, int[] arr2) {
+        if ((arr1 == null && arr2 != null) || (arr1 != null && arr2 == null)) {
+            return false;
+        }
+        if (arr1 == null && arr2 == null) {
+            return true;
+        }
+        if (arr1.length != arr2.length) {
+            return false;
+        }
+        for (int i = 0; i < arr1.length; i++) {
+            if (arr1[i] != arr2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // for test
+    public static void printArray(int[] arr) {
+        if (arr == null) {
+            return;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    // for test
     public static void main(String[] args) {
-    	System.out.println(getBitValue(1034, 1));
-        System.out.println(getMaxBit(3));
-        System.out.println(getMaxBit(34));
-        System.out.println(getMaxBit(345));
-        System.out.println(getMaxBit(345));
+        int testTime = 500000;
+        int maxSize = 100;
+        int maxValue = 100000;
+        boolean succeed = true;
+        for (int i = 0; i < testTime; i++) {
+            int[] arr1 = generateRandomArray(maxSize, maxValue);
+            int[] arr2 = copyArray(arr1);
+            radixSort(arr1);
+            comparator(arr2);
+            if (!isEqual(arr1, arr2)) {
+                succeed = false;
+                printArray(arr1);
+                printArray(arr2);
+                break;
+            }
+        }
+        System.out.println(succeed ? "Nice!" : "Fucking fucked!");
+
+        int[] arr = generateRandomArray(maxSize, maxValue);
+        printArray(arr);
+        radixSort(arr);
+        printArray(arr);
+
     }
 
 }
