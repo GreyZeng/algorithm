f13aaaa580f5a0804a0bf6b39da304d4ddea5236
zenghui <410486047@qq.com>
2022-01-11 11:29:53 +0800

    update

--- src/main/java/lintcode/LintCode_1530_EncodeNaryTreeToBinaryTree.java
+++ src/main/java/lintcode/LintCode_1530_EncodeNaryTreeToBinaryTree.java
@@ -2,73 +2,76 @@ package lintcode;
 
 import java.util.ArrayList;
 import java.util.List;
-
+// leetcode 431
 // https://www.lintcode.com/problem/1530/
 // tips: 每个子节点转换成自己左树的右边界 + 深度优先遍历
 public class LintCode_1530_EncodeNaryTreeToBinaryTree {
-	class UndirectedGraphNode {
-		int label;
-		List<UndirectedGraphNode> neighbors;
+    class UndirectedGraphNode {
+        int label;
+        List<UndirectedGraphNode> neighbors;
 
-		UndirectedGraphNode(int x) {
-			label = x;
-			neighbors = new ArrayList<UndirectedGraphNode>();
-		}
-	}
+        UndirectedGraphNode(int x) {
+            label = x;
+            neighbors = new ArrayList<UndirectedGraphNode>();
+        }
+    }
 
-	public class TreeNode {
-		public int val;
-		public TreeNode left, right;
+    public class TreeNode {
+        public int val;
+        public TreeNode left, right;
 
-		public TreeNode(int val) {
-			this.val = val;
-			this.left = this.right = null;
-		}
-	}
+        public TreeNode(int val) {
+            this.val = val;
+            this.left = this.right = null;
+        }
+    }
 
-	public UndirectedGraphNode decode(TreeNode root) {
-		if (root == null) {
-			return null;
-		}
-		UndirectedGraphNode node =  new UndirectedGraphNode(root.val);
-		node.neighbors = de(root.left);
-		return node;
-	}
-	public ArrayList<UndirectedGraphNode> de(TreeNode root) {
-		ArrayList<UndirectedGraphNode> children = new ArrayList<>();
-		while (root != null) {
-			UndirectedGraphNode cur = new UndirectedGraphNode(root.val );
-			cur.neighbors = de(root.left);
-			children.add(cur);
-			root = root.right;
-		}
-		return children;
-	}
-	/**
-	 * @param root: N-ary tree
-	 * @return: binary tree
-	 */
-	public TreeNode encode(UndirectedGraphNode root) {
-		if (root == null) {
-			return null;
-		}
-		TreeNode head = new TreeNode(root.label);
-		head.left = en(root.neighbors);
-		return head;
-	}
-	private TreeNode en(List<UndirectedGraphNode> children) {
-		TreeNode head = null;
-		TreeNode cur = null;
-		for (UndirectedGraphNode child : children) {
-			TreeNode tNode = new TreeNode(child.label);
-			if (head == null) {
-				head = tNode;
-			} else {
-				cur.right = tNode;
-			}
-			cur = tNode;
-			cur.left = en(child.neighbors);
-		}
-		return head;
-	}
+    public UndirectedGraphNode decode(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        UndirectedGraphNode node = new UndirectedGraphNode(root.val);
+        node.neighbors = de(root.left);
+        return node;
+    }
+
+    public ArrayList<UndirectedGraphNode> de(TreeNode root) {
+        ArrayList<UndirectedGraphNode> children = new ArrayList<>();
+        while (root != null) {
+            UndirectedGraphNode cur = new UndirectedGraphNode(root.val);
+            cur.neighbors = de(root.left);
+            children.add(cur);
+            root = root.right;
+        }
+        return children;
+    }
+
+    /**
+     * @param root: N-ary tree
+     * @return: binary tree
+     */
+    public TreeNode encode(UndirectedGraphNode root) {
+        if (root == null) {
+            return null;
+        }
+        TreeNode head = new TreeNode(root.label);
+        head.left = en(root.neighbors);
+        return head;
+    }
+
+    private TreeNode en(List<UndirectedGraphNode> children) {
+        TreeNode head = null;
+        TreeNode cur = null;
+        for (UndirectedGraphNode child : children) {
+            TreeNode tNode = new TreeNode(child.label);
+            if (head == null) {
+                head = tNode;
+            } else {
+                cur.right = tNode;
+            }
+            cur = tNode;
+            cur.left = en(child.neighbors);
+        }
+        return head;
+    }
 }
