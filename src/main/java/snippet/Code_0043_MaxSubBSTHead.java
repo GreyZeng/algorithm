0b38003ecf261b6892f1645f21c5f960ec97f96f
zenghui <410486047@qq.com>
2022-01-13 11:35:43 +0800

    add todo

--- src/main/java/snippet/Code_0043_MaxSubBSTHead.java
+++ src/main/java/snippet/Code_0043_MaxSubBSTHead.java
@@ -55,56 +55,90 @@ public class Code_0043_MaxSubBSTHead {
         if (head == null) {
             return null;
         }
-        return process(head).maxSubBSTHead;
+        return p(head).maxSubBSTHead;
     }
 
-    // 每一棵子树
-    public static class Info {
-        public Node maxSubBSTHead;
-        public int maxSubBSTSize;
-        public int min;
-        public int max;
-
-        public Info(Node h, int size, int mi, int ma) {
-            maxSubBSTHead = h;
-            maxSubBSTSize = size;
-            min = mi;
-            max = ma;
-        }
-    }
-
-    public static Info process(Node X) {
-        if (X == null) {
+    public static Info p(Node head) {
+        if (head == null) {
             return null;
         }
-        Info leftInfo = process(X.left);
-        Info rightInfo = process(X.right);
-        int min = X.value;
-        int max = X.value;
-        Node maxSubBSTHead = null;
-        int maxSubBSTSize = 0;
-        if (leftInfo != null) {
-            min = Math.min(min, leftInfo.min);
-            max = Math.max(max, leftInfo.max);
-            maxSubBSTHead = leftInfo.maxSubBSTHead;
-            maxSubBSTSize = leftInfo.maxSubBSTSize;
-        }
-        if (rightInfo != null) {
-            min = Math.min(min, rightInfo.min);
-            max = Math.max(max, rightInfo.max);
-            if (rightInfo.maxSubBSTSize > maxSubBSTSize) {
-                maxSubBSTHead = rightInfo.maxSubBSTHead;
-                maxSubBSTSize = rightInfo.maxSubBSTSize;
+        Info left = p(head.left);
+        if (left == null) {
+            Info right = p(head.right);
+            if (right == null) {
+                return new Info(head, head.value, head.value);
             }
         }
-        if ((leftInfo == null || (leftInfo.maxSubBSTHead == X.left && leftInfo.max < X.value))
-                && (rightInfo == null || (rightInfo.maxSubBSTHead == X.right && rightInfo.min > X.value))) {
-            maxSubBSTHead = X;
-            maxSubBSTSize = (leftInfo == null ? 0 : leftInfo.maxSubBSTSize)
-                    + (rightInfo == null ? 0 : rightInfo.maxSubBSTSize) + 1;
-        }
-        return new Info(maxSubBSTHead, maxSubBSTSize, min, max);
+        // TODO
+        return null;
     }
+
+    public static class Info {
+        public Info(Node maxSubBSTHead, int max, int min) {
+            this.maxSubBSTHead = maxSubBSTHead;
+            this.max = max;
+            this.min = min;
+        }
+
+        private Node maxSubBSTHead;
+        private int max;
+        private int min;
+
+    }
+//    public static Node maxSubBSTHead2(Node head) {
+//        if (head == null) {
+//            return null;
+//        }
+//        return process(head).maxSubBSTHead;
+//    }
+//
+//    // 每一棵子树
+//    public static class Info {
+//        public Node maxSubBSTHead;
+//        public int maxSubBSTSize;
+//        public int min;
+//        public int max;
+//
+//        public Info(Node h, int size, int mi, int ma) {
+//            maxSubBSTHead = h;
+//            maxSubBSTSize = size;
+//            min = mi;
+//            max = ma;
+//        }
+//    }
+//
+//    public static Info process(Node X) {
+//        if (X == null) {
+//            return null;
+//        }
+//        Info leftInfo = process(X.left);
+//        Info rightInfo = process(X.right);
+//        int min = X.value;
+//        int max = X.value;
+//        Node maxSubBSTHead = null;
+//        int maxSubBSTSize = 0;
+//        if (leftInfo != null) {
+//            min = Math.min(min, leftInfo.min);
+//            max = Math.max(max, leftInfo.max);
+//            maxSubBSTHead = leftInfo.maxSubBSTHead;
+//            maxSubBSTSize = leftInfo.maxSubBSTSize;
+//        }
+//        if (rightInfo != null) {
+//            min = Math.min(min, rightInfo.min);
+//            max = Math.max(max, rightInfo.max);
+//            if (rightInfo.maxSubBSTSize > maxSubBSTSize) {
+//                maxSubBSTHead = rightInfo.maxSubBSTHead;
+//                maxSubBSTSize = rightInfo.maxSubBSTSize;
+//            }
+//        }
+//        if ((leftInfo == null || (leftInfo.maxSubBSTHead == X.left && leftInfo.max < X.value))
+//                && (rightInfo == null || (rightInfo.maxSubBSTHead == X.right && rightInfo.min > X.value))) {
+//            maxSubBSTHead = X;
+//            maxSubBSTSize = (leftInfo == null ? 0 : leftInfo.maxSubBSTSize)
+//                    + (rightInfo == null ? 0 : rightInfo.maxSubBSTSize) + 1;
+//        }
+//        return new Info(maxSubBSTHead, maxSubBSTSize, min, max);
+//    }
 
     // for test
     public static Node generateRandomBST(int maxLevel, int maxValue) {
