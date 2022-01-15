270ce68b6b79feca39c2f5624f06db9faf8d808d
zenghui <410486047@qq.com>
2022-01-15 12:05:23 +0800

    dfs

--- src/main/java/snippet/Code_0051_DFS.java
+++ src/main/java/snippet/Code_0051_DFS.java
@@ -1,9 +1,12 @@
 package snippet;
 
-import java.util.HashSet;
-import java.util.Stack;
-
 import snippet.graph.Node;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.Stack;
 
 /**
  * 深度优先遍历
@@ -15,32 +18,66 @@ import snippet.graph.Node;
  * 3，每弹出一个点，把该节点下一个没有进过栈的邻接点放入栈
  * <p>
  * 4，直到栈变空
+ * <p>
  */
 public class Code_0051_DFS {
-    public static void dfs(Node node) {
+    // 迭代版本
+    public static List<Node> dfs(Node node) {
         if (node == null) {
-            return;
+            return null;
         }
+        List<Node> ans = new ArrayList<>();
         Stack<Node> stack = new Stack<>();
         HashSet<Node> set = new HashSet<>();
-        System.out.println(node.value);
-        stack.push(node);
+        stack.add(node);
         set.add(node);
+        ans.add(node);
         while (!stack.isEmpty()) {
-            Node t = stack.pop();
-            for (Node n : t.nexts) {
-                if (!set.contains(n)) {
-                    set.add(n);
-                    stack.push(t);
-                    stack.push(n);
-                    System.out.println(n.value);
+            Node cur = stack.pop();
+            for (Node next : cur.nexts) {
+                if (!set.contains(next)) {
+                    stack.push(cur);
+                    stack.push(next);
+                    set.add(next);
+                    ans.add(next);
                     break;
+                }
+            }
+        }
+        return ans;
+    }
+
+    // 递归版本
+    public static List<Node> dfs2(Node node) {
+        if (node == null) {
+            return null;
+        }
+        List<Node> ans = new ArrayList<>();
+        Set<Node> set = new HashSet<>();
+        dfs(node, ans, set);
+        return ans;
+    }
+
+    private static void dfs(Node node, List<Node> ans, Set<Node> set) {
+        ans.add(node);
+        set.add(node);
+        if (node.nexts != null && !node.nexts.isEmpty()) {
+            for (Node n : node.nexts) {
+                if (!set.contains(n)) {
+                    dfs(n, ans, set);
                 }
             }
         }
     }
 
+
     public static void main(String[] args) {
+        test1();
+        System.out.println();
+        test2();
+    }
+
+    private static void test1() {
         Node t3 = new Node(3);
         Node t4 = new Node(4);
         Node t5 = new Node(5);
@@ -48,6 +85,7 @@ public class Code_0051_DFS {
         Node t7 = new Node(7);
         Node t8 = new Node(8);
         Node t9 = new Node(9);
+        Node t10 = new Node(10);
         t3.nexts.add(t6);
         t3.nexts.add(t5);
         t4.nexts.add(t7);
@@ -56,6 +94,38 @@ public class Code_0051_DFS {
         t5.nexts.add(t7);
         t6.nexts.add(t4);
         t7.nexts.add(t8);
-        dfs(t3);
+        t7.nexts.add(t9);
+        t9.nexts.add(t10);
+        t5.nexts.add(t10);
+        List<Node> dfs = dfs(t3);
+        for (Node n : dfs) {
+            System.out.print(n.value + " ");
+        }
+    }
+
+    private static void test2() {
+        Node t3 = new Node(3);
+        Node t4 = new Node(4);
+        Node t5 = new Node(5);
+        Node t6 = new Node(6);
+        Node t7 = new Node(7);
+        Node t8 = new Node(8);
+        Node t9 = new Node(9);
+        Node t10 = new Node(10);
+        t3.nexts.add(t6);
+        t3.nexts.add(t5);
+        t4.nexts.add(t7);
+        t4.nexts.add(t9);
+        t5.nexts.add(t8);
+        t5.nexts.add(t7);
+        t6.nexts.add(t4);
+        t7.nexts.add(t8);
+        t7.nexts.add(t9);
+        t9.nexts.add(t10);
+        t5.nexts.add(t10);
+        List<Node> dfs = dfs2(t3);
+        for (Node n : dfs) {
+            System.out.print(n.value + " ");
+        }
     }
 }
