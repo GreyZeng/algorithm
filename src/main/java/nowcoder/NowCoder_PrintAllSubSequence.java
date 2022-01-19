16b5a4bbbb4ce33ea5a07c0823ee2bd3b5066810
GreyZeng <410486047@qq.com>
2022-01-19 22:32:07 +0800

    update notes

--- src/main/java/nowcoder/NowCoder_PrintAllSubSequence.java
+++ src/main/java/nowcoder/NowCoder_PrintAllSubSequence.java
@@ -1,7 +1,6 @@
 package nowcoder;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 
 //打印一个字符串的全部子序列，包括空字符串
@@ -34,68 +33,4 @@ public class NowCoder_PrintAllSubSequence {
         p(str, i + 1, path, res);
         p(str, i + 1, path + str[i], res);
     }
-
-    public static List<String> printAllSubSequence(String s) {
-        if (s == null || s.length() < 1) {
-            return null;
-        }
-        char[] strs = s.toCharArray();
-        List<String> ans = new ArrayList<>();
-        process(strs, 0, ans, "");
-        return ans;
-    }
-
-    // 0...index - 1都已经做好了选择,现在从index开始做选择
-    // 所有收集的答案存在ans里面
-    public static void process(char[] strs, int index, List<String> ans, String path) {
-        if (index == strs.length) {
-            ans.add(path);
-            return;
-        }
-        // 要index位置的字符
-        process(strs, index + 1, ans, path + strs[index]);
-        // 不要index位置的字符
-        process(strs, index + 1, ans, path);
-    }
-
-    public static List<String> printAllSubSequenceNoRepeat(String s) {
-        if (s == null || s.length() < 1) {
-            return null;
-        }
-        char[] strs = s.toCharArray();
-        HashSet<String> set = new HashSet<>();
-        process2(strs, 0, set, "");
-        List<String> ans = new ArrayList<>();
-        for (String string : set) {
-            ans.add(string);
-        }
-        return ans;
-    }
-
-    public static void process2(char[] strs, int index, HashSet<String> ans, String path) {
-        if (index == strs.length) {
-            ans.add(path);
-            return;
-        }
-        // 要index位置的字符
-        process2(strs, index + 1, ans, path + strs[index]);
-        // 不要index位置的字符
-        process2(strs, index + 1, ans, path);
-    }
-
-    public static void main(String[] args) {
-        String str = "aabc";
-        List<String> ans2 = printAllSubSequence(str);
-        for (String ans : ans2) {
-            System.out.println(ans);
-        }
-
-        System.out.println("=====================");
-        List<String> ans3 = printAllSubSequenceNoRepeat(str);
-        for (String a : ans3) {
-            System.out.println(a);
-        }
-
-    }
-
 }
