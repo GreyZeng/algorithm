5b230794a49f957f175ee80f4e81670f6a1c9d20
GreyZeng <410486047@qq.com>
2022-01-28 19:42:01 +0800

    back pack

--- src/main/java/lintcode/medium/LintCode_0092_Backpack.java
+++ src/main/java/lintcode/medium/LintCode_0092_Backpack.java
@@ -30,94 +30,35 @@ public class LintCode_0092_Backpack {
         return rest >= arr[i] ? Math.max(arr[i] + p(rest - arr[i], i + 1, arr), p1) : p1;
     }
 
-    // 0 ~ i - 1 已经做好选择，还剩余j，从i开始做选择，能拿到的最大不超过M的重量是多少
-    public static int p(int m, List<Integer> notOver, int i, int j) {
-        if (i == notOver.size()) {
-            if (j == 0) {
-                return m;
-            } else {
-                return m - j;
-            }
-        }
-        if (j == 0) {
-            return m;
-        }
-
-        if (j - notOver.get(i) >= 0) {
-            return Math.max(p(m, notOver, i + 1, j), p(m, notOver, i + 1, j - notOver.get(i)));
-        }
-        return p(m, notOver, i + 1, j);
-    }
-
-    // 缓存
-    public static int backPack2(int m, int[] A) {
-        if (null == A || A.length == 0 || m == 0) {
+    public static int backPack2(int m, int[] arr) {
+        if (arr == null || arr.length < 1) {
             return 0;
         }
-        List<Integer> notOver = new ArrayList<>();
-        int sum = 0;
-        boolean over = true;
-        for (int a : A) {
-            if (a == m) {
-                // 如果有一个物品正好等于背包容量，直接返回
-                return m;
-            } else if (a < m) {
-                over = false;
-                notOver.add(a);
-                sum += a;
-            }
-        }
-        if (over) {
-            // 物品重量都大于背包重量，直接返回0
-            return 0;
-        }
-        if (sum <= m) {
-            // 能拿的货物之和的重量都没有超过m
-            // 直接返回货品重量之和
-            return sum;
-        }
-        int n = notOver.size();
-        int[][] dp = new int[n + 1][m + 1];
-        for (int i = 0; i < n + 1; i++) {
-            for (int j = 0; j < m + 1; j++) {
+        int[][] dp = new int[arr.length + 1][m + 1];
+        for (int i = 0; i < dp.length; i++) {
+            for (int j = 0; j < dp[0].length; j++) {
                 dp[i][j] = -1;
             }
         }
-        p2(m, notOver, 0, m, dp);
-        return dp[0][m];
+        return p2(m, 0, arr, dp);
     }
 
-    public static int p2(int m, List<Integer> notOver, int i, int rest, int[][] dp) {
+    // 还剩rest容量,得到的最大容量是多少
+    // 从i开始到最后，得到的最大容量是多少
+    public static int p2(int rest, int i, int[] arr, int[][] dp) {
         if (dp[i][rest] != -1) {
             return dp[i][rest];
         }
-        int ans;
-        if (i == notOver.size()) {
-            if (rest == 0) {
-                dp[i][0] = m;
-                ans = dp[i][0];
-            } else {
-                dp[i][rest] = m - rest;
-                ans = dp[i][rest];
-            }
+        int ans = 0;
+        if (i == arr.length) {
+            dp[i][rest] = ans;
             return ans;
         }
-
-        if (rest == 0) {
-            dp[i][0] = m;
-            ans = dp[i][0];
-            return ans;
-        }
-        dp[i][rest] = p2(m, notOver, i + 1, rest, dp);
-        ans = dp[i][rest];
-        if (rest - notOver.get(i) >= 0) {
-            dp[i][rest] = Math.max(ans, p2(m, notOver, i + 1, rest - notOver.get(i), dp));
-            ans = dp[i][rest];
-            return ans;
-        }
+        int p1 = p2(rest, i + 1, arr, dp);
+        ans = rest >= arr[i] ? Math.max(arr[i] + p2(rest - arr[i], i + 1, arr, dp), p1) : p1;
+        dp[i][rest] = ans;
         return ans;
     }
-
 
     // 动态规划
     public static int backPack3(int m, int[] A) {
