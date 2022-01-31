ca9ab144484ce4574d737c343fd7e69e2f96b5e8
GreyZeng <410486047@qq.com>
2022-01-31 22:09:08 +0800

    lcs

--- src/main/java/leetcode/medium/LeetCode_1143_LongestCommonSubsequence.java
+++ src/main/java/leetcode/medium/LeetCode_1143_LongestCommonSubsequence.java
@@ -1,76 +1,73 @@
-//Given two strings text1 and text2, return the length of their longest common subsequence.
-//
-//        A subsequence of a string is a new string generated from the original string with some characters(can be none) deleted without changing the relative order of the remaining characters. (eg, "ace" is a subsequence of "abcde" while "aec" is not). A common subsequence of two strings is a subsequence that is common to both strings.
-//
-//
-//
-//        If there is no common subsequence, return 0.
-//
-//
-//
-//        Example 1:
-//
-//        Input: text1 = "abcde", text2 = "ace"
-//        Output: 3
-//        Explanation: The longest common subsequence is "ace" and its length is 3.
-//        Example 2:
-//
-//        Input: text1 = "abc", text2 = "abc"
-//        Output: 3
-//        Explanation: The longest common subsequence is "abc" and its length is 3.
-//        Example 3:
-//
-//        Input: text1 = "abc", text2 = "def"
-//        Output: 0
-//        Explanation: There is no such common subsequence, so the result is 0.
-//
-//
-//        Constraints:
-//
-//        1 <= text1.length <= 1000
-//        1 <= text2.length <= 1000
-//        The input strings consist of lowercase English characters only.
 package leetcode.medium;
 
+// 最长公共子序列
+// 子序列是可以不连续的
 // https://leetcode-cn.com/problems/longest-common-subsequence/
 public class LeetCode_1143_LongestCommonSubsequence {
-    public int longestCommonSubsequence(String text1, String text2) {
-        if (text1 == null || text1.length() == 0) {
-            return 0;
-        }
-        if (text2 == null || text2.length() == 0) {
-            return 0;
-        }
-        char[] str1 = text1.toCharArray();
-        char[] str2 = text2.toCharArray();
-        int m = str1.length;
-        int n = str2.length;
-        int[][] dp = new int[m][n];
-        for (int i = 0; i < m; i++) {
-            if (str1[i] == str2[0]) {
-                dp[i][0] = 1;
-            }
-            if (i != 0 && dp[i - 1][0] == 1) {
-                dp[i][0] = 1;
-            }
-        }
-        for (int i = 1; i < n; i++) {
-            if (str1[0] == str2[i]) {
-                dp[0][i] = 1;
-            }
-            if (dp[0][i - 1] == 1) {
-                dp[0][i] = 1;
-            }
-        }
-        for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++) {
-                dp[i][j] = Math.max(Math.max(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1]);
-                if (str1[i] == str2[j]) {
-                    dp[i][j] = Math.max(dp[i][j], dp[i - 1][j - 1] + 1);
-                }
-            }
-        }
 
-        return dp[m - 1][n - 1];
+    public static int longestCommonSubsequence(String s1, String s2) {
+        if (s1 == null || s2 == null || s1.length() < 1 || s2.length() < 1) {
+            return 0;
+        }
+        char[] str1 = s1.toCharArray();
+        char[] str2 = s2.toCharArray();
+        int[][] dp = new int[s1.length()][s2.length()];
+        dp[0][0] = str1[0] == str2[0] ? 1 : 0;
+        for (int i = 1; i < s2.length(); i++) {
+            dp[0][i] = str1[0] == str2[i] ? 1 : dp[0][i - 1];
+        }
+        for (int i = 1; i < s1.length(); i++) {
+            dp[i][0] = str1[i] == str2[0] ? 1 : dp[i - 1][0];
+        }
+        for (int i = 1; i < str1.length; i++) {
+            for (int j = 1; j < str2.length; j++) {
+                int p1 = dp[i - 1][j];
+                // 最长公共子序列不要j位置
+                int p2 = dp[i][j - 1];
+                // 既要i位置，也要j位置(需要满足条件 str[i] == str[j])
+                // 既不要i位置，也不要j位置
+                int p3 = (str1[i] == str2[j] ? 1 : 0) + dp[i - 1][j - 1];
+                dp[i][j] = Math.max(p2, Math.max(p1, p3));
+            }
+        }
+        // 最长公共子序列不要i位置
+        return dp[s1.length() - 1][s2.length() - 1];
     }
+
+    public static int longestCommonSubsequence2(String s1, String s2) {
+        if (s1 == null || s2 == null || s1.length() < 1 || s2.length() < 1) {
+            return 0;
+        }
+        return process(s1.toCharArray(), s2.toCharArray(), s1.length() - 1, s2.length() - 1);
+    }
+
+    // str1 从0....i
+    // str2 从0....j
+    // 最长公共子序列是多少
+    public static int process(char[] str1, char[] str2, int i, int j) {
+        if (i == 0 && j == 0) {
+            return str1[i] == str2[j] ? 1 : 0;
+        }
+        if (i == 0) {
+            if (str1[i] == str2[j]) {
+                return 1;
+            }
+            return process(str1, str2, i, j - 1);
+        }
+        if (j == 0) {
+            if (str1[i] == str2[j]) {
+                return 1;
+            }
+            return process(str1, str2, i - 1, j);
+        }
+        // 最长公共子序列不要i位置
+        int p1 = process(str1, str2, i - 1, j);
+        // 最长公共子序列不要j位置
+        int p2 = process(str1, str2, i, j - 1);
+        // 既要i位置，也要j位置(需要满足条件 str[i] == str[j])
+        // 既不要i位置，也不要j位置
+        int p3 = (str1[i] == str2[j] ? 1 : 0) + process(str1, str2, i - 1, j - 1);
+        return Math.max(p2, Math.max(p1, p3));
+    }
+
 }
