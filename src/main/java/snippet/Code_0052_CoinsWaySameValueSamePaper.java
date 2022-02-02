e567ed513132a9c2b3f4794bb75a7842c8050b20
GreyZeng <410486047@qq.com>
2022-02-02 18:18:13 +0800

    update code

--- src/main/java/snippet/Code_0052_CoinsWaySameValueSamePaper.java
+++ src/main/java/snippet/Code_0052_CoinsWaySameValueSamePaper.java
@@ -15,7 +15,7 @@ public class Code_0052_CoinsWaySameValueSamePaper {
             return 1;
         }
         //统计每一张纸币出现的张数
-        Map<Integer, Integer> map = new HashMap<>();
+        Map<Integer, Integer> map = new HashMap<>(arr.length);
         for (int key : arr) {
             if (map.containsKey(key)) {
                 map.put(key, map.get(key) + 1);
@@ -25,7 +25,7 @@ public class Code_0052_CoinsWaySameValueSamePaper {
         }
         int size = map.size();
         int[] coins = new int[size];
-        int[] zhangs = new int[size];
+        int[] zhangs = new int[coins.length];
         for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
             coins[--size] = entry.getKey();
             zhangs[size] = entry.getValue();
@@ -34,8 +34,8 @@ public class Code_0052_CoinsWaySameValueSamePaper {
     }
 
     // i号硬币及其往后所有硬币自由组合，组成aim的方法数
-    public static int process(int i, int[] coins, int[] zhangs, int aim) {
-        if (aim == 0) {
+    public static int process(int i, int[] coins, int[] zhangs, int rest) {
+        if (rest == 0) {
             return 1;
         }
         // aim不为0
@@ -45,91 +45,104 @@ public class Code_0052_CoinsWaySameValueSamePaper {
         // aim不为0，且i没到头
         // 不选i号
         int ways = 0;
-        for (int index = 0; index <= zhangs[i] && coins[i] * index <= aim; index++) {
+        for (int index = 0; index <= zhangs[i] && coins[i] * index <= rest; index++) {
             // 必须选0张i位置的
-            ways += process(i + 1, coins, zhangs, aim - coins[i] * index);
+            ways += process(i + 1, coins, zhangs, rest - coins[i] * index);
         }
         return ways;
     }
 
-
-    public static class Info {
-        public int[] coins;
-        public int[] zhangs;
-
-        public Info(int[] c, int[] z) {
-            coins = c;
-            zhangs = z;
+    public static int coinsWay2(int[] arr, int aim) {
+        if (arr == null || arr.length == 0 || aim < 0) {
+            return 0;
         }
-    }
-
-    //
-    public static Info getInfo(int[] arr) {
-        HashMap<Integer, Integer> counts = new HashMap<>();
-        for (int value : arr) {
-            if (!counts.containsKey(value)) {
-                counts.put(value, 1);
+        if (aim == 0) {
+            return 1;
+        }
+        //统计每一张纸币出现的张数
+        Map<Integer, Integer> map = new HashMap<>(arr.length);
+        for (int key : arr) {
+            if (map.containsKey(key)) {
+                map.put(key, map.get(key) + 1);
             } else {
-                counts.put(value, counts.get(value) + 1);
+                map.put(key, 1);
             }
         }
-        int N = counts.size();
-        int[] coins = new int[N];
-        int[] zhangs = new int[N];
-        int index = 0;
-        for (Map.Entry<Integer, Integer> entry : counts.entrySet()) {
-            coins[index] = entry.getKey();
-            zhangs[index++] = entry.getValue();
+        int size = map.size();
+        int[] coins = new int[size];
+        int[] zhangs = new int[coins.length];
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            coins[--size] = entry.getKey();
+            zhangs[size] = entry.getValue();
         }
-        return new Info(coins, zhangs);
-    }
+        int[][] dp = new int[coins.length + 1][aim + 1];
 
-    public static int dp1(int[] arr, int aim) {
-        if (arr == null || arr.length == 0 || aim < 0) {
-            return 0;
+        // 第一列
+        for (int i = 0; i < coins.length + 1; i++) {
+            dp[i][0] = 1;
         }
-        Info info = getInfo(arr);
-        int[] coins = info.coins;
-        int[] zhangs = info.zhangs;
-        int N = coins.length;
-        int[][] dp = new int[N + 1][aim + 1];
-        dp[N][0] = 1;
-        for (int index = N - 1; index >= 0; index--) {
-            for (int rest = 0; rest <= aim; rest++) {
+        // 最后一行
+//        for (int i = 1; i < aim + 1; i++) {
+//            dp[coins.length][i] = 0;
+//        }
+        // 不选i号
+        for (int i = coins.length - 1; i >= 0; i--) {
+            for (int j = aim; j >= 0; j--) {
                 int ways = 0;
-                for (int zhang = 0; zhang * coins[index] <= rest && zhang <= zhangs[index]; zhang++) {
-                    ways += dp[index + 1][rest - (zhang * coins[index])];
+                // 枚举行为
+                for (int index = 0; index <= zhangs[i] && coins[i] * index <= j; index++) {
+                    // 必须选0张i位置的
+                    ways += dp[i + 1][j - coins[i] * index];
                 }
-                dp[index][rest] = ways;
+                dp[i][j] = ways;
             }
         }
         return dp[0][aim];
     }
 
-    public static int dp2(int[] arr, int aim) {
+    public static int coinsWay3(int[] arr, int aim) {
         if (arr == null || arr.length == 0 || aim < 0) {
             return 0;
         }
-        Info info = getInfo(arr);
-        int[] coins = info.coins;
-        int[] zhangs = info.zhangs;
-        int N = coins.length;
-        int[][] dp = new int[N + 1][aim + 1];
-        dp[N][0] = 1;
-        for (int index = N - 1; index >= 0; index--) {
-            for (int rest = 0; rest <= aim; rest++) {
-                dp[index][rest] = dp[index + 1][rest];
-                if (rest - coins[index] >= 0) {
-                    dp[index][rest] += dp[index][rest - coins[index]];
-                }
-                if (rest - coins[index] * (zhangs[index] + 1) >= 0) {
-                    dp[index][rest] -= dp[index + 1][rest - coins[index] * (zhangs[index] + 1)];
-                }
+        if (aim == 0) {
+            return 1;
+        }
+        //统计每一张纸币出现的张数
+        Map<Integer, Integer> map = new HashMap<>(arr.length);
+        for (int key : arr) {
+            if (map.containsKey(key)) {
+                map.put(key, map.get(key) + 1);
+            } else {
+                map.put(key, 1);
+            }
+        }
+        int size = map.size();
+        int[] coins = new int[size];
+        int[] zhangs = new int[coins.length];
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            coins[--size] = entry.getKey();
+            zhangs[size] = entry.getValue();
+        }
+        int[][] dp = new int[coins.length + 1][aim + 1];
+
+        // 第一列
+        for (int i = 0; i < coins.length + 1; i++) {
+            dp[i][0] = 1;
+        }
+        // 最后一行
+//        for (int i = 1; i < aim + 1; i++) {
+//            dp[coins.length][i] = 0;
+//        }
+        // 不选i号
+        for (int i = coins.length - 1; i >= 0; i--) {
+            for (int j = 0; j <= aim; j++) {
+                dp[i][j] = dp[i + 1][j] + (j >= coins[i] ? dp[i][j - coins[i]] : 0);
+                dp[i][j] -= (j - coins[i] * (zhangs[i] + 1) >= 0 ? dp[i + 1][j - coins[i] * (zhangs[i] + 1)] : 0);
             }
         }
         return dp[0][aim];
     }
-
+    
     // 为了测试
     public static int[] randomArray(int maxLen, int maxValue) {
         int N = (int) (Math.random() * maxLen);
@@ -158,8 +171,8 @@ public class Code_0052_CoinsWaySameValueSamePaper {
             int[] arr = randomArray(maxLen, maxValue);
             int aim = (int) (Math.random() * maxValue);
             int ans1 = coinsWay(arr, aim);
-            int ans2 = dp1(arr, aim);
-            int ans3 = dp2(arr, aim);
+            int ans2 = coinsWay2(arr, aim);
+            int ans3 = coinsWay3(arr, aim);
             if (ans1 != ans2 || ans1 != ans3) {
                 System.out.println("Oops!");
                 printArray(arr);
