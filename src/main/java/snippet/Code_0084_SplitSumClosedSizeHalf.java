1fb6e1f228e49342e95aa78c323a14622f2abee7
GreyZeng <410486047@qq.com>
2022-02-04 18:15:04 +0800

    update notes

--- src/main/java/snippet/Code_0084_SplitSumClosedSizeHalf.java
+++ src/main/java/snippet/Code_0084_SplitSumClosedSizeHalf.java
@@ -5,6 +5,7 @@ package snippet;
 //
 //       tips: 偶数个的时候，比如8，考虑4个的情况 奇数个的时候，比如7，要考虑4个和3个的情况
 public class Code_0084_SplitSumClosedSizeHalf {
+
     public static int right(int[] arr) {
         if (arr == null || arr.length < 2) {
             return 0;
@@ -16,7 +17,9 @@ public class Code_0084_SplitSumClosedSizeHalf {
         if ((arr.length & 1) == 0) {
             return process(arr, 0, arr.length / 2, sum / 2);
         } else {
-            return Math.max(process(arr, 0, arr.length / 2, sum / 2), process(arr, 0, arr.length / 2 + 1, sum / 2));
+            int[] newArr = new int[arr.length + 1];
+            System.arraycopy(arr, 0, newArr, 0, arr.length);
+            return process(newArr, 0, newArr.length / 2, sum / 2);
         }
     }
 
@@ -84,83 +87,6 @@ public class Code_0084_SplitSumClosedSizeHalf {
             return Math.max(dp[0][arr.length / 2][sum], dp[0][(arr.length / 2) + 1][sum]);
         }
     }
-
-//	public static int right(int[] arr) {
-//		if (arr == null || arr.length < 2) {
-//			return 0;
-//		}
-//		int sum = 0;
-//		for (int num : arr) {
-//			sum += num;
-//		}
-//		return process(arr, 0, 0, sum >> 1);
-//	}
-//
-//	public static int process(int[] arr, int i, int picks, int rest) {
-//		if (i == arr.length) {
-//			if ((arr.length & 1) == 0) {
-//				return picks == (arr.length >> 1) ? 0 : -1;
-//			} else {
-//				return (picks == (arr.length >> 1) || picks == (arr.length >> 1) + 1) ? 0 : -1;
-//			}
-//		}
-//		int p1 = process(arr, i + 1, picks, rest);
-//		int p2 = -1;
-//		int next2 = -1;
-//		if (arr[i] <= rest) {
-//			next2 = process(arr, i + 1, picks + 1, rest - arr[i]);
-//		}
-//		if (next2 != -1) {
-//			p2 = arr[i] + next2;
-//		}
-//		return Math.max(p1, p2);
-//	}
-//
-//	public static int dp1(int[] arr) {
-//		if (arr == null || arr.length < 2) {
-//			return 0;
-//		}
-//		int sum = 0;
-//		for (int num : arr) {
-//			sum += num;
-//		}
-//		sum >>= 1;
-//		int N = arr.length;
-//		int M = (arr.length + 1) >> 1;
-//		int[][][] dp = new int[N + 1][M + 1][sum + 1];
-//		for (int i = 0; i <= N; i++) {
-//			for (int j = 0; j <= M; j++) {
-//				for (int k = 0; k <= sum; k++) {
-//					dp[i][j][k] = -1;
-//				}
-//			}
-//		}
-//		for (int k = 0; k <= sum; k++) {
-//			dp[N][M][k] = 0;
-//		}
-//		if ((arr.length & 1) != 0) {
-//			for (int k = 0; k <= sum; k++) {
-//				dp[N][M - 1][k] = 0;
-//			}
-//		}
-//		for (int i = N - 1; i >= 0; i--) {
-//			for (int picks = 0; picks <= M; picks++) {
-//				for (int rest = 0; rest <= sum; rest++) {
-//					int p1 = dp[i + 1][picks][rest];
-//					int p2 = -1;
-//					int next2 = -1;
-//					if (picks + 1 <= M && arr[i] <= rest) {
-//						next2 = dp[i + 1][picks + 1][rest - arr[i]];
-//					}
-//					if (next2 != -1) {
-//						p2 = arr[i] + next2;
-//					}
-//					dp[i][picks][rest] = Math.max(p1, p2);
-//				}
-//			}
-//		}
-//		return dp[0][0][sum];
-//	}
 
     public static int dp2(int[] arr) {
         if (arr == null || arr.length < 2) {
