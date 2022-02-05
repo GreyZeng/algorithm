e3fd748d409b5dabc33750f45e1c90ee80772ac0
GreyZeng <410486047@qq.com>
2022-02-05 21:26:48 +0800

    n queens

--- src/main/java/snippet/Code_0086_NQueens.java
+++ src/main/java/snippet/Code_0086_NQueens.java
@@ -13,46 +13,38 @@ public class Code_0086_NQueens {
         if (n < 1) {
             return 0;
         }
-        // 编号i从0开始，一直到n-1
-        // record[i] = j 表示：i号皇后放在了第j列上
-        int[] record = new int[n];
-        return process1(0, record, n);
+        if (n == 2 || n == 3) {
+            return 0;
+        }
+        int[] records = new int[n];
+        return process1(0, records, n);
     }
 
-    // 当前来到i行，一共是0~N-1行
-    // 在i行上放皇后，所有列都尝试
-    // 必须要保证跟之前所有的皇后不打架
-    // int[] record record[x] = y 之前的第x行的皇后，放在了y列上
-    // 返回：不关心i以上发生了什么，i.... 后续有多少合法的方法数
-    public static int process1(int i, int[] record, int n) {
+    public static int process1(int i, int[] records, int n) {
         if (i == n) {
-            // 居然可以顺利填到最后，返回一种方法数
             return 1;
         }
-        int res = 0;
-        // i行的皇后，放哪一列呢？j列，
+        int ways = 0;
         for (int j = 0; j < n; j++) {
-            if (isValid(record, i, j)) {
-                // 无需回溯
-                // 因为i只和之前的皇后有关系
-                record[i] = j;
-                res += process1(i + 1, record, n);
+            if (isValid(records, i, j)) {
+                records[i] = j;
+                ways += process1(i + 1, records, n);
             }
         }
-        return res;
+        return ways;
     }
 
-    public static boolean isValid(int[] record, int i, int j) {
-        // 0..i-1
-        for (int k = 0; k < i; k++) {
-            if (j == record[k] || Math.abs(record[k] - j) == Math.abs(i - k)) {
+    public static boolean isValid(int[] records, int i, int j) {
+        for (int s = 0; s < i; s++) {
+            if (records[s] == j || Math.abs(records[s] - j) == Math.abs(i - s)) {
                 return false;
             }
         }
         return true;
     }
 
-    // 请不要超过32皇后问题
+    // TODO
+    //  请不要超过32皇后问题
     public static int num2(int n) {
         if (n < 1 || n > 32) {
             return 0;
