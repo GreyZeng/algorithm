2cd37381f73a6a31fa1f9092aac544246ebc87ea
GreyZeng <410486047@qq.com>
2022-02-12 19:13:55 +0800

    mono stack

---
+++ src/main/java/snippet/Code_0088_MonoStack.java
@@ -0,0 +1,167 @@
+package snippet;
+
+
+import java.util.*;
+// https://www.nowcoder.com/questionTerminal/e3d18ffab9c543da8704ede8da578b55
+// https://www.nowcoder.com/questionTerminal/2a2c00e7a88a498693568cef63a4b7bb
+/**
+ * 单调栈
+ * 左边右边离它最近比它小的数 O（N）
+ * 数组中有重复 Stack<List<Integer>>
+ * 数组中无重复 Stack<Integer>
+ * 栈底到栈顶从小到大
+ * 弹出的时候，假设弹出的值是A，那么让它弹出的值就是它右边离它最近的最小值
+ * 原先A压的是谁，那么谁就是A左边离它最近的最小值
+ */
+public class Code_0088_MonoStack {
+    public static int[][] getNearLessNoRepeat(int[] arr) {
+        int[][] res = new int[arr.length][2];
+        // 只存位置！
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < arr.length; i++) { // 当遍历到i位置的数，arr[i]
+            while (!stack.isEmpty() && arr[stack.peek()] > arr[i]) {
+                int j = stack.pop();
+                int leftLessIndex = stack.isEmpty() ? -1 : stack.peek();
+                res[j][0] = leftLessIndex;
+                res[j][1] = i;
+            }
+            stack.push(i);
+        }
+        while (!stack.isEmpty()) {
+            int j = stack.pop();
+            int leftLessIndex = stack.isEmpty() ? -1 : stack.peek();
+            res[j][0] = leftLessIndex;
+            res[j][1] = -1;
+        }
+        return res;
+    }
+
+    public static int[][] getNearLess(int[] arr) {
+        int[][] res = new int[arr.length][2];
+        Stack<List<Integer>> stack = new Stack<>();
+        for (int i = 0; i < arr.length; i++) { // i -> arr[i] 进栈
+            while (!stack.isEmpty() && arr[stack.peek().get(0)] > arr[i]) {
+                List<Integer> popIs = stack.pop();
+                int leftLessIndex = stack.isEmpty() ? -1 : stack.peek().get(stack.peek().size() - 1);
+                for (Integer popi : popIs) {
+                    res[popi][0] = leftLessIndex;
+                    res[popi][1] = i;
+                }
+            }
+            if (!stack.isEmpty() && arr[stack.peek().get(0)] == arr[i]) {
+                stack.peek().add(Integer.valueOf(i));
+            } else {
+                ArrayList<Integer> list = new ArrayList<>();
+                list.add(i);
+                stack.push(list);
+            }
+        }
+        while (!stack.isEmpty()) {
+            List<Integer> popIs = stack.pop();
+            int leftLessIndex = stack.isEmpty() ? -1 : stack.peek().get(stack.peek().size() - 1);
+            for (Integer popi : popIs) {
+                res[popi][0] = leftLessIndex;
+                res[popi][1] = -1;
+            }
+        }
+        return res;
+    }
+
+    // for test
+    public static int[] getRandomArrayNoRepeat(int size) {
+        int[] arr = new int[(int) (Math.random() * size) + 1];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            int swapIndex = (int) (Math.random() * arr.length);
+            int tmp = arr[swapIndex];
+            arr[swapIndex] = arr[i];
+            arr[i] = tmp;
+        }
+        return arr;
+    }
+
+    // for test
+    public static int[] getRandomArray(int size, int max) {
+        int[] arr = new int[(int) (Math.random() * size) + 1];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) (Math.random() * max) - (int) (Math.random() * max);
+        }
+        return arr;
+    }
+
+    // for test
+    public static int[][] rightWay(int[] arr) {
+        int[][] res = new int[arr.length][2];
+        for (int i = 0; i < arr.length; i++) {
+            int leftLessIndex = -1;
+            int rightLessIndex = -1;
+            int cur = i - 1;
+            while (cur >= 0) {
+                if (arr[cur] < arr[i]) {
+                    leftLessIndex = cur;
+                    break;
+                }
+                cur--;
+            }
+            cur = i + 1;
+            while (cur < arr.length) {
+                if (arr[cur] < arr[i]) {
+                    rightLessIndex = cur;
+                    break;
+                }
+                cur++;
+            }
+            res[i][0] = leftLessIndex;
+            res[i][1] = rightLessIndex;
+        }
+        return res;
+    }
+
+    // for test
+    public static boolean isEqual(int[][] res1, int[][] res2) {
+        if (res1.length != res2.length) {
+            return false;
+        }
+        for (int i = 0; i < res1.length; i++) {
+            if (res1[i][0] != res2[i][0] || res1[i][1] != res2[i][1]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    // for test
+    public static void printArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int size = 10;
+        int max = 20;
+        int testTimes = 2000000;
+        System.out.println("测试开始");
+        for (int i = 0; i < testTimes; i++) {
+            int[] arr1 = getRandomArrayNoRepeat(size);
+            int[] arr2 = getRandomArray(size, max);
+            if (!isEqual(getNearLessNoRepeat(arr1), rightWay(arr1))) {
+                System.out.println("Oops!");
+                printArray(arr1);
+                break;
+            }
+            if (!isEqual(getNearLess(arr2), rightWay(arr2))) {
+                System.out.println("Oops!");
+                printArray(arr2);
+                break;
+            }
+        }
+        System.out.println("测试结束");
+    }
+
+
+}
