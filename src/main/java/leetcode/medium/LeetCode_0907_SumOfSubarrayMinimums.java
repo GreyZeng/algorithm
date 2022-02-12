ccf509b6d5e688a8a048f22ffa10f95e634f218c
GreyZeng <410486047@qq.com>
2022-02-12 21:00:41 +0800

    update mono stack

--- src/main/java/leetcode/medium/LeetCode_0907_SumOfSubarrayMinimums.java
+++ src/main/java/leetcode/medium/LeetCode_0907_SumOfSubarrayMinimums.java
@@ -38,40 +38,40 @@
 //
 //        1 <= arr.length <= 3 * 10^4
 //        1 <= arr[i] <= 3 * 10^4
-package leetcode;
+package leetcode.medium;
 
 import java.util.Stack;
 
 // 单调栈
 public class LeetCode_0907_SumOfSubarrayMinimums {
 
-	public static int sumSubarrayMins(int[] arr) {
-		int MOD = (int) 1e9 + 7;
-		long max = 0;
-		Stack<Integer> stack = new Stack<>();
-		for (int i = 0; i < arr.length; i++) {
-			while (!stack.isEmpty() && arr[stack.peek()] >= arr[i]) {
-				int v = stack.pop();
-				int left = stack.isEmpty() ? -1 : stack.peek();
-				int right = i;
-				long times = (v - left) * (right - v);
-				max += arr[v] * times;
+    public static int sumSubarrayMins(int[] arr) {
+        int MOD = (int) 1e9 + 7;
+        long max = 0;
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < arr.length; i++) {
+            while (!stack.isEmpty() && arr[stack.peek()] >= arr[i]) {
+                int v = stack.pop();
+                int left = stack.isEmpty() ? -1 : stack.peek();
+                int right = i;
+                long times = (v - left) * (right - v);
+                max += arr[v] * times;
 
-			}
-			stack.push(i);
-		}
-		while (!stack.isEmpty()) {
-			int v = stack.pop();
-			int left = stack.isEmpty() ? -1 : stack.peek();
-			int right = arr.length;
-			long times = (v - left) * (right - v);
-			max += arr[v] * times;
-		}
-		return (int) (max % MOD);
-	}
+            }
+            stack.push(i);
+        }
+        while (!stack.isEmpty()) {
+            int v = stack.pop();
+            int left = stack.isEmpty() ? -1 : stack.peek();
+            int right = arr.length;
+            long times = (v - left) * (right - v);
+            max += arr[v] * times;
+        }
+        return (int) (max % MOD);
+    }
 
-	public static void main(String[] args) {
-		int[] nums = { 11, 81, 94, 43 };
-		System.out.println(sumSubarrayMins(nums));
-	}
+    public static void main(String[] args) {
+        int[] nums = {11, 81, 94, 43};
+        System.out.println(sumSubarrayMins(nums));
+    }
 }
