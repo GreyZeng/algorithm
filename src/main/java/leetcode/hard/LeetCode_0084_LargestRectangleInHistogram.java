5f8693c58b5e0d66821bb285a39811faa84e7db6
GreyZeng <410486047@qq.com>
2022-02-13 17:11:33 +0800

    update code

--- src/main/java/leetcode/hard/LeetCode_0084_LargestRectangleInHistogram.java
+++ src/main/java/leetcode/hard/LeetCode_0084_LargestRectangleInHistogram.java
@@ -16,39 +16,33 @@ import java.util.Stack;
 // https://leetcode-cn.com/problems/largest-rectangle-in-histogram/
 // https://www.lintcode.com/problem/122/
 public class LeetCode_0084_LargestRectangleInHistogram {
-
-    // 单调栈
-    // 每个位置做高的矩形
-    // 找到i离你最近的比你小的位置在哪里？
-    public static int largestRectangleArea(int[] heights) {
-        if (null == heights || heights.length == 0) {
+    public static int largestRectangleArea(int[] arr) {
+        if (arr == null || arr.length < 1) {
             return 0;
         }
-        int n = heights.length;
-        if (n == 1) {
-            return heights[0];
+        if (arr.length == 1) {
+            return arr[0];
         }
+        // 可以用数组替代Stack
         Stack<Integer> stack = new Stack<>();
-        int max = 0;
-        for (int i = 0; i < n; i++) {
-            while (!stack.isEmpty() && heights[stack.peek()] >= heights[i]) {
-                int v = stack.pop();
+        int ans = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            while (!stack.isEmpty() && arr[stack.peek()] >= arr[i]) {
+                int popIndex = stack.pop();
+                // 结算
                 int left = stack.isEmpty() ? -1 : stack.peek();
                 int right = i;
-                int area = (right - left - 1) * heights[v];
-                // System.out.println("值：" + heights[v] + " 左边极限"+ left + "右边极限"+right);
-                max = Math.max(max, area);
+                ans = Math.max(ans, arr[popIndex] * (right - left - 1));
             }
             stack.push(i);
         }
         while (!stack.isEmpty()) {
-            int v = stack.pop();
+            int popIndex = stack.pop();
+            // 结算
+            int right = arr.length;
             int left = stack.isEmpty() ? -1 : stack.peek();
-            int right = heights.length;
-            int area = (right - left - 1) * heights[v];
-            max = Math.max(max, area);
-            // System.out.println("值：" + heights[v] + " 左边极限"+ left + "右边极限"+right);
+            ans = Math.max(ans, arr[popIndex] * (right - left - 1));
         }
-        return max;
+        return ans;
     }
 }
