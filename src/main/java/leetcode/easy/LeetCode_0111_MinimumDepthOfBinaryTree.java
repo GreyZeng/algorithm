938094b5474a535a5e9b0db0bbda81428ad8ec71
GreyZeng <410486047@qq.com>
2022-02-22 13:26:47 +0800

    update code

--- src/main/java/leetcode/easy/LeetCode_0111_MinimumDepthOfBinaryTree.java
+++ src/main/java/leetcode/easy/LeetCode_0111_MinimumDepthOfBinaryTree.java
@@ -87,35 +87,4 @@ public class LeetCode_0111_MinimumDepthOfBinaryTree {
         }
         return min;
     }
-
-//    public static int minDepth2(TreeNode head) {
-//        if (head == null) {
-//            return 0;
-//        }
-//        return p(head).minH;
-//    }
-//
-//    public static Info p(TreeNode head) {
-//        if (head == null) {
-//            return new Info(0);
-//        }
-//        if (head.left == null && head.right == null) {
-//            return new Info(1);
-//        }
-//        if (head.left == null) {
-//            return new Info(p(head.right).minH + 1);
-//        }
-//        if (head.right == null) {
-//            return new Info(p(head.left).minH + 1);
-//        }
-//        return new Info(Math.min(p(head.left).minH, p(head.right).minH) + 1);
-//    }
-//
-//    public static class Info {
-//        public int minH;
-//
-//        public Info(int minH) {
-//            this.minH = minH;
-//        }
-//    }
 }
