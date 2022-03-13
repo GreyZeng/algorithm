543b34f2451d6ef3329db5f90b38df4a602e98f0
GreyZeng <410486047@qq.com>
2022-03-13 10:34:18 +0800

    update leetcode 48 rotate image

--- src/main/java/leetcode/medium/LeetCode_0048_RotateImage.java
+++ src/main/java/leetcode/medium/LeetCode_0048_RotateImage.java
@@ -1,32 +1,44 @@
 package leetcode.medium;
+
 // 给定一个正方形矩阵matrix，原地调整成顺时针90度转动的样子
+// 先处理外围的圈 然后同理依次处理每个内圈
+// 每个圈分组，组内每次一个元素占据下一个元素的位置（N*N）就分（N-1）*（N-1）组
+// 每个组的每个数可以通过组号来定位
 // https://leetcode-cn.com/problems/rotate-image/
 // https://www.lintcode.com/problem/rotate-image/description
 public class LeetCode_0048_RotateImage {
-
-    // 先处理外围的圈 然后同理依次处理每个内圈
-    // 每个圈分组，组内每次一个元素占据下一个元素的位置（N*N）就分（N-1）*（N-1）组
-    // 每个组的每个数可以通过组号来定位
+    // matrix需要保证是正方形
     public static void rotate(int[][] matrix) {
-        int M = matrix.length;
-        int LTx = 0;
-        int LTy = 0;
-        int RBx = M - 1;
-        int RBy = M - 1;
-        while (LTx <= RBx) {
-            rotate(M, matrix, LTx++, LTy++, RBx--, RBy--);
-            M -= 2;
+        int n = matrix.length;
+        int zuoshangX = 0;
+        int zuoshangY = 0;
+        int youxiaX = n - 1;
+        int youxiaY = n - 1;
+        while (n > 0) {
+            rotate(n, matrix, zuoshangX++, zuoshangY++, youxiaX--, youxiaY--);
+            n -= 2;
         }
     }
 
-    private static void rotate(int M, int[][] matrix, int LTx, int LTy, int RBx, int RBy) {
-        int f;
-        for (int i = 0; i < M - 1; i++) {
-            f = matrix[LTx][LTy + i];
-            matrix[LTx][LTy + i] = matrix[RBx - i][LTy];
-            matrix[RBx - i][LTy] = matrix[RBx][RBy - i];
-            matrix[RBx][RBy - i] = matrix[LTx + i][RBy];
-            matrix[LTx + i][RBy] = f;
+    private static void rotate(int n, int[][] matrix, int zuoshangX, int zuoshangY, int youxiaX, int youxiaY) {
+        int zu = n - 1;
+        int youshangX = zuoshangX;
+        int youshangY = youxiaY;
+        int zuoxiaX = youxiaX;
+        int zuoxiaY = zuoshangY;
+        for (int i = 1; i <= zu; i++) {
+            // 每组内部调整
+            int tmp = matrix[zuoshangX][zuoshangY];
+            matrix[zuoshangX][zuoshangY++] = matrix[zuoxiaX][zuoxiaY];
+            matrix[zuoxiaX--][zuoxiaY] = matrix[youxiaX][youxiaY];
+            matrix[youxiaX][youxiaY--] = matrix[youshangX][youshangY];
+            matrix[youshangX++][youshangY] = tmp;
         }
+    }
+
+    public static void main(String[] args) {
+        int[][] m = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        rotate(m);
+        System.out.println("--");
     }
 }
