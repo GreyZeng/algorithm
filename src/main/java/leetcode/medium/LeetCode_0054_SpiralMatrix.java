fd068c95268afac554c91ba538a8761020523325
GreyZeng <410486047@qq.com>
2022-03-13 14:20:12 +0800

    spiral matrix

--- src/main/java/leetcode/medium/LeetCode_0054_SpiralMatrix.java
+++ src/main/java/leetcode/medium/LeetCode_0054_SpiralMatrix.java
@@ -3,83 +3,55 @@ package leetcode.medium;
 import java.util.ArrayList;
 import java.util.List;
 
-//
-//Given a matrix of m x n elements (m rows, n columns), return all elements of the matrix in spiral order.
-//
-//        Example 1:
-//
-//        Input:
-//        [
-//        [ 1, 2, 3 ],
-//        [ 4, 5, 6 ],
-//        [ 7, 8, 9 ]
-//        ]
-//        Output: [1,2,3,6,9,8,7,4,5]
-//        Example 2:
-//
-//        Input:
-//        [
-//        [1, 2, 3, 4],
-//        [5, 6, 7, 8],
-//        [9,10,11,12]
-//        ]
-//        Output: [1,2,3,4,8,12,11,10,9,5,6,7]
+// https://www.lintcode.com/problem/spiral-matrix/description
+// https://leetcode-cn.com/problems/spiral-matrix/
+//tips：
+//先打印外围圈圈,然后切换到内圈，用同样的方式打印内圈，依次循环
+//注意最后有可能是一条直线（横线竖线另外处理）
 public class LeetCode_0054_SpiralMatrix {
-
-    //有可能最内层是一条直线或者单点，或者还是一个长方形
     public static List<Integer> spiralOrder(int[][] matrix) {
-        if (null == matrix) {
-            return null;
-        }
-        int M = matrix.length;
-        if (M == 0) {
+        if (null == matrix || matrix.length == 0 || matrix[0].length == 0) {
             return new ArrayList<>();
         }
-        int N = matrix[0].length;
-        // 左上角坐标
-        int ltx = 0;
-        int lty = 0;
-        // 右下角坐标
-        int rbx = M - 1;
-        int rby = N - 1;
-        List<Integer> list = new ArrayList<>();
-        while (lty <= rby && ltx <= rbx) {
-            spiralOrder(matrix, ltx++, lty++, rbx--, rby--, list);
+        int m = matrix.length;
+        int n = matrix[0].length;
+        // 左上角点
+        int a = 0, b = 0;
+        // 右下角点
+        int c = m - 1, d = n - 1;
+        List<Integer> ans = new ArrayList<>();
+        while (a <= c && b <= d) {
+            spiral(matrix, a++, b++, c--, d--, ans);
         }
-        return list;
+        return ans;
     }
 
-    private static void spiralOrder(int[][] matrix, int ltx, int lty, int rbx, int rby, List<Integer> list) {
-        if (ltx == rbx) {
-            // 内侧是一条直线
-            for (int i = lty; i <= rby; i++) {
-                list.add(matrix[ltx][i]);
+    public static void spiral(int[][] matrix, int a, int b, int c, int d, List<Integer> ans) {
+        if (a == c) {
+            // 共行
+            for (int i = b; i <= d; i++) {
+                ans.add(matrix[a][i]);
             }
-        } else if (lty == rby) {
-            // 内侧是一条直线
-            for (int i = ltx; i <= rbx; i++) {
-                list.add(matrix[i][lty]);
+            return;
+        }
+        if (b == d) {
+            // 共列
+            for (int i = a; i <= c; i++) {
+                ans.add(matrix[i][b]);
             }
-        } else {
-            for (int i = lty; i < rby; i++) {
-                list.add(matrix[ltx][i]);
-            }
-            for (int i = ltx; i < rbx; i++) {
-                list.add(matrix[i][rby]);
-            }
-            for (int i = rby; i > lty; i--) {
-                list.add(matrix[rbx][i]);
-            }
-            for (int i = rbx; i > ltx; i--) {
-                list.add(matrix[i][lty]);
-            }
+            return;
+        }
+        for (int i = b; i < d; i++) {
+            ans.add(matrix[a][i]);
+        }
+        for (int i = a; i < c; i++) {
+            ans.add(matrix[i][d]);
+        }
+        for (int i = d; i > b; i--) {
+            ans.add(matrix[c][i]);
+        }
+        for (int i = c; i > a; i--) {
+            ans.add(matrix[i][b]);
         }
     }
-
-    public static void main(String[] args) {
-        int[][] m = {{1}};
-        spiralOrder(m);
-    }
-
-
 }
