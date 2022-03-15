953dcdfbc8e238246f0977955a906a609962d285
GreyZeng <410486047@qq.com>
2022-03-15 22:00:52 +0800

    update notes

--- src/main/java/nowcoder/NowCoder_BeatMonster.java
+++ src/main/java/nowcoder/NowCoder_BeatMonster.java
@@ -17,59 +17,68 @@ public class NowCoder_BeatMonster {
         return p(hp, money, 0, 0);
     }
 
-    public static long p(int[] hp, int[] money, int i, int ability) {
+    public static long p(int[] hp, int[] money, int i, int j) {
         if (i == hp.length) {
             return 0;
         }
         // 选择贿赂
-        long p = money[i] + p(hp, money, i + 1, ability + hp[i]);
+        long p = money[i] + p(hp, money, i + 1, j + hp[i]);
         // 不选贿赂，有条件
-        if (ability >= hp[i]) {
-            return Math.min(p, p(hp, money, i + 1, ability));
+        if (j >= hp[i]) {
+            return Math.min(p, p(hp, money, i + 1, j));
         }
         return p;
     }
 
-    public static long func1dp(int[] hp, int[] money) {
-        int sumAbility = 0;
-        for (int ability : hp) {
-            sumAbility += ability;
+    public static long func1Dp(int[] hp, int[] money) {
+        int sum = 0;
+        for (int a : hp) {
+            sum += a;
         }
-        int[][] dp = new int[hp.length + 1][sumAbility + 1];
-        // TODO
+        long[][] dp = new long[hp.length + 1][sum + 1];
+        for (int i = hp.length - 1; i >= 0; i--) {
+            for (int j = sum - hp[i]; j >= 0; j--) {
+                dp[i][j] = money[i] + dp[i + 1][j + hp[i]];
+                if (j >= hp[i]) {
+                    dp[i][j] = Math.min(dp[i][j], dp[i + 1][j]);
+                }
+            }
+        }
         return dp[0][0];
     }
 
-    public static int func2(int[] hp, int[] money) {
-        int allMoney = 0;
-        for (int i = 0; i < money.length; i++) {
-            allMoney += money[i];
+    public static long func2(int[] hp, int[] money) {
+        int sum = 0;
+        for (int a : money) {
+            sum += a;
         }
-        int N = hp.length;
-        for (int m = 0; m < allMoney; m++) {
-            if (process2(hp, money, N - 1, m) != -1) {
-                return m;
+        for (int i = 0; i < sum; i++) {
+            long p = p2(hp, money, hp.length - 1, i);
+            if (p != -1) {
+                return i;
             }
+
         }
-        return allMoney;
+        return sum;
     }
 
+    // TODO
     // 从0....index号怪兽，花的钱，必须严格==money
     // 如果通过不了，返回-1
     // 如果可以通过，返回能通过情况下的最大能力值
-    public static long process2(int[] hp, int[] money, int index, int m) {
+    public static long p2(int[] hp, int[] money, int index, int m) {
         if (index == -1) { // 一个怪兽也没遇到呢
             return m == 0 ? 0 : -1;
         }
         // index >= 0
         // 1) 不贿赂当前index号怪兽
-        long preMaxAbility = process2(hp, money, index - 1, m);
+        long preMaxAbility = p2(hp, money, index - 1, m);
         long p1 = -1;
         if (preMaxAbility != -1 && preMaxAbility >= hp[index]) {
             p1 = preMaxAbility;
         }
         // 2) 贿赂当前的怪兽 当前的钱 p[index]
-        long preMaxAbility2 = process2(hp, money, index - 1, m - money[index]);
+        long preMaxAbility2 = p2(hp, money, index - 1, m - money[index]);
         long p2 = -1;
         if (preMaxAbility2 != -1) {
             p2 = hp[index] + preMaxAbility2;
@@ -77,32 +86,6 @@ public class NowCoder_BeatMonster {
         return Math.max(p1, p2);
     }
 
-
-    public static long func3(int[] d, int[] p) {
-        int sum = 0;
-        for (int num : d) {
-            sum += num;
-        }
-        long[][] dp = new long[d.length + 1][sum + 1];
-        for (int i = 0; i <= sum; i++) {
-            dp[0][i] = 0;
-        }
-        for (int cur = d.length - 1; cur >= 0; cur--) {
-            for (int hp = 0; hp <= sum; hp++) {
-                // 如果这种情况发生，那么这个hp必然是递归过程中不会出现的状态
-                // 既然动态规划是尝试过程的优化，尝试过程碰不到的状态，不必计算
-                if (hp + d[cur] > sum) {
-                    continue;
-                }
-                if (hp < d[cur]) {
-                    dp[cur][hp] = p[cur] + dp[cur + 1][hp + d[cur]];
-                } else {
-                    dp[cur][hp] = Math.min(p[cur] + dp[cur + 1][hp + d[cur]], dp[cur + 1][hp]);
-                }
-            }
-        }
-        return dp[0][0];
-    }
 
     public static long func4(int[] d, int[] p) {
         int sum = 0;
@@ -169,9 +152,9 @@ public class NowCoder_BeatMonster {
             int[] d = arrs[0];
             int[] p = arrs[1];
             long ans1 = func1(d, p);
-            long ans2 = func3(d, p);
+            long ans4 = func1Dp(d, p);
+            long ans2 = func2(d, p);
             long ans3 = func4(d, p);
-            long ans4 = func2(d, p);
             if (ans1 != ans2 || ans2 != ans3 || ans1 != ans4) {
                 System.out.println("oops!");
             }
