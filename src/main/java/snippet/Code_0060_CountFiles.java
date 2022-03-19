6d404ff1e69bb837b2c029e56926d5f3103e8d95
GreyZeng <410486047@qq.com>
2022-03-19 13:33:55 +0800

    count file

---
+++ src/main/java/snippet/Code_0060_CountFiles.java
@@ -0,0 +1,32 @@
+package snippet;
+
+import java.io.File;
+import java.util.Stack;
+
+public class Code_0060_CountFiles {
+
+    public static int getFileNumber(String folderPath) {
+        File root = new File(folderPath);
+        if (!root.isDirectory() && !root.isFile()) {
+            return 0;
+        }
+        if (root.isFile()) {
+            return 1;
+        }
+        Stack<File> stack = new Stack<>();
+        stack.add(root);
+        int files = 0;
+        while (!stack.isEmpty()) {
+            File folder = stack.pop();
+            for (File next : folder.listFiles()) {
+                if (next.isFile()) {
+                    files++;
+                }
+                if (next.isDirectory()) {
+                    stack.push(next);
+                }
+            }
+        }
+        return files;
+    }
+}
