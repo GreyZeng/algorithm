ec5f973aaaf848b98a7becb88ff0a246627405c7
GreyZeng <410486047@qq.com>
2022-03-20 20:14:39 +0800

    update lintcode 163

--- src/main/java/snippet/Code_0023_10Ways.java
+++ src/main/java/snippet/Code_0023_10Ways.java
@@ -1,36 +1,95 @@
 package snippet;
 
+import java.util.*;
+
 //假设给你N个0，和N个1，你必须用全部数字拼序列
 // 返回有多少个序列满足：任何前缀串，1的数量都不少于0的数量
 // 卡特兰数
 public class Code_0023_10Ways {
-	public static int ways(int N) {
-		return C(2 * N , N) - C(2 * N, N-1);
-	}
-	public static int C(int n, int k) {
-        long res = 1L;
-        long res2 = 1L;
-        int x = n;
-        int y = n - k;
-        long gcd;
-        while (x >= k + 1 && y >= 1) {
-            res *= x--;
-            res2 *= y--;
-            gcd = gcd(res, res2);
-            res /= gcd;
-            res2 /= gcd;
+
+    public static long ways1(int N) {
+        int zero = N;
+        int one = N;
+        LinkedList<Integer> path = new LinkedList<>();
+        LinkedList<LinkedList<Integer>> ans = new LinkedList<>();
+        process(zero, one, path, ans);
+        long count = 0;
+        for (LinkedList<Integer> cur : ans) {
+            int status = 0;
+            for (Integer num : cur) {
+                if (num == 0) {
+                    status++;
+                } else {
+                    status--;
+                }
+                if (status < 0) {
+                    break;
+                }
+            }
+            if (status == 0) {
+                count++;
+            }
         }
-        int f = (int) (res / res2);
-        while (x >= k + 1) {
-            f *= x--;
-        }
-        while (y >= 1) {
-            f /= y--;
-        }
-        return f;
+        return count;
     }
 
-    public static long gcd(long m, long n) {
-        return n == 0 ? m : gcd(n, m % n);
+    public static void process(int zero, int one, LinkedList<Integer> path, LinkedList<LinkedList<Integer>> ans) {
+        if (zero == 0 && one == 0) {
+            LinkedList<Integer> cur = new LinkedList<>();
+            for (Integer num : path) {
+                cur.add(num);
+            }
+            ans.add(cur);
+        } else {
+            if (zero == 0) {
+                path.addLast(1);
+                process(zero, one - 1, path, ans);
+                path.removeLast();
+            } else if (one == 0) {
+                path.addLast(0);
+                process(zero - 1, one, path, ans);
+                path.removeLast();
+            } else {
+                path.addLast(1);
+                process(zero, one - 1, path, ans);
+                path.removeLast();
+                path.addLast(0);
+                process(zero - 1, one, path, ans);
+                path.removeLast();
+            }
+        }
     }
+
+    public static long ways2(int N) {
+        if (N < 0) {
+            return 0;
+        }
+        if (N < 2) {
+            return 1;
+        }
+        long a = 1;
+        long b = 1;
+        long limit = N << 1;
+        for (long i = 1; i <= limit; i++) {
+            if (i <= N) {
+                a *= i;
+            } else {
+                b *= i;
+            }
+        }
+        return (b / a) / (N + 1);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("test begin");
+        for (int i = 0; i < 10; i++) {
+            long ans1 = ways1(i);
+            long ans2 = ways2(i);
+            if (ans1 != ans2) {
+                System.out.println("Oops!");
+            }
+        }
+        System.out.println("test finish");
+    }
+
 }
