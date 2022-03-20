8f2cf515a29da7aa1b8d2e6d5e90e4970aeed3e8
GreyZeng <410486047@qq.com>
2022-03-20 12:21:44 +0800

    sub sequence max mod m

--- src/main/java/snippet/Code_0045_SubSequenceMaxModM.java
+++ src/main/java/snippet/Code_0045_SubSequenceMaxModM.java
@@ -1,97 +1,103 @@
 package snippet;
 
 import java.util.HashSet;
+import java.util.Set;
 import java.util.TreeSet;
 
-//给定一个非负数组arr，和一个正数m
-//返回arr的所有子序列中累加和%m之后的最大值。
+/**
+ * 给定一个非负数组arr，和一个正数m
+ * 返回arr的所有子序列中累加和%m之后的最大值。
+ */
 public class Code_0045_SubSequenceMaxModM {
     // 暴力解法
     public static int max1(int[] arr, int m) {
-        HashSet<Integer> set = new HashSet<>();
+        Set<Integer> set = new HashSet<>();
         process(arr, 0, 0, set);
         int max = arr[0] % m;
-        for (Integer sum : set) {
+        for (int sum : set) {
             max = Math.max(max, sum % m);
         }
         return max;
     }
 
-    // 枚举所有子序列的和
-    public static void process(int[] arr, int index, int sum, HashSet<Integer> set) {
-        if (index == arr.length) {
+    public static void process(int[] arr, int i, int sum, Set<Integer> set) {
+        if (i == arr.length) {
             set.add(sum);
         } else {
-            process(arr, index + 1, sum, set);
-            process(arr, index + 1, sum + arr[index], set);
+            process(arr, i + 1, sum + arr[i], set);
+            process(arr, i + 1, sum, set);
         }
     }
 
+    // dp[i][j] ---> 0...i自由选择，能否正确得到j
+    // 最后一行，dp[i][j] = true的记录，j%m的最大值即为答案
     // 二维表 O(N*sum)
+    // 这种情况下 sum不能太大
     public static int max2(int[] arr, int m) {
-        int sum = 0;
-        int N = arr.length;
-        for (int k : arr) {
-            sum += k;
+        int sum = arr[0];
+        int n = arr.length;
+        for (int i = 1; i < n; i++) {
+            sum += arr[i];
         }
-        boolean[][] dp = new boolean[N][sum + 1];
-        for (int i = 0; i < N; i++) {
+        boolean[][] dp = new boolean[n][sum + 1];
+        // 0...0，可以组成arr[0] 或者 0
+        dp[0][arr[0]] = true;
+        for (int i = 0; i < n; i++) {
             dp[i][0] = true;
         }
-        dp[0][arr[0]] = true;
-        for (int i = 1; i < N; i++) {
-            for (int j = 1; j <= sum; j++) {
+        for (int i = 1; i < dp.length; i++) {
+            for (int j = 1; j < dp[0].length; j++) {
+                // 至少可以不选i位置的值累加
                 dp[i][j] = dp[i - 1][j];
-                if (j - arr[i] >= 0) {
+                if (j >= arr[i]) {
                     dp[i][j] |= dp[i - 1][j - arr[i]];
                 }
             }
         }
-        int ans = 0;
-        for (int j = 0; j <= sum; j++) {
-            if (dp[N - 1][j]) {
-                ans = Math.max(ans, j % m);
+        int max = 0;
+        for (int j = 0; j < dp[0].length; j++) {
+            if (dp[n - 1][j]) {
+                max = Math.max(max, j % m);
             }
         }
-        return ans;
+        return max;
     }
 
+    // sum如果很大，那么max2方法不行，可以考虑如下方法
     // 二维表（n*m）：列是数组下标，行0...m-1
     // 0到i上随意组合，在i%m后能否组成j
     public static int max3(int[] arr, int m) {
-        int N = arr.length;
-        // 0...m-1
-        boolean[][] dp = new boolean[N][m];
-        for (int i = 0; i < N; i++) {
+        int n = arr.length;
+        boolean[][] dp = new boolean[n][m];
+        for (int i = 0; i < dp.length; i++) {
             dp[i][0] = true;
         }
         dp[0][arr[0] % m] = true;
-        for (int i = 1; i < N; i++) {
-            for (int j = 1; j < m; j++) {
-                // dp[i][j] T or F
+        for (int i = 1; i < dp.length; i++) {
+            for (int j = 1; j < dp[0].length; j++) {
+                // 至少可以不选i位置的值累加
                 dp[i][j] = dp[i - 1][j];
                 int cur = arr[i] % m;
-                if (cur <= j) {
-                    dp[i][j] |= dp[i - 1][j - cur];
-                } else {
+                if (j < cur) {
                     dp[i][j] |= dp[i - 1][m + j - cur];
+                } else {
+                    dp[i][j] |= dp[i - 1][j - cur];
                 }
             }
         }
-        int ans = 0;
-        for (int i = 0; i < m; i++) {
-            if (dp[N - 1][i]) {
-                ans = i;
+        for (int i = m - 1; i >= 1; i--) {
+            if (dp[n - 1][i]) {
+                return i;
             }
         }
-        return ans;
+        return 0;
     }
 
     // 如果arr的累加和很大，m也很大
     // 但是arr的长度相对不大
-    //        方法3（分治）：
-//        二维表(n*m（真实的余数）):如果n很小，可以分解两半，暴力求每一半的所有可能子数组的累加和，放入一个List中，List加工出每个值模m后的List‘
-//        左边部分的数量 + 右边部分的数量 + 左边和右边整合的List
+    // 方法3（分治）：
+    // 二维表(n*m（真实的余数）):如果n很小，可以分解两半，暴力求每一半的所有可能子数组的累加和，放入一个List中，List加工出每个值模m后的List‘
+    // 左边部分的数量 + 右边部分的数量 + 左边和右边整合的List
     public static int max4(int[] arr, int m) {
         if (arr.length == 1) {
             return arr[0] % m;
