920d400ff6dc76d094ff75d98c836e726f8f2193
GreyZeng <410486047@qq.com>
2022-03-23 13:37:23 +0800

    update notes

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -4,11 +4,14 @@ public class Test {
     public static void main(String[] args) {
         TreeSet<Integer> set = new TreeSet<>();
         set.add(1);
-        set.add(2);
         set.add(3);
+        set.add(9);
         Integer floor = set.floor(2);
         System.out.println(floor);
         Integer ceiling = set.ceiling(2);
         System.out.println(ceiling);
+        
+        Integer floor2 = set.floor(3);
+        System.out.println(floor2);
     }
 }
