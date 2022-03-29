0c4674687a7d763f5e9617021c8eca3b6adcace5
GreyZeng <410486047@qq.com>
2022-03-29 22:36:30 +0800

    update notes

--- src/main/java/leetcode/medium/LeetCode_0932_BeautifulArray.java
+++ src/main/java/leetcode/medium/LeetCode_0932_BeautifulArray.java
@@ -1,5 +1,8 @@
 package leetcode.medium;
 
+import java.util.HashMap;
+import java.util.Map;
+
 //生成长度为size的达标数组，什么叫达标？
 //        达标：对于任意的 i<k<j，满足 [i] + [j] != [k] * 2
 //        给定一个正数size，返回长度为size的达标数组
@@ -25,27 +28,34 @@ package leetcode.medium;
 // LeetCode 这题有点差别，限制数字一定要是[1...N]
 // https://leetcode-cn.com/problems/beautiful-array
 public class LeetCode_0932_BeautifulArray {
-    public static int[] beautifulArray(int size) {
-        return null;
+
+
+    public static int[] beautifulArray(int N) {
+        Map<Integer, int[]> memo = new HashMap();
+        return f(N, memo);
     }
 
-    // 没有leetcode上的限制解法
-    public static int[] randomArray(int M) {
-        if (M == 1) {
-            return new int[]{1};
+    public static int[] f(int N, Map<Integer, int[]> memo) {
+        if (memo.containsKey(N)) {
+            return memo.get(N);
         }
-        int half = (M + 1) / 2;
-        int[] base = randomArray(half);
-        int[] result = new int[M];
-        int index = 0;
-        for (; index < half; index++) {
-            result[index] = base[index] * 2 - 1;
+
+        int[] ans = new int[N];
+        if (N == 1) {
+            ans[0] = 1;
+        } else {
+            int t = 0;
+            for (int x : f((N + 1) / 2, memo)) {
+                ans[t++] = 2 * x - 1;
+            }
+            for (int x : f(N / 2, memo)) {
+                ans[t++] = 2 * x;
+            }
         }
-        for (int i = 0; index < M; i++, index++) {
-            result[index] = base[i] * 2;
-        }
-        return result;
+        memo.put(N, ans);
+        return ans;
     }
+
 
     // 检验函数
     public static boolean isValid(int[] arr) {
@@ -65,15 +75,15 @@ public class LeetCode_0932_BeautifulArray {
     public static void main(String[] args) {
         System.out.println("test begin");
         for (int N = 1; N < 1000; N++) {
-            int[] arr = randomArray(N);
+            int[] arr = beautifulArray(N);
             if (!isValid(arr)) {
                 System.out.println("Oops!");
             }
         }
         System.out.println("test end");
 
-        System.out.println(isValid(randomArray(1042)));
-        System.out.println(isValid(randomArray(2981)));
+        System.out.println(isValid(beautifulArray(1042)));
+        System.out.println(isValid(beautifulArray(2981)));
 
 
     }
