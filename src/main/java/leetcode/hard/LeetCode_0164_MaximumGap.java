18860671a8559a8622e6211eba1662326338495c
GreyZeng <410486047@qq.com>
2022-04-05 20:50:18 +0800

    leetcode 164 maximum gap

--- src/main/java/leetcode/hard/LeetCode_0164_MaximumGap.java
+++ src/main/java/leetcode/hard/LeetCode_0164_MaximumGap.java
@@ -1,22 +1,3 @@
-/*Given an unsorted array, find the maximum difference between the successive elements in its sorted form.
-
-        Return 0 if the array contains less than 2 elements.
-
-        Example 1:
-
-        Input: [3,6,9,1]
-        Output: 3
-        Explanation: The sorted form of the array is [1,3,6,9], either
-        (3,6) or (6,9) has the maximum difference 3.
-        Example 2:
-
-        Input: [10]
-        Output: 0
-        Explanation: The array contains less than 2 elements, therefore return 0.
-        Note:
-
-        You may assume all elements in the array are non-negative integers and fit in the 32-bit signed integer range.
-        Try to solve it in linear time/space.*/
 package leetcode.hard;
 
 //求最大值和最小值，然后把数分为N+1份，每个桶只装可以进这个桶的最小值和最大值
@@ -25,15 +6,10 @@ package leetcode.hard;
 //        设置空桶的目的是杀死桶内数据的差值
 // https://leetcode-cn.com/problems/maximum-gap/
 public class LeetCode_0164_MaximumGap {
-    public static void main(String[] args) {
-        int[] num = {3, 6, 9, 1};
-        System.out.println(maximumGap(num));
-        int[] num2 = {10};
-        System.out.println(maximumGap(num2));
-    }
 
+    // O(N)解法
     public static int maximumGap(int[] nums) {
-        if (null == nums || nums.length == 0) {
+        if (null == nums || nums.length < 1) {
             return 0;
         }
         int max = nums[0];
@@ -42,12 +18,16 @@ public class LeetCode_0164_MaximumGap {
             max = Math.max(num, max);
             min = Math.min(num, min);
         }
+        // 最大值和最小值一样，说明只有一种数，最大间距就是0
         if (max == min) {
             return 0;
         }
         int len = nums.length;
+        // 标记桶是否为空
         boolean[] flag = new boolean[len + 1];
+        // i号桶的最大值
         int[] maxArr = new int[len + 1];
+        // i号桶的最小值
         int[] minArr = new int[len + 1];
         for (int num : nums) {
             int index = getIndex(num, len, min, max);
@@ -66,6 +46,7 @@ public class LeetCode_0164_MaximumGap {
         return ans;
     }
 
+    // 决定num这个数应该进几号桶
     public static int getIndex(long num, long len, long min, long max) {
         return (int) ((num - min) * len / (max - min));
     }
