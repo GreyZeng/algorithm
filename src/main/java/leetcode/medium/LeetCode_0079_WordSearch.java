7fb70ae32294ebfd8f6e900e4802f7cfadb5223b
GreyZeng <410486047@qq.com>
2022-04-07 22:58:12 +0800

    leetcode 79 word search

--- src/main/java/leetcode/medium/LeetCode_0079_WordSearch.java
+++ src/main/java/leetcode/medium/LeetCode_0079_WordSearch.java
@@ -25,7 +25,9 @@ package leetcode.medium;
 //		1 <= board.length <= 200
 //		1 <= board[i].length <= 200
 //		1 <= word.length <= 10^3
+// https://leetcode-cn.com/problems/word-search/
 public class LeetCode_0079_WordSearch {
+    // 不能走重复路
     // 深度优先遍历
     // 增加现场
     public static boolean exist(char[][] board, String word) {
@@ -59,4 +61,68 @@ public class LeetCode_0079_WordSearch {
             return false;
         }
     }
+
+
+    // 可以走重复的设定
+    public static boolean findWord1(char[][] m, String word) {
+        if (word == null || word.equals("")) {
+            return true;
+        }
+        if (m == null || m.length == 0 || m[0] == null || m[0].length == 0) {
+            return false;
+        }
+        char[] w = word.toCharArray();
+        int N = m.length;
+        int M = m[0].length;
+        int len = w.length;
+        // dp[i][j][k]表示：必须以m[i][j]这个字符结尾的情况下，能不能找到w[0...k]这个前缀串
+        boolean[][][] dp = new boolean[N][M][len];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                dp[i][j][0] = m[i][j] == w[0];
+            }
+        }
+        for (int k = 1; k < len; k++) {
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < M; j++) {
+                    dp[i][j][k] = (m[i][j] == w[k] && checkPrevious(dp, i, j, k));
+                }
+            }
+        }
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (dp[i][j][len - 1]) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    // 可以走重复路
+    // 从m[i][j]这个字符出发，能不能找到str[k...]这个后缀串
+    public static boolean canLoop(char[][] m, int i, int j, char[] str, int k) {
+        if (k == str.length) {
+            return true;
+        }
+        if (i == -1 || i == m.length || j == -1 || j == m[0].length || m[i][j] != str[k]) {
+            return false;
+        }
+        // 不越界！m[i][j] == str[k] 对的上的！
+        // str[k+1....]
+        boolean ans = false;
+        if (canLoop(m, i + 1, j, str, k + 1) || canLoop(m, i - 1, j, str, k + 1) || canLoop(m, i, j + 1, str, k + 1) || canLoop(m, i, j - 1, str, k + 1)) {
+            ans = true;
+        }
+        return ans;
+    }
+
+    public static boolean checkPrevious(boolean[][][] dp, int i, int j, int k) {
+        boolean up = i > 0 ? (dp[i - 1][j][k - 1]) : false;
+        boolean down = i < dp.length - 1 ? (dp[i + 1][j][k - 1]) : false;
+        boolean left = j > 0 ? (dp[i][j - 1][k - 1]) : false;
+        boolean right = j < dp[0].length - 1 ? (dp[i][j + 1][k - 1]) : false;
+        return up || down || left || right;
+    }
+
 }
