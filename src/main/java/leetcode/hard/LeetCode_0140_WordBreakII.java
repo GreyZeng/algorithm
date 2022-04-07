25717e48dc173f382e5f423f355e2c173a8b2986
GreyZeng <410486047@qq.com>
2022-04-07 22:14:28 +0800

    word break II

--- src/main/java/leetcode/hard/LeetCode_0140_WordBreakII.java
+++ src/main/java/leetcode/hard/LeetCode_0140_WordBreakII.java
@@ -1,38 +1,3 @@
-/*Given a non-empty string s and a dictionary wordDict containing a list of non-empty words, add spaces in s to construct a sentence where each word is a valid dictionary word. Return all such possible sentences.
-
-Note:
-
-The same word in the dictionary may be reused multiple times in the segmentation.
-You may assume the dictionary does not contain duplicate words.
-Example 1:
-
-Input:
-s = "catsanddog"
-wordDict = ["cat", "cats", "and", "sand", "dog"]
-Output:
-[
-  "cats and dog",
-  "cat sand dog"
-]
-Example 2:
-
-Input:
-s = "pineapplepenapple"
-wordDict = ["apple", "pen", "applepen", "pine", "pineapple"]
-Output:
-[
-  "pine apple pen apple",
-  "pineapple pen apple",
-  "pine applepen apple"
-]
-Explanation: Note that you are allowed to reuse a dictionary word.
-Example 3:
-
-Input:
-s = "catsandog"
-wordDict = ["cats", "dog", "sand", "and", "cat"]
-Output:
-[]*/
 package leetcode.hard;
 
 import java.util.ArrayList;
@@ -40,6 +5,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+// word break 进阶，返回所有组合的情况
 public class LeetCode_0140_WordBreakII {
 
     public static class Node {
@@ -61,12 +27,12 @@ public class LeetCode_0140_WordBreakII {
         Set<String> wordDict = new HashSet<>(words);
         Node root = buildTrie(wordDict);
         char[] str = s.toCharArray();
-        int N = str.length;
-        boolean[] dp = new boolean[N + 1];
-        dp[N] = true;
-        for (int index = N - 1; index >= 0; index--) {
+        int length = str.length;
+        boolean[] dp = new boolean[length + 1];
+        dp[length] = true;
+        for (int index = length - 1; index >= 0; index--) {
             Node cur = root;
-            for (int i = index; i < N; i++) {
+            for (int i = index; i < length; i++) {
                 int p = str[i] - 'a';
                 if (cur.nexts[p] == null) {
                     break;
