d28dd68c1ae10e9b210d5094829dcc947d45841f
GreyZeng <410486047@qq.com>
2022-04-08 21:25:46 +0800

    leetcode 224 basic cal

--- src/main/java/leetcode/hard/LeetCode_0224_BasicCalculator.java
+++ src/main/java/leetcode/hard/LeetCode_0224_BasicCalculator.java
@@ -1,6 +1,6 @@
 package leetcode.hard;
 
-import java.util.LinkedList;
+import java.util.Stack;
 
 //1 <= s.length <= 3 * 10^5
 //s 由数字、'+'、'-'、'('、')'、和 ' ' 组成
@@ -14,69 +14,60 @@ import java.util.LinkedList;
 //著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 public class LeetCode_0224_BasicCalculator {
     public static int calculate(String str) {
-        return f(str.toCharArray(), 0)[0];
+        return process(str.toCharArray(), 0)[0];
     }
 
-    // 请从str[i...]往下算，遇到字符串终止位置或者右括号，就停止
-    // 返回两个值，长度为2的数组
-    // 0) 负责的这一段的结果是多少
-    // 1) 负责的这一段计算到了哪个位置
-    public static int[] f(char[] str, int i) {
-        LinkedList<String> queue = new LinkedList<>();
-        int curNum = 0;
-        int[] bra;
+    public static int[] process(char[] str, int i) {
+        Stack<String> stack = new Stack<>();
+        int[] b;
+        int num = 0;
         while (i < str.length && str[i] != ')') {
             if (str[i] == ' ') {
                 i++;
             } else if (str[i] >= '0' && str[i] <= '9') {
-                curNum = curNum * 10 + (str[i++] - '0');
-            } else if (str[i] != '(') {
-                // 遇到的是运算符号
-                addNum(queue, curNum);
-                queue.addLast(String.valueOf(str[i++]));
-                curNum = 0;
-            } else { // 遇到左括号了
-                bra = f(str, i + 1);
-                curNum = bra[0];
-                i = bra[1] + 1;
+                num = num * 10 + (str[i++] - '0');
+            } else if (str[i] == '+' || str[i] == '-') {
+                addNum(stack, num);
+                stack.push(String.valueOf(str[i++]));
+                num = 0;
+            } else if (str[i] == '(') {
+                b = process(str, i + 1);
+                i = b[1] + 1;
+                num = b[0];
+            } else {
+                // 根据题目意思，永远不会走到这个分支
             }
         }
-        addNum(queue, curNum);
-        return new int[]{getNum(queue), i};
+        addNum(stack, num);
+        return new int[]{getNum(stack), i};
     }
 
-    public static void addNum(LinkedList<String> queue, int num) {
-        if (!queue.isEmpty()) {
-            int cur;
-            String top = queue.pollLast();
-            // 先结算
-            if (top.equals("+") || top.equals("-")) {
-                queue.addLast(top);
+    public static int getNum(Stack<String> stack) {
+        int result = 0;
+        boolean isAdd = true;
+        while (!stack.isEmpty()) {
+            String pop = stack.pop();
+            if ("+".equals(pop)) {
+                isAdd = true;
+            } else if ("-".equals(pop)) {
+                isAdd = false;
             } else {
-                // 到这里了，就只可能是*和/号
-                cur = Integer.parseInt(queue.pollLast());
-                num = top.equals("*") ? (cur * num) : (cur / num);
+                int v = Integer.parseInt(pop);
+                result += isAdd ? v : (-v);
             }
         }
-        queue.addLast(String.valueOf(num));
+        return result;
     }
 
-    public static int getNum(LinkedList<String> queue) {
-        int res = 0;
-        boolean add = true;
-        String cur;
-        int num;
-        while (!queue.isEmpty()) {
-            cur = queue.pollFirst();
-            if (cur.equals("+")) {
-                add = true;
-            } else if (cur.equals("-")) {
-                add = false;
-            } else {
-                num = Integer.parseInt(cur);
-                res += add ? num : (-num);
+    public static void addNum(Stack<String> stack, int num) {
+        if (!stack.isEmpty()) {
+            String op = stack.pop();
+            if ("+".equals(op)) {
+                num += Integer.valueOf(stack.pop());
+            } else if ("-".equals(op)) {
+                num = Integer.valueOf(stack.pop()) - num;
             }
         }
-        return res;
+        stack.push(String.valueOf(num));
     }
 }
