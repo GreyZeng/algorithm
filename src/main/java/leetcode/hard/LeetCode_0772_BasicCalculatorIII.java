f9161422cfe31d23b834963833cf01230f1c6090
GreyZeng <410486047@qq.com>
2022-04-09 09:48:35 +0800

    leetcode 772 basic calculator iii

--- src/main/java/leetcode/hard/LeetCode_0772_BasicCalculatorIII.java
+++ src/main/java/leetcode/hard/LeetCode_0772_BasicCalculatorIII.java
@@ -4,69 +4,70 @@ import java.util.LinkedList;
 
 // tip 递归过程
 // 压缩串问题
+//1 <= s <= 104
+//        s 由整数、'+'、'-'、'*'、'/'、'(' 和 ')' 组成
+//        s 是一个 有效的 表达式
 public class LeetCode_0772_BasicCalculatorIII {
-    public static int calculate(String s) {
-        return f(s.toCharArray(), 0)[0];
+    public static void main(String[] args) {
+        String st = "(2+6*3+5-(3*14/7+2)*5)+3";
+        System.out.println(calculate(st));
     }
 
-    private static int[] f(char[] str, int i) {
+    public static int calculate(String s) {
+        return process(s.toCharArray(), 0)[0];
+    }
+
+    public static int[] process(char[] str, int i) {
         LinkedList<String> queue = new LinkedList<>();
-        int c = 0;
+        int num = 0;
         int[] b;
         while (i < str.length && str[i] != ')') {
             if (str[i] == ' ') {
                 i++;
             } else if (str[i] >= '0' && str[i] <= '9') {
-                c = c * 10 + str[i++] - '0';
+                num = num * 10 + (str[i++] - '0');
             } else if (str[i] == '(') {
-                b = f(str, i + 1);
-                c = b[0];
+                b = process(str, i + 1);
                 i = b[1] + 1;
+                num = b[0];
             } else {
-                // str[i]是运算符
-                addNum(queue, c);
+                addNum(queue, num);
                 queue.addLast(String.valueOf(str[i++]));
-                c = 0;
+                num = 0;
+                // str[i]是操作符
             }
         }
-        addNum(queue, c);
-
+        addNum(queue, num);
         return new int[]{getNum(queue), i};
     }
 
-    private static int getNum(LinkedList<String> que) {
-        int res = 0;
-        boolean add = true;
-        String cur = null;
-        int num = 0;
-        while (!que.isEmpty()) {
-            cur = que.pollFirst();
-            if (cur.equals("+")) {
-                add = true;
-            } else if (cur.equals("-")) {
-                add = false;
+    private static void addNum(LinkedList<String> queue, int num) {
+        if (!queue.isEmpty()) {
+            String s = queue.pollLast();
+            if ("+".equals(s) || "-".equals(s)) {
+                queue.addLast(s);
             } else {
-                num = Integer.valueOf(cur);
-                res += add ? num : (-num);
+                int v = Integer.parseInt(queue.pollLast());
+                num = "*".equals(s) ? (v * num) : (v / num);
             }
         }
-        return res;
+        queue.addLast(String.valueOf(num));
     }
 
-    private static void addNum(LinkedList<String> queue, int c) {
-        if (!queue.isEmpty()) {
-            int t = 0;
-            String top = queue.pollLast();
-            if ("*".equals(top)) {
-                t = Integer.valueOf(queue.pollLast());
-                c = c * t;
-            } else if ("/".equals(top)) {
-                t = Integer.valueOf(queue.pollLast());
-                c = t / c;
+    public static int getNum(LinkedList<String> queue) {
+        boolean isAdd = true;
+        int num = 0;
+        while (!queue.isEmpty()) {
+            String s = queue.pollFirst();
+            if ("+".equals(s)) {
+                isAdd = true;
+            } else if ("-".equals(s)) {
+                isAdd = false;
             } else {
-                queue.addLast(top);
+                int v = Integer.parseInt(s);
+                num += isAdd ? v : (-v);
             }
         }
-        queue.addLast(String.valueOf(c));
+        return num;
     }
 }
