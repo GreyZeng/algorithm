963c29419f0b843cf923993328e7d14ad3900298
GreyZeng <410486047@qq.com>
2022-04-10 15:27:35 +0800

    leetcode 438 find all anagrams in a string

--- src/main/java/leetcode/medium/LeetCode_0438_FindAllAnagramsInAString.java
+++ src/main/java/leetcode/medium/LeetCode_0438_FindAllAnagramsInAString.java
@@ -27,7 +27,7 @@ Explanation:
 The substring with start index = 0 is "ab", which is an anagram of "ab".
 The substring with start index = 1 is "ba", which is an anagram of "ab".
 The substring with start index = 2 is "ab", which is an anagram of "ab".*/
-package leetcode;
+package leetcode.medium;
 
 import java.util.ArrayList;
 import java.util.List;
