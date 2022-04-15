f81f5a5591540098fe1836e5ba3cdecd28c720dd
GreyZeng <410486047@qq.com>
2022-04-15 19:30:31 +0800

    leetcode 32 longest valid parentheses

--- src/main/java/leetcode/hard/LeetCode_0032_LongestValidParentheses.java
+++ src/main/java/leetcode/hard/LeetCode_0032_LongestValidParentheses.java
@@ -1,66 +1,39 @@
-/*Given a string containing just the characters '(' and ')', find the length of the longest valid (well-formed) parentheses substring.
-
-
-
-        Example 1:
-
-        Input: s = "(()"
-        Output: 2
-        Explanation: The longest valid parentheses substring is "()".
-        Example 2:
-
-        Input: s = ")()())"
-        Output: 4
-        Explanation: The longest valid parentheses substring is "()()".
-        Example 3:
-
-        Input: s = ""
-        Output: 0
-
-
-        Constraints:
-
-        0 <= s.length <= 3 * 10^4
-        s[i] is '(', or ')'.*/
 package leetcode.hard;
 
-
+// 求最长的有效括号的长度
 // tips: 以i结尾的答案全部收集一遍
 // ref: https://www.lintcode.com/problem/longest-valid-parentheses/description
 // https://leetcode-cn.com/problems/longest-valid-parentheses/
 public class LeetCode_0032_LongestValidParentheses {
-    public int longestValidParentheses(String s) {
+    public static int longestValidParentheses(String s) {
         if (s == null || s.length() <= 1) {
             return 0;
         }
         char[] str = s.toCharArray();
-        int ans = 0;
-        // TODO
+        // dp[i]：必须以i位置符号结尾的字符串，最长有效括号串长度是多少
+        int[] dp = new int[str.length];
+        // dp[0] = 0, dp[1] = 0
+        dp[1] = str[0] == '(' && str[1] == ')' ? 2 : 0;
+        int ans = dp[1];
+        for (int i = 2; i < str.length; i++) {
+            if (str[i] == ')') {
+                // 且右括号结尾才可能是有效括号
+                if (str[i - 1] != '(') {
+                    if ((i - 1) - dp[i - 1] >= 0 && str[(i - 1) - dp[i - 1]] == '(') {
+                        dp[i] = dp[i - 1] + 2 + (((i - 1) - dp[i - 1] - 1) >= 0 ? dp[(i - 1) - dp[i - 1] - 1] : 0);
+                    }
+                } else {
+                    dp[i] = 2 + dp[i - 2];
+                }
+            }
+            ans = Math.max(ans, dp[i]);
+        }
         return ans;
     }
-//    public static int longestValidParentheses(String s) {
-//        if (null == s || s.length() <= 1) {
-//            return 0;
-//        }
-//        char[] str = s.toCharArray();
-//        int n = str.length;
-//        // 必须以i结尾最长括号长度是多少
-//        int[] dp = new int[n];
-//        dp[0] = 0;
-//        dp[1] = (str[0] == '(' && str[1] == ')') ? 2 : 0;
-//        int max = dp[1];
-//        for (int i = 2; i < n; i++) {
-//            if (str[i] == ')') {
-//                if (str[i - 1] == '(') {
-//                    dp[i] = 2 + dp[i - 2];
-//                } else {
-//                    if (i - dp[i - 1] - 1 >= 0 && str[i - dp[i - 1] - 1] == '(') {
-//                        dp[i] = dp[i - 1] + 2 + (i - dp[i - 1] - 2 >= 0 ? dp[i - dp[i - 1] - 2] : 0);
-//                    }
-//                }
-//                max = Math.max(max, dp[i]);
-//            }
-//        }
-//        return max;
-//    }
+
+    public static void main(String[] args) {
+        String s = "()(())";
+        int i = longestValidParentheses(s);
+        System.out.println(i);
+    }
 }
