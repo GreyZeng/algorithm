3c9b6b7927e251b8df0d750875eb526b789fc5b1
GreyZeng <410486047@qq.com>
2022-04-15 19:06:09 +0800

    add todo

--- src/main/java/leetcode/hard/LeetCode_0032_LongestValidParentheses.java
+++ src/main/java/leetcode/hard/LeetCode_0032_LongestValidParentheses.java
@@ -27,38 +27,40 @@ package leetcode.hard;
 
 // tips: 以i结尾的答案全部收集一遍
 // ref: https://www.lintcode.com/problem/longest-valid-parentheses/description
+// https://leetcode-cn.com/problems/longest-valid-parentheses/
 public class LeetCode_0032_LongestValidParentheses {
-
-    public static int longestValidParentheses(String s) {
-        if (null == s || s.length() <= 1) {
+    public int longestValidParentheses(String s) {
+        if (s == null || s.length() <= 1) {
             return 0;
         }
         char[] str = s.toCharArray();
-        int n = str.length;
-        // 必须以i结尾最长括号长度是多少
-        int[] dp = new int[n];
-        dp[0] = 0;
-        dp[1] = (str[0] == '(' && str[1] == ')') ? 2 : 0;
-        int max = dp[1];
-        for (int i = 2; i < n; i++) {
-            if (str[i] == ')') {
-                if (str[i - 1] == '(') {
-                    dp[i] = 2 + dp[i - 2];
-                } else {
-                    if (i - dp[i - 1] - 1 >= 0 && str[i - dp[i - 1] - 1] == '(') {
-                        dp[i] = dp[i - 1] + 2 + (i - dp[i - 1] - 2 >= 0 ? dp[i - dp[i - 1] - 2] : 0);
-                    }
-                }
-                max = Math.max(max, dp[i]);
-            }
-        }
-        return max;
+        int ans = 0;
+        // TODO
+        return ans;
     }
-
-    public static void main(String[] args) {
-        String st = "((()))())";
-        int i = longestValidParentheses(st);
-        System.out.println(i);
-    }
-
+//    public static int longestValidParentheses(String s) {
+//        if (null == s || s.length() <= 1) {
+//            return 0;
+//        }
+//        char[] str = s.toCharArray();
+//        int n = str.length;
+//        // 必须以i结尾最长括号长度是多少
+//        int[] dp = new int[n];
+//        dp[0] = 0;
+//        dp[1] = (str[0] == '(' && str[1] == ')') ? 2 : 0;
+//        int max = dp[1];
+//        for (int i = 2; i < n; i++) {
+//            if (str[i] == ')') {
+//                if (str[i - 1] == '(') {
+//                    dp[i] = 2 + dp[i - 2];
+//                } else {
+//                    if (i - dp[i - 1] - 1 >= 0 && str[i - dp[i - 1] - 1] == '(') {
+//                        dp[i] = dp[i - 1] + 2 + (i - dp[i - 1] - 2 >= 0 ? dp[i - dp[i - 1] - 2] : 0);
+//                    }
+//                }
+//                max = Math.max(max, dp[i]);
+//            }
+//        }
+//        return max;
+//    }
 }
