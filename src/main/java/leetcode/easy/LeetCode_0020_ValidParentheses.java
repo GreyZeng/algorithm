8f6a68e7dfda43f5cd0edf7c4b36ef559b21aa3b
zenghui <410486047@qq.com>
2022-04-15 16:51:32 +0800

    leetcode 20

--- src/main/java/leetcode/easy/LeetCode_0020_ValidParentheses.java
+++ src/main/java/leetcode/easy/LeetCode_0020_ValidParentheses.java
@@ -1,45 +1,43 @@
 package leetcode.easy;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
+import java.util.Stack;
 
 // ()[]{} 三类括号
 // 栈
 // 左压 右出 且匹配
 // 栈空即可，只要有不满足就false
 // 如果只有一类括号，则不需要用栈，直接用一个变量控制即可
+// 左括号count++,右括号count--，任意时刻，如果count<0,直接不满足
+// https://leetcode-cn.com/problems/valid-parentheses
 // ref : https://www.lintcode.com/problem/valid-parentheses/description
 public class LeetCode_0020_ValidParentheses {
-	public static void main(String[] args) {
-		boolean b = isValid("()()()()()(){");
-		System.out.println(b);
-	}
-	public static boolean isValid(String s) {
-		if (s == null || s.length() == 0) {
-			return true;
-		}
-		char[] str = s.toCharArray();
-		Deque<Character> stack = new ArrayDeque<>();
-		for (char c : str) {
-			if (isLeft(c)) {
-				stack.push(c);
-			} else {
-				if (stack.isEmpty()) {
-					return false;
-				} else if (!isMatch(stack.poll(), c)) {
-					return false;
-				}
-			}
-		}
-		return stack.isEmpty();
-	}
+    public boolean isValid(String s) {
+        if (s == null || s.length() == 0) {
+            return true;
+        }
+        char[] str = s.toCharArray();
+        Stack<Character> stack = new Stack<>();
+        for (char c : str) {
+            if (isLeft(c)) {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                if (!isValid(stack.pop(), c)) {
+                    return false;
+                }
+            }
+        }
 
-	public static boolean isLeft(char c) {
-		return '(' == c || '{' == c || '[' == c;
-	}
+        return stack.isEmpty();
+    }
 
-	public static boolean isMatch(char left, char right) {
-		return (left == '[' && right == ']') || (left == '(' && right == ')') || (left == '{' && right == '}');
-	}
+    public boolean isLeft(char c) {
+        return c == '(' || c == '[' || c == '{';
+    }
 
+    public boolean isValid(char left, char right) {
+        return (left == '(' && right == ')') || (left == '[' && right == ']') || (left == '{' && right == '}');
+    }
 }
