5d841551cd95792459f88afa5090dd54cca66b0f
GreyZeng <410486047@qq.com>
2022-04-17 09:59:02 +0800

    update notes

--- src/main/java/leetcode/medium/LeetCode_0045_JumpGameII.java
+++ src/main/java/leetcode/medium/LeetCode_0045_JumpGameII.java
@@ -48,7 +48,7 @@ public class LeetCode_0045_JumpGameII {
         if (arr.length == 2) {
             return arr[0] == 0 ? Integer.MAX_VALUE : 1;
         }
-        // dp[i]表示：从i开始，跳到最后，最少步数是什么
+        // dp[i]表示：从i开始，跳到最后，最少步数是什么。
         int[] dp = new int[arr.length];
         // dp[arr.length-1] = 0; // 最后一个位置，跳0步就可以，数组不赋值默认就是0
         // 倒数第二个位置，如果arr[arr.length-2]=0，则永远无法跳到最后一个位置，如果arr[arr.length-2]>=1，则只需要一步就可以跳到最后
