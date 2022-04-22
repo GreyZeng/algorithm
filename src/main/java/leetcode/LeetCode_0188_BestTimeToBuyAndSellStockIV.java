68a5346ef878c9b66a5ca4f95539ed6b675d7386
GreyZeng <410486047@qq.com>
2022-04-23 00:25:25 +0800

    update code

--- src/main/java/leetcode/LeetCode_0188_BestTimeToBuyAndSellStockIV.java
+++ src/main/java/leetcode/LeetCode_0188_BestTimeToBuyAndSellStockIV.java
@@ -22,31 +22,33 @@ package leetcode;
 public class LeetCode_0188_BestTimeToBuyAndSellStockIV {
 
     // 暴力方法
-    // K> N/2 就等于无限次交易
+    // k> N/2 就等于无限次交易
     // 因为爬坡的数量不会大于N/2
     // dp[i][j] 0-i范围上，不超过j次交易
     // i位置如果要参与交易，只需要参加最后一次交易
     // i位置只需要最后一次交易的卖出时机
     // a. 最后一次的买入时机在i
     // b. 最后的买入时机是i-1
-    public static int maxProfit1(int K, int[] arr) {
+    public static int maxProfit1(int k, int[] arr) {
         if (arr == null || arr.length < 2) {
             return 0;
         }
         int N = arr.length;
-        if (K >= N >> 1) {
+        if (k >= N >> 1) {
             return infinityMax(arr);
         }
-        int[][] dp = new int[N][K + 1];
+        int[][] dp = new int[N][k + 1];
         for (int i = 1; i < N; i++) {
-            for (int j = 1; j <= K; j++) {
+            for (int j = 1; j <= k; j++) {
+                // i位置不参与交易，则dp[i][j]至少是dp[i-1][j]
                 dp[i][j] = dp[i - 1][j];
-                for (int k = 0; k <= i; k++) {
-                    dp[i][j] = Math.max(dp[k][j - 1] + arr[i] - arr[k], dp[i][j]);
+                for (int m = 0; m <= i; m++) {
+                    // 枚举每次买入的时机
+                    dp[i][j] = Math.max(dp[m][j - 1] - arr[m] + arr[i], dp[i][j]);
                 }
             }
         }
-        return dp[N - 1][K];
+        return dp[N - 1][k];
     }
 
     // 优化方法
@@ -60,23 +62,26 @@ public class LeetCode_0188_BestTimeToBuyAndSellStockIV {
     // b. 最后的买入时机是i-1
     // c. 最后的买入时机是i-2
     // ...
-    public static int maxProfit(int K, int[] arr) {
+    public static int maxProfit(int k, int[] arr) {
         if (arr == null || arr.length < 2) {
             return 0;
         }
         int N = arr.length;
-        if (K >= N >> 1) {
+        if (k >= N >> 1) {
             return infinityMax(arr);
         }
-        int[][] dp = new int[K + 1][N];
-        for (int i = 1; i <= K; i++) {
-            int maxDiff = dp[i - 1][0] - arr[0];
-            for (int j = 1; j < N; j++) {
-                dp[i][j] = Math.max(dp[i][j - 1], arr[j] + maxDiff);
-                maxDiff = Math.max(maxDiff, dp[i - 1][j] - arr[j]);
+        int[][] dp = new int[N][k + 1];
+        for (int j = 1; j <= k; j++) {
+            int p1 = dp[0][j];
+            int best = Math.max(dp[1][j - 1] - arr[1], dp[0][j - 1] - arr[0]);
+            dp[1][j] = Math.max(p1, best + arr[1]);
+            for (int i = 2; i < N; i++) {
+                p1 = dp[i - 1][j];
+                best = Math.max(dp[i][j - 1] - arr[i], best);
+                dp[i][j] = Math.max(p1, best + arr[i]);
             }
         }
-        return dp[K][N - 1];
+        return dp[N - 1][k];
     }
 
     public static int infinityMax(int[] arr) {
