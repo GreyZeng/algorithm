ad7b1949b984c575f41d26ccb5482a745900c7b3
GreyZeng <410486047@qq.com>
2022-05-15 21:03:37 +0800

    add note

--- src/main/java/leetcode/hard/LeetCode_0010_RegularExpressionMatching.java
+++ src/main/java/leetcode/hard/LeetCode_0010_RegularExpressionMatching.java
@@ -1,47 +1,38 @@
-/*
- * Given an input string (s) and a pattern (p), implement regular expression
- * matching with support for '.' and '*'.
- *
- * '.' Matches any single character. '*' Matches zero or more of the preceding
- * element.
- *
- * The matching should cover the entire input string (not partial).
- *
- * Note:
- *
- * s could be empty and contains only lowercase letters a-z. p could be empty
- * and contains only lowercase letters a-z, and characters like . or *.
- *
- * Example 1:
- *
- * Input: s = "aa" p = "a" Output: false Explanation: "a" does not match the
- * entire string "aa".
- *
- * Example 2:
- *
- * Input: s = "aa" p = "a*" Output: true Explanation: '*' means zero or more of
- * the preceding element, 'a'. Therefore, by repeating 'a' once, it becomes
- * "aa".
- *
- * Example 3:
- *
- * Input: s = "ab" p = ".*" Output: true Explanation: ".*" means
- * "zero or more (*) of any character (.)".
- *
- * Example 4:
- *
- * Input: s = "aab" p = "c*a*b" Output: true Explanation: c can be repeated 0
- * times, a can be repeated 1 time. Therefore, it matches "aab".
- *
- * Example 5:
- *
- * Input: s = "mississippi" p = "mis*is*p*." Output: false
- *1 <= s.length <= 20
- * 1 <= p.length <= 30
- * s contains only lowercase English letters.
- * p contains only lowercase English letters, '.', and '*'.
- * It is guaranteed for each appearance of the character '*', there will be a previous valid character to match.
- */
+// 给你一个字符串 s 和一个字符规律 p，请你来实现一个支持 '.' 和 '*' 的正则表达式匹配。
+
+// '.' 匹配任意单个字符
+// '*' 匹配零个或多个前面的那一个元素
+// 所谓匹配，是要涵盖 整个 字符串 s的，而不是部分字符串。
+
+//  
+// 示例 1：
+
+// 输入：s = "aa", p = "a"
+// 输出：false
+// 解释："a" 无法匹配 "aa" 整个字符串。
+// 示例 2:
+
+// 输入：s = "aa", p = "a*"
+// 输出：true
+// 解释：因为 '*' 代表可以匹配零个或多个前面的那一个元素, 在这里前面的元素就是 'a'。因此，字符串 "aa" 可被视为 'a' 重复了一次。
+// 示例 3：
+
+// 输入：s = "ab", p = ".*"
+// 输出：true
+// 解释：".*" 表示可匹配零个或多个（'*'）任意字符（'.'）。
+//  
+
+// 提示：
+
+// 1 <= s.length <= 20
+// 1 <= p.length <= 30
+// s 只包含从 a-z 的小写字母。
+// p 只包含从 a-z 的小写字母，以及字符 . 和 *。
+// 保证每次出现字符 * 时，前面都匹配到有效的字符
+
+// 来源：力扣（LeetCode）
+// 链接：https://leetcode.cn/problems/regular-expression-matching
+// 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 package leetcode.hard;
 
 public class LeetCode_0010_RegularExpressionMatching {
@@ -129,9 +120,9 @@ public class LeetCode_0010_RegularExpressionMatching {
         }
         // pi 不是最后一个位置，且 p[pi+1] == '*'
         // 看p[pi] 和 s[si] 是否相等
-//        if (p[pi] != s[si] && p[pi] != '.') {
-//            return false;
-//        }
+        // if (p[pi] != s[si] && p[pi] != '.') {
+        // return false;
+        // }
         // p[pi] == '.' 或者 p[pi] == s[si] 且 p[pi + 1] = '*'
         // p[pi] 和 p[pi+1]至少可以消解为空串，即p[pi]位置不做匹配
         if (process0(s, p, si, pi + 2)) {
