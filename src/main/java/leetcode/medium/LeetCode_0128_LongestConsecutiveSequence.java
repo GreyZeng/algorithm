03fb9abe1c834b832d114ff2f3fd3f5b2be5de31
GreyZeng <410486047@qq.com>
2022-05-15 20:59:02 +0800

    LeetCode_0128_LongestConsecutiveSequence

--- src/main/java/leetcode/medium/LeetCode_0128_LongestConsecutiveSequence.java
+++ src/main/java/leetcode/medium/LeetCode_0128_LongestConsecutiveSequence.java
@@ -3,34 +3,29 @@ package leetcode.medium;
 import java.util.HashMap;
 import java.util.Map;
 
-//Given an unsorted array of integers, find the length of the longest consecutive elements sequence.
-//
-//        Your algorithm should run in O(n) complexity.
-//
-//        Example:
-//
-//        Input: [100, 4, 200, 1, 3, 2]
-//        Output: 4
-//        Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefore its length is 4.
+// https://leetcode.com/problems/longest-consecutive-sequence/
+// 最长连续数组长度
 public class LeetCode_0128_LongestConsecutiveSequence {
 
     // Hash表标记开始位置和长度
-    // 两个hash表 ，一个表示头<num, 数量> 一个表示尾<num, 数量>
-    public static int longestConsecutive(int[] nums) {
-        if (nums == null || nums.length == 0) {
+    public static int longestConsecutive(int[] arr) {
+        if (arr == null || arr.length == 0) {
             return 0;
         }
         Map<Integer, Integer> map = new HashMap<>();
         int max = 1;
-        for (int n : nums) {
-            if (!map.containsKey(n)) {
-                map.put(n, 1);
-                int L = map.get(n - 1) == null ? 0 : map.get(n - 1);
-                int R = map.get(n + 1) == null ? 0 : map.get(n + 1);
+        for (int num : arr) {
+            if (!map.containsKey(num)) {
+                // 至少是一个长度为1的连续数组
+                map.put(num, 1);
+                // 由于num的到来，会让num-1位置上和num+1位置上（如果有的话）的最长连续数组变大
+                int L = map.getOrDefault(num - 1, 0);
+                int R = map.getOrDefault(num + 1, 0);
                 int all = L + R + 1;
-                map.put(n - L, all);
-                map.put(n + R, all);
                 max = Math.max(all, max);
+                // 由于num的到来，会扩充两个位置的值，以num-L开头和以num-R结尾的
+                map.put(num - L, all);
+                map.put(num + R, all);
             }
         }
         return max;
