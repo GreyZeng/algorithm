34edee788c7c70ce6a784ead1ffd843cfd4ea8c0
GreyZeng <410486047@qq.com>
2022-05-22 22:12:13 +0800

    update notes

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -2,11 +2,10 @@ import java.util.TreeMap;
 
 public class Test {
     public static void main(String[] args) {
-    	TreeMap<Integer,Integer> map = new TreeMap<>();
-    	map.put(1, 2);
-    	map.put(1, 3);
-    	map.put(2, 2);
-    	System.out.println(map);
-    	System.out.println("hello 测试");
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        map.put(1, 2);
+        map.put(1, 3);
+        System.out.println(map);
+        System.out.println("hello 测试");
     }
 }
