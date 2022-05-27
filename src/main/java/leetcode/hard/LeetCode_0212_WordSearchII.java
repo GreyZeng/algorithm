f37342db9c90e69fd716209ab66f8c437a1f6dce
zenghui <410486047@qq.com>
2022-05-27 16:48:20 +0800

    update notes

--- src/main/java/leetcode/hard/LeetCode_0212_WordSearchII.java
+++ src/main/java/leetcode/hard/LeetCode_0212_WordSearchII.java
@@ -16,105 +16,114 @@ import java.util.List;
 //]
 //words=["oath","pea","eat","rain"]
 //输出：["eat","oath"]
-//LeetCode_0212_WordSearchII
+//提示：
+//
+//        m == board.length
+//        n == board[i].length
+//        1 <= m, n <= 12
+//        board[i][j] 是一个小写英文字母
+//        1 <= words.length <= 3 * 104
+//        1 <= words[i].length <= 10
+//        words[i] 由小写英文字母组成
+//        words 中的所有字符串互不相同
+//
+//        来源：力扣（LeetCode）
+//        链接：https://leetcode.cn/problems/word-search-ii
+//        著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 //tips:
 //0.把words做成前缀树，加速
 //1.不能重复走，置为0，然后要恢复现场
 //2.深度优先
-// https://leetcode-cn.com/problems/word-search-ii/
 public class LeetCode_0212_WordSearchII {
-	public static class Trie {
-		public Trie[] next;
-		public int pass;
-		public int end;
+    public static class Trie {
+        public Trie[] next;
+        public int pass;
+        public int end;
 
-		public Trie() {
-			next = new Trie[26];
-			pass = 0;
-			end = 0;
-		}
+        public Trie() {
+            next = new Trie[26];
+            pass = 0;
+            end = 0;
+        }
+    }
 
-	}
+    // 利用前缀树加速
+    public List<String> findWords(char[][] board, String[] words) {
+        HashSet<String> set = new HashSet<>();
+        Trie trie = new Trie();
+        for (String word : words) {
+            if (!set.contains(word)) {
+                set.add(word);
+                buildTrie(trie, word);
+            }
+        }
+        List<String> ans = new ArrayList<>();
+        LinkedList<Character> path = new LinkedList<>();
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                p(board, i, j, path, trie, ans);
+            }
+        }
+        return ans;
+    }
 
-	// 利用前缀树加速
-	public List<String> findWords(char[][] board, String[] words) {
-		HashSet<String> set = new HashSet<>();
-		Trie trie = new Trie();
-		for (String word : words) {
-			if (!set.contains(word)) {
-				set.add(word);
-				buildTrie(trie, word);
-			}
-		}
-		int M = board.length;
-		int N = board[0].length;
-		List<String> ans = new ArrayList<>();
-		LinkedList<Character> path = new LinkedList<>();
-		for (int i = 0; i < M; i++) {
-			for (int j = 0; j < N; j++) {
-				p(board, i, j, path, trie, ans, M, N);
-			}
-		}
-		return ans;
-	}
+    // 从(i,j)出发，在board中能走出的所有答案都在ans里面，
+    // path为前面已经走的路生成的字符串
+    // 前缀树trie用于杀死不可能的情况
+    private static int p(char[][] board, int i, int j, LinkedList<Character> path, Trie trie, List<String> ans) {
+        if (i == board.length || j == board[0].length || i < 0 || j < 0) {
+            return 0;
+        }
+        char c = board[i][j];
+        if (c == '0') {
+            // 不走回头路
+            return 0;
+        }
+        int index = c - 'a';
+        if (trie.next[index] == null || trie.next[index].pass == 0) {
+            return 0;
+        }
+        path.addLast(c);
+        trie = trie.next[index];
+        int fix = 0;
+        if (trie.end == 1) {
+            ans.add(buildResult(path));
+            trie.end--;
+            fix++;
+        }
+        board[i][j] = '0';
+        fix += p(board, i - 1, j, path, trie, ans);
+        fix += p(board, i, j - 1, path, trie, ans);
+        fix += p(board, i + 1, j, path, trie, ans);
+        fix += p(board, i, j + 1, path, trie, ans);
+        board[i][j] = c;
+        path.pollLast();
+        trie.pass -= fix;
+        return fix;
+    }
 
-	private static int p(char[][] board, int i, int j, LinkedList<Character> path, Trie trie, List<String> ans, int M,
-			int N) {
-		if (i == M || j == N || i < 0 || j < 0) {
-			return 0;
-		}
-		char c = board[i][j];
+    private static String buildResult(LinkedList<Character> path) {
+        LinkedList<Character> t = new LinkedList<>(path);
+        StringBuilder sb = new StringBuilder();
+        while (!t.isEmpty()) {
+            sb.append(t.pollFirst());
+        }
+        return sb.toString();
+    }
 
-		if (c == '0') {
-			return 0;
-		}
-		int index = c - 'a';
-		if (trie.next[index] == null || trie.next[index].pass == 0) {
-			return 0;
-		}
-		path.addLast(c);
-		trie = trie.next[index];
-		int fix = 0;
-		if (trie.end == 1) {
-			ans.add(buildResult(path));
-			trie.end--;
-			fix++;
-		}
-
-		board[i][j] = '0';
-		fix += p(board, i - 1, j, path, trie, ans, M, N);
-		fix += p(board, i, j - 1, path, trie, ans, M, N);
-		fix += p(board, i + 1, j, path, trie, ans, M, N);
-		fix += p(board, i, j + 1, path, trie, ans, M, N);
-		board[i][j] = c;
-		path.pollLast();
-		trie.pass -= fix;
-		return fix;
-	}
-
-	private static String buildResult(LinkedList<Character> path) {
-		LinkedList<Character> t = new LinkedList<>(path);
-		StringBuilder sb = new StringBuilder();
-		while (!t.isEmpty()) {
-			sb.append(t.pollFirst());
-		}
-		return sb.toString();
-
-	}
-
-	// 将word转换成前缀树
-	private void buildTrie(Trie trie, String word) {
-		trie.pass++;
-		char[] s = word.toCharArray();
-		int index = 0;
-		for (int i = 0; i < s.length; i++) {
-			index = s[i] - 'a';
-			if (trie.next[index] == null) {
-				trie.next[index] = new Trie();
-			}
-			trie = trie.next[index];
-			trie.pass++;
-		}
-		trie.end++;
-	}
+    // 将word转换成前缀树
+    private void buildTrie(Trie trie, String word) {
+        trie.pass++;
+        char[] s = word.toCharArray();
+        int index;
+        for (char c : s) {
+            index = c - 'a';
+            if (trie.next[index] == null) {
+                trie.next[index] = new Trie();
+            }
+            trie = trie.next[index];
+            trie.pass++;
+        }
+        trie.end++;
+    }
 }
