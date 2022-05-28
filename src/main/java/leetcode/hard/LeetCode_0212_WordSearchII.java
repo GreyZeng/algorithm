cfbb886ce7026f4ac7122202ff4aff909657e18f
GreyZeng <410486047@qq.com>
2022-05-28 14:42:25 +0800

    leetcode 212

--- src/main/java/leetcode/hard/LeetCode_0212_WordSearchII.java
+++ src/main/java/leetcode/hard/LeetCode_0212_WordSearchII.java
@@ -1,9 +1,7 @@
 package leetcode.hard;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.List;
+
+import java.util.*;
 
 //给定一个字符类型的二维数组board，和一个字符串组成的列表words。可以从board任何位置出发，每一步可以走向上、下、左、右，四个方向，但是一条路径已经走过的位置，不能重复走。
 //返回words哪些单词可以被走出来。
@@ -39,7 +37,6 @@ public class LeetCode_0212_WordSearchII {
         public Trie[] next;
         public int pass;
         public boolean end;
-
         public Trie() {
             next = new Trie[26];
             pass = 0;
@@ -47,22 +44,22 @@ public class LeetCode_0212_WordSearchII {
         }
     }
 
-    // 利用前缀树加速
-    public List<String> findWords(char[][] board, String[] words) {
-        HashSet<String> set = new HashSet<>();
+
+    public static List<String> findWords(char[][] board, String[] words){
+        Set<String> set = new HashSet<>();
         Trie trie = new Trie();
         for (String word : words) {
-            if (!set.contains(word)) {
+            if (!set.contains(word)){
                 set.add(word);
-                buildTrie(trie, word);
+                buildTrie(trie,word);
             }
         }
+        LinkedList<Character> pre= new LinkedList<>();
         List<String> ans = new ArrayList<>();
-        LinkedList<Character> path = new LinkedList<>();
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[0].length; j++) {
-                int size = p(board, i, j, path, trie, ans);
-                if (size == set.size()){
+        for (int i = 0; i < board.length;i++) {
+            for (int j = 0; j < board[0].length;j++) {
+                int times = process(i,j,pre,ans,board,trie);
+                if (times == set.size()) {
                     return new ArrayList<>(set);
                 }
             }
@@ -70,11 +67,10 @@ public class LeetCode_0212_WordSearchII {
         return ans;
     }
 
-    // 从(i,j)出发，在board中能走出的所有答案都在ans里面，
-    // path为前面已经走的路生成的字符串
-    // 前缀树trie用于杀死不可能的情况
-    private static int p(char[][] board, int i, int j, LinkedList<Character> path, Trie trie, List<String> ans) {
-        if (i == board.length || j == board[0].length || i < 0 || j < 0) {
+
+
+    public static int process(int i, int j, LinkedList<Character> pre, List<String> ans, char[][] board, Trie trie){
+        if (i >= board.length || i < 0 || j >= board[0].length || j < 0) {
             return 0;
         }
         char c = board[i][j];
@@ -82,54 +78,49 @@ public class LeetCode_0212_WordSearchII {
             // 不走回头路
             return 0;
         }
-        int index = c - 'a';
+        int index= c - 'a';
         if (trie.next[index] == null || trie.next[index].pass == 0) {
+            // 没有路可以走
             return 0;
         }
-        path.addLast(c);
+        pre.addLast(c);
         trie = trie.next[index];
+
         int fix = 0;
-        if (trie.end) {
-            // 到这一步说明找到了匹配的单词
-            ans.add(buildResult(path));
-            trie.end = false;
+        if(trie.end) {
+            ans.add(buildString(pre));
+            trie.end=false;
             fix++;
         }
         board[i][j] = '0';
-        fix += p(board, i - 1, j, path, trie, ans);
-
-        fix += p(board, i, j - 1, path, trie, ans);
-
-        fix += p(board, i + 1, j, path, trie, ans);
-
-        fix += p(board, i, j + 1, path, trie, ans);
-
+        fix +=process(i+1,j,pre,ans,board,trie);
+        fix+=process(i,j+1,pre,ans,board,trie);
+        fix+=process(i-1,j,pre,ans,board,trie);
+        fix+=process(i,j-1,pre,ans,board,trie);
         board[i][j] = c;
-        path.pollLast();
-        trie.pass -= fix;
+        pre.pollLast();
+        trie.pass-=fix;
         return fix;
     }
 
-    private static String buildResult(LinkedList<Character> path) {
-        LinkedList<Character> t = new LinkedList<>(path);
+    private static String buildString(LinkedList<Character> pre) {
+        LinkedList<Character> preCopy = new LinkedList<>(pre);
         StringBuilder sb = new StringBuilder();
-        while (!t.isEmpty()) {
-            sb.append(t.pollFirst());
+        while (!preCopy.isEmpty()) {
+            Character c = preCopy.pollFirst();
+            sb.append(c);
         }
         return sb.toString();
+
     }
 
-    // 将word转换成前缀树
-    private void buildTrie(Trie trie, String word) {
-        trie.pass++;
-        char[] s = word.toCharArray();
-        int index;
-        for (char c : s) {
-            index = c - 'a';
-            if (trie.next[index] == null) {
-                trie.next[index] = new Trie();
+    private static void buildTrie(Trie trie, String word) {
+        char[] str = word.toCharArray();
+        for (char c : str) {
+            if (trie.next[c - 'a'] == null) {
+                trie.next[c - 'a'] = new Trie();
             }
-            trie = trie.next[index];
+            trie = trie.next[c - 'a'];
             trie.pass++;
         }
         trie.end = true;
