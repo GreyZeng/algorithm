f6ccfcc3998596977fad27d2f89dbecbf8e0fd05
GreyZeng <410486047@qq.com>
2022-05-30 13:53:18 +0800

    update code

--- src/main/java/nowcoder/NowCoder_MaxSumSquare.java
+++ src/main/java/nowcoder/NowCoder_MaxSumSquare.java
@@ -6,23 +6,27 @@ import java.util.Scanner;
 // 测评链接：https://www.nowcoder.com/questionTerminal/cb82a97dcd0d48a7b1f4ee917e2c0409
 // 压缩数组技巧 O(N^2 * M) --> 考虑一下N和M的长度
 // 启发：数组的最大累加和: https://leetcode-cn.com/problems/maximum-subarray/
+// 笔记：https://www.cnblogs.com/greyzeng/p/16326526.html
 public class NowCoder_MaxSumSquare {
-    public static int maxSum(int[][] matrix, int n, int m) {
-        int[] t;
+    public static int maxSum(int[][] matrix) {
+        // max用于记录全局最大值，它至少可以是: 必须是第0行的子数组的最大累加和
         int max = maxSubArray(matrix[0]);
-        for (int i = 0; i < n; i++) {
-            max = Math.max(maxSubArray(matrix[i]), max);
-            t = matrix[i];
-            for (int k = i + 1; k < n; k++) {
-                for (int j = 0; j < m; j++) {
-                    t[j] += matrix[k][j];
+        int[] tmp;
+        for (int i = 1; i < matrix.length; i++) {
+            // 必须以i位置为底的最大子矩阵累加和
+            max = Math.max(max, maxSubArray(matrix[i]));
+            tmp = matrix[i];
+            for (int j = i + 1; j < matrix.length; j++) {
+                for (int k = 0; k < matrix[0].length; k++) {
+                    tmp[k] += matrix[j][k];
                 }
-                max = Math.max(max, maxSubArray(t));
+                max = Math.max(max, maxSubArray(tmp));
             }
         }
         return max;
     }
 
+    // 求子数组的最大累加和
     public static int maxSubArray(int[] arr) {
         int pre = arr[0];
         int max = pre;
@@ -34,7 +38,7 @@ public class NowCoder_MaxSumSquare {
         return max;
     }
 
-    public static void main(String[] args) {
+        public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
         int m = in.nextInt();
@@ -44,7 +48,7 @@ public class NowCoder_MaxSumSquare {
                 matrix[i][j] = in.nextInt();
             }
         }
-        System.out.println(maxSum(matrix, n, m));
+        System.out.println(maxSum(matrix));
         in.close();
     }
 
