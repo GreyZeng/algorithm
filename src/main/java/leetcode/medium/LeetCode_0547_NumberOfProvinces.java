637920a4bbbac68afd3d9be358847e18f46c9bab
GreyZeng <410486047@qq.com>
2022-06-04 23:20:10 +0800

    leetcode 547 number of provinces

--- src/main/java/leetcode/medium/LeetCode_0547_NumberOfProvinces.java
+++ src/main/java/leetcode/medium/LeetCode_0547_NumberOfProvinces.java
@@ -1,78 +1,71 @@
 package leetcode.medium;
 
 // https://leetcode.com/problems/number-of-provinces/
+// 笔记：https://www.cnblogs.com/greyzeng/p/16343068.html
 public class LeetCode_0547_NumberOfProvinces {
-    // m是正方形矩阵
-    public static int findCircleNum(int[][] M) {
-        int N = M.length;
-        // {0} {1} {2} {N-1}
-        UnionFind unionFind = new UnionFind(N);
-        // 只需要遍历对角线上半区即可
-        for (int i = 0; i < N; i++) {
-            for (int j = i + 1; j < N; j++) {
-                if (M[i][j] == 1) { // i和j互相认识
-                    unionFind.union(i, j);
+    public static int findCircleNum(int[][] m) {
+        int n = m.length;
+        UF uf = new UF(n);
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (m[i][j] == 1) {
+                    uf.union(i, j);
                 }
             }
         }
-        return unionFind.sets();
+        return uf.setSize();
     }
 
-    public static class UnionFind {
-        // parent[i] = k ： i的父亲是k
-        private int[] parent;
-        // size[i] = k ： 如果i是代表节点，size[i]才有意义，否则无意义
-        // i所在的集合大小是多少
-        private int[] size;
-        // 辅助结构
-        private int[] help;
-        // 一共有多少个集合
-        private int sets;
+    public static class UF {
+        int[] parent;
+        int[] help;
+        int[] size;
+        int sets;
 
-        public UnionFind(int n) {
-            parent = new int[n];
+        public UF(int n) {
             size = new int[n];
+            parent = new int[n];
             help = new int[n];
-            sets = n;
             for (int i = 0; i < n; i++) {
-                size[i] = 1;
                 parent[i] = i;
+                size[i] = 1;
+            }
+            sets = n;
+        }
+
+        public void union(int i, int j) {
+            if (i == j) {
+                return;
+            }
+            int p1 = find(i);
+            int p2 = find(j);
+            if (p2 != p1) {
+                int size1 = size[p1];
+                int size2 = size[p2];
+                if (size1 > size2) {
+                    parent[p2] = p1;
+                    size[p1] += size2;
+                } else {
+                    parent[p1] = p2;
+                    size[p2] += size1;
+                }
+                sets--;
             }
         }
 
         public int find(int i) {
-            int index = 0;
+            int hi = 0;
             while (i != parent[i]) {
-                help[index++] = i;
+                help[hi++] = i;
                 i = parent[i];
             }
-            index--;
-            for (; index >= 0; index--) {
+            for (int index = 0; index < hi; index++) {
                 parent[help[index]] = i;
             }
             return i;
         }
 
-        public void union(int i, int j) {
-            int f1 = find(i);
-            int f2 = find(j);
-            if (f1 != f2) {
-                sets--;
-                int s1 = size[i];
-                int s2 = size[j];
-                if (s1 > s2) {
-                    size[f1] = s1 + s2;
-                    size[f2] = 0;
-                    parent[f2] = f1;
-                } else {
-                    size[f2] = 0;
-                    size[f1] = s1 + s2;
-                    parent[f1] = f2;
-                }
-            }
-        }
-
-        public int sets() {
+        public int setSize() {
             return sets;
         }
     }
