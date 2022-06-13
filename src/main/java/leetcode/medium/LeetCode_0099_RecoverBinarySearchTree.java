1b501f1313e6e08fb08b7e7d33ed4c59886e8d51
GreyZeng <410486047@qq.com>
2022-06-13 13:44:15 +0800

    leetcode 99

--- src/main/java/leetcode/medium/LeetCode_0099_RecoverBinarySearchTree.java
+++ src/main/java/leetcode/medium/LeetCode_0099_RecoverBinarySearchTree.java
@@ -1,28 +1,15 @@
 /*You are given the root of a binary search tree (BST), where exactly two nodes of the tree were swapped by mistake. Recover the tree without changing its structure.
 
         Follow up: A solution using O(n) space is pretty straight forward. Could you devise a constant space solution?*/
-package leetcode;
+package leetcode.medium;
 
 import java.util.Stack;
-
+// https://leetcode.cn/problems/recover-binary-search-tree/
 public class LeetCode_0099_RecoverBinarySearchTree {
     public class TreeNode {
         int val;
         TreeNode left;
         TreeNode right;
-
-        TreeNode() {
-        }
-
-        TreeNode(int val) {
-            this.val = val;
-        }
-
-        TreeNode(int val, TreeNode left, TreeNode right) {
-            this.val = val;
-            this.left = left;
-            this.right = right;
-        }
     }
 
     // 中序遍历 第一次降序的头节点，第二次降序的尾节点
