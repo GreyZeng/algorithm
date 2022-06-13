8cd419f38f50f217c78c5b37f4e685dc35eff3a3
zenghui <410486047@qq.com>
2022-06-13 16:29:59 +0800

    in order travel bt

--- src/main/java/leetcode/easy/LeetCode_0094_BinaryTreeInorderTraversal.java
+++ src/main/java/leetcode/easy/LeetCode_0094_BinaryTreeInorderTraversal.java
@@ -1,6 +1,8 @@
 package leetcode.easy;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Deque;
 import java.util.List;
 import java.util.Stack;
 
@@ -48,29 +50,33 @@ public class LeetCode_0094_BinaryTreeInorderTraversal {
     }
 
     // 【非递归】中序遍历
-    // 0. 使用栈
-    // 1.整条左边界入栈
-    // 2.弹出就打印
-    // 3.来到右树上继续执行1
-    public static List<Integer> inorderTraversal2(TreeNode head) {
-        List<Integer> ans = new ArrayList<>();
-        if (head == null) {
-            return ans;
+    //    第一步，整条左边界入栈。
+    //    第二步，弹出就收集答案。
+    //    第三步，来到右树上执行同第一步的操作。
+    //    第四步，直到栈为空。
+    public List<Integer> inorderTraversal2(TreeNode root) {
+        if (root == null) {
+            return new ArrayList<>();
         }
-        Stack<TreeNode> stack = new Stack<>();
-        TreeNode cur = head;
+        List<Integer> ans = new ArrayList<>();
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        TreeNode cur = root;
         while (!stack.isEmpty() || cur != null) {
             if (cur != null) {
+                // 整条左边界入栈
                 stack.push(cur);
                 cur = cur.left;
             } else {
-                TreeNode pop = stack.pop();
-                ans.add(pop.val);
-                cur = pop.right;
+                TreeNode node = stack.pop();
+                // 弹出收集答案
+                ans.add(node.val);
+                // 来到右树上做同样的操作
+                cur = node.right;
             }
         }
         return ans;
     }
+
 
     // morris遍历方式
     // 第二次来到自己的时候收集
