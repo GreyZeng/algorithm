c0f7d8b950cf0da3021c57125e0d53fce89ca471
GreyZeng <410486047@qq.com>
2022-06-13 13:37:46 +0800

    leetcode 41 first missing positive

--- src/main/java/leetcode/hard/LeetCode_0041_FirstMissingPositive.java
+++ src/main/java/leetcode/hard/LeetCode_0041_FirstMissingPositive.java
@@ -1,16 +1,3 @@
-/**
- * Given an unsorted integer array, find the smallest missing positive integer.
- * <p>
- * Example 1:
- * <p>
- * Input: [1,2,0] Output: 3 Example 2:
- * <p>
- * Input: [3,4,-1,1] Output: 2 Example 3:
- * <p>
- * Input: [7,8,9,11,12] Output: 1 Follow up:
- * <p>
- * Your algorithm should run in O(n) time and uses constant extra space.
- */
 package leetcode.hard;
 
 //有效区：i位置上一定要保证i上面放着i+1
@@ -18,28 +5,27 @@ package leetcode.hard;
 //R变量：垃圾区开始，期待返回的值就是R+1
 // https://leetcode.cn/problems/first-missing-positive/
 public class LeetCode_0041_FirstMissingPositive {
-    // L 指针和R指针
-    public static int firstMissingPositive(int[] arr) {
-        int L = 0;
-        int R = arr.length - 1;
-        while (L <= R) {
-            if (arr[L] == L + 1) {
-                L++;
-            } else if (arr[L] < L + 1 || arr[L] > R + 1 || arr[arr[L] - 1] == arr[L]) {
-                swap(arr, R--, L);
+    public static int firstMissingPositive(int[] arr){
+        int l = 0;
+        int r = arr.length - 1;
+        while (l <= r) {
+            int v = arr[l];
+            if (v == l + 1) {
+                l++;
+            } else if (v < l + 1 || v > r + 1 || (arr[v - 1] == v)) {
+                // 垃圾数据
+                swap(arr, l,r--);
             } else {
-                swap(arr, L, arr[L] - 1);
+                swap(arr,l,v-1);
             }
         }
-        return L + 1;
+        return l + 1;
     }
-
-    public static void swap(int[] arr, int L, int R) {
-        if (arr.length == 0 || L == R) {
-            return;
+    public static void swap(int[] arr ,int i, int j ) {
+        if (i!=j) {
+            arr[i] = arr[i]^arr[j];
+            arr[j] = arr[i]^arr[j];
+            arr[i] = arr[i]^arr[j];
         }
-        arr[L] = arr[L] ^ arr[R];
-        arr[R] = arr[L] ^ arr[R];
-        arr[L] = arr[L] ^ arr[R];
     }
 }
