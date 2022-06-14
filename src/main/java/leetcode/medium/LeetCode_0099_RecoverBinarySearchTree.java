f7fe1b7c195feeeba6338fbf64d1b43aa5fd6430
zenghui <410486047@qq.com>
2022-06-14 09:24:17 +0800

    leetcode 99 recover bst

--- src/main/java/leetcode/medium/LeetCode_0099_RecoverBinarySearchTree.java
+++ src/main/java/leetcode/medium/LeetCode_0099_RecoverBinarySearchTree.java
@@ -1,9 +1,7 @@
 package leetcode.medium;
 
 import java.util.ArrayDeque;
-import java.util.ArrayList;
 import java.util.Deque;
-import java.util.List;
 
 // https://leetcode.cn/problems/recover-binary-search-tree/
 public class LeetCode_0099_RecoverBinarySearchTree {
@@ -32,40 +30,28 @@ public class LeetCode_0099_RecoverBinarySearchTree {
     }
 
     private static TreeNode[] getTwoErrNodes(TreeNode root) {
-        // TODO
-        return null;
+        TreeNode[] errorNode = new TreeNode[2];
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        TreeNode cur = root;
+        TreeNode pre = null;
+        while (!stack.isEmpty() || cur != null) {
+            if (cur != null) {
+                stack.push(cur);
+                cur = cur.left;
+            } else {
+                TreeNode node = stack.pop();
+                if (pre != null) {
+                    if (pre.val > node.val) {
+                        // 第一次降序的头节点
+                        errorNode[0] = errorNode[0] == null ? pre : errorNode[0];
+                        // 第二次降序的尾节点
+                        errorNode[1] = node;
+                    }
+                }
+                pre = node;
+                cur = node.right;
+            }
+        }
+        return errorNode;
     }
-//    public static void recoverTree2(TreeNode root) {
-//        TreeNode[] twoErrNodes = getTwoErrNodes(root);
-//        twoErrNodes[0].val = twoErrNodes[0].val ^ twoErrNodes[1].val;
-//        twoErrNodes[1].val = twoErrNodes[0].val ^ twoErrNodes[1].val;
-//        twoErrNodes[0].val = twoErrNodes[0].val ^ twoErrNodes[1].val;
-//    }
-//
-//    // 中序遍历
-//    public static TreeNode[] getTwoErrNodes(TreeNode head) {
-//        TreeNode[] errs = new TreeNode[2];
-//        Deque<TreeNode> stack = new ArrayDeque<>();
-//        TreeNode pre = null;
-//        TreeNode cur = head;
-//        while (!stack.isEmpty() || cur != null) {
-//            if (cur != null) {
-//                stack.push(cur);
-//                cur = cur.left;
-//            } else {
-//                cur = stack.pop();
-//                if (pre != null && pre.val > cur.val) {
-//                    // 第一次降序的头节点
-//                    errs[0] = errs[0] == null ? pre : errs[0];
-//                    // 第二次降序的尾节点
-//                    errs[1] = cur;
-//                }
-//                pre = cur;
-//                cur = cur.right;
-//            }
-//        }
-//        return errs;
-//    }
-
-
 }
