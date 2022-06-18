e316ff791b1d0da005e5221913b0d08013021e69
GreyZeng <410486047@qq.com>
2022-06-18 15:38:38 +0800

    leetcode 99

--- src/main/java/leetcode/medium/LeetCode_0099_RecoverBinarySearchTree.java
+++ src/main/java/leetcode/medium/LeetCode_0099_RecoverBinarySearchTree.java
@@ -12,46 +12,57 @@ public class LeetCode_0099_RecoverBinarySearchTree {
     }
 
     // Morris遍历，最优解，空间复杂度O(1)
-    public static void recoverTree(TreeNode root) {
-        // TODO
-    }
-
-    // 空间复杂度O(N)，非最优解
     // 中序遍历
     // 第一个错误节点：第一次降序的头节点
     // 第二个错误节点：第二次降序的尾节点
     // 这里只需要交换两个节点的val就可以AC，
     // 实际上更好的处理方式应该是节点真实的进行交换
-    public static void recoverTree2(TreeNode root) {
-        TreeNode[] twoErrNodes = getTwoErrNodes(root);
+    public static void recoverTree(TreeNode root) {
+        if (null == root) {
+            return;
+        }
+        TreeNode[] twoErrNodes = new TreeNode[2];
+        TreeNode pre = null;
+        TreeNode cur = root;
+        TreeNode mostRight;
+        while (cur != null) {
+            mostRight = cur.left;
+            if (mostRight != null) {
+                while (mostRight.right != null && mostRight.right != cur) {
+                    mostRight = mostRight.right;
+                }
+                if (mostRight.right == null) {
+                    mostRight.right = cur;
+                    cur = cur.left;
+                    continue;
+                } else {
+                    if (pre != null) {
+                        if (pre.val > cur.val) {
+                            // 第一次降序的头节点
+                            twoErrNodes[0] = twoErrNodes[0] == null ? pre : twoErrNodes[0];
+                            // 第二次降序的尾节点
+                            twoErrNodes[1] = cur;
+                        }
+                    }
+                    pre = cur;
+                    mostRight.right = null;
+                }
+            } else {
+                if (pre != null) {
+                    if (pre.val > cur.val) {
+                        // 第一次降序的头节点
+                        twoErrNodes[0] = twoErrNodes[0] == null ? pre : twoErrNodes[0];
+                        // 第二次降序的尾节点
+                        twoErrNodes[1] = cur;
+                    }
+                }
+                pre = cur;
+            }
+            cur = cur.right;
+        }
         twoErrNodes[0].val = twoErrNodes[0].val ^ twoErrNodes[1].val;
         twoErrNodes[1].val = twoErrNodes[0].val ^ twoErrNodes[1].val;
         twoErrNodes[0].val = twoErrNodes[0].val ^ twoErrNodes[1].val;
     }
 
-    private static TreeNode[] getTwoErrNodes(TreeNode root) {
-        TreeNode[] errorNode = new TreeNode[2];
-        Deque<TreeNode> stack = new ArrayDeque<>();
-        TreeNode cur = root;
-        TreeNode pre = null;
-        while (!stack.isEmpty() || cur != null) {
-            if (cur != null) {
-                stack.push(cur);
-                cur = cur.left;
-            } else {
-                TreeNode node = stack.pop();
-                if (pre != null) {
-                    if (pre.val > node.val) {
-                        // 第一次降序的头节点
-                        errorNode[0] = errorNode[0] == null ? pre : errorNode[0];
-                        // 第二次降序的尾节点
-                        errorNode[1] = node;
-                    }
-                }
-                pre = node;
-                cur = node.right;
-            }
-        }
-        return errorNode;
-    }
 }
