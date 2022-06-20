3295a6da82afdd476e380e23d97c99d76990af40
GreyZeng <410486047@qq.com>
2022-06-20 20:31:17 +0800

    update josephus

--- src/main/java/leetcode/cn/LeetCodeCN_0062_Josephus.java
+++ src/main/java/leetcode/cn/LeetCodeCN_0062_Josephus.java
@@ -16,10 +16,20 @@ package leetcode.cn;
 
 // 约瑟夫环问题
 public class LeetCodeCN_0062_Josephus {
-    public static int lastRemaining(int i, int m) {
-        if (i == 1) {
+    public int lastRemaining(int n, int m) {
+        if (n == 1) {
             return 0;
         }
-        return (lastRemaining(i - 1, m) + m) % i;
+        return (lastRemaining(n - 1, m) + m) % n;
     }
+
+    // 迭代方法
+    public int lastRemaining2(int n, int m) {
+        int f = 0;
+        for (int i = 2; i <= n; i++) {
+            f = (m + f) % i;
+        }
+        return f;
+    }
+
 }
