b40e6965a74767efb4d2473361597484b44e30a0
zenghui <410486047@qq.com>
2022-06-24 11:19:13 +0800

    formatter

--- src/main/java/nowcoder/NowCoder_PreAndMidToPos.java
+++ src/main/java/nowcoder/NowCoder_PreAndMidToPos.java
@@ -27,70 +27,69 @@ import java.util.Scanner;
 // a 一定是 后序数组的最后一个位置
 // a在中序数组的位置是4， 这个位置把中序数组分成两部分，左边就是左树元素，右边就是右树元素
 public class NowCoder_PreAndMidToPos {
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		int n = in.nextInt();
-		int[] preOrder = new int[n];
-		int[] inOrder = new int[n];
-		for (int i = 0; i < n; i++) {
-			preOrder[i] = in.nextInt();
-		}
-		Map<Integer, Integer> map = new HashMap<>();
-		for (int i = 0; i < n; i++) {
-			inOrder[i] = in.nextInt();
-			map.put(inOrder[i], i);
-		}
-		int[] posOrder = new int[n];
-		func(preOrder, 0, n - 1, inOrder, 0, n - 1, posOrder, 0, n - 1, map); 
-		for (int i = 0; i < n; i++) {
-			System.out.print(posOrder[i] + " ");
-		}
-		in.close();
-	}
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] preOrder = new int[n];
+        int[] inOrder = new int[n];
+        for (int i = 0; i < n; i++) {
+            preOrder[i] = in.nextInt();
+        }
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            inOrder[i] = in.nextInt();
+            map.put(inOrder[i], i);
+        }
+        int[] posOrder = new int[n];
+        func(preOrder, 0, n - 1, inOrder, 0, n - 1, posOrder, 0, n - 1, map);
+        for (int i = 0; i < n; i++) {
+            System.out.print(posOrder[i] + " ");
+        }
+        in.close();
+    }
 
-	public static void func(int[] pre, int l1, int r1, int[] in, int l2, int r2, int[] pos, int l3, int r3) {
-		if (l1 > r1) {
-			// 避免了无效情况
-			return;
-		}
-		if (l1 == r1) {
-			// 只有一个数的时候
-			pos[l3] = pre[l1];
-		} else {
-			// 不止一个数的时候
-			pos[r3] = pre[l1];
-			// index表示某个头在中序数组中的位置
-			int index;
-			// 可以优化
-			for (index = l2; index <= r2; index++) {
-				if (in[index] == pre[l1]) {
-					break;
-				}
-			}
-			int b = index - l2;
-			func(pre, l1 + 1, l1 + b, in, l2, index - 1, pos, l3, l3 + b - 1);
-			func(pre, l1 + b + 1, r1, in, index + 1, r2, pos, l3 + b, r3 - 1);
-		}
-	}
+    public static void func(int[] pre, int l1, int r1, int[] in, int l2, int r2, int[] pos, int l3, int r3) {
+        if (l1 > r1) {
+            // 避免了无效情况
+            return;
+        }
+        if (l1 == r1) {
+            // 只有一个数的时候
+            pos[l3] = pre[l1];
+        } else {
+            // 不止一个数的时候
+            pos[r3] = pre[l1];
+            // index表示某个头在中序数组中的位置
+            int index;
+            // 可以优化
+            for (index = l2; index <= r2; index++) {
+                if (in[index] == pre[l1]) {
+                    break;
+                }
+            }
+            int b = index - l2;
+            func(pre, l1 + 1, l1 + b, in, l2, index - 1, pos, l3, l3 + b - 1);
+            func(pre, l1 + b + 1, r1, in, index + 1, r2, pos, l3 + b, r3 - 1);
+        }
+    }
 
-	public static void func(int[] pre, int l1, int r1, int[] in, int l2, int r2, int[] pos, int l3, int r3,
-			Map<Integer, Integer> map) {
-		if (l1 > r1) {
-			// 避免了无效情况
-			return;
-		}
-		if (l1 == r1) {
-			// 只有一个数的时候
-			pos[l3] = pre[l1];
-		} else {
-			// 不止一个数的时候
-			pos[r3] = pre[l1];
-			// index表示某个头在中序数组中的位置
-			int index = map.get(pre[l1]);
-			int b = index - l2;
-			func(pre, l1 + 1, l1 + b, in, l2, index - 1, pos, l3, l3 + b - 1, map);
-			func(pre, l1 + b + 1, r1, in, index + 1, r2, pos, l3 + b, r3 - 1, map);
-		}
-	}
+    public static void func(int[] pre, int l1, int r1, int[] in, int l2, int r2, int[] pos, int l3, int r3, Map<Integer, Integer> map) {
+        if (l1 > r1) {
+            // 避免了无效情况
+            return;
+        }
+        if (l1 == r1) {
+            // 只有一个数的时候
+            pos[l3] = pre[l1];
+        } else {
+            // 不止一个数的时候
+            pos[r3] = pre[l1];
+            // index表示某个头在中序数组中的位置
+            int index = map.get(pre[l1]);
+            int b = index - l2;
+            func(pre, l1 + 1, l1 + b, in, l2, index - 1, pos, l3, l3 + b - 1, map);
+            func(pre, l1 + b + 1, r1, in, index + 1, r2, pos, l3 + b, r3 - 1, map);
+        }
+    }
 
 }
