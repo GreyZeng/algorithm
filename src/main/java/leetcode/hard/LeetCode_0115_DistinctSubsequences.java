cac02684d62d81bbff3354cc0af3fd5fdf8cad54
GreyZeng <410486047@qq.com>
2022-06-26 18:54:36 +0800

    leetcode 115

--- src/main/java/leetcode/hard/LeetCode_0115_DistinctSubsequences.java
+++ src/main/java/leetcode/hard/LeetCode_0115_DistinctSubsequences.java
@@ -1,41 +1,75 @@
-/*Given two strings s and t, return the number of distinct subsequences of s which equals t.
 
-        A string's subsequence is a new string formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (i.e., "ACE" is a subsequence of "ABCDE" while "AEC" is not).
-
-        It's guaranteed the answer fits on a 32-bit signed integer.
-
-
-
-        Example 1:
-
-        Input: s = "rabbbit", t = "rabbit"
-        Output: 3
-        Explanation:
-        As shown below, there are 3 ways you can generate "rabbit" from S.
-        rabbbit
-        rabbbit
-        rabbbit
-        Example 2:
-
-        Input: s = "babgbag", t = "bag"
-        Output: 5
-        Explanation:
-        As shown below, there are 5 ways you can generate "bag" from S.
-        babgbag
-        babgbag
-        babgbag
-        babgbag
-        babgbag
-
-
-        Constraints:
-
-        0 <= s.length, t.length <= 1000
-        s and t consist of English letters.*/
 package leetcode.hard;
-// TODO
+
+//给定两个字符串S和T
+//返回S的所有子序列中
+//有多少个子序列的字面值等于T
+// https://leetcode.cn/problems/distinct-subsequences/
 public class LeetCode_0115_DistinctSubsequences {
-    public static int numDistinct(String s, String t) {
-        return -1;
+    // leetcode超时
+    public static int numDistinct1(String s, String t) {
+        if (s.length() < t.length()) {
+            return 0;
+        }
+        return process(s.toCharArray(), t.toCharArray(), 0, 0);
+    }
+
+    // str[0....结尾]搞定t[0....结尾]
+    public static int process(char[] str, char[] t, int i, int j) {
+        if (j == t.length) {
+            // 全部搞定了
+            return 1;
+        }
+        if (i == str.length) {
+            // 没有了，搞不定
+            return 0;
+        }
+        // 不用i位置的去搞定
+        int ans = process(str, t, i + 1, j);
+        if (str[i] == t[j]) {
+            ans += process(str, t, i + 1, j + 1);
+        }
+        return ans;
+    }
+
+    // 二维动态规划
+    public static int numDistinct2(String s, String t) {
+        if (s.length() < t.length()) {
+            return 0;
+        }
+        char[] str = s.toCharArray();
+        char[] target = t.toCharArray();
+        int m = str.length;
+        int n = target.length;
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 0; i < m + 1; i++) {
+            dp[i][n] = 1;
+        }
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                dp[i][j] = dp[i + 1][j] + (str[i] == target[j] ? dp[i + 1][j + 1] : 0);
+            }
+        }
+        return dp[0][0];
+    }
+
+    // 压缩数组技巧
+    public static int numDistinct3(String s, String t) {
+        if (s.length() < t.length()) {
+            return 0;
+        }
+        char[] str = s.toCharArray();
+        char[] target = t.toCharArray();
+        int m = str.length;
+        int n = target.length;
+        int[] dp = new int[n + 1];
+        dp[n] = 1;
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = 0; j <= n - 1; j++) {
+                dp[j] += (str[i] == target[j] ? dp[j + 1] : 0);
+            }
+
+        }
+        return dp[0];
     }
 }
