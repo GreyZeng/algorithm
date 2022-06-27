17b4e07f777f3e8444131cb6f967faa681ef41e1
zenghui <410486047@qq.com>
2022-06-27 10:12:41 +0800

    最大路径和问题

--- src/main/java/leetcode/hard/LeetCode_0741_CherryPickup.java
+++ src/main/java/leetcode/hard/LeetCode_0741_CherryPickup.java
@@ -8,6 +8,8 @@ package leetcode.hard;
 // 三维dp
 // 设置两个小人，走到同一位置的时候，只算一份，路径和，就是来回的最大路径和
 public class LeetCode_0741_CherryPickup {
+    // 动态规划
+    // 三维表
     public static int cherryPickup(int[][] matrix) {
         int m = matrix.length;
         int n = matrix[0].length;
@@ -22,48 +24,134 @@ public class LeetCode_0741_CherryPickup {
         return Math.max(0, p(matrix, m, n, 0, 0, 0, dp));
     }
 
-    // 第一个点：(x1,y1), 第二个点(x2,y2)
-    // 重要限制：来到同一个位置时，只获得一份
-    // 本来是四个可变参数，可以变为三个，因为: x1+y1=x2+y2
+    // 定义两个小人，两个人都从(0,0)位置出发，到右下角位置，每人同时选择不同的下一步，如果两个小人跳到了同一个位置，只计算一份。
     public static int p(int[][] matrix, int m, int n, int x1, int y1, int x2, int[][][] dp) {
         if (dp[x1][y1][x2] != Integer.MIN_VALUE) {
             return dp[x1][y1][x2];
         }
         if (x1 == m - 1 && y1 == n - 1) {
-            dp[m - 1][n - 1][x2] = matrix[x1][y1];
-            return dp[m - 1][n - 1][x2];
+            // 已经到了最右下角了
+            // 隐含条件：另外一个点也一定到达右下角
+            // 获得一份樱桃数
+            dp[x1][y1][x2] = matrix[x1][y1];
+            return matrix[x1][y1];
         }
-        int y2 = getY2(m, n, x1, y1, x2);
+
         int next = -1;
-        // 下右
-        if (x1 < m - 1 && y2 < n - 1 && matrix[x1 + 1][y1] != -1 && matrix[x2][y2 + 1] != -1) {
-            next = Math.max(next, p(matrix, m, n, x1 + 1, y1, x2, dp));
+        // 下，下
+        if (x1 + 1 < m && x2 + 1 < m && matrix[x1 + 1][y1] != -1 && matrix[x2 + 1][getY2(x1, y1, x2)] != -1) {
+            next = Math.max(p(matrix, m, n, x1 + 1, y1, x2 + 1, dp), next);
         }
-        // 右下
-        if (y1 < n - 1 && x2 < m - 1 && matrix[x1][y1 + 1] != -1 && matrix[x2 + 1][y2] != -1) {
-            next = Math.max(next, p(matrix, m, n, x1, y1 + 1, x2 + 1, dp));
+        // 下，右
+        if (x1 + 1 < m && getY2(x1, y1, x2) + 1 < n && matrix[x1 + 1][y1] != -1 && matrix[x2][getY2(x1, y1, x2) + 1] != -1) {
+            next = Math.max(p(matrix, m, n, x1 + 1, y1, x2, dp), next);
         }
-        // 右右
-        if (y1 < n - 1 && y2 < n - 1 && matrix[x1][y1 + 1] != -1 && matrix[x2][y2 + 1] != -1) {
-            next = Math.max(next, p(matrix, m, n, x1, y1 + 1, x2, dp));
+        // 右，下
+        if (y1 + 1 < n && x2 + 1 < m && matrix[x1][y1 + 1] != -1 && matrix[x2 + 1][getY2(x1, y1, x2)] != -1) {
+            next = Math.max(p(matrix, m, n, x1, y1 + 1, x2 + 1, dp), next);
         }
-        // 下下
-        if (x1 < m - 1 && x2 < m - 1 && matrix[x1 + 1][y1] != -1 && matrix[x2 + 1][y2] != -1) {
-            next = Math.max(next, p(matrix, m, n, x1 + 1, y1, x2 + 1, dp));
+        // 右，右
+        if (y1 + 1 < n && getY2(x1, y1, x2) + 1 < m && matrix[x1][y1 + 1] != -1 && matrix[x2][getY2(x1, y1, x2) + 1] != -1) {
+            next = Math.max(p(matrix, m, n, x1, y1 + 1, x2, dp), next);
         }
-        if (matrix[x1][y1] == -1 || matrix[x2][y2] == -1 || next == -1) {
+        if (next == -1) {
             dp[x1][y1][x2] = -1;
-            return dp[x1][y1][x2];
+            return -1;
         }
         if (x1 == x2) {
-            dp[x1][y1][x2] = matrix[x1][y1] + next;
+            // 到达同一个位置，只取一个值
+            dp[x1][y1][x2] = next + matrix[x1][y1];
             return dp[x1][y1][x2];
         }
-        dp[x1][y1][x2] = matrix[x1][y1] + matrix[x2][y2] + next;
+        dp[x1][y1][x2] = next + matrix[x1][y1] + matrix[x2][getY2(x1, y1, x2)];
         return dp[x1][y1][x2];
     }
 
-    private static int getY2(int m, int n, int x1, int y1, int x2) {
+    public static int getY2(int x1, int y1, int x2) {
         return x1 + y1 - x2;
+    }
+
+    // 暴力解法 leetcode 超时
+    // 四个参数
+    public static int cherryPickup1(int[][] matrix) {
+        return Math.max(0, p(matrix, matrix.length, matrix[0].length, 0, 0, 0, 0));
+    }
+
+    // 定义两个小人，两个人都从(0,0)位置出发，到右下角位置，每人同时选择不同的下一步，如果两个小人跳到了同一个位置，只计算一份。
+    public static int p(int[][] matrix, int m, int n, int x1, int y1, int x2, int y2) {
+        if (x1 == m - 1 && y1 == n - 1) {
+            // 已经到了最右下角了
+            // 隐含条件：另外一个点也一定到达右下角
+            // 获得一份樱桃数
+            return matrix[x1][y1];
+        }
+
+        int next = -1;
+        // 下，下
+        if (x1 + 1 < m && x2 + 1 < m && matrix[x1 + 1][y1] != -1 && matrix[x2 + 1][y2] != -1) {
+            next = Math.max(p(matrix, m, n, x1 + 1, y1, x2 + 1, y2), next);
+        }
+        // 下，右
+        if (x1 + 1 < m && y2 + 1 < n && matrix[x1 + 1][y1] != -1 && matrix[x2][y2 + 1] != -1) {
+            next = Math.max(p(matrix, m, n, x1 + 1, y1, x2, y2 + 1), next);
+        }
+        // 右，下
+        if (y1 + 1 < n && x2 + 1 < m && matrix[x1][y1 + 1] != -1 && matrix[x2 + 1][y2] != -1) {
+            next = Math.max(p(matrix, m, n, x1, y1 + 1, x2 + 1, y2), next);
+        }
+        // 右，右
+        if (y1 + 1 < n && y2 + 1 < m && matrix[x1][y1 + 1] != -1 && matrix[x2][y2 + 1] != -1) {
+            next = Math.max(p(matrix, m, n, x1, y1 + 1, x2, y2 + 1), next);
+        }
+        if (next == -1) {
+            return -1;
+        }
+        if (x1 == x2) {
+            // 到达同一个位置，只取一个值
+            return next + matrix[x1][y1];
+        }
+        return next + matrix[x1][y1] + matrix[x2][y2];
+    }
+
+    // 暴力解法 leetcode 超时
+    // 三个参数
+    public static int cherryPickup2(int[][] matrix) {
+        return Math.max(0, p(matrix, matrix.length, matrix[0].length, 0, 0, 0));
+    }
+
+    // 定义两个小人，两个人都从(0,0)位置出发，到右下角位置，每人同时选择不同的下一步，如果两个小人跳到了同一个位置，只计算一份。
+    public static int p(int[][] matrix, int m, int n, int x1, int y1, int x2) {
+        if (x1 == m - 1 && y1 == n - 1) {
+            // 已经到了最右下角了
+            // 隐含条件：另外一个点也一定到达右下角
+            // 获得一份樱桃数
+            return matrix[x1][y1];
+        }
+
+        int next = -1;
+        // 下，下
+        if (x1 + 1 < m && x2 + 1 < m && matrix[x1 + 1][y1] != -1 && matrix[x2 + 1][getY2(x1, y1, x2)] != -1) {
+            next = Math.max(p(matrix, m, n, x1 + 1, y1, x2 + 1), next);
+        }
+        // 下，右
+        if (x1 + 1 < m && getY2(x1, y1, x2) + 1 < n && matrix[x1 + 1][y1] != -1 && matrix[x2][getY2(x1, y1, x2) + 1] != -1) {
+            next = Math.max(p(matrix, m, n, x1 + 1, y1, x2), next);
+        }
+        // 右，下
+        if (y1 + 1 < n && x2 + 1 < m && matrix[x1][y1 + 1] != -1 && matrix[x2 + 1][getY2(x1, y1, x2)] != -1) {
+            next = Math.max(p(matrix, m, n, x1, y1 + 1, x2 + 1), next);
+        }
+        // 右，右
+        if (y1 + 1 < n && getY2(x1, y1, x2) + 1 < m && matrix[x1][y1 + 1] != -1 && matrix[x2][getY2(x1, y1, x2) + 1] != -1) {
+            next = Math.max(p(matrix, m, n, x1, y1 + 1, x2), next);
+        }
+        if (next == -1) {
+            return -1;
+        }
+        if (x1 == x2) {
+            // 到达同一个位置，只取一个值
+            return next + matrix[x1][y1];
+        }
+        return next + matrix[x1][y1] + matrix[x2][getY2(x1, y1, x2)];
     }
 }
