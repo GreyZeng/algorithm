a4616c0e126d6903c3398a3231bcc78d838d0a65
GreyZeng <410486047@qq.com>
2022-06-28 13:23:45 +0800

    leetcode 407 trap rain water

--- src/main/java/leetcode/hard/LeetCode_0407_TrappingRainWaterII.java
+++ src/main/java/leetcode/hard/LeetCode_0407_TrappingRainWaterII.java
@@ -5,49 +5,28 @@ import java.util.PriorityQueue;
 
 //Constraints:
 //
-//        1 <= m, n <= 110
-//        0 <= heightMap[i][j] <= 20000
+// 1 <= m, n <= 110
+// 0 <= heightMap[i][j] <= 20000
+// https://leetcode.cn/problems/trapping-rain-water-ii/
+// 笔记：https://www.cnblogs.com/greyzeng/p/16418808.html
 public class LeetCode_0407_TrappingRainWaterII {
-    public static class Node {
-        public int value;
-        public int i;
-        public int j;
-
-        public Node(int v, int i, int j) {
-            value = v;
-            this.i = i;
-            this.j = j;
-        }
-    }
-
-    public static class MyComparator implements Comparator<Node> {
-
-        @Override
-        public int compare(Node o1, Node o2) {
-            return o1.value - o2.value;
-        }
-    }
-
-
     public static int trapRainWater(int[][] heightMap) {
-        // 0. 准备一个全局max
-        int max = 0;
-        // 2. 加入过的标志一下
-        // 3. 从小根堆弹出一个数字（这个数字必定为周围一圈最薄弱的点）
-        // 4. 然后把这个数字的上下左右都加入小根堆，标志为加入过的状态
+        if (null == heightMap || heightMap.length <= 2 || heightMap[0].length <= 2) {
+            return 0;
+        }
         int m = heightMap.length;
         int n = heightMap[0].length;
+        int max = 0;
+        PriorityQueue<Node> heap = new PriorityQueue<>(Comparator.comparingInt(o -> o.v));
         boolean[][] isEntered = new boolean[m][n];
-        PriorityQueue<Node> heap = new PriorityQueue<>(new MyComparator());
-
-        // 1. 把周围一圈加入小根堆,加入的点标注为true
+        // 以下两个for循环，是把四周都加入小根堆
         for (int i = 0; i < m; i++) {
             heap.offer(new Node(heightMap[i][0], i, 0));
             heap.offer(new Node(heightMap[i][n - 1], i, n - 1));
             isEntered[i][0] = true;
             isEntered[i][n - 1] = true;
         }
-        for (int i = 1; i < n; i++) {
+        for (int i = 0; i < n; i++) {
             heap.offer(new Node(heightMap[0][i], 0, i));
             heap.offer(new Node(heightMap[m - 1][i], m - 1, i));
             isEntered[0][i] = true;
@@ -55,45 +34,44 @@ public class LeetCode_0407_TrappingRainWaterII {
         }
         int water = 0;
         while (!heap.isEmpty()) {
+            // 最薄弱的位置
             Node weakest = heap.poll();
-            max = Math.max(weakest.value, max);
+            max = Math.max(weakest.v, max);
             int i = weakest.i;
             int j = weakest.j;
             if (i + 1 < m && !isEntered[i + 1][j]) {
-                if (max > heightMap[i + 1][j]) {
-                    water += (max - heightMap[i + 1][j]);
-                }
-                heap.add(new Node(heightMap[i + 1][j], i + 1, j));
+                water += Math.max(0, max - heightMap[i + 1][j]);
                 isEntered[i + 1][j] = true;
+                heap.offer(new Node(heightMap[i + 1][j], i + 1, j));
             }
             if (i - 1 >= 0 && !isEntered[i - 1][j]) {
-
-                if (max > heightMap[i - 1][j]) {
-                    water += (max - heightMap[i - 1][j]);
-                }
-                heap.add(new Node(heightMap[i - 1][j], i - 1, j));
+                water += Math.max(0, max - heightMap[i - 1][j]);
                 isEntered[i - 1][j] = true;
+                heap.offer(new Node(heightMap[i - 1][j], i - 1, j));
             }
             if (j + 1 < n && !isEntered[i][j + 1]) {
-                if (max > heightMap[i][j + 1]) {
-                    water += (max - heightMap[i][j + 1]);
-                }
-                heap.add(new Node(heightMap[i][j + 1], i, j + 1));
+                water += Math.max(0, max - heightMap[i][j + 1]);
                 isEntered[i][j + 1] = true;
+                heap.offer(new Node(heightMap[i][j + 1], i, j + 1));
             }
             if (j - 1 >= 0 && !isEntered[i][j - 1]) {
-                if (max > heightMap[i][j - 1]) {
-                    water += (max - heightMap[i][j - 1]);
-                }
-                heap.add(new Node(heightMap[i][j - 1], i, j - 1));
+                water += Math.max(0, max - heightMap[i][j - 1]);
                 isEntered[i][j - 1] = true;
+                heap.offer(new Node(heightMap[i][j - 1], i, j - 1));
             }
         }
         return water;
     }
 
-    public static void main(String[] args) {
-        int[][] grid = {{1, 4, 3, 1, 3, 2}, {3, 2, 1, 3, 2, 4}, {2, 3, 3, 2, 3, 1}};
-        System.out.println(trapRainWater(grid));
+    public static class Node {
+        public int v;
+        public int i;
+        public int j;
+
+        public Node(int v, int i, int j) {
+            this.i = i;
+            this.j = j;
+            this.v = v;
+        }
     }
 }
