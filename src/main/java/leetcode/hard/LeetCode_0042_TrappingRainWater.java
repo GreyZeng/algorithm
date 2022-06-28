20790f1166cc841a89581b6dd3db506174064831
zenghui <410486047@qq.com>
2022-06-28 10:06:56 +0800

    trap rain water

--- src/main/java/leetcode/hard/LeetCode_0042_TrappingRainWater.java
+++ src/main/java/leetcode/hard/LeetCode_0042_TrappingRainWater.java
@@ -1,49 +1,32 @@
 package leetcode.hard;
 
-/**
- * @author Grey
- */
+
 //Constraints:
 // n == height.length
 // 0 <= n <= 3 * 10^4
 // 0 <= height[i] <= 10^5
 public class LeetCode_0042_TrappingRainWater {
-
     // 双指针
     public static int trap(int[] arr) {
-        if (null == arr || arr.length <= 2) {
+        if (arr == null || arr.length <= 2) {
             return 0;
         }
-        int n = arr.length;
-        // 考虑每个位置上的水能留下多少
-        int leftMax = arr[0];
-        int rightMax = arr[n - 1];
+        int result = 0;
         int l = 1;
-        int r = n - 2;
-        int water = 0;
+        int r = arr.length - 2;
+        int lMax = arr[0];
+        int rMax = arr[arr.length - 1];
         while (l <= r) {
-            if (leftMax > rightMax) {
-                if (arr[r] < rightMax) {
-                    water += (rightMax - arr[r]);
-                } else {
-                    rightMax = arr[r];
-                }
-                r--;
-            } else { 
-                if (arr[l] < leftMax) {
-                    water += (leftMax - arr[l]);
-                } else {
-                    leftMax = arr[l];
-                }
-                l++;
+            if (lMax < rMax) {
+                // 结算左侧
+                result += Math.max(0, lMax - arr[l]);
+                lMax = Math.max(lMax, arr[l++]);
+            } else {
+                // 结算右侧
+                result += Math.max(0, rMax - arr[r]);
+                rMax = Math.max(rMax, arr[r--]);
             }
         }
-        return water;
+        return result;
     }
-
-    public static void main(String[] args) {
-        int[] arr = {0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
-        System.out.println(trap(arr));
-    }
-
 }
