c4680deb6b66885120f5c795deeb5c174a408b1a
GreyZeng <410486047@qq.com>
2022-06-30 13:16:59 +0800

    update majar k

--- src/main/java/leetcode/medium/LeetCode_0229_MajorityElementII.java
+++ src/main/java/leetcode/medium/LeetCode_0229_MajorityElementII.java
@@ -12,7 +12,9 @@ import java.util.List;
 public class LeetCode_0229_MajorityElementII {
     public List<Integer> majorityElement(int[] nums) {
         List<Integer> res = new ArrayList<>();
-        if (nums == null || nums.length == 0) return res;
+        if (nums == null || nums.length == 0) {
+            return res;
+        }
         int cand1 = nums[0], count1 = 0;
         int cand2 = nums[0], count2 = 0;
 
@@ -42,12 +44,19 @@ public class LeetCode_0229_MajorityElementII {
         count1 = 0;
         count2 = 0;
         for (int num : nums) {
-            if (cand1 == num) count1++;
-            else if (cand2 == num) count2++;
+            if (cand1 == num) {
+                count1++;
+            } else if (cand2 == num) {
+                count2++;
+            }
         }
 
-        if (count1 > nums.length / 3) res.add(cand1);
-        if (count2 > nums.length / 3) res.add(cand2);
+        if (count1 > nums.length / 3) {
+            res.add(cand1);
+        }
+        if (count2 > nums.length / 3) {
+            res.add(cand2);
+        }
 
         return res;
 
