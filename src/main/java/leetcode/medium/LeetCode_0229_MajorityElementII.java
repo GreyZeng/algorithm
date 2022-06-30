510b75e8efe12e9eafac1716d6caddfe4f0128a8
GreyZeng <410486047@qq.com>
2022-06-30 13:40:25 +0800

    k major

--- src/main/java/leetcode/medium/LeetCode_0229_MajorityElementII.java
+++ src/main/java/leetcode/medium/LeetCode_0229_MajorityElementII.java
@@ -1,14 +1,14 @@
 package leetcode.medium;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
-/**
- * @author <a href="mailto:410486047@qq.com">Grey</a>
- * @date 2021/12/3
- * @since 1.8
- */
-// https://leetcode-cn.com/problems/majority-element-ii/
+// n = 2 https://leetcode-cn.com/problems/majority-element
+// n = 3 https://leetcode-cn.com/problems/majority-element-ii/
+// n = k https://www.nowcoder.com/questionTerminal/4d448650c0324df08c40c614226026ad
 public class LeetCode_0229_MajorityElementII {
     public List<Integer> majorityElement(int[] nums) {
         List<Integer> res = new ArrayList<>();
@@ -17,7 +17,6 @@ public class LeetCode_0229_MajorityElementII {
         }
         int cand1 = nums[0], count1 = 0;
         int cand2 = nums[0], count2 = 0;
-
         for (int num : nums) {
             if (cand1 == num) {
                 count1++;
@@ -40,7 +39,6 @@ public class LeetCode_0229_MajorityElementII {
             count1--;
             count2--;
         }
-
         count1 = 0;
         count2 = 0;
         for (int num : nums) {
@@ -50,16 +48,75 @@ public class LeetCode_0229_MajorityElementII {
                 count2++;
             }
         }
-
         if (count1 > nums.length / 3) {
             res.add(cand1);
         }
         if (count2 > nums.length / 3) {
             res.add(cand2);
         }
-
         return res;
+    }
 
+    // 推广到k
+    // https://www.nowcoder.com/questionTerminal/4d448650c0324df08c40c614226026ad
+    public static List<Integer> major(int[] arr, int K) {
+        List<Integer> ans = new ArrayList<>();
+        if (K < 2) {
+            return new ArrayList<>();
+        }
+        // 攒候选，cands，候选表，最多K-1条记录！ > N / K次的数字，最多有K-1个
+        HashMap<Integer, Integer> cands = new HashMap<>();
+        for (int i = 0; i != arr.length; i++) {
+            if (cands.containsKey(arr[i])) {
+                cands.put(arr[i], cands.get(arr[i]) + 1);
+            } else { // arr[i] 不是候选
+                if (cands.size() == K - 1) { // 当前数肯定不要！，每一个候选付出1点血量，血量变成0的候选，要删掉！
+                    allCandsMinusOne(cands);
+                } else {
+                    cands.put(arr[i], 1);
+                }
+            }
+        }
+        // 所有可能的候选，都在cands表中！遍历一遍arr，每个候选收集真实次数
+        HashMap<Integer, Integer> reals = getReals(arr, cands);
+        boolean hasPrint = false;
+        for (Map.Entry<Integer, Integer> set : cands.entrySet()) {
+            Integer key = set.getKey();
+            if (reals.get(key) > arr.length / K) {
+                hasPrint = true;
+                ans.add(key);
+            }
+        }
+        return hasPrint ? ans : new ArrayList<>();
+    }
 
+    public static void allCandsMinusOne(HashMap<Integer, Integer> map) {
+        List<Integer> removeList = new LinkedList<>();
+        for (Map.Entry<Integer, Integer> set : map.entrySet()) {
+            Integer key = set.getKey();
+            Integer value = set.getValue();
+            if (value == 1) {
+                removeList.add(key);
+            }
+            map.put(key, value - 1);
+        }
+        for (Integer removeKey : removeList) {
+            map.remove(removeKey);
+        }
+    }
+
+    public static HashMap<Integer, Integer> getReals(int[] arr, HashMap<Integer, Integer> cands) {
+        HashMap<Integer, Integer> reals = new HashMap<>();
+        for (int i = 0; i != arr.length; i++) {
+            int curNum = arr[i];
+            if (cands.containsKey(curNum)) {
+                if (reals.containsKey(curNum)) {
+                    reals.put(curNum, reals.get(curNum) + 1);
+                } else {
+                    reals.put(curNum, 1);
+                }
+            }
+        }
+        return reals;
     }
 }
