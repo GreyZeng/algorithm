54cf0d494138fc6204cb247445e38ed7a4675dc8
zenghui <410486047@qq.com>
2022-07-04 09:44:17 +0800

    leetcode majority element

--- src/main/java/leetcode/easy/LeetCode_0169_MajorityElement.java
+++ src/main/java/leetcode/easy/LeetCode_0169_MajorityElement.java
@@ -1,72 +1,40 @@
 package leetcode.easy;
 
 
-//Given an array of size n, find the majority element. The majority element is the element that appears more than ⌊ n/2 ⌋ times.
-//
-//		You may assume that the array is non-empty and the majority element always exist in the array.
-//
-//		Example 1:
-//
-//		Input: [3,2,3]
-//		Output: 3
-//		Example 2:
-//
-//		Input: [2,2,1,1,1,2,2]
-//		Output: 2
-// 水王问题
+// 水王数问题
+// 1. 找到数组中，严格大于一半的数
+// 扩展
+// 2. 找到数组中，严格大于1/3的数
+// 3. 找到数组中，严格大于1/K的数
 // https://www.cnblogs.com/greyzeng/p/14410280.html
 public class LeetCode_0169_MajorityElement {
-    public static void main(String[] args) {
-        int[] nums1 = {3, 2, 3};
-        int[] nums2 = {2, 2, 1, 1, 1, 2, 2};
-        System.out.println(majorityElement(nums1));
-        System.out.println(majorityElement(nums2));
-    }
-
-    // 已知数组中一定有某个数个数大于N/2个，求这个数
     // tips:每次删除两个不同的数，这个数肯定会剩下来
-    public static int majorityElement(int[] nums) {
-        if (nums == null || nums.length == 0) {
-            return -1;
-        }
-        if (nums.length == 1) {
-            return nums[0];
-        }
-
-        int cand = nums[0];
-        int HP = 1;
-        int i = 1;
-        int limit = nums.length >> 1;
-        while (i < nums.length) {
-            if (cand == nums[i]) {
-                HP++;
-                // 如果某时，HP已经大于一半的数了，直接返回
-                if (HP > limit) {
-                    return nums[i];
-                }
+    // 但是最后剩下来的数，不一定是水王数，比如:[1,2,3,4,5]
+    // 所以还需要重新验证一下
+    public static int majorityElement(int[] arr) {
+        int n = arr.length;
+        int ans = arr[0];
+        int hp = 1;
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] == ans) {
+                hp++;
             } else {
-                if (HP == 0) {
-                    cand = nums[i];
-                    HP++;
-                } else {
-                    HP--;
+                hp--;
+                if (hp == 0) {
+                    hp = 1;
+                    ans = arr[i];
                 }
             }
-            i++;
         }
-        if (HP == 0) {
-            return -1;
-        }
-        int c = 0;
-        for (int num : nums) {
-            if (cand == num) {
-                c++;
+        int count = 0;
+        for (int e : arr) {
+            if (e == ans) {
+                count++;
             }
         }
-        if (c > limit) {
-            return cand;
-        } else {
-            return -1;
+        if (count > n / 2) {
+            return ans;
         }
+        return -1;
     }
 }
