53385c3e41051deddea5a7ba1d38a9a4147a0a24
GreyZeng <410486047@qq.com>
2022-07-10 17:45:20 +0800

    min range

--- src/main/java/leetcode/hard/LeetCode_0632_SmallestRangeCoveringElementsFromKLists.java
+++ src/main/java/leetcode/hard/LeetCode_0632_SmallestRangeCoveringElementsFromKLists.java
@@ -80,4 +80,65 @@ public class LeetCode_0632_SmallestRangeCoveringElementsFromKLists {
         }
         return a[0] > b[0] ? b : a;
     }
+    // 类似问题：有三个有序数组，分别在三个数组中挑出3个数，x、y、z。返回 |x-y| + |y-z| + |z-x|最小是多少？
+    public static int minRange1(int[][] m) {
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < m[0].length; i++) {
+            for (int j = 0; j < m[1].length; j++) {
+                for (int k = 0; k < m[2].length; k++) {
+                    min = Math.min(min,
+                            Math.abs(m[0][i] - m[1][j]) + Math.abs(m[1][j] - m[2][k]) + Math.abs(m[2][k] - m[0][i]));
+                }
+            }
+        }
+        return min;
+    }
+
+    public static int minRange2(int[][] matrix) {
+        int N = matrix.length;
+        TreeSet<Node> set = new TreeSet<>((o1, o2) -> o1.value != o2.value ? o1.value - o2.value : o1.bucket - o2.bucket);
+        for (int i = 0; i < N; i++) {
+            set.add(new Node(matrix[i][0], 0, i));
+        }
+        int min = Integer.MAX_VALUE;
+        while (set.size() == N) {
+            min = Math.min(min, set.last().value - set.first().value);
+            Node cur = set.pollFirst();
+            if (cur.position < matrix[cur.bucket].length - 1) {
+                set.add(new Node(matrix[cur.bucket][cur.position + 1], cur.position+1, cur.bucket));
+            }
+        }
+        return min << 1;
+    }
+
+    public static int[][] generateRandomMatrix(int n, int v) {
+        int[][] m = new int[3][];
+        int s = 0;
+        for (int i = 0; i < 3; i++) {
+            s = (int) (Math.random() * n) + 1;
+            m[i] = new int[s];
+            for (int j = 0; j < s; j++) {
+                m[i][j] = (int) (Math.random() * v);
+            }
+            Arrays.sort(m[i]);
+        }
+        return m;
+    }
+
+    public static void main(String[] args) {
+        int n = 20;
+        int v = 200;
+        int t = 1000000;
+        System.out.println("测试开始");
+        for (int i = 0; i < t; i++) {
+            int[][] m = generateRandomMatrix(n, v);
+            int ans1 = minRange1(m);
+            int ans2 = minRange2(m);
+            if (ans1 != ans2) {
+                System.out.println("出错了!");
+            }
+        }
+        System.out.println("测试结束");
+    }
+
 }
