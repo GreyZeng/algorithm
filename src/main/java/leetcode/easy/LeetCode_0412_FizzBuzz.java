23e730c01b131ce9211543b9a8c47c45a4db74cd
GreyZeng <410486047@qq.com>
2022-07-11 23:49:25 +0800

    fizz buzz

--- src/main/java/leetcode/easy/LeetCode_0412_FizzBuzz.java
+++ src/main/java/leetcode/easy/LeetCode_0412_FizzBuzz.java
@@ -28,17 +28,26 @@ Return:
 ]*/
 package leetcode.easy;
 
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
+// https://leetcode.cn/problems/fizz-buzz/
+@Deprecated
 public class LeetCode_0412_FizzBuzz {
 
-	public static List<String> fizzBuzz(int n) {
-		List<String> l = new LinkedList<>();
-		for (int i = 1; i <= n; i++) {
-			l.add(i % 15 == 0 ? "FizzBuzz" : (i % 5 == 0 ? "Buzz" : (i % 3 == 0 ? "Fizz" : String.valueOf(i))));
-		}
-		return l;
-	}
+    public List<String> fizzBuzz(int n) {
+        List<String> ans = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            if (i % 15 == 0) {
+                ans.add("FizzBuzz");
+            } else if (i % 3 == 0) {
+                ans.add("Fizz");
+            } else if (i % 5 == 0) {
+                ans.add("Buzz");
+            } else {
+                ans.add(String.valueOf(i));
+            }
+        }
+        return ans;
+    }
 
 }
