7046760d9377cb3ccca9d21a44db979761aff319
zenghui <410486047@qq.com>
2022-07-12 12:36:18 +0800

    update

--- src/main/java/leetcode/medium/LeetCode_0380_InsertDeleteGetRandom.java
+++ src/main/java/leetcode/medium/LeetCode_0380_InsertDeleteGetRandom.java
@@ -1,96 +1,85 @@
-/*Implement the RandomizedSet class:
-
-        bool insert(int val) Inserts an item val into the set if not present. Returns true if the item was not present, false otherwise.
-        bool remove(int val) Removes an item val from the set if present. Returns true if the item was present, false otherwise.
-        int getRandom() Returns a random element from the current set of elements (it's guaranteed that at least one element exists when this method is called). Each element must have the same probability of being returned.
-        Follow up: Could you implement the functions of the class with each function works in average O(1) time?
-
-
-
-        Example 1:
-
-        Input
-        ["RandomizedSet", "insert", "remove", "insert", "getRandom", "remove", "insert", "getRandom"]
-        [[], [1], [2], [2], [], [1], [2], []]
-        Output
-        [null, true, false, true, 2, true, false, 2]
-
-        Explanation
-        RandomizedSet randomizedSet = new RandomizedSet();
-        randomizedSet.insert(1); // Inserts 1 to the set. Returns true as 1 was inserted successfully.
-        randomizedSet.remove(2); // Returns false as 2 does not exist in the set.
-        randomizedSet.insert(2); // Inserts 2 to the set, returns true. Set now contains [1,2].
-        randomizedSet.getRandom(); // getRandom() should return either 1 or 2 randomly.
-        randomizedSet.remove(1); // Removes 1 from the set, returns true. Set now contains [2].
-        randomizedSet.insert(2); // 2 was already in the set, so return false.
-        randomizedSet.getRandom(); // Since 2 is the only number in the set, getRandom() will always return 2.
-
-
-        Constraints:
-
-        -2^31 <= val <= 2^31 - 1
-        At most 105 calls will be made to insert, remove, and getRandom.
-        There will be at least one element in the data structure when getRandom is called.*/
 package leetcode.medium;
 
 import java.util.HashMap;
+import java.util.Map;
 
-
+//实现RandomizedSet 类：
+//
+//        RandomizedSet() 初始化 RandomizedSet 对象
+//        bool insert(int val) 当元素 val 不存在时，向集合中插入该项，并返回 true ；否则，返回 false 。
+//        bool remove(int val) 当元素 val 存在时，从集合中移除该项，并返回 true ；否则，返回 false 。
+//        int getRandom() 随机返回现有集合中的一项（测试用例保证调用此方法时集合中至少存在一个元素）。每个元素应该有 相同的概率 被返回。
+//        你必须实现类的所有函数，并满足每个函数的 平均 时间复杂度为 O(1) 。
+//
+//        
+//
+//        示例：
+//
+//        输入
+//        ["RandomizedSet", "insert", "remove", "insert", "getRandom", "remove", "insert", "getRandom"]
+//        [[], [1], [2], [2], [], [1], [2], []]
+//        输出
+//        [null, true, false, true, 2, true, false, 2]
+//
+//        解释
+//        RandomizedSet randomizedSet = new RandomizedSet();
+//        randomizedSet.insert(1); // 向集合中插入 1 。返回 true 表示 1 被成功地插入。
+//        randomizedSet.remove(2); // 返回 false ，表示集合中不存在 2 。
+//        randomizedSet.insert(2); // 向集合中插入 2 。返回 true 。集合现在包含 [1,2] 。
+//        randomizedSet.getRandom(); // getRandom 应随机返回 1 或 2 。
+//        randomizedSet.remove(1); // 从集合中移除 1 ，返回 true 。集合现在包含 [2] 。
+//        randomizedSet.insert(2); // 2 已在集合中，所以返回 false 。
+//        randomizedSet.getRandom(); // 由于 2 是集合中唯一的数字，getRandom 总是返回 2 。
+//        
+//
+//        提示：
+//
+//        -2^31 <= val <= 2^31 - 1
+//        最多调用 insert、remove 和 getRandom 函数 2 *10^5 次
+//        在调用 getRandom 方法时，数据结构中 至少存在一个 元素。
+//
+//        来源：力扣（LeetCode）
+//        链接：https://leetcode-cn.com/problems/insert-delete-getrandom-o1
+//        著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 public class LeetCode_0380_InsertDeleteGetRandom {
     public static class RandomizedSet {
-        HashMap<Integer, Integer> indexMap;
-        HashMap<Integer, Integer> sizeMap;
-        int size;
+        // 某个val在哪个位置
+        private Map<Integer, Integer> indexMap;
+        // 某个位置上的val是什么
+        private Map<Integer, Integer> valueMap;
+        private int size;
 
-        /**
-         * Initialize your data structure here.
-         */
         public RandomizedSet() {
+            size = 0;
             indexMap = new HashMap<>();
-            sizeMap = new HashMap<>();
+            valueMap = new HashMap<>();
         }
 
-        /**
-         * Inserts a value to the set. Returns true if the set did not already contain the specified element.
-         */
         public boolean insert(int val) {
             if (!indexMap.containsKey(val)) {
-                indexMap.put(val, size);
-                sizeMap.put(size++, val);
+                valueMap.put(size, val);
+                indexMap.put(val, size++);
                 return true;
             }
             return false;
         }
 
-        /**
-         * Removes a value from the set. Returns true if the set contained the specified element.
-         */
         public boolean remove(int val) {
             if (!indexMap.containsKey(val)) {
                 return false;
             }
-            int top = sizeMap.get(size - 1);
-            int s = indexMap.get(val);
-
-            indexMap.put(top, s);
-            sizeMap.put(s, top);
+            size--;
+            int removeIndex = indexMap.get(val);
+            int lastValue = valueMap.get(size);
+            valueMap.put(removeIndex, lastValue);
+            indexMap.put(lastValue, removeIndex);
             indexMap.remove(val);
-            sizeMap.remove(--size);
-
+            valueMap.remove(size);
             return true;
         }
 
-        /**
-         * Get a random element from the set.
-         */
         public int getRandom() {
-            if (size == 0) {
-                return -1;
-            }
-            int r = (int) (Math.random() * size);
-            return sizeMap.get(r);
+            return valueMap.get((int) (Math.random() * size));
         }
     }
-
-
 }
