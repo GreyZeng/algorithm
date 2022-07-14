8cf7e75c67b0422921f055e0992c3669dc5ff67f
GreyZeng <410486047@qq.com>
2022-07-14 22:05:43 +0800

    leetcode 295

--- src/main/java/leetcode/hard/LeetCode_0295_FindMedianFromDataStream.java
+++ src/main/java/leetcode/hard/LeetCode_0295_FindMedianFromDataStream.java
@@ -1,37 +1,17 @@
-/*Median is the middle value in an ordered integer list. If the size of the list is even, there is no middle value.
-So the median is the mean of the two middle value.
-
-        For example,
-        [2,3,4], the median is 3
-
-        [2,3], the median is (2 + 3) / 2 = 2.5
-
-        Design a data structure that supports the following two operations:
-
-        void addNum(int num) - Add a integer number from the data stream to the data structure.
-        double findMedian() - Return the median of all elements so far.
-
-
-        Example:
-
-        addNum(1)
-        addNum(2)
-        findMedian() -> 1.5
-        addNum(3)
-        findMedian() -> 2
-
-
-        Follow up:
-
-        If all integer numbers from the stream are between 0 and 100, how would you optimize it?
-        If 99% of all integer numbers from the stream are between 0 and 100, how would you optimize it?*/
 package leetcode.hard;
-
 
 import java.util.Comparator;
 import java.util.PriorityQueue;
 
+//中位数是有序列表中间的数。如果列表长度是偶数，中位数则是中间两个数的平均值。
+//例如，
+//[2,3,4] 的中位数是 3
+//[2,3] 的中位数是 (2 + 3) / 2 = 2.5
+//设计一个支持以下两种操作的数据结构：
+//void addNum(int num) - 从数据流中添加一个整数到数据结构中。
+//double findMedian() - 返回目前所有元素的中位数
 // 一个大根堆，一个小根堆  size维持差值<2
+// 笔记：https://www.cnblogs.com/greyzeng/p/16479520.html
 public class LeetCode_0295_FindMedianFromDataStream {
     public static class MedianFinder {
 
