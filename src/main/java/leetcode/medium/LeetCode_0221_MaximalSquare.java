4a56f99efa3a569e060bb63a43cee9bfeb0c3511
zenghui <410486047@qq.com>
2022-07-17 23:22:31 +0800

    leetcode 114 and leetcode 221

--- src/main/java/leetcode/medium/LeetCode_0221_MaximalSquare.java
+++ src/main/java/leetcode/medium/LeetCode_0221_MaximalSquare.java
@@ -14,33 +14,32 @@ package leetcode.medium;
 
 // tips 正方形必须以i,j作为右下角情况，哪个正方形内部都是1且最大
 // 依赖 左边一个位置，上面一个位置，左上角位置
+// https://leetcode.cn/problems/maximal-square/
 public class LeetCode_0221_MaximalSquare {
 
-    public static int maximalSquare(char[][] m) {
-        if (null == m || m.length == 0 || m[0].length == 0) {
+    public static int maximalSquare(char[][] matrix) {
+        if (null == matrix || matrix.length == 0 || matrix[0].length == 0) {
             return 0;
         }
-        int M = m.length;
-        int N = m[0].length;
+        int m = matrix.length;
+        int n = matrix[0].length;
         int max = 0;
         // tips 正方形必须以i,j作为右下角情况，哪个正方形内部都是1且最大
-        int[][] dp = new int[M][N];
-        for (int i = 0; i < M; i++) {
-            dp[i][0] = m[i][0] == '1' ? 1 : 0;
+        int[][] dp = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            dp[i][0] = matrix[i][0] == '1' ? 1 : 0;
             max = Math.max(dp[i][0], max);
         }
-        for (int i = 0; i < N; i++) {
-            dp[0][i] = m[0][i] == '1' ? 1 : 0;
+        for (int i = 0; i < n; i++) {
+            dp[0][i] = matrix[0][i] == '1' ? 1 : 0;
             max = Math.max(dp[0][i], max);
         }
-        for (int i = 1; i < M; i++) {
-            for (int j = 1; j < N; j++) {
-                dp[i][j] = m[i][j] == '1' ? Math.min(Math.min(dp[i - 1][j - 1], dp[i - 1][j]), dp[i][j - 1]) + 1 : 0;
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = matrix[i][j] == '1' ? Math.min(Math.min(dp[i - 1][j - 1], dp[i - 1][j]), dp[i][j - 1]) + 1 : 0;
                 max = Math.max(dp[i][j], max);
             }
         }
         return max * max;
     }
-
-
 }
