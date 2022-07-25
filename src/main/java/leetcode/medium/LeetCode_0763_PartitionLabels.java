bec44c3c845cf210b28da1e0d21750aa3f5f7cfa
GreyZeng <410486047@qq.com>
2022-07-25 18:29:23 +0800

    partition labels

--- src/main/java/leetcode/medium/LeetCode_0763_PartitionLabels.java
+++ src/main/java/leetcode/medium/LeetCode_0763_PartitionLabels.java
@@ -24,7 +24,18 @@ package leetcode.medium;
 
 import java.util.ArrayList;
 import java.util.List;
-
+//字符串 S 由小写字母组成。我们要把这个字符串划分为尽可能多的片段，同一字母最多出现在一个片段中。返回一个表示每个字符串片段的长度的列表。
+//        示例：
+//        输入：S = "ababcbacadefegdehijhklij"
+//        输出：[9,7,8]
+//        解释：
+//        划分结果为 "ababcbaca", "defegde", "hijhklij"。
+//        每个字母最多出现在一个片段中。
+//        像 "ababcbacadefegde", "hijhklij" 的划分是错误的，因为划分的片段数较少。
+//        提示：
+//        S的长度在[1, 500]之间。
+//        S只包含小写字母 'a' 到 'z' 。
+//        Leetcode题目 : https://leetcode.cn/problems/partition-labels/
 // 字符串尽可能分割成多个片段，每个字符最多出现在一个片段中
 // 笔记：https://www.cnblogs.com/greyzeng/p/15334109.html
 public class LeetCode_0763_PartitionLabels {
