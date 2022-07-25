3b6dc830d6dbb27906ba89783fbb77dd01e56f85
GreyZeng <410486047@qq.com>
2022-07-25 19:17:41 +0800

    update notes

--- src/main/java/leetcode/medium/LeetCode_0437_PathSumIII.java
+++ src/main/java/leetcode/medium/LeetCode_0437_PathSumIII.java
@@ -27,7 +27,10 @@ package leetcode.medium;
 
 import java.util.HashMap;
 
-// 先序遍历
+//给定一个二叉树的根节点 root，和一个整数 targetSum ，求该二叉树里节点值之和等于 targetSum 的 路径 的数目。
+//        路径 不需要从根节点开始，也不需要在叶子节点结束，但是路径方向必须是向下的（只能从父节点到子节点）。
+//        Leetcode题目 : https://leetcode.com/problems/path-sum-iii/
+// tips: 先序遍历
 // 类似数组三连的第二问
 // https://leetcode.cn/problems/path-sum-iii/
 public class LeetCode_0437_PathSumIII {
