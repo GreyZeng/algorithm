da7d835347094f355ee4994269c4b4e319058b38
zenghui <410486047@qq.com>
2022-07-26 16:38:23 +0800

    update notes

--- src/main/java/leetcode/medium/LeetCode_0347_TopKFrequentElements.java
+++ src/main/java/leetcode/medium/LeetCode_0347_TopKFrequentElements.java
@@ -1,28 +1,23 @@
-/*
-Given a non-empty array of integers, return the k most frequent elements.
 
-        Example 1:
-
-        Input: nums = [1,1,1,2,2,3], k = 2
-        Output: [1,2]
-        Example 2:
-
-        Input: nums = [1], k = 1
-        Output: [1]
-        Note:
-
-        You may assume k is always valid, 1 ≤ k ≤ number of unique elements.
-        Your algorithm's time complexity must be better than O(n log n), where n is the array's size.
-        It's guaranteed that the answer is unique, in other words the set of the top k frequent elements is unique.
-        You can return the answer in any order.
-*/
 package leetcode.medium;
 
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.PriorityQueue;
-
+//给你一个整数数组 nums 和一个整数 k ，请你返回其中出现频率前 k 高的元素。你可以按 任意顺序 返回答案。
+//        示例 1:
+//        输入: nums = [1,1,1,2,2,3], k = 2
+//        输出: [1,2]
+//        示例 2:
+//        输入: nums = [1], k = 1
+//        输出: [1]
+//        提示：
+//        1 <= nums.length <= 10^5
+//        k 的取值范围是 [1, 数组中不相同的元素的个数]
+//        题目数据保证答案唯一，换句话说，数组中前 k 个高频元素的集合是唯一的
+//        进阶：你所设计算法的时间复杂度 必须 优于 O(n log n) ，其中 n是数组大小。
+//        Leetcode题目 : https://leetcode.com/problems/top-k-frequent-elements/
 // https://leetcode.cn/problems/top-k-frequent-elements/
 // 笔记：https://www.cnblogs.com/greyzeng/p/16469101.html
 // Hash表词频 + 小根堆（拿次数排序，大小是K就开始收集）
