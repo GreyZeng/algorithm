b3f501e43897021ab9c7b44efb034b9ae5ed30d8
zenghui <410486047@qq.com>
2022-07-26 16:26:27 +0800

    fizz buzz

--- src/main/java/leetcode/easy/LeetCode_0412_FizzBuzz.java
+++ src/main/java/leetcode/easy/LeetCode_0412_FizzBuzz.java
@@ -1,36 +1,33 @@
-/*Write a program that outputs the string representation of numbers from 1 to n.
 
-But for multiples of three it should output “Fizz” 
-instead of the number and for the multiples of five output “Buzz”. 
-For numbers which are multiples of both three and five output “FizzBuzz”.
-
-Example:
-
-n = 15,
-
-Return:
-[
-    "1",
-    "2",
-    "Fizz",
-    "4",
-    "Buzz",
-    "Fizz",
-    "7",
-    "8",
-    "Fizz",
-    "Buzz",
-    "11",
-    "Fizz",
-    "13",
-    "14",
-    "FizzBuzz"
-]*/
 package leetcode.easy;
 
 import java.util.*;
 
-// https://leetcode.cn/problems/fizz-buzz/
+/*写一个程序，输出从 1 到 n 数字的字符串表示。
+        1. 如果n是3的倍数，输出“Fizz”；
+        2. 如果n是5的倍数，输出“Buzz”；
+        3.如果n同时是3和5的倍数，输出 “FizzBuzz”。
+        示例：
+        n = 15,
+        返回:
+        [
+        "1",
+        "2",
+        "Fizz",
+        "4",
+        "Buzz",
+        "Fizz",
+        "7",
+        "8",
+        "Fizz",
+        "Buzz",
+        "11",
+        "Fizz",
+        "13",
+        "14",
+        "FizzBuzz"
+        ]
+        Leetcode题目 : https://leetcode.cn/problems/fizz-buzz/*/
 @Deprecated
 public class LeetCode_0412_FizzBuzz {
 
