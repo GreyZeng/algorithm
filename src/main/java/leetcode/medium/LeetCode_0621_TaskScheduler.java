f3393479728181c15ad0f80429c4144e12deb646
zenghui <410486047@qq.com>
2022-07-27 16:28:16 +0800

    update

--- src/main/java/leetcode/medium/LeetCode_0621_TaskScheduler.java
+++ src/main/java/leetcode/medium/LeetCode_0621_TaskScheduler.java
@@ -1,43 +1,30 @@
-// Given a characters array tasks, representing the tasks a CPU needs to do, where each letter represents a different task. 
-// Tasks could be done in any order. Each task is done in one unit of time. For each unit of time, 
-// the CPU could complete either one task or just be idle.
 
-// However, there is a non-negative integer n that represents the cooldown period between two same tasks (the same letter in the array), that is that there must be at least n units of time between any two same tasks.
-
-// Return the least number of units of times that the CPU will take to finish all the given tasks.
-
-// Example 1:
-
-// Input: tasks = ["A","A","A","B","B","B"], n = 2
-// Output: 8
-// Explanation: 
-// A -> B -> idle -> A -> B -> idle -> A -> B
-// There is at least 2 units of time between any two same tasks.
-// Example 2:
-
-// Input: tasks = ["A","A","A","B","B","B"], n = 0
-// Output: 6
-// Explanation: On this case any permutation of size 6 would work since n = 0.
-// ["A","A","A","B","B","B"]
-// ["A","B","A","B","A","B"]
-// ["B","B","B","A","A","A"]
-// ...
-// And so on.
-// Example 3:
-
-// Input: tasks = ["A","A","A","A","A","A","B","C","D","E","F","G"], n = 2
-// Output: 16
-// Explanation: 
-// One possible solution is
-// A -> B -> C -> A -> D -> E -> A -> F -> G -> A -> idle -> idle -> A -> idle -> idle -> A
-
-// Constraints:
-
-// 1 <= task.length <= 104
-// tasks[i] is upper-case English letter.
-// The integer n is in the range [0, 100].
 package leetcode.medium;
 
+//给你一个用字符数组tasks 表示的 CPU 需要执行的任务列表。其中每个字母表示一种不同种类的任务。任务可以以任意顺序执行，并且每个任务都可以在 1 个单位时间内执行完。
+//        在任何一个单位时间，CPU 可以完成一个任务，或者处于待命状态。
+//        然而，两个 相同种类 的任务之间必须有长度为整数 n 的冷却时间，因此至少有连续 n 个单位时间内 CPU 在执行不同的任务，或者在待命状态。
+//        你需要计算完成所有任务所需要的 最短时间 。
+//        示例 1：
+//        输入：tasks = ["A","A","A","B","B","B"], n = 2
+//        输出：8
+//        解释：A -> B -> (待命) -> A -> B -> (待命) -> A -> B
+//        在本示例中，两个相同类型任务之间必须间隔长度为 n = 2 的冷却时间，而执行一个任务只需要一个单位时间，所以中间出现了（待命）状态。
+//        示例 2：
+//        输入：tasks = ["A","A","A","B","B","B"], n = 0
+//        输出：6
+//        解释：在这种情况下，任何大小为 6 的排列都可以满足要求，因为 n = 0
+//        ["A","A","A","B","B","B"]
+//        ["A","B","A","B","A","B"]
+//        ["B","B","B","A","A","A"]
+//        ...
+//        诸如此类
+//        示例 3：
+//        输入：tasks = ["A","A","A","A","A","A","B","C","D","E","F","G"], n = 2
+//        输出：16
+//        解释：一种可能的解决方案是：
+//        A -> B -> C -> A -> D -> E -> A -> F -> G -> A -> (待命) -> (待命) -> A -> (待命) -> (待命) -> A
+//        Leetcode题目 : https://leetcode.com/problems/task-scheduler/
 public class LeetCode_0621_TaskScheduler {
 
     public static int leastInterval(char[] tasks, int n) {
