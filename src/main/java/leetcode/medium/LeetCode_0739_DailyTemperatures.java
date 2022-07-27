602a4c8068a97e350d6e50bf60cf6041d39c65ef
zenghui <410486047@qq.com>
2022-07-27 15:54:46 +0800

    update

--- src/main/java/leetcode/medium/LeetCode_0739_DailyTemperatures.java
+++ src/main/java/leetcode/medium/LeetCode_0739_DailyTemperatures.java
@@ -15,8 +15,21 @@ package leetcode.medium;
 import java.util.ArrayDeque;
 import java.util.Deque;
 
-// 单调栈
-// https://leetcode.cn/problems/daily-temperatures/
+//请根据每日 气温 列表 temperatures，请计算在每一天需要等几天才会有更高的温度。如果气温在这之后都不会升高，请在该位置用0 来代替。
+//        示例 1:
+//        输入: temperatures = [73,74,75,71,69,72,76,73]
+//        输出:[1,1,4,2,1,1,0,0]
+//        示例 2:
+//        输入: temperatures = [30,40,50,60]
+//        输出:[1,1,1,0]
+//        示例 3:
+//        输入: temperatures = [30,60,90]
+//        输出: [1,1,0]
+//        提示：
+//        1 <=temperatures.length <= 10^5
+//        30 <=temperatures[i]<= 100
+//        Leetcode题目 : https://leetcode.com/problems/daily-temperatures/
+// tips: 单调栈
 public class LeetCode_0739_DailyTemperatures {
     // 一个数右边离它最近的比它大的数是什么
     public static int[] dailyTemperatures(int[] arr) {
