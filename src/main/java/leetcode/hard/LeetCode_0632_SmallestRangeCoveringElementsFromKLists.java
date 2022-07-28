e44621f619314fb2708b94a6ca10e096fa8cd371
zenghui <410486047@qq.com>
2022-07-28 10:41:22 +0800

    update notes

--- src/main/java/leetcode/hard/LeetCode_0632_SmallestRangeCoveringElementsFromKLists.java
+++ src/main/java/leetcode/hard/LeetCode_0632_SmallestRangeCoveringElementsFromKLists.java
@@ -1,33 +1,9 @@
 package leetcode.hard;
 
 import java.util.*;
-// 你有 k 个 非递减排列 的整数列表。找到一个 最小 区间，使得 k 个列表中的每个列表至少有一个数包含在其中。
-
-// 我们定义如果 b-a<d-c 或者在 b-a==d-c 时 a<c，则区间[a,b]比[c,d]小。
-
-//  
-
-// 示例 1：
-
-// 输入：nums=[[4,10,15,24,26],[0,9,12,20],[5,18,22,30]]
-// 输出：[20,24]
-// 解释：列表 1：[4,10,15,24,26]，24 在区间[20,24]中。列表 2：[0,9,12,20]，20 在区间[20,24]中。列表 3：[5,18,22,30]，22 在区间[20,24]中。
-
-//示例 2：
-
-// 输入：nums=[[1,2,3],[1,2,3],[1,2,3]]  
-// 输出：[1,1] 
-
-// 提示：
-
-// nums.length==k 
-// 1<=k<=3500 
-// 1<=nums[i].length<=50
-// -10^5<=nums[i][j]<=10^5 
-// nums[i]按非递减顺序排列
-
-// 来源：力扣（LeetCode）链接：https://leetcode.cn/problems/smallest-range-covering-elements-from-k-lists
-// 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+//你有k个非递减排列的整数列表。找到一个最小区间，使得k个列表中的每个列表至少有一个数包含在其中
+//        我们定义如果b-a < d-c或者在b-a == d-c时a < c，则区间 [a,b] 比 [c,d] 小。
+//        Leetcode题目：https://leetcode.com/problems/smallest-range-covering-elements-from-k-lists/
 // 笔记：https://www.cnblogs.com/greyzeng/p/16463385.html
 public class LeetCode_0632_SmallestRangeCoveringElementsFromKLists {
     public static class Node {
