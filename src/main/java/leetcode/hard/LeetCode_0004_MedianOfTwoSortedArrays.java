d65ee65538e288dc0aa397980e8a5bec8c8bb8b0
zenghui <410486047@qq.com>
2022-07-28 09:41:16 +0800

    update notes

--- src/main/java/leetcode/hard/LeetCode_0004_MedianOfTwoSortedArrays.java
+++ src/main/java/leetcode/hard/LeetCode_0004_MedianOfTwoSortedArrays.java
@@ -1,6 +1,7 @@
 
 package leetcode.hard;
-
+//给定两个大小分别为 m 和 n 的正序（从小到大）数组 nums1 和 nums2。请你找出并返回这两个正序数组的中位数
+//        进阶，在两个都有序的数组中找整体第K小的数，可以做到O(log(Min(M,N)))
 // 寻找两个正序数组中的中位数
 // 笔记：https://www.cnblogs.com/greyzeng/p/16324785.html
 // https://leetcode.com/problems/median-of-two-sorted-arrays/
--- src/main/java/leetcode/medium/LeetCode_0128_LongestConsecutiveSequence.java
+++ src/main/java/leetcode/medium/LeetCode_0128_LongestConsecutiveSequence.java
@@ -2,6 +2,8 @@ package leetcode.medium;
 
 import java.util.HashMap;
 import java.util.Map;
+//给定一个未排序的整数数组 nums ，找出数字连续的最长序列（不要求序列元素在原数组中连续）的长度。请你设计并实现时间复杂度为O(n)
+//        的算法解决此问题。
 //tips:
 //        最优解 可以做到O(N)
 //        头map key:值 value：长度
