e4c04545cbe78e2552130a384cbd919fe145e1a6
zenghui <410486047@qq.com>
2022-07-28 15:38:28 +0800

    update notes

--- src/main/java/nowcoder/NowCoder_NearestLeft.java
+++ src/main/java/nowcoder/NowCoder_NearestLeft.java
@@ -1,6 +1,5 @@
 package nowcoder;
 
-
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -8,28 +7,22 @@ import java.util.Scanner;
 // 二分查找： 在一个有序数组中，找大于等于某个数最左侧的位置
 // 链接：https://www.nowcoder.com/questionTerminal/fcd66d61e04e476d9bd116ff6b4d04e7
 public class NowCoder_NearestLeft {
-
-    public static int getNearestLeft(int[] nums, int target) {
-        if (nums == null || nums.length < 1) {
-            return -1;
-        }
+    public static int getNearestLeft(int[] arr, int t) {
+        int ans = -1;
         int l = 0;
-        int r = nums.length - 1;
-        int ans = 0;
+        int r = arr.length - 1;
         while (l <= r) {
-            int mid = l + ((r - l) >> 1);
-            if (target < nums[mid]) {
-                ans = mid;
-                r = mid - 1;
-            } else if (target > nums[mid]) {
-                l = mid + 1;
+            int m = l + ((r - l) >> 1);
+            if (arr[m] < t) {
+                l = m + 1;
             } else {
-                ans = mid;
-                r = mid - 1;
+                r = m - 1;
+                ans = m;
             }
         }
         return ans;
     }
+
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
