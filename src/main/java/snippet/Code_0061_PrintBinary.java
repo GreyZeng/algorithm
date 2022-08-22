c626bb5605e17a3cb9184e950ac621dd35786343
zenghui <410486047@qq.com>
2022-08-22 10:37:00 +0800

    print binary of num

--- src/main/java/snippet/Code_0061_PrintBinary.java
+++ src/main/java/snippet/Code_0061_PrintBinary.java
@@ -1,15 +1,8 @@
 package snippet;
 
-// 打印一个数的二进制形式
+
 public class Code_0061_PrintBinary {
-
-    public static void print(int num) {
-        for (int i = 31; i >= 0; i--) {
-            System.out.print((num & (1 << i)) == 0 ? "0" : "1");
-        }
-        System.out.println();
-    }
-
+    // 打印一个32位整数的二进制形式
     public static void printBinary(int num) {
         for (int i = 31; i >= 0; i--) {
             System.out.print((num & (1 << i)) == 0 ? "0" : "1");
@@ -18,61 +11,23 @@ public class Code_0061_PrintBinary {
     }
 
     public static void main(String[] args) {
-        int num = 0b0000_1110_1010;
+        int num = 0b10001010_11101010_10001000_11101010;
+        System.out.println("原始串：");
         printBinary(num);
+        System.out.println("取反：");
+        printBinary(~num);
+        System.out.println("左移一位：");
         printBinary(num << 1);
+        System.out.println("不带符号右移一位：");
+        printBinary(num >> 1);
+        System.out.println("带符号右移动一位：");
+        printBinary(num >>> 1);
+        System.out.println("系统最大值的二进制：");
         printBinary(Integer.MAX_VALUE);
+        System.out.println("系统最小值的二进制：");
         printBinary(Integer.MIN_VALUE);
-        // 32位
-//		int num = 4;
-//
-//		print(num);
-//		
-//		
-//		int test = 1123123;
-//		print(test);
-//		print(test<<1);
-//		print(test<<2);
-//		print(test<<8);
-//		
-//		
-//		int a = Integer.MAX_VALUE;
-//		System.out.println(a);
-
-//		print(-1);
-//		int a = Integer.MIN_VALUE;
-//		print(a);
-
-//		int b = 123823138;
-//		int c = ~b;
-//		print(b);
-//		print(c);
-
-//		print(-5);
-
-//		System.out.println(Integer.MIN_VALUE);
-//		System.out.println(Integer.MAX_VALUE);
-
-//		int a = 12319283;
-//		int b = 3819283;
-//		print(a);
-//		print(b);
-//		System.out.println("=============");
-//		print(a | b);
-//		print(a & b);
-//		print(a ^ b);
-
-//		int a = Integer.MIN_VALUE;
-//		print(a);
-//		print(a >> 1);
-//		print(a >>> 1);
-//		
-//		int c = Integer.MIN_VALUE;
-//		int d = -c ;
-//		
-//		print(c);
-//		print(d);
-
+        System.out.println("负的系统最小值的二进制(还是本身)：");
+        printBinary(-Integer.MIN_VALUE);
+        System.out.println((-Integer.MIN_VALUE) == Integer.MIN_VALUE);
     }
-
 }
