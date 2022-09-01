44eee746325a7f1645aabeb2bb4b9ad564920d1c
GreyZeng <410486047@qq.com>
2022-09-01 19:05:59 +0800

    update

--- src/leetcode/medium/LeetCode_0208_Trie.java
+++ src/leetcode/medium/LeetCode_0208_Trie.java
@@ -1,21 +1,6 @@
 package leetcode.medium;
 
-//Implement a trie with insert, search, and startsWith methods.
-//
-//        Example:
-//
-//        Trie trie = new Trie();
-//
-//        trie.insert("apple");
-//        trie.search("apple");   // returns true
-//        trie.search("app");     // returns false
-//        trie.startsWith("app"); // returns true
-//        trie.insert("app");
-//        trie.search("app");     // returns true
-//        Note:
-//
-//        You may assume that all inputs are consist of lowercase letters a-z.
-//        All inputs are guaranteed to be non-empty strings.
+// 笔记：
 // https://leetcode-cn.com/problems/implement-trie-prefix-tree/
 public class LeetCode_0208_Trie {
 
