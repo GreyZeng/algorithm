050cb7e9b3f8ee1304519753e208255eeeb195bc
GreyZeng <410486047@qq.com>
2022-09-20 22:14:31 +0800

    add notes

--- src/lintcode/easy/LintCode_0110_MinimumPathSum.java
+++ src/lintcode/easy/LintCode_0110_MinimumPathSum.java
@@ -4,6 +4,7 @@ package lintcode.easy;
 // 给定一个只含非负整数的m*nm∗n网格，找到一条从左上角到右下角的可以使数字和最小的路径。
 // 你在同一时间只能向下或者向右移动一步
 // https://www.lintcode.com/problem/110/
+// 笔记：https://www.cnblogs.com/greyzeng/p/16712777.html
 public class LintCode_0110_MinimumPathSum {
     // 暴力解，超时
     public static int minPathSum(int[][] grid) {
