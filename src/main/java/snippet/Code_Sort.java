125c7d3d0e4623ae5038cdceacbb6f9f29b55b2c
GreyZeng <410486047@qq.com>
2022-10-18 10:30:58 +0800

    update notes

--- src/main/java/snippet/Code_Sort.java
+++ src/main/java/snippet/Code_Sort.java
@@ -3,41 +3,18 @@ package snippet;
 import java.util.Arrays;
 
 /**
- * 简单排序：插入排序，冒泡排序，选择排序
- *
  * @author Grey
- * @see <a href="https://www.cnblogs.com/greyzeng/p/15186769.html">笔记：简单排序（冒泡排序，插入排序，选择排序）</a>
+ * @see <a href="https://www.cnblogs.com/greyzeng/p/15186769.html">简单排序（冒泡排序，插入排序，选择排序）</a>
  */
 public class Code_Sort {
 
-    /**
-     * <b>插入排序</b>流程
-     * <p>
-     * 想让arr[0...0]上有序，这个范围只有一个数，当然是有序的。
-     * <p>
-     * 想让arr[0...1]上有序，所以从arr[1]开始往前看，如果arr[1] < arr[0]，就交换。否则什么也不做。
-     * <p>
-     * 依此类推……
-     * <p>
-     * 想让arr[0...i]上有序，所以从arr[i]开始往前看，arr[i]这个数不停向左移动，一直移动到左边的数字不再比自己大，停止移动。
-     * <p>
-     * 最后一步，
-     * <p>
-     * 想让arr[0...N-1]上有序， arr[N-1]这个数不停向左移动，一直移动到左边的数字不再比自己大，停止移动。
-     * <p>
-     * 估算时<b>发现这个算法流程的复杂程度，会因为数据状况的不同而不同</b>。
-     * <p>
-     * 如果数组本身是有序的，那么插入排序的过程不需要移动任何数字，
-     * <p>
-     * 但是时间复杂度是以最坏情况估计，所以插入排序的时间复杂度仍然是<code>O(N^2)</code>
-     *
-     * @param arr 未排序数组
-     */
+    // 插入排序
     public static void insertionSort(int[] arr) {
         if (arr == null || arr.length < 2) {
             return;
         }
         for (int i = 1; i < arr.length; i++) {
+            // 不断向前看
             for (int j = i - 1; j >= 0; j--) {
                 if (arr[j] > arr[j + 1]) {
                     swap(arr, j + 1, j);
@@ -46,23 +23,7 @@ public class Code_Sort {
         }
     }
 
-    /**
-     * 选择排序
-     * <p>
-     * arr[0...N-1]范围上，找到最小值所在的位置，然后把最小值交换到0位置；
-     * <p>
-     * arr[1...N-1]范围上，找到最小值所在的位置，然后把最小值交换到1位置；
-     * <p>
-     * arr[2...N-1]范围上，找到最小值所在的位置，然后把最小值交换到2位置；
-     * <p>
-     * 依此类推……
-     * <p>
-     * arr[N-1...N-1]范围上，找到最小值位置，然后把最小值交换到N-1位置；
-     * <p>
-     * 所以选择排序的时间复杂度为<code>O(N^2)</code>
-     *
-     * @param arr 未排序数组
-     */
+    // 选择排序
     public static void selectionSort(int[] arr) {
         if (arr == null || arr.length < 2) {
             return;
@@ -76,31 +37,7 @@ public class Code_Sort {
         }
     }
 
-    /**
-     * 冒泡排序
-     * <p>
-     * 在arr[0...N-1]范围上：
-     * <p>
-     * arr[0]和arr[1]，谁大谁来到1位置；
-     * <p>
-     * arr[1]和arr[2]，谁大谁来到2位置；
-     * <p>
-     * 依此类推……
-     * <p>
-     * arr[N-2]和arr[N-1]，谁大谁来到第N-1个位置上；
-     * <p>
-     * 在arr[0...N-2]范围上，重复上面的过程，但最后一步是arr[N-3]和arr[N-2]，谁大谁来到第N-2个位置上；
-     * <p>
-     * 在arr[0...N-3]范围上，重复上面的过程，但最后一步是arr[N-4]和arr[N-3]，谁大谁来到第N-3个位置上；
-     * <p>
-     * 依此类推……
-     * <p>
-     * 最后在arr[0...1]范围上，重复上面的过程，但最后一步是arr[0]和arr[1]，谁大谁来到第一个位置上；
-     * <p>
-     * 时间复杂度<code>O(N^2)</code>
-     *
-     * @param arr 未排序数组
-     */
+    // 冒泡排序
     public static void bubbleSort(int[] arr) {
         if (arr == null || arr.length < 2) {
             return;
@@ -115,6 +52,9 @@ public class Code_Sort {
     }
 
     private static void swap(int[] arr, int i, int j) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
         if (i != j) {
             arr[i] = arr[i] ^ arr[j];
             arr[j] = arr[i] ^ arr[j];
