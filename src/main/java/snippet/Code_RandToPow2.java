5b4fa241fc01b6f2c5c106362c3cc2afd36002e4
GreyZeng <410486047@qq.com>
2022-10-19 10:37:42 +0800

    update code

--- src/main/java/snippet/Code_RandToPow2.java
+++ src/main/java/snippet/Code_RandToPow2.java
@@ -2,7 +2,7 @@ package snippet;
 
 // 将`[0,x)`中的数出现的的概率调整成`x^2`
 // 笔记见：https://www.cnblogs.com/greyzeng/p/16618329.html
-public class Code_0004_RandToPow2 {
+public class Code_RandToPow2 {
     // 将`[0,x)`中的数出现的的概率调整成`x^2`
     public static double randToPow2() {
         return Math.max(Math.random(), Math.random());
@@ -11,13 +11,14 @@ public class Code_0004_RandToPow2 {
     // 测试用例
     public static void main(String[] args) {
         int count = 0;
-        int testTimes = 10000000;
+        int testTimes = 20000000;
         double x = 0.17;
         for (int i = 0; i < testTimes; i++) {
             if (randToPow2() < x) {
                 count++;
             }
         }
+        // 以下两个数值应该大小一致
         System.out.println((double) count / (double) testTimes);
         System.out.println(Math.pow(x, 2));
     }
