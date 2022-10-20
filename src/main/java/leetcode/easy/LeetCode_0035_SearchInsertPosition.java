82254b47221cd30607ffb955be66ac40197d13c5
GreyZeng <410486047@qq.com>
2022-10-20 20:37:08 +0800

    update binary search ref notes

--- src/main/java/leetcode/easy/LeetCode_0035_SearchInsertPosition.java
+++ src/main/java/leetcode/easy/LeetCode_0035_SearchInsertPosition.java
@@ -1,5 +1,6 @@
 package leetcode.easy;
 
+// 笔记：https://www.cnblogs.com/greyzeng/p/16622554.html
 // 二分查找： 在一个有序数组中，找大于等于某个数最左侧的位置，如果找不到，则返回数组长度
 // https://leetcode.cn/problems/search-insert-position/
 public class LeetCode_0035_SearchInsertPosition {
