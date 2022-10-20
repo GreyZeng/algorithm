f134e028253f4fb99050d60c781ef96cee13a988
GreyZeng <410486047@qq.com>
2022-10-20 16:27:25 +0800

    update leetcode 704

--- src/main/java/leetcode/easy/LeetCode_0704_BinarySearch.java
+++ src/main/java/leetcode/easy/LeetCode_0704_BinarySearch.java
@@ -1,22 +1,24 @@
 package leetcode.easy;
-
+// 笔记：https://www.cnblogs.com/greyzeng/p/16622554.html)
 // 二分查找：在一个有序数组中，找某个数是否存在
 // https://leetcode.cn/problems/binary-search/
 public class LeetCode_0704_BinarySearch {
-    public int search(int[] arr, int t) {
+
+    public static int search(int[] arr, int target) {
         if (arr == null || arr.length < 1) {
             return -1;
         }
         int l = 0;
         int r = arr.length - 1;
         while (l <= r) {
-            int m = l + ((r - l) >> 1);
-            if (arr[m] == t) {
-                return m;
-            } else if (arr[m] > t) {
+            int m = l + ((r - l)>>1);
+            if (arr[m] < target) {
+                l = m + 1;
+            } else if (arr[m] > target) {
                 r = m - 1;
             } else {
-                l = m + 1;
+                // arr[m] == target
+                return m;
             }
         }
         return -1;
