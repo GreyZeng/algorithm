bfa1424bd267168f6b7c28e989dab3a693103a23
GreyZeng <410486047@qq.com>
2022-12-09 21:50:17 +0800

    update notes

--- src/main/java/snippet/Code_SplitSumClosed.java
+++ src/main/java/snippet/Code_SplitSumClosed.java
@@ -1,8 +1,9 @@
 package snippet;
 
+// 笔记：https://www.cnblogs.com/greyzeng/p/16970094.html
 // 给定一个正数数组arr， 请把arr中所有的数分成两个集合，尽量让两个集合的累加和接近
 // 返回： 最接近的情况下，较小集合的累加和
-public class Code_0083_SplitSumClosed {
+public class Code_SplitSumClosed {
     public static int splitSumClosed(int[] arr) {
         if (arr == null || arr.length < 2) {
             return 0;
