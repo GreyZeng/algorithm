9ba3dc39487bbdcb5e348f42e1f51bfe97ea1acc
GreyZeng <410486047@qq.com>
2022-12-27 18:37:23 +0800

    update lru cache notes

--- src/main/java/数据结构/LFU/LeetCode_0460_LFUCache.java
+++ src/main/java/数据结构/LFU/LeetCode_0460_LFUCache.java
@@ -1,46 +1,3 @@
-/*
- * Design and implement a data structure for a Least Frequently Used (LFU) cache. Implement the
- * LFUCache class:
- * 
- * LFUCache(int capacity) Initializes the object with the capacity of the data structure. int
- * get(int key) Gets the value of the key if the key exists in the cache. Otherwise, returns -1.
- * void put(int key, int value) Update the value of the key if present, or inserts the key if not
- * already present. When the cache reaches its capacity, it should invalidate and remove the least
- * frequently used key before inserting a new item. For this problem, when there is a tie (i.e., two
- * or more keys with the same frequency), the least recently used key would be invalidated. To
- * determine the least frequently used key, a use counter is maintained for each key in the cache.
- * The key with the smallest use counter is the least frequently used key.
- * 
- * When a key is first inserted into the cache, its use counter is set to 1 (due to the put
- * operation). The use counter for a key in the cache is incremented either a get or put operation
- * is called on it.
- * 
- * 
- * 
- * Example 1:
- * 
- * Input ["LFUCache", "put", "put", "get", "put", "get", "get", "put", "get", "get", "get"] [[2],
- * [1, 1], [2, 2], [1], [3, 3], [2], [3], [4, 4], [1], [3], [4]] Output [null, null, null, 1, null,
- * -1, 3, null, -1, 3, 4]
- * 
- * Explanation // cnt(x) = the use counter for key x // cache=[] will show the last used order for
- * tiebreakers (leftmost element is most recent) LFUCache lfu = new LFUCache(2); lfu.put(1, 1); //
- * cache=[1,_], cnt(1)=1 lfu.put(2, 2); // cache=[2,1], cnt(2)=1, cnt(1)=1 lfu.get(1); // return 1
- * // cache=[1,2], cnt(2)=1, cnt(1)=2 lfu.put(3, 3); // 2 is the LFU key because cnt(2)=1 is the
- * smallest, invalidate 2. // cache=[3,1], cnt(3)=1, cnt(1)=2 lfu.get(2); // return -1 (not found)
- * lfu.get(3); // return 3 // cache=[3,1], cnt(3)=2, cnt(1)=2 lfu.put(4, 4); // Both 1 and 3 have
- * the same cnt, but 1 is LRU, invalidate 1. // cache=[4,3], cnt(4)=1, cnt(3)=2 lfu.get(1); //
- * return -1 (not found) lfu.get(3); // return 3 // cache=[3,4], cnt(4)=1, cnt(3)=3 lfu.get(4); //
- * return 4 // cache=[3,4], cnt(4)=2, cnt(3)=3
- * 
- * 
- * Constraints:
- * 
- * 0 <= capacity, key, value <= 10^4 At most 10^5 calls will be made to get and put.
- * 
- * 
- * Follow up: Could you do both operations in O(1) time complexity?
- */
 package 数据结构.LFU;
 
 import java.util.HashMap;
@@ -52,10 +9,11 @@ import java.util.HashMap;
 // 同样词频的数据只会进一个桶
 // 桶内双向链表，桶之间也是双向链表
 // tips:
-// Put/Get方法都用O(1)
+// Put/Get方法都O(1)
 // 二维双向链表
 // map1 : str->node
 // map2: node->bucket(频度为n的桶)
+// 笔记：
 public class LeetCode_0460_LFUCache {
   static class LFUCache {
 
@@ -69,7 +27,7 @@ public class LeetCode_0460_LFUCache {
 
     private int capacity; // 缓存的大小限制，即K
     private int size; // 缓存目前有多少个节点
-    private HashMap<Integer, Node> records;// 表示key(Integer)由哪个节点(Node)代表
+    private HashMap<Integer, Node> records; // 表示key(Integer)由哪个节点(Node)代表
     private HashMap<Node, NodeList> heads; // 表示节点(Node)在哪个桶(NodeList)里
     private NodeList headList; // 整个结构中位于最左的桶
 
@@ -79,7 +37,7 @@ public class LeetCode_0460_LFUCache {
       public Integer value;
       public Integer times; // 这个节点发生get或者set的次数总和
       public Node up; // 节点之间是双向链表所以有上一个节点
-      public Node down;// 节点之间是双向链表所以有下一个节点
+      public Node down; // 节点之间是双向链表所以有下一个节点
 
       public Node(int k, int v, int t) {
         key = k;
@@ -253,6 +211,5 @@ public class LeetCode_0460_LFUCache {
       move(node, curNodeList);
       return node.value;
     }
-
   }
 }
