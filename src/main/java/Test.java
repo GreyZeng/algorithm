3127e93a89c6022f6b3a4fed372fead69355b95a
GreyZeng <410486047@qq.com>
2023-02-04 19:04:19 +0800

    update notes

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,6 +1,7 @@
 public class Test {
     public static void main(String[] args) {
-        int[] arr = {11, 1, 15, 10, 13, 4};
+        int[] arr = { 11, 1, 15, 10, 13, 4 };
+        printArray(arr);
         int[] eor = new int[arr.length];
         eor[0] = arr[0];
         for (int i = 1; i < arr.length; i++) {
@@ -8,6 +9,15 @@ public class Test {
         }
         for (int i : eor) {
             System.out.println(Integer.toBinaryString(i));
+        }
+    }
+
+    static void printArray(int[] arr) {
+        if (null == arr || arr.length == 0) {
+            return;
+        }
+        for (int i : arr) {
+            System.out.print(i + " ");
         }
     }
 }
