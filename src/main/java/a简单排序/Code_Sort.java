9553f103bc2043aa892615494c52e9ab3f661641
GreyZeng <410486047@qq.com>
2023-02-13 23:19:27 +0800

    basic sort

--- src/main/java/a简单排序/Code_Sort.java
+++ src/main/java/a简单排序/Code_Sort.java
@@ -1,4 +1,4 @@
-package 算法.排序;
+package a简单排序;
 
 import java.util.Arrays;
 
@@ -10,11 +10,20 @@ import java.util.Arrays;
  */
 public class Code_Sort {
     // 冒泡排序
+    //    在 arr[0...N-1] 范围上：
+    //    arr[0] 和 arr[1]，谁大谁来到 1 号位置；
+    //    arr[1] 和 arr[2]，谁大谁来到 2 号位置；
+    //    依此类推……
+    //    arr[N-2] 和 arr[N-1]，谁大谁来到第 N-1 号位置上；
+    //    在 arr[0...N-2] 范围上，重复上面的过程，但最后一步是 arr[N-3] 和 arr[N-2] ，谁大谁来到第 N-2 号位置上；
+    //    在 arr[0...N-3] 范围上，重复上面的过程，但最后一步是 arr[N-4] 和 arr[N-3]，谁大谁来到第 N-3 号位置上；
+    //    依此类推……
+    //    最后在 arr[0...1] 范围上，重复上面的过程，但最后一步是 arr[0] 和 arr[1]，谁大谁来到第 1 号位置上；
     public static void bubbleSort(int[] arr) {
         if (arr == null || arr.length < 2) {
             return;
         }
-        for (int i = arr.length - 1; i >= 0; i--) {
+        for (int i = arr.length - 1; i > 0; i--) {
             for (int j = 0; j < i; j++) {
                 if (arr[j] > arr[j + 1]) {
                     swap(arr, j, j + 1);
@@ -24,6 +33,14 @@ public class Code_Sort {
     }
 
     // 插入排序
+    //    流程如下
+    //    想让 arr[0...0] 上有序，这个范围只有一个数，当然是有序的。
+    //    想让 arr[0...1] 上有序，所以从 arr[1] 开始往前看，如果 arr[1] < arr[0]，就交换。否则什么也不做。
+    //    依此类推……
+    //    想让 arr[0...i] 上有序，所以从 arr[i] 开始往前看，arr[i] 这个数不停向左移动，一直移动到左边的数字不再比自己大，停止移动。
+    //    最后一步，
+    //    想让 arr[0...N-1] 上有序，arr[N-1] 这个数不停向左移动，一直移动到左边的数字不再比自己大，停止移动。
+    //    估算时发现这个算法流程的复杂程度，会因为数据状况的不同而不同。如果数组本身是有序的，那么插入排序的过程不需要移动任何数字，但是时间复杂度是以最坏情况估计，所以插入排序的时间复杂度仍然是 O(N^2)。
     public static void insertionSort(int[] arr) {
         if (arr == null || arr.length < 2) {
             return;
@@ -38,6 +55,11 @@ public class Code_Sort {
 
 
     // 选择排序
+//    arr[0...N-1] 范围上，找到最小值所在的位置，然后把最小值交换到 0 号位置；
+//    arr[1...N-1] 范围上，找到最小值所在的位置，然后把最小值交换到 1 号位置；
+//    arr[2...N-1] 范围上，找到最小值所在的位置，然后把最小值交换到 2 号位置；
+//    依此类推……
+//    arr[N-1...N-1] 范围上，找到最小值位置，然后把最小值交换到 N-1 号位置；
     public static void selectionSort(int[] arr) {
         if (null == arr || arr.length < 2) {
             return;
