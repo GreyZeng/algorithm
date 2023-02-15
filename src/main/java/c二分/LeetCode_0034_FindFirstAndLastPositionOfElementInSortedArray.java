288d21ef1d353d3322265de89c86df8fc37024e9
GreyZeng <410486047@qq.com>
2023-02-15 19:58:59 +0800

    leetcode 34 find first and last position of element in sorted array

--- src/main/java/c二分/LeetCode_0034_FindFirstAndLastPositionOfElementInSortedArray.java
+++ src/main/java/c二分/LeetCode_0034_FindFirstAndLastPositionOfElementInSortedArray.java
@@ -5,58 +5,51 @@ package c二分;
 // 如果数组中不存在目标值 target，返回[-1, -1]。
 // 你必须设计并实现时间复杂度为O(log n)的算法解决此问题。
 // https://www.cnblogs.com/greyzeng/p/16622554.html
-// https://leetcode-cn.com/problems/find-first-and-last-position-of-element-in-sorted-array/
+// https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/description/
 public class LeetCode_0034_FindFirstAndLastPositionOfElementInSortedArray {
 
     // O(logN)
-    public static int[] searchRange(int[] arr, int t) {
-        if (arr == null || arr.length < 1) {
+    public int[] searchRange(int[] nums, int target) {
+        if (null == nums || 0 == nums.length) {
             return new int[]{-1, -1};
         }
-        return new int[]{left(arr, t), right(arr, t)};
+        return new int[]{left(nums, target), right(nums, target)};
     }
 
-    public static int left(int[] arr, int t) {
-        if (arr == null || arr.length < 1) {
-            return -1;
-        }
+    public int left(int[] nums, int target) {
         int ans = -1;
-        int l = 0;
-        int r = arr.length - 1;
-        while (l <= r) {
-            int m = l + ((r - l) >> 1);
-            if (arr[m] == t) {
-                ans = m;
-                r = m - 1;
-            } else if (arr[m] < t) {
-                l = m + 1;
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = ((right - left) >> 1) + left;
+            if (nums[mid] == target) {
+                ans = mid;
+                right = mid - 1;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
             } else {
-                // arr[m] > t
-                r = m - 1;
+                left = mid + 1;
             }
         }
         return ans;
     }
 
-    public static int right(int[] arr, int t) {
-        if (arr == null || arr.length < 1) {
-            return -1;
-        }
+    public int right(int[] nums, int target) {
         int ans = -1;
-        int l = 0;
-        int r = arr.length - 1;
-        while (l <= r) {
-            int m = l + ((r - l) >> 1);
-            if (arr[m] == t) {
-                ans = m;
-                l = m + 1;
-            } else if (arr[m] < t) {
-                l = m + 1;
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = ((right - left) >> 1) + left;
+            if (nums[mid] == target) {
+                ans = mid;
+                left = mid + 1;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
             } else {
-                // arr[m] > t
-                r = m - 1;
+                left = mid + 1;
             }
         }
         return ans;
     }
+
 }
