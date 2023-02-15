da1b8a0b69f37f6a20a0491f39dbd8f9b76ca405
GreyZeng <410486047@qq.com>
2023-02-15 19:44:56 +0800

    leetcode 35 search insert position

--- src/main/java/c二分/LeetCode_0035_SearchInsertPosition.java
+++ src/main/java/c二分/LeetCode_0035_SearchInsertPosition.java
@@ -4,17 +4,17 @@ package c二分;
 // 二分查找： 在一个有序数组中，找大于等于某个数最左侧的位置，如果找不到，则返回数组长度
 // https://leetcode.cn/problems/search-insert-position/
 public class LeetCode_0035_SearchInsertPosition {
-    public static int searchInsert(int[] arr, int t) {
-        int ans = arr.length;
-        int l = 0;
-        int r = arr.length - 1;
-        while (l <= r) {
-            int m = l + ((r - l) >> 1);
-            if (arr[m] >= t) {
-                ans = m;
-                r = m - 1;
+    public int searchInsert(int[] nums, int target) {
+        int ans = nums.length;
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + ((right - left) >> 1);
+            if (nums[mid] >= target) {
+                right = mid - 1;
+                ans = mid;
             } else {
-                l = m + 1;
+                left = mid + 1;
             }
         }
         return ans;
