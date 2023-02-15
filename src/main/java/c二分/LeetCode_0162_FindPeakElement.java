c56a3a1e540231f9871e196e3bb696c146bbdaf7
GreyZeng <410486047@qq.com>
2023-02-15 20:17:11 +0800

    leetcode 162 find peak element

--- src/main/java/c二分/LeetCode_0162_FindPeakElement.java
+++ src/main/java/c二分/LeetCode_0162_FindPeakElement.java
@@ -4,40 +4,40 @@ package c二分;
 // 1 <= nums.length <= 1000
 // -2^31 <= nums[i] <= 2^31 - 1
 // 对于所有有效的 i 都有 nums[i] != nums[i + 1]
-// https://leetcode-cn.com/problems/find-peak-element/
+// https://leetcode.com/problems/find-peak-element/
 // 返回局部高点的位置，如果有多个，返回一个峰值位置即可
-// 即对于所有有效的 i 都有 arr[i] != arr[i + 1]
+// 时间复杂度要求 O(logN)
+// 有一个限制条件：即对于所有有效的 i 都有 arr[i] != arr[i + 1]
 // 二分法
 // 笔记：https://www.cnblogs.com/greyzeng/p/16622554.html
 public class LeetCode_0162_FindPeakElement {
-    public static int findPeakElement(int[] arr) {
-        if (arr == null || arr.length == 0) {
-            return -1;
-        }
-        if (arr.length == 1) {
+    public int findPeakElement(int[] nums) {
+        if (nums.length == 1) {
             return 0;
         }
-        if (arr[1] < arr[0]) {
-            return 0;
+        if (nums.length == 2) {
+            return nums[0] > nums[1] ? 0 : 1;
         }
-        if (arr[arr.length - 1] > arr[arr.length - 2]) {
-            return arr.length - 1;
+        int left = 0;
+        int right = nums.length - 1;
+        if (nums[left] > nums[left + 1]) {
+            return left;
         }
-        int ans = -1;
-        int l = 1;
-        int r = arr.length - 2;
-        while (l <= r) {
-            int mid = l + ((r - l) >> 1);
-            if (arr[mid] > arr[mid + 1] && arr[mid] > arr[mid - 1]) {
+        if (nums[right] > nums[right - 1]) {
+            return right;
+        }
+        left = left + 1;
+        right = right - 1;
+        while (left <= right) {
+            int mid = left + ((right - left) >> 1);
+            if (nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1]) {
                 return mid;
-            } else if (arr[mid] < arr[mid + 1]) {
-                l = mid + 1;
-            } else if (arr[mid] < arr[mid - 1]) {
-                r = mid - 1;
-            } else {
-                // 题目要求下，不会走到这个分支
+            } else if (nums[mid] < nums[mid - 1]) {
+                right = mid - 1;
+            } else if (nums[mid] < nums[mid + 1]) {
+                left = mid + 1;
             }
         }
-        return ans;
+        return -1;
     }
 }
