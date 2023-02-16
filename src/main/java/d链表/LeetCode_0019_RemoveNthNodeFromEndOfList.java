d18d5af918f28c15d20ad305eaeef356976ac859
GreyZeng <410486047@qq.com>
2023-02-16 23:12:20 +0800

    remove nth node from end of listt

--- src/main/java/d链表/LeetCode_0019_RemoveNthNodeFromEndOfList.java
+++ src/main/java/d链表/LeetCode_0019_RemoveNthNodeFromEndOfList.java
@@ -3,7 +3,7 @@ package d链表;
 // note 数够n+1个，然后依次遍历到尾部
 // https://www.cnblogs.com/greyzeng/p/14675184.html
 // 和 https://www.lintcode.com/problem/174/ 一样
-public class LeetCode_0019_RemoveNthNodeFromEndofList {
+public class LeetCode_0019_RemoveNthNodeFromEndOfList {
 
     public static class ListNode {
         int val;
@@ -19,17 +19,21 @@ public class LeetCode_0019_RemoveNthNodeFromEndofList {
         ListNode pre = null;
         while (cur != null) {
             n--;
+            cur = cur.next;
             if (n == -1) {
-                // 遍历到n+1位置后，记录一下pre的位置
+                // 走了 n + 1 步
+                // pre 可以开始走了
                 pre = head;
             }
             if (n < -1) {
+                // 走余下的步
+                // pre 和 cur 一起走
                 pre = pre.next;
             }
-            cur = cur.next;
         }
+        // cur 走 n 步直接到头了，说明是倒数第 n 个元素一定是头节点
+        // 此时会换头
         if (pre == null) {
-            // 删除的节点就是头部节点
             return head.next;
         }
         pre.next = pre.next.next;
