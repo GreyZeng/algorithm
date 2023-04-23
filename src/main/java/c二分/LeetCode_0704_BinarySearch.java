98171231b04bb18f4dbcc85cf226f36dc97f6bed
GreyZeng <410486047@qq.com>
2023-04-23 22:14:38 +0800

    binary search

--- src/main/java/c二分/LeetCode_0704_BinarySearch.java
+++ src/main/java/c二分/LeetCode_0704_BinarySearch.java
@@ -2,25 +2,23 @@ package c二分;
 
 // 笔记：https://www.cnblogs.com/greyzeng/p/16622554.html
 // 二分查找：在一个有序数组中，找某个数是否存在
-// https://leetcode.cn/problems/binary-search/
+// https://leetcode.com/problems/binary-search/
+@Deprecated
 public class LeetCode_0704_BinarySearch {
-
     public int search(int[] nums, int target) {
-        if (null == nums || 0 == nums.length) {
-            return -1;
-        }
-        int left = 0;
-        int right = nums.length - 1;
-        while (left <= right) {
-            int mid = left + ((right - left) >> 1);
-            if (nums[mid] == target) {
-                return mid;
-            } else if (nums[mid] > target) {
-                right = mid - 1;
+        int l = 0;
+        int r = nums.length - 1;
+        while (l <= r) {
+            int m = ((r - l) >> 1) + l;
+            if (nums[m] == target) {
+                return m;
+            } else if (nums[m] < target) {
+                l = m + 1;
             } else {
-                left = mid + 1;
+                r = m - 1;
             }
         }
         return -1;
     }
+
 }
