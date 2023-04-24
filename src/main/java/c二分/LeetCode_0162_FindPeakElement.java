bce11d846f0d472ccd3a3a1ac1eccfb5941d2dfb
GreyZeng <410486047@qq.com>
2023-04-24 13:45:32 +0800

    leetcode find peek element

--- src/main/java/c二分/LeetCode_0162_FindPeakElement.java
+++ src/main/java/c二分/LeetCode_0162_FindPeakElement.java
@@ -18,26 +18,31 @@ public class LeetCode_0162_FindPeakElement {
         if (nums.length == 2) {
             return nums[0] > nums[1] ? 0 : 1;
         }
-        int left = 0;
-        int right = nums.length - 1;
-        if (nums[left] > nums[left + 1]) {
-            return left;
+        if (nums[0] > nums[1]) {
+            return 0;
         }
-        if (nums[right] > nums[right - 1]) {
-            return right;
+        if (nums[nums.length - 1] > nums[nums.length - 2]) {
+            return nums.length - 1;
         }
-        left = left + 1;
-        right = right - 1;
-        while (left <= right) {
-            int mid = left + ((right - left) >> 1);
-            if (nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1]) {
-                return mid;
-            } else if (nums[mid] < nums[mid - 1]) {
-                right = mid - 1;
-            } else if (nums[mid] < nums[mid + 1]) {
-                left = mid + 1;
+        int l = 1;
+        int r = nums.length - 2;
+        while (l <= r) {
+            int m = l + ((r - l) >> 1);
+            if (nums[m] > nums[m - 1] && nums[m] > nums[m + 1]) {
+                return m;
+            } else if (nums[m] <= nums[m - 1]) {
+                r = m - 1;
+            } else if (nums[m] <= nums[m + 1]) {
+                l = m + 1;
             }
         }
         return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 1, 3, 5, 6, 4};
+        System.out.println(new LeetCode_0162_FindPeakElement().findPeakElement(arr));
+        int[] arr2 = {1, 2, 3, 1};
+        System.out.println(new LeetCode_0162_FindPeakElement().findPeakElement(arr2));
     }
 }
