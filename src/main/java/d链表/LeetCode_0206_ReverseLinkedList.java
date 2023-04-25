c69493bee3ef8299ee1077d07940e0f4aa2141fd
GreyZeng <410486047@qq.com>
2023-04-25 19:41:25 +0800

    reverse list

--- src/main/java/d链表/LeetCode_0206_ReverseLinkedList.java
+++ src/main/java/d链表/LeetCode_0206_ReverseLinkedList.java
@@ -28,10 +28,10 @@ public class LeetCode_0206_ReverseLinkedList {
             ListNode pre = null;
             ListNode cur = head;
             while (cur != null) {
-                ListNode tmp = cur.next;
+                ListNode t = cur.next;
                 cur.next = pre;
                 pre = cur;
-                cur = tmp;
+                cur = t;
             }
             return pre;
         }
