bb579c8b5591f008834dd3c30cc0e1fda4ee125e
GreyZeng <410486047@qq.com>
2023-04-28 14:13:00 +0800

    merge two sorted list

--- src/main/java/d链表/LeetCode_0021_MergeTwoSortedLists.java
+++ src/main/java/d链表/LeetCode_0021_MergeTwoSortedLists.java
@@ -1,50 +1,59 @@
 package d链表;
 
+// merge two sorted lists
 // leetcode: https://leetcode.com/problems/merge-two-sorted-lists
+// lintcode: https://www.lintcode.com/problem/165/
 // https://www.cnblogs.com/greyzeng/p/7551789.html
 public class LeetCode_0021_MergeTwoSortedLists {
 
-    public static class ListNode {
-        public int val;
-        public ListNode next;
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
     }
 
-    public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        if (l1 == null || l2 == null) {
-            // 如果任何一个链表为空，那么直接返回另外一个链表即可
-            return l1 == null ? l2 : l1;
+    // Constraints:
+    // The number of nodes in both lists is in the range [0, 50].
+    // -100 <= Node.val <= 100
+    // Both list1 and list2 are sorted in non-decreasing order.
+    // eg:
+    // list1: 1 -> 3 -> 6 -> 9
+    // list2: 2 -> 6 -> 8 -> 12 -> 64
+    // 1 -> 2 -> 3 -> 6 -> 6 -> 8 -> 9 -> 12 -> 64
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (null == list1 || null == list2) {
+            return list1 == null ? list2 : list1;
         }
-        // 谁小谁作为头
-        ListNode head = l1.val > l2.val ? l2 : l1;
-        // t1 和 t2 表示l1和l2下一个要遍历的位置
-        ListNode t1 = head == l1 ? l1.next : l1;
-        ListNode t2 = head == l2 ? l2.next : l2;
-        ListNode cur = head;
-        while (t1 != null || t2 != null) {
-            if (t1 == null) {
-                // l1链表已经到头，剩下只需要把l2链表接入进来即可
-                cur.next = t2;
-                t2 = t2.next;
-                cur = cur.next;
-                continue;
-            }
-            if (t2 == null) {
-                // l2链表已经到头，剩下只需要把l2链表接入进来即可
-                cur.next = t1;
-                t1 = t1.next;
-                cur = cur.next;
-                continue;
-            }
-            // l1和l2都没有到头，那么谁小谁接入进来即可。
-            if (t1.val > t2.val) {
-                cur.next = t2;
-                t2 = t2.next;
+        ListNode start = list1.val < list2.val ? list1 : list2;
+        if (start == list1) {
+            list1 = list1.next;
+        } else {
+            list2 = list2.next;
+        }
+        ListNode cur = start;
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                cur.next = list1;
+                list1 = list1.next;
             } else {
-                cur.next = t1;
-                t1 = t1.next;
+                cur.next = list2;
+                list2 = list2.next;
             }
             cur = cur.next;
         }
-        return head;
+        cur.next = list1 != null ? list1 : list2;
+        return start;
     }
+
 }
