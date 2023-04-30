60a0da9a84669237d736545ac857faf9d6914b7b
GreyZeng <410486047@qq.com>
2023-04-30 17:14:34 +0800

    update notes

--- src/main/java/bit/Code_BitMap.java
+++ src/main/java/bit/Code_BitMap.java
@@ -10,7 +10,7 @@ public class Code_BitMap {
     // 位图
     public static class BitMap {
 
-        private long[] bits;
+        private final long[] bits;
 
         // 初始化
         public BitMap(int max) {
@@ -23,6 +23,7 @@ public class Code_BitMap {
             // bits[num / 64] |= (1L << (num % 64));
             // num % 64 ---> num & 63 即：0b111111
             // 只适用于 2 的 n 次方
+            // 注意：这里是1L非1，如果是1，因为要管64位
             bits[num >> 6] |= (1L << (num & 0b111111));
         }
 
