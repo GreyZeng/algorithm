9197a22aae1ba5c20040cf8961cafb36f819bb01
GreyZeng <410486047@qq.com>
2023-05-05 13:51:50 +0800

    update code

--- src/main/java/list/LeetCode_0023_MergeKSortedLists.java
+++ src/main/java/list/LeetCode_0023_MergeKSortedLists.java
@@ -9,42 +9,40 @@ import java.util.PriorityQueue;
 // 如果X比堆顶元素小，则直接不需要进入堆顶，作为结果链表
 // https://www.cnblogs.com/greyzeng/p/7551789.html
 public class LeetCode_0023_MergeKSortedLists {
-
-    public static class ListNode {
+    public class ListNode {
         int val;
         ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
     }
 
-    public static ListNode mergeKLists(ListNode[] lists) {
-        if (null == lists || lists.length == 0) {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists == null || lists.length == 0) {
             return null;
         }
-        if (1 == lists.length) {
+        if (lists.length == 1) {
             return lists[0];
         }
-        // 小根堆
-        PriorityQueue<ListNode> queue = new PriorityQueue<>(Comparator.comparingInt(o -> o.val));
-        for (ListNode list : lists) {
-            if (null != list) {
-                queue.add(list);
+        PriorityQueue<ListNode> heap = new PriorityQueue<>(Comparator.comparingInt(o -> o.val));
+        for (ListNode node : lists) {
+            if (node != null) {
+                heap.offer(node);
             }
         }
-        ListNode res = queue.poll();
-        ListNode head = res;
-        while (!queue.isEmpty()) {
-            if (res != null) {
-                ListNode n = res.next;
-                if (n == null) {
-                    res.next = queue.poll();
-                    res = res.next;
-                } else if (n.val > queue.peek().val) {
-                    res.next = queue.poll();
-                    res = res.next;
-                    queue.add(n);
-                } else {
-                    res = res.next;
-                }
-            }
+        ListNode head = heap.poll();
+        ListNode cur = head;
+        while (!heap.isEmpty()) {
+            // FIXME
         }
         return head;
     }
