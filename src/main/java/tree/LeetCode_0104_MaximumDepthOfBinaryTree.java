34b54dcc1ffbde54e1b0109a83c8c950826f27ba
GreyZeng <410486047@qq.com>
2023-05-05 18:54:03 +0800

    leetcode 104 maximum depth of binary tree

---
+++ src/main/java/tree/LeetCode_0104_MaximumDepthOfBinaryTree.java
@@ -0,0 +1,84 @@
+package tree;
+
+// 二叉树是的最大深度问题
+// 类似问题，二叉树的最小深度问题：https://www.cnblogs.com/greyzeng/p/16963808.html
+// https://leetcode.com/problems/maximum-depth-of-binary-tree/
+// ref:https://www.lintcode.com/problem/maximum-depth-of-binary-tree/description
+// A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+public class LeetCode_0104_MaximumDepthOfBinaryTree {
+
+	/*
+	 * 注意最小高度比这个复杂，要额外小心判断空
+	 */
+	public class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode() {
+		}
+
+		TreeNode(int val) {
+			this.val = val;
+		}
+
+		TreeNode(int val, TreeNode left, TreeNode right) {
+			this.val = val;
+			this.left = left;
+			this.right = right;
+		}
+	}
+
+	public int maxDepth(TreeNode root) {
+		if (root == null) {
+			return 0;
+		}
+		return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+	}
+
+	// by morris，空间复杂度可以优化成 O(1)
+	public int maxDepthByMorris(TreeNode head) {
+		if (head == null) {
+			return 0;
+		}
+		TreeNode cur = head;
+		TreeNode mostRight;
+		int curLevel = 0;
+		int maxHeight = Integer.MIN_VALUE;
+		while (cur != null) {
+			mostRight = cur.left;
+			if (mostRight != null) {
+				int rightBoardSize = 1;
+				while (mostRight.right != null && mostRight.right != cur) {
+					rightBoardSize++;
+					mostRight = mostRight.right;
+				}
+				if (mostRight.right == null) { // 第一次到达
+					curLevel++;
+					mostRight.right = cur;
+					cur = cur.left;
+					continue;
+				} else { // 第二次到达
+					if (mostRight.left == null) {
+						maxHeight = Math.max(maxHeight, curLevel);
+					}
+					curLevel -= rightBoardSize;
+					mostRight.right = null;
+				}
+			} else { // 只有一次到达
+				curLevel++;
+			}
+			cur = cur.right;
+		}
+		int finalRight = 1;
+		cur = head;
+		while (cur.right != null) {
+			finalRight++;
+			cur = cur.right;
+		}
+		if (cur.left == null) {
+			maxHeight = Math.max(maxHeight, finalRight);
+		}
+		return maxHeight;
+	}
+}
