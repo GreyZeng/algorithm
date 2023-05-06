f1abd216886bae4ef5b5c0316601b3ff91f75773
GreyZeng <410486047@qq.com>
2023-05-06 19:23:27 +0800

    leetcode 113 path sum iii

--- src/main/java/tree/LeetCode_0113_PathSumII.java
+++ src/main/java/tree/LeetCode_0113_PathSumII.java
@@ -5,28 +5,48 @@ import java.util.List;
 
 // https://leetcode.com/problems/path-sum-ii
 public class LeetCode_0113_PathSumII {
-    private static boolean isSum = false;
-    private static int sum = 0;
-    private static List<List<Integer>> ans = new ArrayList<>();
-    // FIXME
-    // TODO
+    private static int SUM = 0;
+
     public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
         if (root == null) {
-            return ans;
+            return new ArrayList<>();
         }
-        sum = targetSum;
+        SUM = targetSum;
+        List<List<Integer>> ans = new ArrayList<>();
+        p(root, 0, new ArrayList<>(), ans);
         return ans;
     }
-    public void p(TreeNode root, int preSum, List<Integer> preList) {
+
+    public void p(TreeNode root, int preSum, List<Integer> preList, List<List<Integer>> ans) {
         if (root.left == null && root.right == null) {
             // leaf note
-            if (preSum + root.val == sum) {
+            if (preSum + root.val == SUM) {
                 preList.add(root.val);
-                ans.add(preList);
-                return;
-            } 
+                ans.add(copy(preList));
+                // 清理现场
+                preList.remove(preList.size() - 1);
+            }
+            return;
         }
+        preList.add(root.val);
+        preSum += root.val;
+        if (root.left != null) {
+            p(root.left, preSum, preList, ans);
+        }
+        if (root.right != null) {
+            p(root.right, preSum, preList, ans);
+        }
+        preList.remove(preList.size() - 1);
     }
+
+    public List<Integer> copy(List<Integer> list) {
+        List<Integer> copy = new ArrayList<>();
+        for (Integer i : list) {
+            copy.add(i);
+        }
+        return copy;
+    }
+
     public class TreeNode {
 
         int val;
