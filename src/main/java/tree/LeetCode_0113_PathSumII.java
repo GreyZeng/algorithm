d829d1b11688d7a5748bad657292f3a3cfa4cbf5
GreyZeng <410486047@qq.com>
2023-05-06 18:42:58 +0800

    add todo

--- src/main/java/tree/LeetCode_0113_PathSumII.java
+++ src/main/java/tree/LeetCode_0113_PathSumII.java
@@ -1,15 +1,32 @@
 package tree;
 
+import java.util.ArrayList;
 import java.util.List;
 
+// https://leetcode.com/problems/path-sum-ii
 public class LeetCode_0113_PathSumII {
+    private static boolean isSum = false;
+    private static int sum = 0;
+    private static List<List<Integer>> ans = new ArrayList<>();
+    // FIXME
     // TODO
     public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
-
-
-        return null;
+        if (root == null) {
+            return ans;
+        }
+        sum = targetSum;
+        return ans;
     }
-
+    public void p(TreeNode root, int preSum, List<Integer> preList) {
+        if (root.left == null && root.right == null) {
+            // leaf note
+            if (preSum + root.val == sum) {
+                preList.add(root.val);
+                ans.add(preList);
+                return;
+            } 
+        }
+    }
     public class TreeNode {
 
         int val;
