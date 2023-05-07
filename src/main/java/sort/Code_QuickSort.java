075800350ed711ef15eb983e23a48e463590f711
GreyZeng <410486047@qq.com>
2023-05-07 14:56:27 +0800

    update code

--- src/main/java/sort/Code_QuickSort.java
+++ src/main/java/sort/Code_QuickSort.java
@@ -40,12 +40,11 @@ public class Code_QuickSort {
     }
 
     public static void swap(int[] arr, int i, int j) {
-        if (i == j) {
-            return;
+        if (i != j) {
+            arr[i] = arr[i] ^ arr[j];
+            arr[j] = arr[i] ^ arr[j];
+            arr[i] = arr[i] ^ arr[j];
         }
-        arr[i] = arr[i] ^ arr[j];
-        arr[j] = arr[i] ^ arr[j];
-        arr[i] = arr[i] ^ arr[j];
     }
 
     public static int[] sortColors(int[] arr, int s, int e) {
