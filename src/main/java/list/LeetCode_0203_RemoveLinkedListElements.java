6092c323015cc2f4c1ac0e8e317d6dcd87f080b3
GreyZeng <410486047@qq.com>
2023-05-08 13:56:10 +0800

    remove linked list elements

--- src/main/java/list/LeetCode_0203_RemoveLinkedListElements.java
+++ src/main/java/list/LeetCode_0203_RemoveLinkedListElements.java
@@ -1,11 +1,10 @@
-
 package list;
 
 // 删除链表的指定节点
 // https://leetcode.com/problems/remove-linked-list-elements/
 // 笔记：https://www.cnblogs.com/greyzeng/p/16629407.html
 public class LeetCode_0203_RemoveLinkedListElements {
-    public  class ListNode {
+    public class ListNode {
         int val;
         ListNode next;
 
@@ -21,6 +20,7 @@ public class LeetCode_0203_RemoveLinkedListElements {
             this.next = next;
         }
     }
+
     public ListNode removeElements(ListNode head, int val) {
         ListNode newHead = head;
         while (newHead != null && newHead.val == val) {
@@ -31,9 +31,17 @@ public class LeetCode_0203_RemoveLinkedListElements {
         }
         ListNode pre = newHead;
         ListNode cur = newHead.next;
-        // TODO
+        while (cur != null) {
+            if (cur.val == val) {
+                pre.next = cur.next;
+                cur = cur.next;
+            } else {
+                pre = pre.next;
+                cur = cur.next;
+            }
+        }
         return newHead;
     }
 
-    
+
 }
