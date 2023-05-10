0123d02b43c14aafe6d56d0c4661518550c7d1b5
GreyZeng <410486047@qq.com>
2023-05-10 10:48:23 +0800

    leetcode 232 implement queue using stack

--- src/main/java/queueandstack/LeetCode_0232_ImplementQueueUsingStacks.java
+++ src/main/java/queueandstack/LeetCode_0232_ImplementQueueUsingStacks.java
@@ -10,44 +10,44 @@ import java.util.Stack;
 // https://leetcode.com/problems/implement-queue-using-stacks/
 // 笔记：https://www.cnblogs.com/greyzeng/p/16631644.html
 public class LeetCode_0232_ImplementQueueUsingStacks {
-    class MyQueue {
-        private final Stack<Integer> push;
-        private final Stack<Integer> pop;
+	class MyQueue {
+		private Stack<Integer> popStack;
+		private Stack<Integer> pushStack;
 
-        public MyQueue() {
-            push = new Stack<>();
-            pop = new Stack<>();
-        }
+		public MyQueue() {
+			popStack = new Stack<>();
+			pushStack = new Stack<>();
+		}
 
-        public void push(int x) {
-            push.push(x);
-        }
+		public void push(int x) {
+			pushStack.push(x);
+		}
 
-        public int pop() {
-            while (!push.isEmpty()) {
-                pop.push(push.pop());
-            }
-            int result = pop.pop();
-            while (!pop.isEmpty()) {
-                push.push(pop.pop());
-            }
-            return result;
-        }
+		public int pop() {
+			while (!pushStack.isEmpty()) {
+				popStack.push(pushStack.pop());
+			}
+			int v = popStack.pop();
+			while (!popStack.isEmpty()) {
+				pushStack.push(popStack.pop());
+			}
+			return v;
+		}
 
-        public int peek() {
-            while (!push.isEmpty()) {
-                pop.push(push.pop());
-            }
-            int result = pop.peek();
-            while (!pop.isEmpty()) {
-                push.push(pop.pop());
-            }
-            return result;
-        }
+		public int peek() {
+			while (!pushStack.isEmpty()) {
+				popStack.push(pushStack.pop());
+			}
+			int v = popStack.peek();
+			while (!popStack.isEmpty()) {
+				pushStack.push(popStack.pop());
+			}
+			return v;
+		}
 
-        public boolean empty() {
-            return push.isEmpty();
-        }
-    }
+		public boolean empty() {
+			return pushStack.isEmpty();
+		}
+	}
 
 }
