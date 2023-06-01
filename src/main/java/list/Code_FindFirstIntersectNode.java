a8c1f571c789b3a7ce68e20c854536fd58a7a2ef
GreyZeng <410486047@qq.com>
2023-06-01 20:52:34 +0800

    update code

--- src/main/java/list/Code_FindFirstIntersectNode.java
+++ src/main/java/list/Code_FindFirstIntersectNode.java
@@ -3,10 +3,10 @@ package list;
 
 // 笔记：https://www.cnblogs.com/greyzeng/p/16926303.html
 
-/**
- * 给定两个可能有环也可能无环的单链表，头节点head1和head2。请实现一个函数，如果两个链表相交，请返回相交的 第一个节点。如果不相交，返回null 【要求】
- * 如果两个链表长度之和为N，时间复杂度请达到O(N)，额外空间复杂度 请达到O(1)。
- */
+// 给定两个可能有环也可能无环的单链表，头节点head1和head2。
+// 请实现一个函数，如果两个链表相交，请返回相交的 第一个节点。如果不相交，返回null
+// 【要求】
+// 如果两个链表长度之和为N，时间复杂度请达到O(N)，额外空间复杂度 请达到O(1)。
 public class Code_FindFirstIntersectNode {
     public static class ListNode {
         public int val;
