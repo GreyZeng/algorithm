317365981a7ea2f37d3a283a97cf2211d9b24769
GreyZeng <410486047@qq.com>
2023-06-01 20:44:21 +0800

    leetcode 160 intersection of two linked list

--- src/main/java/list/LeetCode_0160_IntersectionOfTwoLinkedLists.java
+++ src/main/java/list/LeetCode_0160_IntersectionOfTwoLinkedLists.java
@@ -1,7 +1,7 @@
 package list;
 
 // 笔记：https://www.cnblogs.com/greyzeng/p/16753140.html
-// https://leetcode.cn/problems/intersection-of-two-linked-lists/
+// https://leetcode.com/problems/intersection-of-two-linked-lists/
 // 找到两个链表相交的起始节点
 // tips：最后一个节点是否相等，不相等则一定不相交
 // 如果一个长度是100，另外一个长度是80， 则让100的节点先走20个节点，然后开始两个链表开始走。
@@ -10,35 +10,44 @@ public class LeetCode_0160_IntersectionOfTwoLinkedLists {
     public class ListNode {
         int val;
         ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
     }
 
-    public static ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+    //
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
         if (null == headA || null == headB) {
             return null;
         }
-        if (headA.next == null && headB.next == null) {
-            return headA == headB ? headA : null;
+        if (headA == headB) {
+            return headA;
         }
-        int len1 = getLen(headA);
-        int len2 = getLen(headB);
-        ListNode bigger = len1 > len2 ? headA : headB;
-        ListNode smaller = bigger == headA ? headB : headA;
-        int gap = Math.abs(len1 - len2);
-        while (gap != 0) {
-            bigger = bigger.next;
-            gap--;
+        int lenOfA = getLen(headA);
+        int lenOfB = getLen(headB);
+        int maxLen = Math.max(lenOfA, lenOfB);
+        int gap = Math.abs(lenOfA - lenOfB);
+        ListNode longList = maxLen == lenOfA ? headA : headB;
+        ListNode shortList = longList == headA ? headB : headA;
+        for (int i = 0; i < gap; i++) {
+            longList = longList.next;
         }
-        while (bigger != null && smaller != null) {
-            if (bigger == smaller) {
-                return bigger;
+        if (longList == shortList) {
+            return longList;
+        }
+        while (shortList != null) {
+            shortList = shortList.next;
+            longList = longList.next;
+            if (shortList == longList) {
+                return shortList;
             }
-            bigger = bigger.next;
-            smaller = smaller.next;
         }
         return null;
     }
 
-    public static int getLen(ListNode head) {
+    public int getLen(ListNode head) {
         int len = 0;
         while (head != null) {
             len++;
@@ -46,5 +55,39 @@ public class LeetCode_0160_IntersectionOfTwoLinkedLists {
         }
         return len;
     }
+//    public  ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+//        if (null == headA || null == headB) {
+//            return null;
+//        }
+//        if (headA.next == null && headB.next == null) {
+//            return headA == headB ? headA : null;
+//        }
+//        int len1 = getLen(headA);
+//        int len2 = getLen(headB);
+//        ListNode bigger = len1 > len2 ? headA : headB;
+//        ListNode smaller = bigger == headA ? headB : headA;
+//        int gap = Math.abs(len1 - len2);
+//        while (gap != 0) {
+//            bigger = bigger.next;
+//            gap--;
+//        }
+//        while (bigger != null && smaller != null) {
+//            if (bigger == smaller) {
+//                return bigger;
+//            }
+//            bigger = bigger.next;
+//            smaller = smaller.next;
+//        }
+//        return null;
+//    }
+//
+//    public  int getLen(ListNode head) {
+//        int len = 0;
+//        while (head != null) {
+//            len++;
+//            head = head.next;
+//        }
+//        return len;
+//    }
 
 }
