ee082ca28b80960739188305a04c6cebe5f555c6
GreyZeng <410486047@qq.com>
2023-06-01 17:16:26 +0800

    leetcode 138 copy list with random pointer

--- src/main/java/list/LeetCode_0138_CopyListWithRandomPointer.java
+++ src/main/java/list/LeetCode_0138_CopyListWithRandomPointer.java
@@ -37,58 +37,18 @@ public class LeetCode_0138_CopyListWithRandomPointer {
             cur.next.random = cur.random == null ? null : cur.random.next;
             cur = cur.next.next;
         }
-        
+        cur = head;
         // 切割原链表和复制链表
         Node newHead = cur.next;
-        // FIXME
+        cur = head;
+        Node copyCur = newHead;
+        while (cur != null) {
+            cur.next = cur.next.next;
+            copyCur.next = copyCur.next == null ? null:copyCur.next.next;
+            cur = cur.next;
+            copyCur = copyCur.next;
+        }
         return newHead;
     }
-//    public Node copyRandomList(Node head) {
-//        if (head == null) {
-//            return null;
-//        }
-//        // 以下方法将每个节点的复制节点都在这个节点后面
-//        // 如果链表是:1->2->3->4 复制以后，会变成： 1->1->2->2->3->3->4->4
-//        Node cur = head;
-//        while (cur != null) {
-//            Node tmp = cur.next;
-//            Node copy = new Node(cur.val);
-//            cur.next = copy;
-//            copy.next = tmp;
-//            cur = tmp;
-//        }
-//
-//        // 以下方法是设置每个复制节点的random指针
-//        cur = head;
-//        while (cur != null) {
-//            cur.next.random = cur.random == null ? null : cur.random.next;
-//            // 无须判断cur.next是否空指针，因为cur永远是原链表的位置，cur只要不为null
-//            // cur.next必为复制节点，所以cur只要不为空，cur.next一定存在
-//            cur = cur.next.next;
-//        }
-//
-//        // 以下方法是断开原链表和复制链表,注意最后一个节点
-//        cur = head;
-//        Node copyHead = head.next;
-//        Node copyCur = copyHead;
-//        Node start = copyHead.next;
-//        head.next = null;
-//        int i = 1;
-//        while (start != null) {
-//            Node next = start.next;
-//            if ((i & 1) == 1) {
-//                cur.next = start;
-//                cur = start;
-//            } else {
-//                copyCur.next = start;
-//                copyCur = start;
-//            }
-//            i++;
-//            start = next;
-//        }
-//        cur.next = null;
-//        copyCur.next = null;
-//        return copyHead;
-//    }
 
 }
