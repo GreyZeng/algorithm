2275b832089a09b53f3d5854f32de636c694fb03
GreyZeng <410486047@qq.com>
2023-06-01 10:06:51 +0800

    update leetcode 876 mid of the linked list

--- src/main/java/list/LeetCode_0876_MiddleOfTheLinkedList.java
+++ src/main/java/list/LeetCode_0876_MiddleOfTheLinkedList.java
@@ -1,13 +1,14 @@
-package leetcode;
+package list;
 
-// https://leetcode.cn/problems/middle-of-the-linked-list/
+// https://leetcode.com/problems/middle-of-the-linked-list/
 // 使用快慢指针
 // 奇数返回中点，偶数返回下中点
 // 笔记：https://www.cnblogs.com/greyzeng/p/16629407.html
 public class LeetCode_0876_MiddleOfTheLinkedList {
-    public static class ListNode {
+	public  class ListNode {
         int val;
         ListNode next;
+
 
         ListNode() {
         }
@@ -25,13 +26,16 @@ public class LeetCode_0876_MiddleOfTheLinkedList {
     // [1,2,3,4,5] --> 3
     // [1,2,3,4,5,6] --> 4
     // 奇数返回中点，偶数返回下中点
-    public static ListNode middleNode(ListNode head) {
-        ListNode slow = head;
-        ListNode fast = head;
-        while (fast != null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-        return slow;
+    public  ListNode middleNode(ListNode head) {
+    	if (null == head || head.next == null) {
+    		return head;
+    	}
+    	ListNode slow = head;
+    	ListNode fast = head;
+    	while (fast != null && fast.next != null) {
+    		fast = fast.next.next;
+    		slow = slow.next;
+    	}
+    	return slow;
     }
 }
