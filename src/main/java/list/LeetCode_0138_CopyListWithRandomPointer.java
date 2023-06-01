9ac45c33ecf5842f2843a14c43739328c65d459f
GreyZeng <410486047@qq.com>
2023-06-01 17:13:12 +0800

    update code

--- src/main/java/list/LeetCode_0138_CopyListWithRandomPointer.java
+++ src/main/java/list/LeetCode_0138_CopyListWithRandomPointer.java
@@ -1,11 +1,12 @@
 package list;
 
 // 复制带随机指针的链表
-// https://leetcode.cn/problems/copy-list-with-random-pointer
+// https://leetcode.com/problems/copy-list-with-random-pointer
 // 笔记：https://www.cnblogs.com/greyzeng/p/16750999.html
 public class LeetCode_0138_CopyListWithRandomPointer {
 
-    public static class Node {
+    public class Node {
+
         int val;
         Node next;
         Node random;
@@ -17,52 +18,77 @@ public class LeetCode_0138_CopyListWithRandomPointer {
         }
     }
 
-    public static Node copyRandomList(Node head) {
+    public Node copyRandomList(Node head) {
         if (head == null) {
-            return null;
+            return head;
         }
-        // 以下方法将每个节点的复制节点都在这个节点后面
-        // 如果链表是:1->2->3->4 复制以后，会变成： 1->1->2->2->3->3->4->4
         Node cur = head;
+        // 复制节点接在当前节点的后面
         while (cur != null) {
-            Node tmp = cur.next;
+            Node next = cur.next;
             Node copy = new Node(cur.val);
             cur.next = copy;
-            copy.next = tmp;
-            cur = tmp;
+            copy.next = next;
+            cur = next;
         }
-
-        // 以下方法是设置每个复制节点的random指针
+        // 设置复制节点的random指针
         cur = head;
         while (cur != null) {
             cur.next.random = cur.random == null ? null : cur.random.next;
-            // 无须判断cur.next是否空指针，因为cur永远是原链表的位置，cur只要不为null
-            // cur.next必为复制节点，所以cur只要不为空，cur.next一定存在
             cur = cur.next.next;
         }
-
-        // 以下方法是断开原链表和复制链表,注意最后一个节点
-        cur = head;
-        Node copyHead = head.next;
-        Node copyCur = copyHead;
-        Node start = copyHead.next;
-        head.next = null;
-        int i = 1;
-        while (start != null) {
-            Node next = start.next;
-            if ((i & 1) == 1) {
-                cur.next = start;
-                cur = start;
-            } else {
-                copyCur.next = start;
-                copyCur = start;
-            }
-            i++;
-            start = next;
-        }
-        cur.next = null;
-        copyCur.next = null;
-        return copyHead;
+        
+        // 切割原链表和复制链表
+        Node newHead = cur.next;
+        // FIXME
+        return newHead;
     }
+//    public Node copyRandomList(Node head) {
+//        if (head == null) {
+//            return null;
+//        }
+//        // 以下方法将每个节点的复制节点都在这个节点后面
+//        // 如果链表是:1->2->3->4 复制以后，会变成： 1->1->2->2->3->3->4->4
+//        Node cur = head;
+//        while (cur != null) {
+//            Node tmp = cur.next;
+//            Node copy = new Node(cur.val);
+//            cur.next = copy;
+//            copy.next = tmp;
+//            cur = tmp;
+//        }
+//
+//        // 以下方法是设置每个复制节点的random指针
+//        cur = head;
+//        while (cur != null) {
+//            cur.next.random = cur.random == null ? null : cur.random.next;
+//            // 无须判断cur.next是否空指针，因为cur永远是原链表的位置，cur只要不为null
+//            // cur.next必为复制节点，所以cur只要不为空，cur.next一定存在
+//            cur = cur.next.next;
+//        }
+//
+//        // 以下方法是断开原链表和复制链表,注意最后一个节点
+//        cur = head;
+//        Node copyHead = head.next;
+//        Node copyCur = copyHead;
+//        Node start = copyHead.next;
+//        head.next = null;
+//        int i = 1;
+//        while (start != null) {
+//            Node next = start.next;
+//            if ((i & 1) == 1) {
+//                cur.next = start;
+//                cur = start;
+//            } else {
+//                copyCur.next = start;
+//                copyCur = start;
+//            }
+//            i++;
+//            start = next;
+//        }
+//        cur.next = null;
+//        copyCur.next = null;
+//        return copyHead;
+//    }
 
 }
