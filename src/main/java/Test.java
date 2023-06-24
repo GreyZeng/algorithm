1883c80f402c21ab3a59437cec059bb4bf8d9fbe
GreyZeng <410486047@qq.com>
2023-06-24 10:39:08 +0800

    just test

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,6 +1,9 @@
 
 public class Test {
 	public static void main(String[] args) {
-		System.out.println("Eclipse 2023-06 feature test");
+		print("Eclipse 2023-06 feature test");
+	}
+	private static void print(String str) {
+		System.out.println(str);
 	}
 }
