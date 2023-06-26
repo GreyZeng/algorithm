ccc8c7b6ad2ce57b02fb01b2a3a6a1abdfcf4b90
GreyZeng <410486047@qq.com>
2023-06-26 08:52:10 +0800

    remove useless class

--- src/main/java/Test.java
+++
@@ -1,9 +0,0 @@
-
-public class Test {
-	public static void main(String[] args) {
-		print("Eclipse 2023-06 feature test");
-	}
-	private static void print(String str) {
-		System.out.println(str);
-	}
-}
