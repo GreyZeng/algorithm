732b365928f352fb967b44277be74caaa8c076f3
GreyZeng <410486047@qq.com>
2023-08-06 13:33:37 +0800

    print binary string

--- src/main/java/bit/Code_PrintBinary.java
+++ src/main/java/bit/Code_PrintBinary.java
@@ -6,11 +6,13 @@ package bit;
 // 2. 除去最高位，其他位数取反以后，再加1
 public class Code_PrintBinary {
     // 打印一个32位整数的二进制形式
-    public static void printBinary(int n) {
+    public static String printBinary(int n) {
+        StringBuilder sb = new StringBuilder();
         for (int i = 31; i >= 0; i--) {
-            System.out.print((n & (1 << i)) == 0 ? "0" : "1");
+            sb.append((n & (1 << i)) == 0 ? "0" : "1");
         }
-        System.out.println();
+        System.out.println(sb);
+        return sb.toString();
     }
 
     public static void main(String[] args) {
@@ -18,7 +20,8 @@ public class Code_PrintBinary {
         System.out.println("要处理的数据是：");
         System.out.println("10001010111010101000100011101010");
         System.out.println("打印原始数的二进制：");
-        printBinary(num);
+        String printNum = printBinary(num);
+        System.out.println("原始内容和打印内容是否一致（如果一致则正确）" + printNum.equals(Integer.toBinaryString(num)));
         System.out.println("左移一位：");
         printBinary(num << 1);
         System.out.println(">> 表示有符号右移一位：");
