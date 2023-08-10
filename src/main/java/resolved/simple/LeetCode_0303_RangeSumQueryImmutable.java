2798266f3c5812a0420d62a3d3027b4cd50254d0
GreyZeng <410486047@qq.com>
2023-08-10 15:20:38 +0800

    leetcode 303 range sum query immutable

--- src/main/java/resolved/simple/LeetCode_0303_RangeSumQueryImmutable.java
+++ src/main/java/resolved/simple/LeetCode_0303_RangeSumQueryImmutable.java
@@ -1,23 +1,26 @@
-package random;
+package resolved.simple;
 
 // 实现前缀和
 // 303. 区域和检索 - 数组不可变
-// https://leetcode.cn/problems/range-sum-query-immutable/
+// https://leetcode.com/problems/range-sum-query-immutable/
 public class LeetCode_0303_RangeSumQueryImmutable {
     // 在数组不可变的情况下，可以使用前缀和数组加速求区间和
     class NumArray {
         int[] preSum;
 
         public NumArray(int[] nums) {
-            // 0 位置弃而不用，这样方便一些
-            preSum = nums;
+            preSum = new int[nums.length];
+            preSum[0] = nums[0];
             for (int i = 1; i < preSum.length; i++) {
-                preSum[i] = preSum[i] + preSum[i - 1];
+                preSum[i] = preSum[i - 1] + nums[i];
             }
         }
 
         public int sumRange(int left, int right) {
-            return preSum[right] - (left == 0 ? 0 : preSum[left - 1]);
+            if (left == 0) {
+                return preSum[right];
+            }
+            return preSum[right] - preSum[left - 1];
         }
     }
 }
