c03bc44d9024ef6fcac6d1b5758d05120d553031
GreyZeng <410486047@qq.com>
2023-08-12 20:49:20 +0800

    update leetcode 231

--- src/main/java/resolved/bit/LeetCode_0231_PowerOfTwo.java
+++ src/main/java/resolved/bit/LeetCode_0231_PowerOfTwo.java
@@ -1,10 +1,7 @@
 package resolved.bit;
 
-/**
- * @author <a href="mailto:410486047@qq.com">Grey</a>
- * @date 2022/9/21
- * @since
- */
+// 判断一个数是否是2的某次幂
+// https://leetcode.com/problems/power-of-two
 public class LeetCode_0231_PowerOfTwo {
     // 借鉴 Netty 代码
     public boolean isPowerOfTwo(int n) {
