edaa35bb593b0e2f6dd55fb55e591787c17a73f1
GreyZeng <410486047@qq.com>
2023-08-12 20:57:38 +0800

    update leetcode 231

--- src/main/java/resolved/bit/LeetCode_0231_PowerOfTwo.java
+++ src/main/java/resolved/bit/LeetCode_0231_PowerOfTwo.java
@@ -4,7 +4,12 @@ package resolved.bit;
 // https://leetcode.com/problems/power-of-two
 public class LeetCode_0231_PowerOfTwo {
     // 借鉴 Netty 代码
-    public boolean isPowerOfTwo(int n) {
+    public static boolean isPowerOfTwo(int n) {
         return n > 0 && (n & -n) == n;
+    }
+
+    public static void main(String[] args) {
+        int i = 1024;
+        System.out.println(isPowerOfTwo(i));
     }
 }
