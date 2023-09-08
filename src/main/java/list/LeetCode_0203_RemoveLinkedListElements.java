ede31b5607b29a50786b679cdd4375ed2d6647a3
GreyZeng <410486047@qq.com>
2023-09-09 00:17:41 +0800

    remove linked list elements

--- src/main/java/list/LeetCode_0203_RemoveLinkedListElements.java
+++ src/main/java/list/LeetCode_0203_RemoveLinkedListElements.java
@@ -1,47 +1,50 @@
 package list;
 
+import java.util.List;
+
 // 删除链表的指定节点
 // https://leetcode.com/problems/remove-linked-list-elements/
 // 笔记：https://www.cnblogs.com/greyzeng/p/16629407.html
 public class LeetCode_0203_RemoveLinkedListElements {
-    public class ListNode {
-        int val;
-        ListNode next;
+  public class ListNode {
+    int val;
+    ListNode next;
 
-        ListNode() {
-        }
+    ListNode() {}
 
-        ListNode(int val) {
-            this.val = val;
-        }
-
-        ListNode(int val, ListNode next) {
-            this.val = val;
-            this.next = next;
-        }
+    ListNode(int val) {
+      this.val = val;
     }
 
-    public ListNode removeElements(ListNode head, int val) {
-        ListNode newHead = head;
-        while (newHead != null && newHead.val == val) {
-            newHead = newHead.next;
-        }
-        if (newHead == null) {
-            return null;
-        }
-        ListNode pre = newHead;
-        ListNode cur = newHead.next;
-        while (cur != null) {
-            if (cur.val == val) {
-                pre.next = cur.next;
-                cur = cur.next;
-            } else {
-                pre = pre.next;
-                cur = cur.next;
-            }
-        }
-        return newHead;
+    ListNode(int val, ListNode next) {
+      this.val = val;
+      this.next = next;
     }
+  }
 
-
+  public ListNode removeElements(ListNode head, int val) {
+    if (head == null) {
+      return head;
+    }
+    while (head != null && head.val == val) {
+      head = head.next;
+    }
+    // 元素已经被删除光了
+    if (head == null) {
+      return head;
+    }
+    ListNode newHead = head;
+    ListNode cur = newHead;
+    ListNode dummy = new ListNode(val, newHead);
+    while (cur != null) {
+      if (cur.val == val) {
+        cur = cur.next;
+        dummy.next = cur;
+      } else {
+        cur = cur.next;
+        dummy = dummy.next;
+      }
+    }
+    return newHead;
+  }
 }
