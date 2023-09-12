4cd587fca67bf9f8c0c4a6159efdc6047d949447
GreyZeng <410486047@qq.com>
2023-09-12 23:18:19 +0800

    merge two list

--- src/main/java/resolved/list/LeetCode_0021_MergeTwoSortedLists.java
+++ src/main/java/resolved/list/LeetCode_0021_MergeTwoSortedLists.java
@@ -1,4 +1,4 @@
-package list;
+package resolved.list;
 
 // merge two sorted lists
 // leetcode: https://leetcode.com/problems/merge-two-sorted-lists
@@ -9,18 +9,6 @@ public class LeetCode_0021_MergeTwoSortedLists {
     public class ListNode {
         int val;
         ListNode next;
-
-        ListNode() {
-        }
-
-        ListNode(int val) {
-            this.val = val;
-        }
-
-        ListNode(int val, ListNode next) {
-            this.val = val;
-            this.next = next;
-        }
     }
 
     // Constraints:
@@ -32,28 +20,37 @@ public class LeetCode_0021_MergeTwoSortedLists {
     // list2: 2 -> 6 -> 8 -> 12 -> 64
     // 1 -> 2 -> 3 -> 6 -> 6 -> 8 -> 9 -> 12 -> 64
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        if (null == list1 || null == list2) {
-            return list1 == null ? list2 : list1;
+        if (list1 == null || list2 == null) {
+            return list1 != null ? list1 : list2;
         }
-        ListNode start = list1.val < list2.val ? list1 : list2;
-        if (start == list1) {
-            list1 = list1.next;
-        } else {
-            list2 = list2.next;
-        }
+        ListNode start = list1.val > list2.val ? list2 : list1;
+        ListNode l1 = start == list1 ? list1.next : list1;
+        ListNode l2 = start == list1 ? list2 : list2.next;
+
         ListNode cur = start;
-        while (list1 != null && list2 != null) {
-            if (list1.val < list2.val) {
-                cur.next = list1;
-                list1 = list1.next;
+        while (l1 != null && l2 != null) {
+            if (l1.val > l2.val) {
+                cur.next = l2;
+                l2 = l2.next;
             } else {
-                cur.next = list2;
-                list2 = list2.next;
+                cur.next = l1;
+                l1 = l1.next;
             }
             cur = cur.next;
         }
-        cur.next = list1 != null ? list1 : list2;
+        while (l1 != null) {
+            cur.next = l1;
+            cur = cur.next;
+            l1 = l1.next;
+        }
+
+        while (l2 != null) {
+            cur.next = l2;
+            cur = cur.next;
+            l2 = l2.next;
+        }
         return start;
     }
+
 
 }
