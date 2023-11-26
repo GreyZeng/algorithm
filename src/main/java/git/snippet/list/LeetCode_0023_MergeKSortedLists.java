87fc6f706efd6704e7ff02e3a1d29a87fd35ad5b
GreyZeng <410486047@qq.com>
2023-11-26 17:38:28 +0800

    update code

--- src/main/java/git/snippet/list/LeetCode_0023_MergeKSortedLists.java
+++ src/main/java/git/snippet/list/LeetCode_0023_MergeKSortedLists.java
@@ -9,41 +9,48 @@ import java.util.PriorityQueue;
 // 如果X比堆顶元素小，则直接不需要进入堆顶，作为结果链表
 // https://www.cnblogs.com/greyzeng/p/7551789.html
 public class LeetCode_0023_MergeKSortedLists {
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode() {}
+     * ListNode(int val) { this.val = val; }
+     * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
     public ListNode mergeKLists(ListNode[] lists) {
-        if (lists == null || lists.length == 0) {
+        if (null == lists || lists.length == 0) {
             return null;
         }
         if (lists.length == 1) {
             return lists[0];
         }
-        PriorityQueue<ListNode> heap = new PriorityQueue<>(Comparator.comparingInt(o -> o.val));
-        for (ListNode h : lists) {
-            if (h != null) {
-                heap.offer(h);
+        PriorityQueue<ListNode> heap = new PriorityQueue<>((o1, o2) -> (o1.val - o2.val));
+        for (ListNode list : lists) {
+            if (list != null) {
+                heap.offer(list);
             }
         }
-        ListNode start = heap.poll();
-        ListNode cur = start;
+        ListNode newHead = heap.poll();
+        ListNode cur = newHead;
         while (!heap.isEmpty()) {
             ListNode next = cur.next;
             if (next == null) {
+                // 某个链表走到头了
                 cur.next = heap.poll();
                 cur = cur.next;
             } else {
-                if (heap.peek().val < next.val) {
-                    // 先弹出，再入堆
-                    ListNode poll = heap.poll();
-                    cur.next = poll;
+                if (next.val <= heap.peek().val) {
+                    cur = cur.next;
+                } else {
+                    cur.next = heap.poll();
                     cur = cur.next;
                     heap.offer(next);
-                } else if (heap.peek().val == next.val) {
-                    cur = next;
-                } else {
-                    cur = next;
                 }
             }
         }
-        return start;
+        return newHead;
     }
 
     public class ListNode {
