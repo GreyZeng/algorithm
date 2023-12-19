676b587406765c539da2c23f9bc9dafdc11eca86
GreyZeng <410486047@qq.com>
2023-12-19 08:43:20 +0800

    update code

--- src/main/java/git/snippet/leetcode/LeetCode_0029_DivideTwoIntegers.java
+++ src/main/java/git/snippet/leetcode/LeetCode_0029_DivideTwoIntegers.java
@@ -1,8 +1,9 @@
 package git.snippet.leetcode;
 
 // 记住结论即可
-// 只用位运算实现加减乘除
-// https://leetcode.cn/problems/divide-two-integers
+// 位运算实现除法
+// https://leetcode.com/problems/divide-two-integers
+// 进阶：只用位运算实现加减乘
 // 笔记：https://www.cnblogs.com/greyzeng/p/16637476.html
 public class LeetCode_0029_DivideTwoIntegers {
     public static void main(String[] args) {
@@ -14,16 +15,17 @@ public class LeetCode_0029_DivideTwoIntegers {
         System.out.println(13 ^ 20);
         System.out.println(-Integer.MIN_VALUE);
         System.out.println(Integer.MIN_VALUE);
-        System.out.println(Integer.toBinaryString(418));
+        System.out.println(Long.toBinaryString(418L));
     }
 
-    // 原始加法就是：无进位信息（异或）+进位信息
-    public static int add(int a, int b) {
+    // 原始加法就是：无进位信息（异或） 结合(+) 进位信息
+    public int add(int a, int b) {
         int sum = a;
         while (b != 0) {
             // 异或运算就是无进位相加
             sum = a ^ b;
             // 进位信息
+            // 每一轮一个进位信息
             b = (a & b) << 1;
             a = sum;
         }
@@ -32,16 +34,16 @@ public class LeetCode_0029_DivideTwoIntegers {
 
     // 某个数n的相反数就是 ~n + 1，由于不能用+号
     // 所以是 add(~n,1)
-    public static int negNum(int n) {
+    public int negNum(int n) {
         return add(~n, 1);
     }
 
-    public static int minus(int a, int b) {
+    public int minus(int a, int b) {
         return add(a, negNum(b));
     }
 
     // 参考小学算乘法的过程。
-    public static int multi(int a, int b) {
+    public int multi(int a, int b) {
         int res = 0;
         while (b != 0) {
             if ((b & 1) != 0) {
@@ -53,12 +55,12 @@ public class LeetCode_0029_DivideTwoIntegers {
         return res;
     }
 
-    public static boolean isNeg(int n) {
+    public boolean isNeg(int n) {
         return n < 0;
     }
 
     // 全部转成正数来计算
-    public static int div(int x, int y) {
+    public int div(int x, int y) {
         int a = isNeg(x) ? negNum(x) : x;
         int b = isNeg(y) ? negNum(y) : y;
         int res = 0;
@@ -71,7 +73,7 @@ public class LeetCode_0029_DivideTwoIntegers {
         return isNeg(x) ^ isNeg(y) ? negNum(res) : res;
     }
 
-    public static int divide(int a, int b) {
+    public int divide(int a, int b) {
         if (b == Integer.MIN_VALUE) {
             return a == Integer.MIN_VALUE ? 1 : 0;
         }
