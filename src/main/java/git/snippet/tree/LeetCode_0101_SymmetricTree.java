25f048e8641ff16518eaa025cabc6ad74209f663
GreyZeng <410486047@qq.com>
2023-12-19 20:59:13 +0800

    leetcode 101 symmetric-tree

--- src/main/java/git/snippet/tree/LeetCode_0101_SymmetricTree.java
+++ src/main/java/git/snippet/tree/LeetCode_0101_SymmetricTree.java
@@ -1,31 +1,28 @@
 package git.snippet.tree;
 
 // 判断一棵树是否是镜面树
+// https://leetcode.com/problems/symmetric-tree
 public class LeetCode_0101_SymmetricTree {
-
     public boolean isSymmetric(TreeNode root) {
-        if (root == null) {
+        if (null == root) {
             return true;
         }
-        if (root.right == null || root.left == null) {
-            return root.right == null && root.left == null;
+        if (root.left == null || root.right == null) {
+            return root.left == null && root.right == null;
         }
-        // root.left != null && root.right != null
-        return isSymmetric(root.left, root.right);
+
+        return root.left.val == root.right.val && isSymmetric(root.left, root.right);
     }
 
+    // 判断 left 和 right 两棵树是否是镜面对称
     public boolean isSymmetric(TreeNode left, TreeNode right) {
         if (left == null || right == null) {
             return left == null && right == null;
         }
-        // left != null && right != null
-        int lv = left.val;
-        int rv = right.val;
-        if (lv != rv) {
-            return false;
-        }
-        return isSymmetric(left.left, right.right) && isSymmetric(left.right, right.left);
+        // left.val == right.val
+        return left.val == right.val && isSymmetric(left.left, right.right) && isSymmetric(left.right, right.left);
     }
+
 
     public class TreeNode {
         int val;
