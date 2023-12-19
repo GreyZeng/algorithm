66dfe67553e0c83ab1e50bf910efde57179971eb
GreyZeng <410486047@qq.com>
2023-12-19 20:33:36 +0800

    leetcode 100 same tree

--- src/main/java/git/snippet/tree/LeetCode_0100_SameTree.java
+++ src/main/java/git/snippet/tree/LeetCode_0100_SameTree.java
@@ -3,11 +3,13 @@ package git.snippet.tree;
 // 判断两颗树是否结构相同
 // Two binary trees are considered the same if they are structurally identical, and the nodes have
 // the same value.
+// https://leetcode.com/problems/same-tree
 public class LeetCode_0100_SameTree {
 
     public boolean isSameTree(TreeNode p, TreeNode q) {
         if (p == null || q == null) {
-            return p == null && q == null;
+            // 两个链表同时为空才表示 same tree
+            return q == null && p == null;
         }
         return p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
     }
