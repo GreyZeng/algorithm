573b219574196ebcddfc5193d845857b74b6e7e9
GreyZeng <410486047@qq.com>
2023-12-24 21:55:35 +0800

    level travel bst

--- src/main/java/git/snippet/tree/LeetCode_0107_BinaryTreeLevelOrderTraversalII.java
+++ src/main/java/git/snippet/tree/LeetCode_0107_BinaryTreeLevelOrderTraversalII.java
@@ -7,7 +7,30 @@ import java.util.*;
 // 按层遍历进阶：从底部开始遍历
 // 只需要把按层遍历的队列结构改成栈就可以
 public class LeetCode_0107_BinaryTreeLevelOrderTraversalII {
-
+    public List<List<Integer>> levelOrderBottom1(TreeNode root) {
+        List<List<Integer>> ans = new LinkedList<>();
+        if (root == null) {
+            return ans;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> curAns = new LinkedList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode curNode = queue.poll();
+                curAns.add(curNode.val);
+                if (curNode.left != null) {
+                    queue.add(curNode.left);
+                }
+                if (curNode.right != null) {
+                    queue.add(curNode.right);
+                }
+            }
+            ans.addFirst(curAns);
+        }
+        return ans;
+    }
     public List<List<Integer>> levelOrderBottom(TreeNode root) {
         if (null == root) {
             return new ArrayList<>();
