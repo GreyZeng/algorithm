9e114e5ceb706c03f4ee2d7fad62e48f641fae9f
GreyZeng <410486047@qq.com>
2023-12-25 16:58:03 +0800

    is bst

--- src/main/java/git/snippet/tree/LeetCode_0110_BalancedBinaryTree.java
+++ src/main/java/git/snippet/tree/LeetCode_0110_BalancedBinaryTree.java
@@ -1,7 +1,6 @@
 package git.snippet.tree;
 
-// A height-balanced binary tree is a binary tree in which the depth of the two subtrees of every
-// node never differs by more than one.
+
 // 判断一棵树是否是平衡二叉树
 // 平衡二叉树要么是一棵空树.
 // 要么保证左右子树的高度之差不大于 1.
@@ -9,49 +8,31 @@ package git.snippet.tree;
 // https://leetcode.com/problems/balanced-binary-tree/
 // 笔记：https://www.cnblogs.com/greyzeng/p/16703346.html
 public class LeetCode_0110_BalancedBinaryTree {
-
     public boolean isBalanced(TreeNode root) {
-        return p(root).isBT;
+        return process(root).isBST;
     }
 
-    public Info p(TreeNode root) {
+    public Info process(TreeNode root) {
         if (root == null) {
             return new Info(0, true);
         }
-        Info left = p(root.left);
-        Info right = p(root.right);
-        int height = Math.max(left.height, right.height) + 1;
-        boolean isBT = left.isBT && right.isBT && Math.abs(left.height - right.height) <= 1;
-        return new Info(height, isBT);
-    }
-
-    public class TreeNode {
-
-        int val;
-        TreeNode left;
-        TreeNode right;
-
-        TreeNode() {
-        }
-
-        TreeNode(int val) {
-            this.val = val;
-        }
-
-        TreeNode(int val, TreeNode left, TreeNode right) {
-            this.val = val;
-            this.left = left;
-            this.right = right;
-        }
+        Info left = process(root.left);
+        Info right = process(root.right);
+        return new Info(Math.max(left.height, right.height) + 1, left.isBST && right.isBST && Math.abs(left.height - right.height) <= 1);
     }
 
     public class Info {
         public int height;
-        public boolean isBT;
+        public boolean isBST;
 
-        public Info(int height, boolean isBT) {
+        public Info(int height, boolean isBST) {
             this.height = height;
-            this.isBT = isBT;
+            this.isBST = isBST;
         }
+    }
+
+    public class TreeNode {
+        TreeNode left;
+        TreeNode right;
     }
 }
