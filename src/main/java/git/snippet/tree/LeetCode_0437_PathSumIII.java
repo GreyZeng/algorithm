19cd7396330dd9da1cf4f97609e527e2cabb7b12
GreyZeng <410486047@qq.com>
2023-12-26 16:34:03 +0800

    leetcode 437 path sum iii

--- src/main/java/git/snippet/tree/LeetCode_0437_PathSumIII.java
+++ src/main/java/git/snippet/tree/LeetCode_0437_PathSumIII.java
@@ -10,29 +10,35 @@ import java.util.HashMap;
 // 类似数组三连的第二问
 public class LeetCode_0437_PathSumIII {
 
-    public int pathSum(TreeNode root, int sum) {
+    public int pathSum(TreeNode root, int targetSum) {
         HashMap<Long, Integer> preSumMap = new HashMap<>();
+        // 累加和为0的情况，默认就有一种了（空树）
         preSumMap.put(0L, 1);
-        return process(root, sum, 0, preSumMap);
+        return process(root, targetSum, 0, preSumMap);
     }
 
     // 返回方法数
-    public int process(TreeNode x, int sum, long preAll, HashMap<Long, Integer> preSumMap) {
-        if (x == null) {
+    // 从 node 到结尾，前面累计的结果是 preSum，前面组成的路径中，每个路径之和的结果有多少种，存在 preSumMap
+    // preSum(i,j) 表示 前面路径的累加和为 i 的路径有 j 种
+    public int process(TreeNode node, int targetSum, long preSum, HashMap<Long, Integer> preSumMap) {
+        if (node == null) {
             return 0;
         }
-        long all = preAll + x.val;
+        long all = preSum + node.val;
         int ans = 0;
-        if (preSumMap.containsKey(all - sum)) {
-            ans = preSumMap.get(all - sum);
+        if (preSumMap.containsKey(all - targetSum)) {
+            // 说明之前有路径可以得到
+            ans = preSumMap.get(all - targetSum);
         }
+        // 登记当前结果到preSumMap中
         if (!preSumMap.containsKey(all)) {
             preSumMap.put(all, 1);
         } else {
             preSumMap.put(all, preSumMap.get(all) + 1);
         }
-        ans += process(x.left, sum, all, preSumMap);
-        ans += process(x.right, sum, all, preSumMap);
+        ans += process(node.left, targetSum, all, preSumMap);
+        ans += process(node.right, targetSum, all, preSumMap);
+        // 清理现场
         if (preSumMap.get(all) == 1) {
             preSumMap.remove(all);
         } else {
