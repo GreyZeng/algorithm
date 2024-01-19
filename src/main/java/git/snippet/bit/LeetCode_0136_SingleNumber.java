364dcc11e35c036eac9cdede400160e64d3b64c5
GreyZeng <410486047@qq.com>
2024-01-19 16:10:20 +0800

    single number leetcode 136

--- src/main/java/git/snippet/bit/LeetCode_0136_SingleNumber.java
+++ src/main/java/git/snippet/bit/LeetCode_0136_SingleNumber.java
@@ -6,12 +6,15 @@ package git.snippet.bit;
 // 时间复杂度O(n),额外空间复杂度O(1)。
 // https://www.cnblogs.com/greyzeng/p/15385402.html
 // 牛客的测评链接：https://www.nowcoder.com/questionTerminal/d0ef3e33e63a49dd99c90aeef306b0fc
+// LeetCode https://leetcode.com/problems/single-number/
 public class LeetCode_0136_SingleNumber {
 
-    public int singleNumber(int[] nums) {
-        for (int i = 1; i < nums.length; i++) {
-            nums[0] ^= nums[i];
-        }
-        return nums[0];
-    }
+	public int singleNumber(int[] nums) {
+		int result = 0;
+		for (int i = 0; i < nums.length; i++) {
+			result ^= nums[i];
+		}
+		return result;
+	}
+
 }
