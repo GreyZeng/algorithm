ba653a31e118e0fcd67d8a1e8246cd616fa71755
GreyZeng <410486047@qq.com>
2024-01-24 14:31:02 +0800

    update code

--- src/main/java/git/snippet/queueandstack/LeetCode_0225_ImplementStackUsingQueues.java
+++ src/main/java/git/snippet/queueandstack/LeetCode_0225_ImplementStackUsingQueues.java
@@ -3,47 +3,34 @@ package git.snippet.queueandstack;
 import java.util.LinkedList;
 import java.util.Queue;
 
-// 使用队列实现栈
+// 使用一个队列实现栈
+// 测评链接：https://leetcode.com/problems/implement-stack-using-queues/
 // 笔记：https://www.cnblogs.com/greyzeng/p/16631644.html
 public class LeetCode_0225_ImplementStackUsingQueues {
     class MyStack {
         // only use poll() and offer()
-        private Queue<Integer> data;
-        private Queue<Integer> helper;
+        private final Queue<Integer> data;
 
         public MyStack() {
             data = new LinkedList<>();
-            helper = new LinkedList<>();
         }
 
+        // O(n)
         public void push(int x) {
+            int size = data.size();
             data.offer(x);
+            // 其他元素都移动到 x 后面
+            for (int i = 0; i < size; i++) {
+                data.offer(data.poll());
+            }
         }
 
         public int pop() {
-            int rv = -1;
-            while (!data.isEmpty()) {
-                rv = data.poll();
-                if (!data.isEmpty()) {
-                    helper.offer(rv);
-                }
-            }
-            Queue<Integer> tmp = data;
-            data = helper;
-            helper = tmp;
-            return rv;
+            return data.poll();
         }
 
         public int top() {
-            int rv = -1;
-            while (!data.isEmpty()) {
-                rv = data.poll();
-                helper.offer(rv);
-            }
-            Queue<Integer> tmp = data;
-            data = helper;
-            helper = tmp;
-            return rv;
+            return data.peek();
         }
 
         public boolean empty() {
