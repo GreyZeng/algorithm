888687acdb65ad592d2311e6fa351fba5946c350
GreyZeng <410486047@qq.com>
2024-10-25 19:28:58 +0800

    reverse linked list

--- src/main/java/git/grey/algorithm/Code_0006_LintCode_0035_ReverseLinkedList.java
+++ src/main/java/git/grey/algorithm/Code_0006_LintCode_0035_ReverseLinkedList.java
@@ -1,38 +1,39 @@
-package git.snippet.list;
+package git.grey.algorithm;
 
 // 单链表反转
 // 笔记：https://www.cnblogs.com/greyzeng/p/17852345.html
 // https://leetcode.com/problems/reverse-linked-list/
+// https://www.lintcode.com/problem/35/
 // Follow up: A linked list can be reversed either iteratively or recursively. Could you implement
 // both?
-public class LeetCode_0206_ReverseLinkedList {
+public class Code_0006_LintCode_0035_ReverseLinkedList {
     // 非递归版本
-    public ListNode reverseList2(ListNode head) {
+    // lintcode版本
+    public ListNode reverse(ListNode head) {
         ListNode pre = null;
-        ListNode cur = head;
-        while (cur != null) {
-            ListNode tmp = cur.next;
-            cur.next = pre;
-            pre = cur;
-            cur = tmp;
+        ListNode next;
+        while (head != null) {
+            next = head.next;
+            head.next = pre;
+            pre = head;
+            head = next;
         }
         return pre;
     }
 
     // 递归版本
+    // leetcode版本
+    // 递归函数定义：反转以 head 为头的链表，并把反转后的头节点返回。
     public ListNode reverseList(ListNode head) {
-        return reverse(head);
-    }
-
-    public ListNode reverse(ListNode head) {
         if (head == null || head.next == null) {
             return head;
         }
-        ListNode preHead = reverse(head.next);
+        ListNode preHead = reverseList(head.next);
         head.next.next = head;
         head.next = null;
         return preHead;
     }
+
 
     public class ListNode {
         int val;
