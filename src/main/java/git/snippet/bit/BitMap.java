b8e52495abb03cd856351caa5324469c7b4d5658
GreyZeng <410486047@qq.com>
2024-10-29 15:59:35 +0800

    update code ref about oj

--- src/main/java/git/snippet/bit/BitMap.java
+++ src/main/java/git/snippet/bit/BitMap.java
@@ -1,35 +1,91 @@
 package git.snippet.bit;
 
+import java.util.HashSet;
+import java.util.Set;
+
 // 位图
 // 笔记：https://www.cnblogs.com/greyzeng/p/16634282.html
 public class BitMap {
+	public static void main(String[] args) {
+		System.out.println("test begin");
+		int max = 70000;
+		BitMap bitMap = new BitMap(max);
+		Set<Integer> set = new HashSet<>();
+		int testTime = 90000000;
+		for (int i = 0; i < testTime; i++) {
+			int num = (int) (Math.random() * (max + 1));
+			double decide = Math.random();
+			if (decide < 0.333) {
+				bitMap.add(num);
+				set.add(num);
+			} else if (decide < 0.666) {
+				bitMap.remove(num);
+				set.remove(num);
+			} else {
+				if (bitMap.contains(num)) {
+					if (set.contains(num)) {
+						continue;
+					} else {
+						System.out.println("出错了");
+						break;
+					}
+				} else {
+					if (!set.contains(num)) {
+						continue;
+					} else {
+						System.out.println("出错了");
+						break;
+					}
+				}
+			}
+		}
+		for (int num = 0; num <= max; num++) {
+			if (bitMap.contains(num)) {
+				if (set.contains(num)) {
+					continue;
+				} else {
+					System.out.println("出错了");
+					break;
+				}
+			} else {
+				if (!set.contains(num)) {
+					continue;
+				} else {
+					System.out.println("出错了");
+					break;
+				}
+			}
 
-    private final long[] bits;
+		}
+		System.out.println("test end");
+	}
 
-    // 初始化
-    public BitMap(int max) {
-        // 准备多少个整数？ 0 ~ 63 需要1个整数
-        // >> 6 就是 除以 64 ，>> 效率比除法高
-        // bits = new long[(max + 64) >> 6];
-        bits = new long[(max + 64) / 64];
-    }
+	private final long[] bits;
 
-    public void add(int num) {
-        bits[num / 64] |= (1L << (num % 64));
-        // num % 64 ---> num & 63 即：0b111111
-        // 只适用于 2 的 n 次方
-        // 注意：这里是1L非1，如果是1，因为要管64位
-        // 所以上述公式也可以写成如下
-        // bits[num >> 6] |= (1L << (num & 0b111111));
-    }
+	// 初始化
+	public BitMap(int max) {
+		// 准备多少个整数？ 0 ~ 63 需要1个整数
+		// >> 6 就是 除以 64 ，>> 效率比除法高
+		// bits = new long[(max + 64) >> 6];
+		bits = new long[(max + 64) / 64];
+	}
 
-    public void remove(int num) {
-        bits[num / 64] &= ~(1L << (num % 64));
-        // bits[num >> 6] &= ~(1L << (num & 0b111111));
-    }
+	public void add(int num) {
+		bits[num / 64] |= (1L << (num % 64));
+		// num % 64 ---> num & 63 即：0b111111
+		// 只适用于 2 的 n 次方
+		// 注意：这里是1L非1，如果是1，因为要管64位
+		// 所以上述公式也可以写成如下
+		// bits[num >> 6] |= (1L << (num & 0b111111));
+	}
 
-    public boolean contains(int num) {
-        // return (bits[num >> 6] & (1L << (num & 0b111111))) != 0;
-        return (bits[num / 64] & (1L << (num % 64))) != 0;
-    }
+	public void remove(int num) {
+		bits[num / 64] &= ~(1L << (num % 64));
+		// bits[num >> 6] &= ~(1L << (num & 0b111111));
+	}
+
+	public boolean contains(int num) {
+		// return (bits[num >> 6] & (1L << (num & 0b111111))) != 0;
+		return (bits[num / 64] & (1L << (num % 64))) != 0;
+	}
 }
--- src/main/java/git/snippet/snippet/Code_SumOfFactorial.java
+++ src/main/java/git/snippet/snippet/Code_SumOfFactorial.java
@@ -2,29 +2,49 @@ package git.snippet.snippet;
 
 // 给定一个参数N，返回1!+2!+3!+4!+…+N!的结果
 public class Code_SumOfFactorial {
-    public long f1(int num) {
-        long result = 0;
-        for (int i = 1; i <= num; i++) {
-            result += factorial(i);
-        }
-        return result;
-    }
 
-    public long factorial(int n) {
-        long result = 1L;
-        for (int i = 1; i <= n; i++) {
-            result *= i;
-        }
-        return result;
-    }
+	public static void main(String[] args) {
+		System.out.println("test begin");
+		int times = 18;
+		Code_SumOfFactorial test = new Code_SumOfFactorial();
+		if (test.f1(11) != 43954713L) {
+			System.out.println("出错了");
+		}
+		if (test.f2(11) != 43954713L) {
+			System.out.println("出错了");
+		}
+		for (int i = 1; i <= times; i++) {
+			if (test.f1(i) != test.f2(i)) {
+				System.out.println("出错了");
+				break;
+			}
+		}
+		System.out.println("test end");
+	}
 
-    public long f2(int num) {
-        long result = 0;
-        long pre = 1;
-        for (int i = 1; i <= num; i++) {
-            pre = pre * i;
-            result += pre;
-        }
-        return result;
-    }
+	public long f1(int num) {
+		long result = 0;
+		for (int i = 1; i <= num; i++) {
+			result += factorial(i);
+		}
+		return result;
+	}
+
+	public long factorial(int n) {
+		long result = 1L;
+		for (int i = 1; i <= n; i++) {
+			result *= i;
+		}
+		return result;
+	}
+
+	public long f2(int num) {
+		long result = 0;
+		long pre = 1;
+		for (int i = 1; i <= num; i++) {
+			pre = pre * i;
+			result += pre;
+		}
+		return result;
+	}
 }
