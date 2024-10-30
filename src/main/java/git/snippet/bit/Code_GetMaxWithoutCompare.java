0ead28adfd7b63bdc3e984aec47463630d3508c1
GreyZeng <410486047@qq.com>
2024-10-30 16:07:19 +0800

    small sum

--- src/main/java/git/snippet/bit/Code_GetMaxWithoutCompare.java
+++ src/main/java/git/snippet/bit/Code_GetMaxWithoutCompare.java
@@ -3,38 +3,61 @@ package git.snippet.bit;
 // 笔记：https://www.cnblogs.com/greyzeng/p/16641111.html
 // 不要用任何比较判断，返回两个数中较大的数
 public class Code_GetMaxWithoutCompare {
-    // 1 -> 0
-    // 0 -> 1
-    public static int flip(int n) {
-        return n ^ 1;
-    }
+	// 1 -> 0
+	// 0 -> 1
+	public static int flip(int n) {
+		return n ^ 1;
+	}
 
-    // 如果n是负数，则返回 1
-    // 如果n是正数，则返回 0
-    public static int sign(int n) {
-        return flip((n >> 31) & 1);
-    }
+	// 如果n是负数，则返回 1
+	// 如果n是正数，则返回 0
+	public static int sign(int n) {
+		return flip((n >> 31) & 1);
+	}
 
-    public static int getMax1(int a, int b) {
-        // 如果 c 是负数，则 b 大，返回 b
-        // 如果 c 是正数，则 a 大，返回 a
-        int c = a - b;
-        // 如果 c 是负数，则返回 1
-        // 如果 c 是正数，则返回 0
-        int scA = sign(c);
-        int scB = flip(scA);
-        return a * scA + b * scB;
-    }
+	public static int getMax1(int a, int b) {
+		// 如果 c 是负数，则 b 大，返回 b
+		// 如果 c 是正数，则 a 大，返回 a
+		int c = a - b;
+		// 如果 c 是负数，则返回 1
+		// 如果 c 是正数，则返回 0
+		int scA = sign(c);
+		int scB = flip(scA);
+		return a * scA + b * scB;
+	}
 
-    public static int getMax2(int a, int b) {
-        int c = a - b;
-        int sa = sign(a);
-        int sb = sign(b);
-        int sc = sign(c);
-        int difSab = sa ^ sb;
-        int sameSab = flip(difSab);
-        int returnA = difSab * sa + sameSab * sc;
-        int returnB = flip(returnA);
-        return a * returnA + b * returnB;
-    }
+	public static int getMax2(int a, int b) {
+		int c = a - b;
+		int sa = sign(a);
+		int sb = sign(b);
+		int sc = sign(c);
+		int difSab = sa ^ sb;
+		int sameSab = flip(difSab);
+		int returnA = difSab * sa + sameSab * sc;
+		int returnB = flip(returnA);
+		return a * returnA + b * returnB;
+	}
+
+	public static void main(String[] args) {
+		int min = -999999;
+		int max = 9999999;
+		int testTimes = 100000;
+		System.out.println("test begin");
+		for (int i = 0; i < testTimes; i++) {
+			int a = (int) ((max - min) * Math.random() + min);
+			int b = (int) ((max - min) * Math.random() + min);
+			if (a == b) {
+				continue;
+			}
+			if (!(Math.max(a, b) == getMax1(a, b))) {
+				System.out.println("出错了");
+				break;
+			}
+			if (!(Math.max(a, b) == getMax2(a, b))) {
+				System.out.println("出错了");
+				break;
+			}
+		}
+		System.out.println("test end");
+	}
 }
---
+++ src/main/java/grey/algorithm/Code_0024_NowCoder_SmallSum.java
@@ -0,0 +1,84 @@
+package grey.algorithm;
+
+import java.io.*;
+
+//假设数组 s = [ 1, 3, 5, 2, 4, 6]
+//在s[0]的左边所有 <= s[0]的数的总和为0
+//在s[1]的左边所有 <= s[1]的数的总和为1
+//在s[2]的左边所有 <= s[2]的数的总和为4
+//在s[3]的左边所有 <= s[3]的数的总和为1
+//在s[4]的左边所有 <= s[4]的数的总和为6
+//在s[5]的左边所有 <= s[5]的数的总和为15
+//所以s数组的“小和”为 : 0 + 1 + 4 + 1 + 6 + 15 = 27
+//给定一个数组arr，实现函数返回arr的“小和”
+//测试链接 : 
+//https://www.nowcoder.com/practice/edfe05a1d45c4ea89101d936cac32469
+
+public class Code_0024_NowCoder_SmallSum {
+	private static final int MAXN = 100001;
+	private static final int[] arr = new int[MAXN];
+	private static final int[] help = new int[MAXN];
+	private static int n;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StreamTokenizer in = new StreamTokenizer(br);
+		PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
+		in.nextToken();
+		n = (int) in.nval;
+		for (int i = 0; i < n; i++) {
+			in.nextToken();
+			arr[i] = (int) in.nval;
+		}
+		out.println(smallSum(0, n - 1));
+		out.flush();
+		out.close();
+		br.close();
+	}
+
+	// 结果比较大，用int会溢出的，所以返回long类型
+	// 时间复杂度O(n * logn)
+	public static long smallSum(int l, int r) {
+		if (l == r) {
+			return 0;
+		}
+		int m = l + ((r - l) >> 1);
+		return smallSum(l, m) + smallSum(m + 1, r) + merge(l, m, r);
+	}
+
+	// 返回跨左右产生的小和累加和，左侧有序、右侧有序，让左右两侧整体有序
+	public static long merge(int l, int m, int r) {
+		long ans = 0L;
+		int i = l; // 卡左边界
+		int j = m + 1; // 卡右边界
+		long sum = 0L;
+		while (j <= r) {
+			// 滑动窗口，不回退
+			while (i <= m && arr[i] <= arr[j]) {
+				sum += arr[i++];
+			}
+			ans += sum;
+			j++;
+		}
+		int s = l;
+		int e = m + 1;
+		i = l;
+		while (s <= m && e <= r) {
+			if (arr[s] <= arr[e]) {
+				help[i++] = arr[s++];
+			} else {
+				help[i++] = arr[e++];
+			}
+		}
+		while (s <= m) {
+			help[i++] = arr[s++];
+		}
+		while (e <= r) {
+			help[i++] = arr[e++];
+		}
+		for (i = l; i <= r; i++) {
+			arr[i] = help[i];
+		}
+		return ans;
+	}
+}
