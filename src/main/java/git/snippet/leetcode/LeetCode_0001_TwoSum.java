f0302a81f16c58faa61c1d26111bd10109702de2
GreyZeng <410486047@qq.com>
2025-07-12 11:56:15 +0800

    update code

--- src/main/java/git/snippet/leetcode/LeetCode_0001_TwoSum.java
+++ src/main/java/git/snippet/leetcode/LeetCode_0001_TwoSum.java
@@ -1,19 +1,26 @@
 package git.snippet.leetcode;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.HashMap;
 
-// https://leetcode.cn/problems/two-sum/
-@Deprecated
+// https://leetcode.com/problems/two-sum/
 public class LeetCode_0001_TwoSum {
-    public int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> map = new HashMap<>(nums.length);
-        for (int i = 0; i < nums.length; i++) {
-            if (map.containsKey(target - nums[i])) {
-                return new int[]{map.get(target - nums[i]), i};
-            }
-            map.put(nums[i], i);
-        }
-        return new int[]{-1, -1};
-    }
+	public static int[] twoSum(int[] nums, int target) {
+		Map<Integer, Integer> map = new HashMap<>();
+		for (int i = 0; i < nums.length; i++) {
+			if (map.containsKey(target - nums[i])) {
+				return new int[] {map.get(target - nums[i]),i};
+			} 
+			map.put(nums[i], i);
+		}
+		return new int[] {-1,-1};
+	}
+	public static void main(String[] args) {
+		int[] arr = {2,3,4,5};
+		int target = 9;
+		int[] twoSum = twoSum(arr, target);
+		for (int v : twoSum) {
+			System.out.println(v);
+		}
+	}
 }
