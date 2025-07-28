e58db7d2b949bd7cfdfec9635f309681fd778bb4
GreyZeng <410486047@qq.com>
2025-07-28 16:11:11 +0800

    binary tree post order traversal

--- src/main/java/grey/algorithm/Code_0012_LeetCode_0145_BinaryTreePostorderTraversal.java
+++ src/main/java/grey/algorithm/Code_0012_LeetCode_0145_BinaryTreePostorderTraversal.java
@@ -2,6 +2,7 @@ package grey.algorithm;
 
 import java.util.*;
 
+
 // https://leetcode.com/problems/binary-tree-postorder-traversal/
 // 二叉树的后序遍历
 // 笔记：https://www.cnblogs.com/greyzeng/articles/15941957.html
@@ -54,37 +55,31 @@ public class Code_0012_LeetCode_0145_BinaryTreePostorderTraversal {
 
     // TODO
     // 【非递归】【单栈】后序遍历
-    public static List<Integer> postorderTraversal1(TreeNode root) {
-        List<Integer> result = new ArrayList<>();
-        if (root == null) {
-            return result;
-        }
-
-        Stack<TreeNode> stack = new Stack<>();
-        TreeNode current = root; // 当前探索指针
-        TreeNode lastVisit = null; // 记录上一个被访问的节点
-
-        while (current != null || !stack.isEmpty()) {
-            // 1. 左链入栈：一直向左走，把所有左孩子压栈
-            while (current != null) {
-                stack.push(current);
-                current = current.left;
-            }
-
-            // 2. 查看栈顶节点（不弹出，先判断右子树）
-            TreeNode peekNode = stack.peek();
-
-            // 3. 如果右子树存在且未被访问过，则转向右子树
-            if (peekNode.right != null && peekNode.right != lastVisit) {
-                current = peekNode.right; // 处理右子树
-            } // 4. 否则（右子树为空或已访问），可以访问当前节点
-            else {
-                result.add(peekNode.val); // 访问节点
-                lastVisit = stack.pop();   // 记录已访问
-            }
-        }
-        return result;
-    }
+    public static List<Integer> postorderTraversal1(TreeNode h) {
+    	List<Integer> ans = new ArrayList<>();
+		if (h != null) {
+			Stack<TreeNode> stack = new Stack<>();
+			stack.push(h);
+			// 如果始终没有打印过节点，h就一直是头节点
+			// 一旦打印过节点，h就变成打印节点
+			// 之后h的含义 : 上一次打印的节点
+			while (!stack.isEmpty()) {
+				TreeNode cur = stack.peek();
+				if (cur.left != null && h != cur.left && h != cur.right) {
+					// 有左树且左树没处理过
+					stack.push(cur.left);
+				} else if (cur.right != null && h != cur.right) {
+					// 有右树且右树没处理过
+					stack.push(cur.right);
+				} else {
+					// 左树、右树 没有 或者 都处理过了
+					ans.add(cur.val);
+					h = stack.pop();
+				}
+			}
+		}
+		return ans;
+	}
 
     // morris遍历实现后序遍历
     // 处理时机放在能回到自己两次的点，且第二次回到自己的时刻,第二次回到他自己的时候，
