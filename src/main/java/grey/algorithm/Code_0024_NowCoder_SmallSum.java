5619ae06eb76873e35934f42006f52876418e0d5
GreyZeng <410486047@qq.com>
2025-08-15 16:13:48 +0800

    update code

--- src/main/java/grey/algorithm/Code_0024_NowCoder_SmallSum.java
+++ src/main/java/grey/algorithm/Code_0024_NowCoder_SmallSum.java
@@ -13,72 +13,72 @@ import java.io.*;
 //给定一个数组arr，实现函数返回arr的“小和”
 //测试链接 : 
 //https://www.nowcoder.com/practice/edfe05a1d45c4ea89101d936cac32469
-
 public class Code_0024_NowCoder_SmallSum {
-	private static final int MAXN = 100001;
-	private static final int[] arr = new int[MAXN];
-	private static final int[] help = new int[MAXN];
-	private static int n;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StreamTokenizer in = new StreamTokenizer(br);
-		PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
-		in.nextToken();
-		n = (int) in.nval;
-		for (int i = 0; i < n; i++) {
-			in.nextToken();
-			arr[i] = (int) in.nval;
-		}
-		out.println(smallSum(0, n - 1));
-		out.flush();
-		out.close();
-		br.close();
-	}
+    private static final int MAXN = 100001;
+    private static final int[] arr = new int[MAXN];
+    private static final int[] help = new int[MAXN];
+    private static int n;
 
-	// 结果比较大，用int会溢出的，所以返回long类型
-	// 时间复杂度O(n * logn)
-	public static long smallSum(int l, int r) {
-		if (l == r) {
-			return 0;
-		}
-		int m = l + ((r - l) >> 1);
-		return smallSum(l, m) + smallSum(m + 1, r) + merge(l, m, r);
-	}
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            StreamTokenizer in = new StreamTokenizer(br);
+            try (PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out))) {
+                in.nextToken();
+                n = (int) in.nval;
+                for (int i = 0; i < n; i++) {
+                    in.nextToken();
+                    arr[i] = (int) in.nval;
+                }
+                out.println(smallSum(0, n - 1));
+                out.flush();
+            }
+        }
+    }
 
-	// 返回跨左右产生的小和累加和，左侧有序、右侧有序，让左右两侧整体有序
-	public static long merge(int l, int m, int r) {
-		long ans = 0L;
-		int i = l; // 卡左边界
-		int j = m + 1; // 卡右边界
-		long sum = 0L;
-		while (j <= r) {
-			// 滑动窗口，不回退
-			while (i <= m && arr[i] <= arr[j]) {
-				sum += arr[i++];
-			}
-			ans += sum;
-			j++;
-		}
-		int s = l;
-		int e = m + 1;
-		i = l;
-		while (s <= m && e <= r) {
-			if (arr[s] <= arr[e]) {
-				help[i++] = arr[s++];
-			} else {
-				help[i++] = arr[e++];
-			}
-		}
-		while (s <= m) {
-			help[i++] = arr[s++];
-		}
-		while (e <= r) {
-			help[i++] = arr[e++];
-		}
-		for (i = l; i <= r; i++) {
-			arr[i] = help[i];
-		}
-		return ans;
-	}
+    // 结果比较大，用int会溢出的，所以返回long类型
+    // 时间复杂度O(n * logn)
+    public static long smallSum(int l, int r) {
+        if (l == r) {
+            return 0;
+        }
+        int m = l + ((r - l) >> 1);
+        return smallSum(l, m) + smallSum(m + 1, r) + merge(l, m, r);
+    }
+
+    // 返回跨左右产生的小和累加和，左侧有序、右侧有序，让左右两侧整体有序
+    public static long merge(int l, int m, int r) {
+        long ans = 0L;
+        int i = l; // 卡左边界
+        int j = m + 1; // 卡右边界
+        long sum = 0L;
+        while (j <= r) {
+            // 滑动窗口，不回退
+            while (i <= m && arr[i] <= arr[j]) {
+                sum += arr[i++];
+            }
+            ans += sum;
+            j++;
+        }
+        int s = l;
+        int e = m + 1;
+        i = l;
+        while (s <= m && e <= r) {
+            if (arr[s] <= arr[e]) {
+                help[i++] = arr[s++];
+            } else {
+                help[i++] = arr[e++];
+            }
+        }
+        while (s <= m) {
+            help[i++] = arr[s++];
+        }
+        while (e <= r) {
+            help[i++] = arr[e++];
+        }
+        for (i = l; i <= r; i++) {
+            arr[i] = help[i];
+        }
+        return ans;
+    }
 }
