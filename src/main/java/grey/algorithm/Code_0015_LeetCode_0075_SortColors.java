07efc610a27e21df2b7ddd3a30fc2cd95f5bc771
GreyZeng <410486047@qq.com>
2025-08-18 17:35:28 +0800

    sort colors

--- src/main/java/grey/algorithm/Code_0015_LeetCode_0075_SortColors.java
+++ src/main/java/grey/algorithm/Code_0015_LeetCode_0075_SortColors.java
@@ -4,21 +4,19 @@ package grey.algorithm;
 // 荷兰国旗问题
 // 仅0，1，2，所有的0放左边，所有的1放中间，所有的2放右边
 // https://leetcode.com/problems/sort-colors/
-public class Code_0026_LeetCode_0075_SortColors {
+public class Code_0015_LeetCode_0075_SortColors {
 	public static void sortColors(int[] arr) {
-		int l = -1;
-		int r = arr.length;
+		int l = -1; // 0 ~ l 是小于区域
+		int r = arr.length; // l ~ r 是等于区域 ， r ~ arr.length - 1 是大于区域
 		int i = 0;
 		while (i < r) {
 			if (arr[i] == 0) {
-				// 小于区域
-				swap(arr, i++, ++l);
+				swap(arr, ++l, i++);
 			} else if (arr[i] == 1) {
-				// 等于区域
 				i++;
 			} else {
-				// 大于区域
-				swap(arr, i, --r);
+				// i == 2
+				swap(arr, --r, i);
 			}
 		}
 	}
@@ -27,14 +25,6 @@ public class Code_0026_LeetCode_0075_SortColors {
 		int tmp = arr[i];
 		arr[i] = arr[j];
 		arr[j] = tmp;
-	}
-
-	public static void main(String[] args) {
-		int[] arr = { 0, 0, 1, 2, 2, 2, 1, 0 };
-		sortColors(arr);
-		for (int i = 0; i < arr.length; i++) {
-			System.out.println(arr[i]);
-		}
 	}
 
 }
